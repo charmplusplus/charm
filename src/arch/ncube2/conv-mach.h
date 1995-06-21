1248a3da08ed6a88db14fca02100572b7adfef9d
Attila Gursoy <agursoy@uiuc.edu>
1995-06-21 16:03:05 +0000

    ncueb cc doesn't like space before #define

--- src/arch/ncube2/conv-mach.h
+++ src/arch/ncube2/conv-mach.h
@@ -15,12 +15,12 @@
  ***************************************************************************/
 
 /* #define CMK_CMIMYPE_IS_A_BUILTIN */
-   #define CMK_CMIMYPE_IS_A_VARIABLE
+#define CMK_CMIMYPE_IS_A_VARIABLE
 
 /* #define CMK_CMIPRINTF_IS_A_BUILTIN */
-   #define CMK_CMIPRINTF_IS_JUST_PRINTF
+#define CMK_CMIPRINTF_IS_JUST_PRINTF
 
-   #define CMK_NO_SHARED_VARS_AT_ALL
+#define CMK_NO_SHARED_VARS_AT_ALL
 
-   #define CMK_COMPILER_LIKES_STATIC_PROTO
+#define CMK_COMPILER_LIKES_STATIC_PROTO
 /* #define CMK_COMPILER_HATES_STATIC_PROTO */
