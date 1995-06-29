fda8b7b6653d3289c638b3a291d7624f07aa0475
Narain Jagathesan <njagathesan@uiuc.edu>
1995-06-29 22:34:30 +0000

    Changed prototype of  SendMsgIfConditionArises to static

--- src/ck-core/condsend.h
+++ src/ck-core/condsend.h
@@ -13,7 +13,10 @@
  * REVISION HISTORY:
  *
  * $Log$
- * Revision 2.2  1995-06-18 21:30:15  sanjeev
+ * Revision 2.3  1995-06-29 22:34:30  narain
+ * Changed prototype of  SendMsgIfConditionArises to static
+ *
+ * Revision 2.2  1995/06/18  21:30:15  sanjeev
  * separated charm and converse condsends
  *
  ***************************************************************************/
@@ -35,7 +38,7 @@ typedef struct {
 } CallBocStuff;
 
 /* Function implemented but not to be used .. */
-void SendMsgIfConditionArises(int condnum, int entry, void *msg, int size,
+static void SendMsgIfConditionArises(int condnum, int entry, void *msg, int size,
 			      ChareIDType *cid);
 void CallBocIfConditionArises(int condnum, FUNCTION_PTR fnp, int bocNum);      
 void SendMsgAfter(unsigned int deltaT, int entry, void *msg, int size, 
