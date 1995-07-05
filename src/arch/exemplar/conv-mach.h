d1805d9296714556abf26b65fea2fb7604f966e9
Attila Gursoy <agursoy@uiuc.edu>
1995-07-05 20:02:50 +0000

    removed blank spaces before # sign

--- src/arch/exemplar/conv-mach.h
+++ src/arch/exemplar/conv-mach.h
@@ -15,13 +15,13 @@
  ***************************************************************************/
 
 /* #define CMK_CMIMYPE_IS_A_BUILTIN */
-   #define CMK_CMIMYPE_IS_A_VARIABLE
+#define CMK_CMIMYPE_IS_A_VARIABLE
 
 /* #define CMK_CMIPRINTF_IS_A_BUILTIN */
-   #define CMK_CMIPRINTF_IS_JUST_PRINTF
+#define CMK_CMIPRINTF_IS_JUST_PRINTF
 
 
-   #define CMK_SHARED_VARS_EXEMPLAR
+#define CMK_SHARED_VARS_EXEMPLAR
 
 /* #define CMK_COMPILER_LIKES_STATIC_PROTO */
-   #define CMK_COMPILER_HATES_STATIC_PROTO
+#define CMK_COMPILER_HATES_STATIC_PROTO
