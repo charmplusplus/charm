46e5a57c54ed74003e13f72b7fe46b2becbfd415
Josh Yelon <jyelon@uiuc.edu>
1995-10-30 20:31:15 +0000

    Initial revision

---
+++ src/arch/uth/conv-host.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+usage()
+{
+  fprintf(stderr,"usage: conv-host <pgm> <arguments>\n");
+  exit(1);
+}
+
+main(argc, argv)
+int argc;
+char **argv;
+{
+  if (argc<2) usage();
+  execvp(argv[1],argv+1);
+  fprintf(stderr,"not found: %s\n",argv[1]);
+}
