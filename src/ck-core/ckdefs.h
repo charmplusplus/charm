5a7c178cf9fe1fd8215af071aa7328ddbee88181
Sanjeev Krishnan <skrishnan@uiuc.edu>
1996-03-22 17:34:11 +0000

    *** empty log message ***

--- src/ck-core/ckdefs.h
+++ src/ck-core/ckdefs.h
@@ -12,7 +12,10 @@
  * REVISION HISTORY:
  *
  * $Log$
- * Revision 2.15  1996-03-22 17:24:09  sanjeev
+ * Revision 2.16  1996-03-22 17:34:11  sanjeev
+ * *** empty log message ***
+ *
+ * Revision 2.15  1996/03/22 17:24:09  sanjeev
  * modified CSendMsgBranch and CBroadcastMsgBranch
  *
  * Revision 2.14  1996/02/22 21:38:20  sanjeev
@@ -211,9 +214,9 @@
 
 #define CSendMsg(ChareType,EP,msg,ChareId) 	SendMsg(GetEntryPtr(ChareType,EP), msg, ChareId)
 
-#define CSendMsgBranch(ChareType,EP,msg,ChareId,Pe) 	GeneralSendMsgBranch(GetEntryPtr(ChareType,EP), msg, ChareId, Pe)
+#define CSendMsgBranch(ChareType,EP,msg,ChareId,Pe) 	GeneralSendMsgBranch(GetEntryPtr(ChareType,EP), msg, Pe, -1, ChareId)
 
-#define CBroadcastMsgBranch(ChareType,EP,msg,ChareId) 	GeneralBroadcastMsgBranch(GetEntryPtr(ChareType,EP), msg, ChareId)
+#define CBroadcastMsgBranch(ChareType,EP,msg,ChareId) 	GeneralBroadcastMsgBranch(GetEntryPtr(ChareType,EP), msg, -1, ChareId)
 
 #define CLocalBranch(BocType, BocId)	((BocType *)GetBocDataPtr(BocId))
 
