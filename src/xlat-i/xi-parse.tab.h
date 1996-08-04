eb967f6caa8fdbbfde30072fd6072a6bfadd1df4
Josh Yelon <jyelon@uiuc.edu>
1996-08-04 22:56:44 +0000

    Initial revision

---
+++ src/xlat-i/xi-parse.tab.h
@@ -0,0 +1,16 @@
+typedef union {
+	char *strval;
+	int intval;
+} YYSTYPE;
+#define	BOC	258
+#define	CHARE	259
+#define	ENTRY	260
+#define	MESSAGE	261
+#define	READONLY	262
+#define	TABLE	263
+#define	THREADED	264
+#define	EXTERN	265
+#define	IDENTIFIER	266
+
+
+extern YYSTYPE yylval;
