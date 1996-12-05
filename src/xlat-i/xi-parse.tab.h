b275cd10c2011faad8a5b6602237a3bdbaa76fd3
Milind Bhandarkar <milind@cs.uiuc.edu>
1996-12-05 23:40:54 +0000

    rebisoned....

--- src/xlat-i/xi-parse.tab.h
+++ src/xlat-i/xi-parse.tab.h
@@ -1,14 +1,17 @@
-#define BOC 257
-#define CHARE 258
-#define ENTRY 259
-#define MESSAGE 260
-#define READONLY 261
-#define TABLE 262
-#define THREADED 263
-#define EXTERN 264
-#define IDENTIFIER 265
 typedef union {
 	char *strval;
 	int intval;
 } YYSTYPE;
+#define	BOC	258
+#define	CHARE	259
+#define	ENTRY	260
+#define	MESSAGE	261
+#define	PACKMESSAGE	262
+#define	READONLY	263
+#define	TABLE	264
+#define	THREADED	265
+#define	EXTERN	266
+#define	IDENTIFIER	267
+
+
 extern YYSTYPE yylval;
