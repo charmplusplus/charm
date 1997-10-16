9f5e769d15d7833641ff33039cc73eef8e8b5a71
Milind Bhandarkar <milind@cs.uiuc.edu>
1997-10-16 17:34:51 +0000

    Added Message Streams to Common/langs

---
+++ src/langs/streams/Communicate.h
@@ -0,0 +1,40 @@
+//-*-c++-*-
+/***************************************************************************/
+/*       (C) Copyright 1995,1996,1997 The Board of Trustees of the         */
+/*                          University of Illinois                         */
+/*                           All Rights Reserved                           */
+/***************************************************************************/
+
+#ifndef COMMUNICATE_H
+#define COMMUNICATE_H
+
+extern "C" {
+#include "converse.h"
+extern void CmiGrabBuffer(char **msg);
+}
+
+class MIStream;
+class MOStream;
+
+#define ALL      -1
+#define ALLBUTME -2
+#define BUFSIZE  4096
+#define ANY      -1
+
+class Communicate {
+
+private:
+  int CsmHandlerIndex;
+
+public:
+  Communicate(void);
+  ~Communicate();
+  MIStream *newInputStream(int pe, int tag);
+  MOStream *newOutputStream(int pe, int tag, unsigned int bufsize);
+  void *getMessage(int PE, int tag);
+  void sendMessage(int PE, void *msg, int size);
+};
+
+#include "MStream.h"
+
+#endif
