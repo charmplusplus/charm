1db71e8a9022030b7c117a08e6cba81122f49e61
Jayant DeSouza <jdesouza@uiuc.edu>
1997-11-15 00:18:28 +0000

    Minor change of exit value.

--- src/xlat-i/xi-main.C
+++ src/xlat-i/xi-main.C
@@ -3,17 +3,20 @@
 
 extern void Generate(char *interfacefile) ;
 
+const int SUCCESS = 0;
+const int FAILURE = 1;
+
 main(int argc, char *argv[])
 {
 	if ( argc != 2 ) {
 		cout << "Usage : " << argv[0] << " module.ci" << endl ;
-		exit(1) ;
+		exit(FAILURE) ;
 	}
 
 	Module *m = Parse(argv[1]) ;
 
 	Generate(argv[1]) ;
 
-	return 0 ;
+	return SUCCESS ;
 }
 
