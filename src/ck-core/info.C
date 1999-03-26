5919ff881c5f1d73a414a3b5f45b1e7ec322596e
Parthasarathy Ramachandran <pramacha@uiuc.edu>
1999-03-26 03:48:07 +0000

    moved the charm++ specific Debugger runtime to init.C

--- src/ck-core/info.C
+++ src/ck-core/info.C
@@ -3,13 +3,6 @@
 
 #if CMK_DEBUG_MODE
 
-int getCharmMsgHandlers(int *handleArray)
-{
-  *(handleArray) = _charmHandlerIdx;
-  *(handleArray+1) = _initHandlerIdx;
-  return(2);
-}
-
 CsvDeclare(int*,  BreakPoints);
 CsvDeclare(char*, SymbolTableInfo);
 CpvDeclare(symbolTableType, SymbolTableFnArray);
@@ -19,56 +12,6 @@ CpvDeclare(int, numBreakPoints);
 CpvDeclare(indirectionType, indirectionFnArray);
 
 CpvExtern(handlerType, handlerArray);
-
-char* getEnvInfo(envelope *env)
-{
-  char *returnInfo;
-  int size;
-  int chareIndex;
-  int epIndex = env->getEpIdx();
-  size = strlen(_entryTable[epIndex]->name)+1;
-  chareIndex = _entryTable[epIndex]->chareIdx;
-  size += strlen(_chareTable[chareIndex]->name)+1;
-  
-  returnInfo = (char *)malloc((size + 2) * sizeof(char));
-  strcpy(returnInfo, _entryTable[epIndex]->name);
-  strcat(returnInfo, "#");
-  strcat(returnInfo, _chareTable[chareIndex]->name);
-  strcat(returnInfo, "#");
-  return(returnInfo);
-}
-
-char* makeCharmSymbolTableInfo(void)
-{
-  int i, chareIndex;
-  int size;
-  char *returnInfo;
-  char temp[10];
-  
-  size = _numEntries * 100;
-  returnInfo = (char *)malloc(size * sizeof(char));
-  strcpy(returnInfo, "");
-  for(i = 0; i < _numEntries; i++){
-    strcat(returnInfo, _entryTable[i]->name);
-    strcat(returnInfo, "#");
-    chareIndex = _entryTable[i]->chareIdx;
-    strcat(returnInfo, _chareTable[chareIndex]->name);
-    strcat(returnInfo, "#");
-    sprintf(temp, "%d", i);
-    strcat(returnInfo, temp);
-    strcat(returnInfo, "#");
-  }
-
-  return(returnInfo);
-}
-
-int getEpIdx(char *msg)
-{
-  envelope *env;
-  
-  env = (envelope *)msg;
-  return(env->getEpIdx());
-}
 
 char *makeSymbolTableInfo(void)
 {
