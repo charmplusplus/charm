0ed33b62aa0fbc34217966ed34e25437ace8fcbd
Jeffrey Wright <jmwrght1@uiuc.edu>
1999-09-08 22:33:41 +0000

    Changed CMK_USE_PERSISTENT_CCS to 1 for origin2000

--- src/arch/origin2000/conv-mach.h
+++ src/arch/origin2000/conv-mach.h
@@ -121,7 +121,7 @@
 #define NODE_0_IS_CONVHOST                                 1
 #define CMK_DEBUG_MODE                                     0
 #define CMK_WEB_MODE                                       1
-#define CMK_USE_PERSISTENT_CCS                             0
+#define CMK_USE_PERSISTENT_CCS                             1
 
 #define CMK_COMPILEMODE_ORIG                               1
 #define CMK_COMPILEMODE_ANSI                               0
