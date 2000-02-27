e49233dad3d3ea78f55abeddf911d64b475bf0c6
Milind Bhandarkar <milind@cs.uiuc.edu>
2000-02-27 11:06:42 +0000

    Removed obsolete files.

--- src/conv-core/internal.h
+++
@@ -1,3 +0,0 @@
-#define PVECTOR int
-int   *CmiAlloc();
-void   CmiFree();
--- src/conv-core/spantree.c
+++
@@ -1,8 +0,0 @@
-#include <converse.h>
-
-#if CMK_SPANTREE_USE_COMMON_CODE
-
-/* Node-level Spanning Tree functions have been made macros in converse.h */
-
-#endif
-
