fd8a1672314246245bdc9f3762d2d082f0ebea6e
Robert Brunner <rbrunner@uiuc.edu>
2000-03-14 02:48:37 +0000

    Turned load balancing on

--- src/arch/net-axp-cc/conv-mach.h
+++ src/arch/net-axp-cc/conv-mach.h
@@ -122,8 +122,8 @@
 
 #define CPP_LOCATION "/lib/cpp"
 
-#define CMK_LBDB_ON					   0
-#define CMK_LBDB_OFF					   1
+#define CMK_LBDB_ON					   1
+#define CMK_LBDB_OFF					   0
 
 #define CMK_STL_USE_DOT_H                                  0
 #define CMK_STL_DONT_USE_DOT_H                             1
