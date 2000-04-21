0f920e0f8c0a5dcfcef7a4e4b38513d027b6b73a
Gengbin Zheng <gzheng@illinois.edu>
2000-04-21 04:21:11 +0000

    add irecv into Charm++ libs

---
+++ src/libs/irecv/receiver.C
@@ -0,0 +1,232 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "charm++.h"
+
+#include "receiver.h"
+
+
+receiver::receiver() 
+{
+  msgTbl = CmmNew();
+  reqTbl = CmmNew();
+
+  callback = NULL;
+  counter = -1;
+  startwaiting = 0;
+}
+
+receiver::receiver(ArrayElementMigrateMessage *m) : ArrayElement1D(m)
+{
+  msgTbl = CmmNew();
+  reqTbl = CmmNew();
+
+  callback = NULL;
+}
+
+void receiver::pup(PUP::er &p)
+{
+  ArrayElement1D::pup(p);
+  p(counter);
+  p(startwaiting);
+  // pack CmmTable: msgTbl, reqTbl
+  pupCmmTable(msgTbl, p);
+  pupCmmTable(reqTbl, p);
+}
+
+receiver::~receiver()
+{
+  CmmFree(msgTbl);
+  CmmFree(reqTbl);
+}
+
+#define MIN(a,b) (a)<(b)?(a):(b)
+
+void receiver::sendTo(int tag, char *pointer, int size, int from, int refno)
+{
+  int tags[3], ret_tags[3];
+
+//CkPrintf("Sendto (msgTbl): tag:%d, from:%d, refno:%d size:%d. \n", tag, from, refno,size);
+  tags[0] = tag; tags[1] = from; tags[2] = refno;
+  tblEntry *req = (tblEntry *)CmmGet(reqTbl, 3, tags, ret_tags);
+
+  if (req) {
+    //  irecv called before; copy buffer
+    memcpy(req->buf, pointer, MIN(size, req->size)); 
+    delete req;
+
+    recvAlready();
+  }
+  else {
+    // msg come before irecv called
+    tags[0] = tag; tags[1] = from; tags[2] = refno;
+    req = new tblEntry;
+    req->buf = new char[size];
+    memcpy(req->buf, pointer, size); 
+    req->size = size;
+    CmmPut(msgTbl, 3, tags, req);
+  }
+
+}
+
+void receiver::generic(receiverMsg *msg)
+{
+  sendTo(msg->tag, msg->buf, msg->size, msg->sendFrom, msg->refno);
+  delete msg;
+}
+
+static int typesize(int type, int count)
+{
+  switch(type) {
+    case CMPI_DOUBLE_PRECISION : return count*sizeof(double);
+    case CMPI_INTEGER : return count*sizeof(int);
+    case CMPI_REAL : return count*sizeof(float);
+    case CMPI_COMPLEX: return 2*count*sizeof(double);
+    case CMPI_LOGICAL: return 2*count*sizeof(int);
+    case CMPI_CHARACTER:
+    case CMPI_BYTE:
+    case CMPI_PACKED:
+    default:
+      return 2*count;
+  }
+}
+
+void receiver::isend(void *buf, int size, int dest, int tag, int refno)
+{
+// int size = typesize(datatype, count);
+ receiverMsg * d = new (&size, 0) receiverMsg;
+ d->tag = tag;
+ d->sendFrom = thisIndex;
+ d->refno = refno;
+ memcpy(d->buf, buf, size);
+ CProxy_receiver B(thisArrayID);
+ B[dest].generic(d);
+}
+
+void receiver::irecv(void *buf, int size, int source, int tag, int refno)
+{
+  int tags[3], ret_tags[3];
+//  int size = typesize(datatype, count);
+
+  tags[0] = tag; tags[1] = source; tags[2] = refno;
+  tblEntry *req = (tblEntry *)CmmGet(msgTbl, 3, tags, ret_tags);
+
+  if (req) {
+    // send called before; copy buffer into
+    memcpy(buf, req->buf, MIN(size, req->size));
+    delete [] req->buf;
+    delete req;
+  }
+  else {
+//CkPrintf("irecv (reqtbl): tag:%d, senderTag:%d, refno:%d. \n", tag, senderTag, refno);
+   // recv called before send
+    tags[0] = tag; tags[1] = source; tags[2] = refno;
+    req = new tblEntry;
+    req->buf = (char *)buf;
+    req->size = size;
+    CmmPut(reqTbl, 3, tags, req);
+  }
+}
+
+void receiver::iwaitAll(recvCallBack f, void *data, int ref)
+{
+  if (callback != NULL) 
+  {
+    CkPrintf("iwaitAll wrong!\n");
+    CkExit();
+  }
+  callback = f;
+  this->cb_data = data;
+  counter = ref;
+
+  startwaiting = 1;
+
+  recvAlready();
+}
+
+void receiver::iwaitAll(int ref)
+{
+  callback = NULL;
+  this->cb_data = NULL;
+  counter = ref;
+  startwaiting = 1;
+
+  recvAlready();
+}
+
+void receiver::recvAlready()
+{
+//   if (callback == NULL) return;
+   if (!startwaiting) return;
+
+   int tags[3], ret_tags[3];
+   tags[0] = CmmWildCard; tags[1] = CmmWildCard; tags[2] = counter;
+   tblEntry *req1 = (tblEntry *)CmmProbe(reqTbl, 3, tags, ret_tags);
+   tblEntry *req2 = (tblEntry *)CmmProbe(msgTbl, 3, tags, ret_tags);
+   if (req1 == NULL && req2 == NULL && startwaiting)  //  && callback != NULL) 
+   {
+      startwaiting = 0;
+      CProxy_receiver B(thisArrayID);
+      B[thisIndex].ready2go();
+//CkPrintf("[%d] ready to go on %d\n", thisIndex, counter);
+   }
+}
+
+void receiver::ready2go()
+{
+/*
+    if (callback == NULL) CkPrintf("Fatal error in Call back on [%d]. \n", thisIndex);
+
+    recvCallBack tmpfn = callback;
+    callback = NULL;
+    tmpfn(cb_data);
+*/
+    if (callback) {
+        recvCallBack tmpfn = callback;
+        callback = NULL;
+        tmpfn(cb_data);
+    }
+    else {
+//CkPrintf("[%d] resumeFromWait\n", CkMyPe());
+	resumeFromWait();
+    }
+}
+
+void receiver::resumeFromWait()
+{
+    CkPrintf("Please write your own resumeFromWait\n"); 
+}
+
+void receiver::pupCmmTable(CmmTable &t, PUP::er &p)
+{
+    tblEntry *msg;
+    int tags[3], rtags[3];
+    int num = CmmEntries(t);
+    p(num);
+    if (p.isPacking()) {
+	tags[0] = tags[1] = tags[2] = CmmWildCard;
+	tblEntry *msg = (tblEntry *)CmmProbe(t, 3, tags, rtags);
+	while (msg) {
+	    p(rtags, 3);
+	    p(msg->size);
+	    p(msg->buf, msg->size);
+	    msg = (tblEntry *)CmmProbe(t, 3, tags, rtags);
+	}
+    }
+    else {
+	int s;
+//	t = CmmNew();
+	for (int i=0; i<num; i++)  {
+	    p(rtags, 3);
+	    msg = new tblEntry;
+	    p(msg->size);
+	    msg->buf = new char[msg->size];
+	    p(msg->buf, msg->size);
+	}
+    }
+}
+
+#include "receiver.def.h"
+
+
---
+++ src/libs/irecv/receiver.h
@@ -0,0 +1,96 @@
+
+#ifndef _RECEIVER_H
+#define _RECEIVER_H
+
+#include "converse.h"
+#include "receiver.decl.h"
+
+typedef void (* recvCallBack)(void *);
+
+#define CMPI_DOUBLE_PRECISION 0
+#define CMPI_INTEGER 1
+#define CMPI_REAL 2
+#define CMPI_COMPLEX 3
+#define CMPI_LOGICAL 4
+#define CMPI_CHARACTER 5
+#define CMPI_BYTE 6
+#define CMPI_PACKED 7
+
+class receiverMsg: public CMessage_receiverMsg
+{
+public:
+  int tag;
+  int sendFrom;
+  int refno;
+  int size;
+  char *buf;
+
+  static void *alloc(int mnum, size_t size, int *sizes, int pbits)
+  {
+    int stmp = sizes[0]*sizeof(char);
+    receiverMsg *m = (receiverMsg *) CkAllocMsg(mnum, size+stmp, pbits);
+    m->size = sizes[0];
+    m->buf = (char *)((char *)m + size);
+    return (void *)m;
+  }
+
+  static void *pack(receiverMsg *m)
+  {
+//    return (void *)m;
+    m->buf = (char *)((char *)m->buf - (char *)&m->buf);
+    return (void *)m;
+  }
+
+  static receiverMsg *unpack(void *buf)
+  {
+    receiverMsg *m = (receiverMsg *) buf;
+//    m->buf = (char *)((char *)m+sizeof(receiverMsg));
+    m->buf = (char *)((char*)&(m->buf) + (size_t)(m->buf));
+    return m;
+  }
+};
+
+
+class receiver: public ArrayElement1D
+{
+private:
+  CmmTable  msgTbl;
+  CmmTable  reqTbl;
+  int counter;
+  int startwaiting;
+
+  recvCallBack callback;
+  void *cb_data;
+
+  typedef struct _tblEntry {
+    char *buf;
+    int size;
+  } tblEntry;
+
+
+public:
+  receiver();
+  receiver(ArrayElementMigrateMessage *);
+  ~receiver();
+
+  // interface
+  void isend(void *buf, int size, int dest, int tag, int refno);
+  void irecv(void *buf, int size, int source, int tag, int refno);   
+  void iwaitAll(int refno);
+  void iwaitAll(recvCallBack f, void *data, int refno);     // for fortran
+
+  // entry
+  void generic(receiverMsg *);
+  void ready2go();
+
+private:
+  void sendTo(int tag, char *pointer, int size, int from, int refno);
+  void recvAlready();
+  void pupCmmTable(CmmTable &t, PUP::er &p);
+
+protected:
+  void pup(PUP::er &p);
+  virtual void resumeFromWait();
+};
+
+#endif
