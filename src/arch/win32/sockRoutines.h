d21110b182b8b8c59c128197b585f7b76e1d6bb5
Sameer Paranjpye <paranjpy@uiuc.edu>
2000-04-22 19:56:13 +0000

    Updated Makefile for NT to include generalized arrays stuff.

--- src/arch/win32/sockRoutines.h
+++ src/arch/win32/sockRoutines.h
@@ -9,6 +9,6 @@
 
 
 unsigned int skt_ip();
-void skt_server(unsigned int *pip,unsigned int *ppo,unsigned int *pfd);
+void skt_server(unsigned int *pip,unsigned short *ppo,unsigned int *pfd);
 void skt_accept(int src,unsigned int *pip,unsigned int *ppo,unsigned int *pfd);
 int skt_connect(unsigned int ip, int port);
