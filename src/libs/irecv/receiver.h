46895d960cf78f8deeb20cee40199c6283e1fe62
Gengbin Zheng <gzheng@illinois.edu>
2000-04-25 22:13:07 +0000

    add receiverf.h for Fortran

--- src/libs/irecv/receiver.h
+++ src/libs/irecv/receiver.h
@@ -12,7 +12,7 @@ typedef void (* recvCallBack)(void *);
 #define CMPI_REAL 2
 #define CMPI_COMPLEX 3
 #define CMPI_LOGICAL 4
-#define CMPI_CHARACTER 5
+#define CMPI_CHAR 5
 #define CMPI_BYTE 6
 #define CMPI_PACKED 7
 
---
+++ src/libs/irecv/receiverf.h
@@ -0,0 +1,14 @@
+#ifndef _RECEIVERF_H
+#define _RECEIVERF_H
+
+
+  integer, parameter :: AMPI_DOUBLE_PRECISION=0
+  integer, parameter :: AMPI_INTEGER=1
+  integer, parameter :: AMPI_REAL=2
+  integer, parameter :: AMPI_COMPLEX=3
+  integer, parameter :: AMPI_LOGICAL=4
+  integer, parameter :: AMPI_CHARACTER=5
+  integer, parameter :: AMPI_BYTE=6
+  integer, parameter :: AMPI_PACKED=7
+
+#endif
