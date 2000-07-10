ea6aac4b99837882856c7d27c099f019a3c1b96e
Milind Bhandarkar <milind@cs.uiuc.edu>
2000-07-10 18:23:51 +0000

    Added fortran wrappers for the fem framework.

---
+++ src/libs/ck-libs/fem/femf.h
@@ -0,0 +1,13 @@
+#ifndef _FEMF_H
+#define _FEMF_H
+
+  integer, parameter :: FEM_BYTE=0
+  integer, parameter :: FEM_INT=1
+  integer, parameter :: FEM_REAL=2
+  integer, parameter :: FEM_DOUBLE=3
+
+  integer, parameter :: FEM_SUM=0
+  integer, parameter :: FEM_MAX=1
+  integer, parameter :: FEM_MIN=2
+
+#endif
