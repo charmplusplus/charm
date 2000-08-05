ecf33f9ed397eae5e9820663a947924cb001d6bb
Milind Bhandarkar <milind@cs.uiuc.edu>
2000-08-05 07:51:57 +0000

    Removed the debugging prints for each partition, and instead made it into a function in the fem framework.

--- tests/fem/femtest/pgm.C
+++ tests/fem/femtest/pgm.C
@@ -28,35 +28,7 @@ init_(void)
 extern "C" void
 driver_(int *nnodes, int *nnums, int *nelems, int *enums, int *npere, int *conn)
 {
-  char str[1024];
-  char tmpstr[80];
-  CkPrintf("[%d] Number of Elements = %d\n", FEM_My_Partition(), *nelems);
-  CkPrintf("[%d] Number of Nodes = %d\n", FEM_My_Partition(), *nnodes);
-  int i, j;
-  sprintf(str, "[%d] List of Elements:\n", FEM_My_Partition());
-  for(i=0;i<*nelems;i++) {
-    sprintf(tmpstr, "\t%d\n", enums[i]);
-    strcat(str,tmpstr);
-  }
-  CkPrintf("%s", str);
-  sprintf(str, "[%d] List of Nodes:\n", FEM_My_Partition());
-  for(i=0;i<*nnodes;i++) {
-    sprintf(tmpstr, "\t%d\n", nnums[i]);
-    strcat(str,tmpstr);
-  }
-  CkPrintf("%s", str);
-  sprintf(str, "[%d] Connectivity:\n", FEM_My_Partition());
-  for(i=0;i<*nelems;i++) {
-    sprintf(tmpstr, "\t[%d] ", enums[i]);
-    strcat(str,tmpstr);
-    for(j=0;j<*npere;j++) {
-      sprintf(tmpstr, "%d ", nnums[conn[i*(*npere)+j]]);
-      strcat(str,tmpstr);
-    }
-    sprintf(tmpstr, "\n");
-    strcat(str,tmpstr);
-  }
-  CkPrintf("%s", str);
+  FEM_Print_Partition();
   FEM_Done();
 }
 
