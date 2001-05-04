9012880d8292a9fe38e2a47791e35fab84220e14
Gengbin Zheng <gzheng@illinois.edu>
2001-05-04 03:38:03 +0000

    Intel C/C++ support

---
+++ src/arch/net-linux/conv-mach-ecl.h
@@ -0,0 +1,4 @@
+#undef  CMK_COMPILEMODE_ORIG
+#undef  CMK_COMPILEMODE_ANSI
+#define CMK_COMPILEMODE_ORIG                               0
+#define CMK_COMPILEMODE_ANSI                               1
---
+++ src/arch/net-linux/conv-mach-ecl.sh
@@ -0,0 +1,8 @@
+CMK_CPP_C='icc -E '
+CMK_CC='icc '
+CMK_CC_RELIABLE='icc -I/usr/local/mpich/include'
+CMK_CC_FASTEST='icc -I/usr/local/mpich/include'
+CMK_CXX='icpc -I/usr/local/mpich/include'
+CMK_CXXPP='icpc -E -I/usr/local/mpich/include'
+CMK_LD='icc'
+CMK_LDXX='icpc'
