e7a6034c61c1e8d8f69bfc89347dab3d202b7c86
Gengbin Zheng <gzheng@illinois.edu>
2001-05-16 21:25:19 +0000

    net-axp

---
+++ src/arch/net-axp/conv-mach-cxx.h
@@ -0,0 +1,5 @@
+
+#undef  CMK_COMPILEMODE_ORIG
+#undef  CMK_COMPILEMODE_ANSI
+#define CMK_COMPILEMODE_ORIG                               0
+#define CMK_COMPILEMODE_ANSI                               1
