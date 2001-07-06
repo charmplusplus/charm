0f7c4a9aaef890836417bdc79c3cc6e7aa23a5ab
Orion Lawlor <olawlor@acm.org>
2001-07-06 18:21:22 +0000

    Major refactoring of memory allocation-- allows easy switching between OS, GNU, and new allocation methods at link time.
    This file now dispatches to memory-*, depending on the
    -DCMK_MEMORY_* flag set on the command line.

--- src/conv-core/memory.c
+++ src/conv-core/memory.c
@@ -10,148 +10,184 @@
  * This module provides the functions malloc, free, calloc, cfree,
  * realloc, valloc, and memalign.
  *
- * There are several possible implementations provided here, each machine can
- * choose whichever one is best using one of the following flags.
+ * There are several possible implementations provided here-- the user
+ * can link in whichever is best using the -malloc option with charmc.
  *
- * CMK_MALLOC_USE_OS_BUILTIN
- * CMK_MALLOC_USE_GNU
- * CMK_MALLOC_USE_GNU_WITH_CMIMEMLOCK
+ * The major possibilities here are empty (use the system's malloc),
+ * GNU (use malloc-gnu.c), and meta (use malloc-cache.c or malloc-paranoid.c).
+ * On machines without sbrk(), only the system's malloc is available.
+ * 
+ * The CMK_MEMORY_BUILD_* symbols come in from the compiler command line.
  *
  *****************************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "converse.h"
 
-/*****************************************************************************
- *
- * CMK_MALLOC_USE_OS_BUILTIN
- *
- * Just use the OS's built-in malloc.  All we provide is CmiMemoryInit.
- *
- *****************************************************************************/
+/*Choose the proper default configuration*/
+#if CMK_MEMORY_BUILD_DEFAULT
 
-#if CMK_MALLOC_USE_OS_BUILTIN
+# if CMK_MALLOC_USE_OS_BUILTIN
+/*Default to the system malloc-- perhaps the only possibility*/
+#  define CMK_MEMORY_BUILD_OS 1
+# else
+/*Choose a good all-around default malloc*/
+#  define CMK_MEMORY_BUILD_GNU 1
+# endif
 
+#endif
+
+
+#if CMK_MEMORY_BUILD_OS
+/* Just use the OS's built-in malloc.  All we provide is CmiMemoryInit.
+*/
 void CmiMemoryInit(argv)
   char **argv;
 {
 }
 
-#endif
+#else 
+/*************************************************************
+*Not* using the system malloc-- first pick the implementation:
+*/
 
-/*****************************************************************************
- *
- * CMK_MALLOC_USE_GNU_MALLOC
- *
+#if CMK_MEMORY_BUILD_GNU
+/*
  * The GNU memory allocator is a good all-round memory allocator for
  * distributed memory machines.  It has the advantage that you can define
- * CmiMemLock and CmiMemUnlock to provide locking around it's operations.
- *
- *****************************************************************************/
+ * CmiMemLock and CmiMemUnlock to provide locking around its operations.
+ */
 
-#if CMK_MALLOC_USE_GNU_MALLOC
+static void meta_init(char **argv) {}
+#define meta_malloc   mm_malloc
+#define meta_free     mm_free
+#define meta_calloc   mm_calloc
+#define meta_cfree    mm_cfree
+#define meta_realloc  mm_realloc
+#define meta_memalign mm_memalign
+#define meta_valloc   mm_valloc
 
-#define malloc   CmiMemory_Gnu_malloc
-#define free     CmiMemory_Gnu_free
-#define calloc   CmiMemory_Gnu_calloc
-#define cfree    CmiMemory_Gnu_cfree
-#define realloc  CmiMemory_Gnu_realloc
-#define memalign CmiMemory_Gnu_memalign
-#define valloc   CmiMemory_Gnu_valloc
+#include "memory-gnu.c"
+#endif /* CMK_MEMORY_BUILD_GNU */
 
-#undef sun /* I don't care if it's a sun, dangit.  No special treatment. */
-#undef BSD /* I don't care if it's BSD.  Same thing. */
-#if CMK_GETPAGESIZE_AVAILABLE
-#define HAVE_GETPAGESIZE
+#if CMK_MEMORY_BUILD_VERBOSE
+#include "memory-verbose.c"
 #endif
 
-#include "gnumalloc.c"
-
-#undef malloc
-#undef free
-#undef calloc
-#undef cfree
-#undef realloc
-#undef memalign
-#undef valloc
-
-void CmiMemoryInit(argv)
-char **argv;
-{
-}
-
-void *malloc(size)
-#ifdef _SIZE_T
-    size_t size;
-#else
-    unsigned size;
+#if CMK_MEMORY_BUILD_PARANOID
+#include "memory-paranoid.c"
 #endif
-{
-  char *result;
-  CmiMemLock();
-  result = CmiMemory_Gnu_malloc(size);
-  CmiMemUnlock();
-  return (void *) result;
-}
 
-void free(mem)
-    void *mem;
-{
-  CmiMemLock();
-  CmiMemory_Gnu_free(mem);
-  CmiMemUnlock();
-}
+#if CMK_MEMORY_BUILD_CACHE
+#include "memory-cache.c"
+#endif 
 
-void *calloc(nelem, size)
-#ifdef _SIZE_T
-    size_t nelem, size;
-#else
-    unsigned nelem, size;
+/*A trivial sample implementation of the meta_* calls:*/
+#if 0
+/* Use Gnumalloc as meta-meta malloc fallbacks (mm_*) */
+#include "memory-gnu.c"
+static void meta_init(char **argv)
+{
+  
+}
+static void *meta_malloc(size_t size)
+{
+  return mm_malloc(size);
+}
+static void meta_free(void *mem)
+{
+  mm_free(mem);
+}
+static void *meta_calloc(size_t nelem, size_t size)
+{
+  return mm_calloc(nelem,size);
+}
+static void meta_cfree(void *mem)
+{
+  mm_cfree(m);
+}
+static void *meta_realloc(void *mem, size_t size)
+{
+  return mm_realloc(mem,size);
+}
+static void *meta_memalign(size_t align, size_t size)
+{
+  return mm_memalign(align,size);
+}
+static void *meta_valloc(size_t size)
+{
+  return mm_valloc(size);
+}
 #endif
+
+
+/*******************************************************************
+The locking code is common to all implementations except OS-builtin.
+*/
+void CmiMemoryInit(char **argv)
 {
-  char *result;
-  CmiMemLock();
-  result = CmiMemory_Gnu_calloc(nelem, size);
-  CmiMemUnlock();
-  return (void *) result;
+  meta_init(argv);  
 }
 
-void cfree(mem)
-    char *mem;
+void *malloc(size_t size)
+{
+  void *result;
+  CmiMemLock();
+  result = meta_malloc(size);
+  CmiMemUnlock();
+  return result;
+}
+
+void free(void *mem)
 {
   CmiMemLock();
-  CmiMemory_Gnu_cfree(mem);
+  meta_free(mem);
   CmiMemUnlock();
 }
 
-void *realloc(mem, size)
-    void *mem;
-    size_t size;
+void *calloc(size_t nelem, size_t size)
 {
-  char *result;
+  void *result;
   CmiMemLock();
-  result = CmiMemory_Gnu_realloc(mem, size);
+  result = meta_calloc(nelem, size);
   CmiMemUnlock();
-  return (void *) result;
+  return result;
 }
 
-void *memalign(align, size)
-    int align, size;
+void cfree(void *mem)
 {
-  char *result;
   CmiMemLock();
-  result = CmiMemory_Gnu_memalign(align, size);
+  meta_cfree(mem);
+  CmiMemUnlock();
+}
+
+void *realloc(void *mem, size_t size)
+{
+  void *result;
+  CmiMemLock();
+  result = meta_realloc(mem, size);
+  CmiMemUnlock();
+  return result;
+}
+
+void *memalign(size_t align, size_t size)
+{
+  void *result;
+  CmiMemLock();
+  result = meta_memalign(align, size);
   CmiMemUnlock();
   return result;    
 }
 
-void *valloc(size)
-    size_t size;
+void *valloc(size_t size)
 {
-  char *result;
+  void *result;
   CmiMemLock();
-  result = CmiMemory_Gnu_valloc(size);
+  result = meta_valloc(size);
   CmiMemUnlock();
-  return (void *) result;
+  return result;
 }
 
-#endif /* CMK_MALLOC_USE_GNU_MALLOC */
+#endif /* ! CMK_MEMORY_BUILD_BUILTIN*/
+
