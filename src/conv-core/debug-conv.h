1329a1d61d3bc430f1e69bb0621947b0bde53f08
Orion Lawlor <olawlor@acm.org>
2001-08-23 17:06:43 +0000

    All object/message view functions are now subsumed by CpdList API.

--- src/conv-core/debug-conv.h
+++ src/conv-core/debug-conv.h
@@ -5,13 +5,11 @@
 #ifndef __CMK_DEBUG_CONV_H
 #define __CMK_DEBUG_CONV_H
 
-#include "conv-ccs.h"
+#include "pup_c.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-CpvExtern(void *,debugQueue);
 
 void CpdInit(void); 
 void CpdFreeze(void);  
@@ -19,45 +17,21 @@ void CpdUnFreeze(void);
 void CpdFreezeModeScheduler(void);
 void Cpd_CmiHandleMessage(void *msg);
 
-void CpdInitializeObjectTable();
-void CpdInitializeHandlerArray();
-void CpdInitializeBreakPoints();
+/* C bindings for CpdList functions: */
+typedef int  (*CpdListLengthFn_c)(void *lenParam);
 
-    /*Note: these global symbol names are incredibly stupid.*/
-#define MAX_NUM_HANDLERS 1000
-typedef char* (*hndlrIDFunction)(char *);
-typedef hndlrIDFunction handlerType[MAX_NUM_HANDLERS][2];
-void handlerArrayRegister(int, hndlrIDFunction, hndlrIDFunction);
+typedef struct {
+	int lo,hi; /*Range of requested items in list is (lo .. hi-1)*/
+	int extraLen; /*Amount of data pointed to below*/
+	void *extra; /*List-defined request data*/
+} CpdListItemsRequest;
+void CpdListBeginItem(pup_er p,int itemNo);
+typedef void (*CpdListItemsFn_c)(void *itemsParam,pup_er p,
+				CpdListItemsRequest *req);
 
-typedef int (*indirectionFunction)(char *);
-typedef indirectionFunction indirectionType[MAX_NUM_HANDLERS];
-
-typedef char* (*symbolTableFunction)(void);
-typedef symbolTableFunction symbolTableType[MAX_NUM_HANDLERS];
-
-void symbolTableFnArrayRegister(int hndlrID, int noOfBreakPoints,
-				symbolTableFunction f, indirectionFunction g);
-char* getSymbolTableInfo();
-int isBreakPoint(char *msg);
-int isEntryPoint(char *msg);
-void setBreakPoints(char *);
-char *getBreakPoints();
-
-char* getObjectList();
-char* getObjectContents(int);
-
-void msgListCache();
-void msgListCleanup();
-
-char* genericViewMsgFunction(char *msg, int type);
-char* getMsgListSched();
-char* getMsgListPCQueue();
-char* getMsgListFIFO();
-char* getMsgListDebug();
-char* getMsgContentsSched(int index);
-char* getMsgContentsPCQueue(int index);
-char* getMsgContentsFIFO(int index);
-char* getMsgContentsDebug(int index);
+void CpdListRegister_c(const char *path,
+	    CpdListLengthFn_c len,void *lenParam,
+	    CpdListItemsFn_c items,void *itemsParam);
 
 #ifdef __cplusplus
 };
