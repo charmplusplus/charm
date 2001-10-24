b8a7d64ba7a70fd5b558da58dfe5476315208fa1
Gengbin Zheng <gzheng@illinois.edu>
2001-10-24 02:13:49 +0000

    remove some unwanted printout.

--- src/ck-ldb/NborBaseLB.C
+++ src/ck-ldb/NborBaseLB.C
@@ -101,17 +101,17 @@ void NborBaseLB::AtSync()
 {
   //  CkPrintf("[%d] NborBaseLB At Sync step %d!!!!\n",CkMyPe(),mystep);
 
-  if (CkMyPe() == 0) {
-    start_lb_time = CmiWallTimer();
-    CkPrintf("Load balancing step %d starting at %f\n",
-	     step(),start_lb_time);
-  }
-
   if (neighbor_pes == 0) FindNeighbors();
 
   if (!QueryBalanceNow(step()) || num_neighbors() == 0) {
     MigrationDone();
     return;
+  }
+
+  if (CkMyPe() == 0) {
+    start_lb_time = CmiWallTimer();
+    CkPrintf("Load balancing step %d starting at %f\n",
+	     step(),start_lb_time);
   }
 
   NLBStatsMsg* msg = AssembleStats();
@@ -318,7 +318,7 @@ void NborBaseLB::ReceiveMigration(NLBMigrateMsg *msg)
 
 void NborBaseLB::MigrationDone()
 {
-  if (CkMyPe() == 0) {
+  if (CkMyPe() == 0 && migrates_completed) {
     double end_lb_time = CmiWallTimer();
     CkPrintf("Load balancing step %d finished at %f duration %f\n",
 	     step(),end_lb_time,end_lb_time - start_lb_time);
