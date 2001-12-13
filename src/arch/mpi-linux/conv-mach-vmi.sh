054526447d1bda2c2592809ef14299cf7dbad759
Gengbin Zheng <gzheng@illinois.edu>
2001-12-13 23:27:42 +0000

    changed vmi.sh, so that it will take whatever other compiler(pgcc)'s settings.

--- src/arch/mpi-linux/conv-mach-vmi.sh
+++ src/arch/mpi-linux/conv-mach-vmi.sh
@@ -1,10 +1,17 @@
-# src/mpi-linux/conv-mach-vmi.sh
-CMK_CPP_C='gcc -E'
-CMK_CC='gcc -I/usr/local/vmi/mpich/include '
+#in vmi, no mpicc, so switch to gcc if no overriding.
+if test "$CMK_CC" = 'mpicc '
+then
+CMK_CPP_C='gcc -E '
+CMK_CC='gcc'
 CMK_CC_RELIABLE='gcc '
 CMK_CC_FASTEST='gcc '
-CMK_CXX='g++ -I/usr/local/vmi/mpich/include '
+CMK_CXX='g++'
 CMK_CXXPP='gcc -E '
-CMK_LD='gcc -L /usr/local/vmi/mpich/lib/gcc'
-CMK_LDXX='g++ -L /usr/local/vmi/mpich/lib/gcc'
-CMK_LIBS='-lckqt -lmpich -lvmi -ldl -lpthread'
+CMK_LD='gcc'
+CMK_LDXX='g++'
+fi
+CMK_CC="$CMK_CC -I/usr/local/vmi/mpich/include "
+CMK_CXX="$CMK_CXX -I/usr/local/vmi/mpich/include "
+CMK_LD="$CMK_LD -L /usr/local/vmi/mpich/lib/gcc"
+CMK_LDXX="$CMK_LDXX -L /usr/local/vmi/mpich/lib/gcc"
+CMK_LIBS="$CMK_LIBS -lmpich -lvmi -ldl -lpthread"
