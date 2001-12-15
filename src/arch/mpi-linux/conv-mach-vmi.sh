7ca68dab65528789bedcc6f70f7d4d955c26e477
Gengbin Zheng <gzheng@illinois.edu>
2001-12-15 05:40:46 +0000

    allow it take user defined directories.

--- src/arch/mpi-linux/conv-mach-vmi.sh
+++ src/arch/mpi-linux/conv-mach-vmi.sh
@@ -1,13 +1,15 @@
 #in vmi, no mpicc, so switch to gcc if no overriding.
 if test "$CMK_CC" = 'mpicc '
 then
-CMK_CPP_C='gcc -E -I/usr/local/vmi/mpich/include '
-CMK_CC='gcc -I/usr/local/vmi/mpich/include '
-CMK_CC_RELIABLE='gcc -I/usr/local/vmi/mpich/include '
-CMK_CC_FASTEST='gcc -I/usr/local/vmi/mpich/include '
-CMK_CXX='g++ -I/usr/local/vmi/mpich/include '
-CMK_CXXPP='gcc -E -I/usr/local/vmi/mpich/include '
-CMK_LD='gcc -L /usr/local/vmi/mpich/lib/gcc'
-CMK_LDXX='g++ -L /usr/local/vmi/mpich/lib/gcc'
+test -z "$CMK_INCDIR" && CMK_INCDIR='-I /usr/local/vmi/mpich/include'
+test -z "$CMK_LIBDIR" && CMK_LIBDIR='-L /usr/local/vmi/mpich/lib/gcc'
+CMK_CPP_C="gcc -E $CMK_INCDIR "
+CMK_CC="gcc $CMK_INCDIR "
+CMK_CC_RELIABLE="gcc $CMK_INCDIR "
+CMK_CC_FASTEST="gcc $CMK_INCDIR "
+CMK_CXX="g++ $CMK_INCDIR "
+CMK_CXXPP="gcc -E $CMK_INCDIR "
+CMK_LD="gcc $CMK_LIBDIR "
+CMK_LDXX="g++ $CMK_LIBDIR "
 fi
 CMK_LIBS="$CMK_LIBS -lmpich -lvmi -ldl -lpthread"
