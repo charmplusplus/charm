c90f2e3b04cd79b1b9cb416ac8729102e8ca4d38
Gengbin Zheng <gzheng@illinois.edu>
2001-12-17 21:59:52 +0000

    compiler set ansi to remove warnings.

--- src/arch/mpi-linux/conv-mach-pgcc.h
+++ src/arch/mpi-linux/conv-mach-pgcc.h
@@ -1,6 +1,6 @@
 #undef  CMK_COMPILEMODE_ORIG
 #undef  CMK_COMPILEMODE_ANSI
-#define CMK_COMPILEMODE_ORIG                               1
-#define CMK_COMPILEMODE_ANSI                               0
+#define CMK_COMPILEMODE_ORIG                               0
+#define CMK_COMPILEMODE_ANSI                               1
 
 
