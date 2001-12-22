92af327d7a11df4160d9a1f7c6ad8ef2a4baf07f
Gengbin Zheng <gzheng@illinois.edu>
2001-12-22 00:28:37 +0000

    mpi-sol-cc to mpi-sol

---
+++ src/arch/mpi-sol/conv-mach-mpcc.sh
@@ -0,0 +1,9 @@
+CMK_CPP_C='mpcc -E'
+CMK_CC='mpcc '
+CMK_CC_RELIABLE='mpcc '
+CMK_CC_FASTEST='mpcc '
+CMK_CXX='mpCC '
+CMK_CXXPP='mpCC -E '
+CMK_LD='mpcc'
+CMK_LDXX='mpCC'
+CMK_LIBS="$CMK_LIBS -lnsl -lsocket -lmpi"
