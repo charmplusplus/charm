99a0d6a8201ee9ab069aac091e8dcb99cf6e43d3
Gengbin Zheng <gzheng@illinois.edu>
2002-01-06 22:36:42 +0000

    removed mpi commons.

--- src/arch/mpi-sol/conv-mach.h
+++ src/arch/mpi-sol/conv-mach.h
@@ -8,17 +8,11 @@
 #ifndef _CONV_MACH_H
 #define _CONV_MACH_H
 
-#define CMK_CMIDELIVERS_USE_COMMON_CODE                    1
-#define CMK_CMIDELIVERS_USE_SPECIAL_CODE                   0
-
-#define CMK_CMIPRINTF_IS_A_BUILTIN                         0
-#define CMK_CMIPRINTF_IS_JUST_PRINTF                       1
+#include "conv-common.h"
 
 #define CMK_DEFAULT_MAIN_USES_COMMON_CODE                  1
 
 #define CMK_GETPAGESIZE_AVAILABLE                          1
-
-#define CMK_HANDLE_SIGUSR                                  1
 
 #define CMK_IS_HETERO                                      0
 
@@ -28,22 +22,7 @@
 #define CMK_MEMORY_PAGESIZE                                8192
 #define CMK_MEMORY_PROTECTABLE                             1
 
-#define CMK_MSG_HEADER_BASIC  CMK_MSG_HEADER_EXT
-#define CMK_MSG_HEADER_EXT    { CmiUInt2 hdl,xhdl,info,d3; }
-
-#define CMK_MULTICAST_GROUP_TYPE                struct { unsigned pe, id; }
-#define CMK_MULTICAST_DEF_USE_COMMON_CODE                  1
-#define CMK_MULTICAST_LIST_USE_COMMON_CODE                 1
-#define CMK_MULTICAST_GROUP_USE_COMMON_CODE                1
-
 #define CMK_NODE_QUEUE_AVAILABLE                           0
-
-#define CMK_REDUCTION_USES_COMMON_CODE                     1
-#define CMK_REDUCTION_USES_SPECIAL_CODE                    0
-
-#define CMK_RSH_IS_A_COMMAND                               0
-#define CMK_RSH_NOT_NEEDED                                 1
-#define CMK_RSH_USE_REMSH                                  0
 
 #define CMK_SHARED_VARS_EXEMPLAR                           0
 #define CMK_SHARED_VARS_UNAVAILABLE                        1
@@ -53,10 +32,6 @@
 #define CMK_SIGNAL_NOT_NEEDED                              0
 #define CMK_SIGNAL_USE_SIGACTION                           0
 #define CMK_SIGNAL_USE_SIGACTION_WITH_RESTART              1
-
-#define CMK_SPANTREE_MAXSPAN                               4
-#define CMK_SPANTREE_USE_COMMON_CODE                       1
-#define CMK_SPANTREE_USE_SPECIAL_CODE                      0
 
 #define CMK_SYNCHRONIZE_ON_TCP_CLOSE                       0
 
@@ -75,17 +50,12 @@
 #define CMK_TYPEDEF_FLOAT4 float
 #define CMK_TYPEDEF_FLOAT8 double
 
-#define CMK_VECTOR_SEND_USES_COMMON_CODE                   1
-#define CMK_VECTOR_SEND_USES_SPECIAL_CODE                  0
-
 #define CMK_WHEN_PROCESSOR_IDLE_BUSYWAIT                   1
 #define CMK_WHEN_PROCESSOR_IDLE_USLEEP                     0
 
 #define CMK_USE_HP_MAIN_FIX                                0
 #define CMK_DONT_USE_HP_MAIN_FIX                           1
 
-#define CMK_CCS_AVAILABLE                                  1
-#define NODE_0_IS_CONVHOST                                 1
 #define CMK_WEB_MODE                                       1
 #define CMK_DEBUG_MODE                                     0
 
