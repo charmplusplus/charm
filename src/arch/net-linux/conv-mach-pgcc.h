a2bb1919b60c34f7043e71f0990cb4db04af97b4
Gengbin Zheng <gzheng@illinois.edu>
2002-02-19 19:33:55 +0000

    disable rdtsc timer.

--- src/arch/net-linux/conv-mach-pgcc.h
+++ src/arch/net-linux/conv-mach-pgcc.h
@@ -3,4 +3,7 @@
 #define CMK_COMPILEMODE_ORIG                               1
 #define CMK_COMPILEMODE_ANSI                               0
 
-
+#undef CMK_TIMER_USE_GETRUSAGE
+#undef CMK_TIMER_USE_RDTSC
+#define CMK_TIMER_USE_GETRUSAGE                            1
+#define CMK_TIMER_USE_RDTSC                                0
