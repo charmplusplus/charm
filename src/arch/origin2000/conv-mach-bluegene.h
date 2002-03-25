d522cb5f3d197f3cbc310cc392ff286457e88faa
Gengbin Zheng <gzheng@illinois.edu>
2002-03-25 07:52:13 +0000

    updated.

--- src/arch/origin2000/conv-mach-bluegene.h
+++ src/arch/origin2000/conv-mach-bluegene.h
@@ -3,3 +3,9 @@
 
 #undef CMK_CONDS_USE_SPECIAL_CODE
 #define CMK_CONDS_USE_SPECIAL_CODE 			  1
+
+#define CMK_BLUEGENE_NODE                                 1
+#define CMK_BLUEGENE_THREAD                               0
+
+#define CMK_TRACE_IN_CHARM                                1
+
