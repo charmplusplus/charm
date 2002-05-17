1273dfb096aeb2f92426d2b353d264eabca23f01
Orion Lawlor <olawlor@acm.org>
2002-05-17 17:14:50 +0000

    Fixed duplicate underscore, added descriptive comment.

--- src/arch/net-linux/conv-mach-lcs.sh
+++ src/arch/net-linux/conv-mach-lcs.sh
@@ -1,1 +1,2 @@
-CMK_CF90='f90 -W132 -YEXT_NAMES=LCS -YEXT_SFX=__'
+COMMENT="Force Absoft f90 compiler to produce external symbols that are compatible with g77."
+CMK_CF90='f90 -W132 -YEXT_NAMES=LCS -YEXT_SFX=_'
