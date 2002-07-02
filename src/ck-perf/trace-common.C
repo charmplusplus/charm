25cd26e348536702d893b36594d8934e8250b161
Gengbin Zheng <gzheng@illinois.edu>
2002-07-02 16:22:22 +0000

    added traceIsOn(), also traceUsrEvent now only trace when traceOn is on.

--- src/ck-perf/trace-common.C
+++ src/ck-perf/trace-common.C
@@ -69,10 +69,14 @@ static void traceCommonInit(char **argv)
   }
 }
 
+extern "C" int traceIsOn(void) {
+  return CpvAccess(traceOn);
+}
+
 /*Install the beginIdle/endIdle condition handlers.*/
 extern "C" void traceBegin(void) {
   OPTIMIZE_WARNING
-  DEBUGF(("[%d] traceBegin called with %d\n", CkMyPe(), CpvAccess(traceOn)));
+  DEBUGF(("[%d] traceBegin called with %d at %f\n", CkMyPe(), CpvAccess(traceOn), TraceTimer()));
   if (CpvAccess(traceOn)==1) return;
   CkpvAccess(_traces)->traceBegin();
   CpvAccess(traceOn) = 1;
@@ -81,6 +85,7 @@ extern "C" void traceBegin(void) {
 /*Cancel the beginIdle/endIdle condition handlers.*/
 extern "C" void traceEnd(void) {
   OPTIMIZE_WARNING
+  DEBUGF(("[%d] traceEnd called with %d at %f\n", CkMyPe(), CpvAccess(traceOn), TraceTimer()));
   if (CpvAccess(traceOn)==0) return;
   CkpvAccess(_traces)->traceEnd();
   CpvAccess(traceOn) = 0;
@@ -163,7 +168,8 @@ extern "C"
 void traceUserEvent(int e)
 {
 #ifndef CMK_OPTIMIZE
-  CkpvAccess(_traces)->userEvent(e);
+  if (CpvAccess(traceOn))
+    CkpvAccess(_traces)->userEvent(e);
 #endif
 }
 
