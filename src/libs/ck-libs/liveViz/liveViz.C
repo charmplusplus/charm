10eb17adb07f33f5882fc13fddf5a4449f4ce8e3
Orion Lawlor <olawlor@acm.org>
2002-07-12 16:20:15 +0000

    Need to make lv_config a readonly, because the bytes per pixel (bpp) field is needed on all processors.
    Thanks to Jonathan for finding this bug.

--- src/libs/ck-libs/liveViz/liveViz.C
+++ src/libs/ck-libs/liveViz/liveViz.C
@@ -1,42 +1,53 @@
 /*
   liveViz: image-assembly interface.
+
 Orion Sky Lawlor, olawlor@acm.org, 6/11/2002
 */
 #include "liveViz.h"
 #include "image.h"
 
-static liveVizConfig config;
+/*readonly*/ liveVizConfig lv_config;
 static CkCallback clientGetImageCallback;
 
-//Called by clients to start liveViz
+//Called by clients to start liveViz.
+//  Because of the readonly initialization, this has to be called from
+//  the main chare; if we could set lv_config some other way, we wouldn't
+//  need this restriction.
 void liveVizInit(const liveVizConfig &cfg, CkArrayID a, CkCallback c)
 {
   if (CkMyPe()!=0) CkAbort("liveVizInit must be called only on processor 0!");
-  config=cfg;
+  lv_config=cfg;
   clientGetImageCallback=c;
   liveViz0Init(cfg);
 }
 
-//Called by lower layers when an image request comes in
+//Called by lower layers when an image request comes in on processor 0.
+//  Just forwards request on to user.
 void liveViz0Get(const liveVizRequest3d &req)
 {
   clientGetImageCallback.send(new liveVizRequestMsg(req));
 }
 
+//Image combining reduction type: defined below.
 static CkReductionMsg *imageCombine(int nMsg,CkReductionMsg **msgs);
 static void vizReductionHandler(void *ignored,void *r_msg);
-
 static CkReduction::reducerType imageCombineReducer;
+
 static void liveVizNodeInit(void) {
 	imageCombineReducer=CkReduction::addReducer(imageCombine);
 }
 
 #if 1
-/****************** Fairly Slow, but simple image combining *****************
+/****************** Fairly Slow, but straightforward image combining *****************
 The contributed data looks like:
 	liveVizRequest
 	Rectangle of image, in final image coordinates (fully clipped)
 	image data
+
+This turned out to be incredibly hideous, because the simple data 
+structure above has to be compressed into a flat run of bytes.
+I'm considering changing the reduction interface to use messages,
+or some sort of pup'd object.
 */
 
 class imageHeader {
@@ -52,7 +63,7 @@ static CkReductionMsg *allocateImageMsg(const liveVizRequest &req,const Rect &r,
 {
   imageHeader hdr(req,r);
   CkReductionMsg *msg=CkReductionMsg::buildNew(
-  	sizeof(imageHeader)+r.area()*config.getBytesPerPixel(),
+  	sizeof(imageHeader)+r.area()*lv_config.getBytesPerPixel(),
 	NULL,imageCombineReducer);
   byte *dest=(byte *)msg->getData();
   *(imageHeader *)dest=hdr;
@@ -66,7 +77,7 @@ void liveVizDeposit(const liveVizRequest &req,
 		    int sizex, int sizey, const byte * src,
 		    ArrayElement* client)
 {
-  if (config.getVerbose(2))
+  if (lv_config.getVerbose(2))
     CkPrintf("liveVizDeposit> Deposited image at (%d,%d), (%d x %d) pixels, on pe %d\n",
     	startx,starty,sizex,sizey,CkMyPe());
 
@@ -74,7 +85,7 @@ void liveVizDeposit(const liveVizRequest &req,
   Rect r(startx,starty, startx+sizex,starty+sizey);
   r=r.getIntersect(Rect(req.wid,req.ht)); //Never copy out-of-bounds regions
   if (r.isEmpty()) r.zero();
-  int bpp=config.getBytesPerPixel();
+  int bpp=lv_config.getBytesPerPixel();
   byte *dest;
   CkReductionMsg *msg=allocateImageMsg(req,r,&dest);
   
@@ -92,15 +103,20 @@ void liveVizDeposit(const liveVizRequest &req,
   client->contribute(msg);
 }
 
+/*Called by the reduction manager to combine all the source images 
+received on one processor.
+*/
 static CkReductionMsg *imageCombine(int nMsg,CkReductionMsg **msgs)
 {
   if (nMsg==1) { //Don't bother copying if there's only one source
+        if (lv_config.getVerbose(2))
+	    CkPrintf("imageCombine> Skipping combine on pe %d\n",CkMyPe());
   	CkReductionMsg *ret=msgs[0]; 
 	msgs[0]=NULL; //Prevent reduction manager from double-delete
 	return ret;
   }
   int m;
-  int bpp=config.getBytesPerPixel();
+  int bpp=lv_config.getBytesPerPixel();
   imageHeader *firstHdr=(imageHeader *)msgs[0]->getData();
 
 //Determine the size of the output image
@@ -112,12 +128,16 @@ static CkReductionMsg *imageCombine(int nMsg,CkReductionMsg **msgs)
   CkReductionMsg *msg=allocateImageMsg(firstHdr->req,destRect,&dest);
   
 //Add each source image to the destination
+// Everything should be pre-clippped, so no further clipping is needed.
   Image destImage(destRect.wid(),destRect.ht(),bpp,dest);
   destImage.clear();
   for (m=0;m<nMsg;m++) {
   	byte *src=(byte *)msgs[m]->getData();
   	imageHeader *mHdr=(imageHeader *)src;
-	src+=sizeof(imageHeader);
+	src+=sizeof(imageHeader); 
+	if (lv_config.getVerbose(2))
+	    CkPrintf("imageCombine>    pe %d  image %d is (%d,%d, %d,%d)\n",
+    	          CkMyPe(),m,mHdr->r.l,mHdr->r.t,mHdr->r.r,mHdr->r.b);
 	Image srcImage(mHdr->r.wid(),mHdr->r.ht(),bpp,src);
 	destImage.add(mHdr->r.l-destRect.l,mHdr->r.t-destRect.t,srcImage);
   }
@@ -125,12 +145,16 @@ static CkReductionMsg *imageCombine(int nMsg,CkReductionMsg **msgs)
   return msg;
 }
 
+/*
+Called once final image has been assembled (reduction handler).
+Unpacks image, and passes it on to layer 0.
+*/
 static void vizReductionHandler(void *ignored,void *r_msg)
 {
   CkReductionMsg *msg = (CkReductionMsg*)r_msg;
   imageHeader *hdr=(imageHeader *)msg->getData();
   byte *srcData=sizeof(imageHeader)+(byte *)msg->getData();
-  int bpp=config.getBytesPerPixel();
+  int bpp=lv_config.getBytesPerPixel();
   Rect destRect(0,0,hdr->req.wid,hdr->req.ht);
   if (destRect==hdr->r) { //Client contributed entire image-- pass along unmodified
     liveViz0Deposit(hdr->req,srcData);
@@ -149,42 +173,10 @@ static void vizReductionHandler(void *ignored,void *r_msg)
 #else
 /****************** Fast but complex image combining *****************
 
+Aught to do some sort of run-length encoding, or list-of-rectangles here.
+
 INCOMPLETE
 */
-
-//Called by clients to deposit a piece of the final image
-void liveVizDeposit(const liveVizRequest &req,
-		    int startx, int starty, 
-		    int sizex, int sizey, const byte * src,
-		    ArrayElement* client)
-{
-//Allocate a reductionMessage:
-  int nRect=1;
-  Rect r(startx,starty, startx+sizex,starty+sizey);
-  r.intersect(Rect(req.wid,req.ht)); //Never copy out-of-bounds regions
-  int bpp=config.getBytesPerPixel();
-  int imgLen=r.area()*bpp;
-  CkReductionMsg *msg=buildNew(sizeof(liveVizRequest)+
-  	sizeof(nRect)+nRect*sizeof(Rect)+imgLen,imageCombineReducer);
-
-//Copy our image into the reductionMessage:
-  byte *dest=(byte *)msg->getData();
-  *(liveVizRequest *)dest=req; dest+=sizeof(liveVizRequest);
-  *(int *)dest=nRect; dest+=sizeof(int);
-  *(Rect *)dest=r; dest+=sizeof(Rect);
-  //We can't just copy image with memcpy, because we may be clipping user data here:
-  int wid=r.r-r.l, ht=r.b-r.t;
-  for (int y=0;y<ht;y++)
-  for (int x=0;x<wid;x++) {
-  	for (int i=0;i<bpp;i++) //Ick!  Need an abstraction here...
-  	  dest[(y*wid+x)*bpp+i]=src[((y+r.r-starty)*sizex+(x+r.l-startx))*bpp+i];
-  }
-  
-  msg->setCallback(CkCallback(vizReductionHandler));
-  client->contribute(msg);
-}
-
-
 
 #endif
 
