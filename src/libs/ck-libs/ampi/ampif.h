4f495ec06c02e6991a98466627e9b43191e1f3fc
Orion Lawlor <olawlor@acm.org>
2002-07-24 20:45:56 +0000

    Added "include"-able version of AMPI headers, and made the MPIINCL module use that version.

--- src/libs/ck-libs/ampi/ampif.h
+++ src/libs/ck-libs/ampi/ampif.h
@@ -1,25 +1,34 @@
-        integer, parameter :: MPI_SUCCESS = 0
-	
-	integer, parameter :: MPI_DOUBLE_PRECISION = 0
-	integer, parameter :: MPI_INTEGER = 1
-	integer, parameter :: MPI_REAL = 2
-	integer, parameter :: MPI_COMPLEX = 3
-	integer, parameter :: MPI_LOGICAL = 4
-	integer, parameter :: MPI_CHARACTER = 5
-	integer, parameter :: MPI_BYTE = 6
-	integer, parameter :: MPI_PACKED = 7
-	
-	integer, parameter :: MPI_COMM_WORLD = 0
-	integer, parameter :: MPI_ANY_SOURCE = -1
-	integer, parameter :: MPI_ANY_TAG = -1
-	integer, parameter :: MPI_REQUEST_NULL = -1
-	integer, parameter :: MPI_TYPE_NULL = -1
-	
-	integer, parameter :: MPI_MAX = 1
-	integer, parameter :: MPI_MIN = 2
-	integer, parameter :: MPI_SUM = 3
-	integer, parameter :: MPI_PROD = 4
+       integer, parameter :: MPI_SUCCESS = 0
 
-	integer, parameter :: MPI_TAG_UB = 1024
-	
-	double precision, external :: MPI_WTIME
+       integer, parameter :: MPI_DOUBLE_PRECISION = 0
+       integer, parameter :: MPI_INTEGER = 1
+       integer, parameter :: MPI_REAL = 2
+       integer, parameter :: MPI_COMPLEX = 3
+       integer, parameter :: MPI_LOGICAL = 4
+       integer, parameter :: MPI_CHARACTER = 5
+       integer, parameter :: MPI_BYTE = 6
+       integer, parameter :: MPI_PACKED = 7
+
+       integer, parameter :: MPI_MAX = 1
+       integer, parameter :: MPI_MIN = 2
+       integer, parameter :: MPI_SUM = 3
+       integer, parameter :: MPI_PROD = 4
+
+       integer, parameter :: MPI_COMM_WORLD = 0
+       integer, parameter :: MPI_ANY_SOURCE = -1
+       integer, parameter :: MPI_ANY_TAG = -1
+       integer, parameter :: MPI_REQUEST_NULL = -1
+       integer, parameter :: MPI_TYPE_NULL = -1
+
+       integer, parameter :: MPI_TAG_UB = 1024
+       
+       integer, parameter :: MPI_STATUS_SIZE=4
+       integer, parameter :: MPI_TAG=1
+       integer, parameter :: MPI_SOURCE=2
+       integer, parameter :: MPI_COMM=3
+
+       integer, parameter :: MPI_MAX_COMM=8
+       integer :: MPI_COMM_UNIVERSE(1:MPI_MAX_COMM)
+
+       integer, external :: MPI_Register
+       double precision, external :: MPI_WTIME
--- src/libs/ck-libs/ampi/ampimod.f90
+++ src/libs/ck-libs/ampi/ampimod.f90
@@ -1,86 +1,52 @@
-module AMPI
+module MPIINCL
+   include 'ampif.h'
+   
+   external MPI_Init_universe
+   external MPI_Comm_rank
+   external MPI_Comm_size
+   external MPI_Finalize
+   external MPI_Send
+   external MPI_Ssend
+   external MPI_Recv
+   external MPI_Isend
+   external MPI_Issend
+   external MPI_Irecv
+   external MPI_Sendrecv
+   external MPI_Barrier
+   external MPI_Bcast
+   external MPI_Reduce
+   external MPI_Allreduce
+   external MPI_Start
+   external MPI_Waitall
+   external MPI_Waitany
+   external MPI_Wait
+   external MPI_Send_init
+   external MPI_Recv_init
+   external MPI_Type_contiguous
+   external MPI_Type_vector
+   external MPI_Type_hvector
+   external MPI_Type_indexed
+   external MPI_Type_hindexed
+   external MPI_Type_struct
+   external MPI_Type_commit
+   external MPI_Type_free
+   external MPI_Type_extent
+   external MPI_Type_size
+   external MPI_Allgatherv
+   external MPI_Allgather
+   external MPI_Gatherv
+   external MPI_Gather
+   external MPI_Alltoallv
+   external MPI_Alltoall
+   external MPI_Comm_dup
+   external MPI_Comm_free
+   external MPI_Abort
+   external MPI_Print
+   external MPI_Migrate
 
-  integer, parameter :: MPI_COMM_WORLD=0
-  integer, parameter :: MPI_STATUS_SIZE=4
-  integer, parameter :: MPI_MAX_COMM=8
+   external MPI_Register_main
+   external MPI_Attach
 
-  integer, parameter :: MPI_DOUBLE_PRECISION=0
-  integer, parameter :: MPI_INTEGER=1
-  integer, parameter :: MPI_REAL=2
-  integer, parameter :: MPI_COMPLEX=3
-  integer, parameter :: MPI_LOGICAL=4
-  integer, parameter :: MPI_CHARACTER=5
-  integer, parameter :: MPI_BYTE=6
-  integer, parameter :: MPI_PACKED=7
-
-  integer, parameter :: MPI_SHORT=8
-  integer, parameter :: MPI_LONG=9
-  integer, parameter :: MPI_UNSIGNED_CHAR=10
-  integer, parameter :: MPI_UNSIGNED_SHORT=11
-  integer, parameter :: MPI_UNSIGNED=12
-  integer, parameter :: MPI_UNSIGNED_LONG=13
-  integer, parameter :: MPI_LONG_DOUBLE=14
-
-  integer, parameter :: MPI_MAX=1
-  integer, parameter :: MPI_MIN=2
-  integer, parameter :: MPI_SUM=3
-  integer, parameter :: MPI_PROD=4
-
-  integer, parameter :: MPI_SOURCE=2
-  integer, parameter :: MPI_TAG=1
-  integer, parameter :: MPI_COMM=3
-
-  integer :: MPI_COMM_UNIVERSE(1:MPI_MAX_COMM)
-
-  integer, external :: MPI_Register
-  double precision, external :: MPI_Wtime
-
-  external MPI_Init_universe
-  external MPI_Comm_rank
-  external MPI_Comm_size
-  external MPI_Finalize
-  external MPI_Send
-  external MPI_Ssend
-  external MPI_Recv
-  external MPI_Isend
-  external MPI_Issend
-  external MPI_Irecv
-  external MPI_Sendrecv
-  external MPI_Barrier
-  external MPI_Bcast
-  external MPI_Reduce
-  external MPI_Allreduce
-  external MPI_Start
-  external MPI_Waitall
-  external MPI_Waitany
-  external MPI_Wait
-  external MPI_Send_init
-  external MPI_Recv_init
-  external MPI_Type_contiguous
-  external MPI_Type_vector
-  external MPI_Type_hvector
-  external MPI_Type_indexed
-  external MPI_Type_hindexed
-  external MPI_Type_struct
-  external MPI_Type_commit
-  external MPI_Type_free
-  external MPI_Type_extent
-  external MPI_Type_size
-  external MPI_Allgatherv
-  external MPI_Allgather
-  external MPI_Gatherv
-  external MPI_Gather
-  external MPI_Alltoallv
-  external MPI_Alltoall
-  external MPI_Comm_dup
-  external MPI_Comm_free
-  external MPI_Abort
-  external MPI_Print
-  external MPI_Migrate
-
-  external MPI_Register_main
-  external MPI_Attach
-  
 contains
 
   subroutine MPI_Init(ierr)
@@ -89,4 +55,4 @@ contains
     ierr = 0
   end subroutine
 
-end module AMPI
+end module MPIINCL
