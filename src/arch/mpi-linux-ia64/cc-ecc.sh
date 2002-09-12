b788d37c0fe3443b58d48fb254378bdb653c9111
Gengbin Zheng <gzheng@illinois.edu>
2002-09-12 20:10:09 +0000

    changed default compiler to gcc from gcc3, and add gcc3 as option.

--- src/arch/mpi-linux-ia64/cc-ecc.sh
+++ src/arch/mpi-linux-ia64/cc-ecc.sh
@@ -1,8 +1,8 @@
-CMK_CPP_C='ecc -E '
-CMK_CC='ecc '
-CMK_CC_RELIABLE='ecc '
-CMK_CC_FASTEST='ecc '
-CMK_CXX='ecpc '
-CMK_CXXPP='ecpc -E '
-CMK_LD='ecc '
-CMK_LDXX='ecpc '
+CMK_CPP_C="ecc -E "
+CMK_CC="ecc $CMK_INCDIR "
+CMK_CC_RELIABLE="ecc $CMK_INCDIR "
+CMK_CC_FASTEST="ecc $CMK_INCDIR "
+CMK_CXX="ecpc $CMK_INCDIR "
+CMK_CXXPP="ecpc -E "
+CMK_LD="ecc $CMK_LIBDIR "
+CMK_LDXX="ecpc $CMK_LIBDIR "
