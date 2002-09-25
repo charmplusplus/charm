4539df669f11fb37dfd386a126313e5f53ebb41b
Orion Lawlor <olawlor@acm.org>
2002-09-25 18:26:11 +0000

    Check for out-of-bounds array rank.

--- src/libs/ck-libs/ampi/ampiimpl.h
+++ src/libs/ck-libs/ampi/ampiimpl.h
@@ -40,6 +40,9 @@ public:
 	
 	//Get the array index for rank r in this communicator
 	int getIndexForRank(int r) const {
+#ifndef CMK_OPTIMIZE
+		if (r>=size) CkAbort("AMPI> You passed in an out-of-bounds process rank!");
+#endif
 		if (isWorld) return r;
 		else return indices[r];
 	}
