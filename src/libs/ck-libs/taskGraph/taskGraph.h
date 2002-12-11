4a8630b9381512eaa0c875c42aa07bd0d6fd6185
Jonathan Booth <jbooth@uiuc.edu>
2002-12-11 22:27:03 +0000

    Adding taskGraph into the charm system.

---
+++ src/libs/ck-libs/taskGraph/taskGraph.h
@@ -0,0 +1,135 @@
+/*
+ */
+#ifndef __UIUC_CHARM_TASKGRAPH_H
+#define __UIUC_CHARM_TASKGRAPH_H
+
+#include <charm++.h>
+
+class taskGraphSolver; // forward decl so taskGraph.decl.h doesn't die
+
+#include "taskGraph.decl.h"
+
+/*
+ * taskGraphSolver: A parent solver class that users will sub-class off
+ * of to write their solvers.
+ *
+ * Users must implement the pup and solve methods in their classes.
+ */
+class taskGraphSolver : public PUP::able {
+private:
+  CkArrayID __taskSet;
+  CkArrayIndexMax __taskIndex;
+  CkVec<CkArrayIndexMax> __taskDeps;
+public:
+  virtual void dependsOn(int x) { dependsOn(CkArrayIndex1D(x)); }
+  virtual void dependsOn(int x, int y) { dependsOn(CkArrayIndex2D(x,y)); }
+  virtual void dependsOn(int x, int y, int z) { dependsOn(CkArrayIndex3D(x,y,z)); }
+  virtual void dependsOn(CkArrayIndexMax taskDep) { __taskDeps.push_back(CkArrayIndexMax(taskDep)); }
+
+  static CkArrayID newTaskGraph() { return CProxy_taskGraphArray::ckNew(); }
+  virtual void startTask() {
+    CProxy_taskGraphArray array(__taskSet);
+    array(__taskIndex).insert(__taskDeps, (taskGraphSolver *)this, CkCallback::ignore);
+  }
+  virtual void removeTask() {
+    CProxy_taskGraphArray array(__taskSet);
+    array(__taskIndex).deleteElement();
+  }
+
+public:
+  taskGraphSolver(CkArrayID set, int x)
+    : __taskDeps(), __taskSet(set), __taskIndex(CkArrayIndex1D(x)) {};
+  taskGraphSolver(CkArrayID set, int x, int y)
+    : __taskDeps(), __taskSet(set), __taskIndex(CkArrayIndex2D(x,y)) {};
+  taskGraphSolver(CkArrayID set, int x, int y, int z)
+    : __taskDeps(), __taskSet(set), __taskIndex(CkArrayIndex3D(x,y,z)) {};
+  taskGraphSolver(CkArrayID set, CkArrayIndexMax taskIndex)
+    : __taskDeps(), __taskSet(set), __taskIndex(taskIndex) {};
+  taskGraphSolver(CkMigrateMessage *m) : PUP::able(m) {};
+
+  virtual void pup(PUP::er &p) {
+    PUP::able::pup(p);
+  }
+  PUPable_abstract(taskGraphSolver);
+
+  virtual void solve(int depsCount, taskGraphSolver *data[]) = 0;
+  virtual void setup() = 0;
+};
+
+
+
+class callbackMsg : public CMessage_callbackMsg {
+public:
+  PUPable_marshall<taskGraphSolver> Data;
+  CkArrayIndexMax Index;
+  callbackMsg(taskGraphSolver* self, CkArrayIndexMax ind)
+    : Data(self), Index(ind) {};
+};
+
+
+
+/*
+ * Start the taskGraph abstraction. This function returns a new CkArrayID
+ * of the array it creates to handle this abstraction. A user's code should
+ * call this function then save the CkArrayID to pass to the other
+ * taskGraph functions.
+ */
+CkArrayID taskGraphInit();
+
+/*
+ * Add a new task to the task graph. The array ID, task number to add,
+ * and dependancies must all be specified at this time. A function which
+ * will do the actual evaluation must also be specified.
+ *
+ * There is also a field for any static data that this task should be passed.
+ */
+template <class T>
+void taskGraphAdd(CkArrayID id, T taskID,
+		  CkVec<T> deps,
+		  taskGraphSolver *self,
+		  CkCallback returnResults = CkCallback::ignore) {
+  CkVec<CkArrayIndexMax> newDeps;
+
+  for ( int i = 0 ; i < deps.length() ; i++ ) {
+    newDeps.push_back( CkArrayIndexMax(deps[i]) );
+  }
+
+  CkArrayIndexMax newTaskID( taskID );
+  CProxy_taskGraphArray array(id);
+  array(newTaskID).insert(newDeps, self, returnResults);
+}
+
+/*
+ * Delete an old task. The array ID and task number to delete are required.
+ */
+void taskGraphDelete(CkArrayID id, CkArrayIndexMax taskID);
+
+
+/*
+ * Define the taskGraphArray that handles that work.
+ */
+class taskGraphArray : public ArrayElementMax {
+protected:
+  taskGraphSolver *Self;
+  int isSolved;
+  CkVec<CkArrayIndexMax> Waiting;
+
+  int DepsCount;
+  taskGraphSolver **DepsData;
+  int DepsReceived;
+
+  CkCallback ReturnResults;
+
+  void tryToSolve();
+
+public:
+  taskGraphArray(CkVec<CkArrayIndexMax> deps,
+                 PUPable_marshall<taskGraphSolver> &data,
+		 CkCallback returnResults);
+  taskGraphArray(CkMigrateMessage *m) {};
+  void requestData(CkArrayIndexMax from);
+  void depositData(PUPable_marshall<taskGraphSolver> &data);
+  void deleteElement();
+};
+
+#endif /* def(thisHeader) */
