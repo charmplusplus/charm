4a3398dc4d24aee5712d7073e478b4048549fd57
Gengbin Zheng <gzheng@illinois.edu>
2003-01-16 23:43:44 +0000

    updated to use ckcallback.

--- tests/charm++/delegation/multicast/hello.C
+++ tests/charm++/delegation/multicast/hello.C
@@ -8,10 +8,11 @@ CkChareID mid;
 CProxy_Hello arr;
 CkGroupID mCastGrpId;
 CProxySection_Hello *mcast;
-CProxySection_Hello mcp;
 
 #define SECTIONSIZE  5
 #define REDUCE_TIME  100
+
+void client(CkSectionCookie sid, void *param, int dataSize, void *data);
 
 class HiMsg : public CkMcastBaseMsg, public CMessage_HiMsg
 {
@@ -59,6 +60,128 @@ public:
   };
 };
 
+
+class Hello : public CBase_Hello
+{
+private:
+  CkSectionCookie sid;
+  int init;
+  myReductionCounter *cnt;
+  CProxySection_Hello mcp;
+
+public:
+  Hello()
+  {
+    CkPrintf("Hello %d created\n",thisIndex);
+    init = 0;
+    cnt = NULL;
+  }
+
+  Hello(CkMigrateMessage *m) {}
+
+  void start()
+  {
+CmiPrintf("start\n");
+    CkMulticastMgr *mg = CProxy_CkMulticastMgr(mCastGrpId).ckLocalBranch();
+
+    CkArrayIndexMax al[SECTIONSIZE];
+    for (int i=0; i<SECTIONSIZE; i++) {
+      al[i] = CkArrayIndex1D(i);
+    }
+#if 0
+    mcast = new CProxySection_Hello(thisArrayID, al, SECTIONSIZE, mCastGrpId);
+#endif
+    mcp = CProxySection_Hello::ckNew(thisArrayID, al, SECTIONSIZE);
+    mcp.ckDelegate(mCastGrpId);
+
+    mg->setSection(mcp);
+
+#if 0
+    mg->setSection(sid, thisArrayID, al, SECTIONSIZE);
+    sid.create(thisArrayID);
+    for (int i=0; i<SECTIONSIZE; i++) 
+      sid.addMember(CkArrayIndex1D(i));
+    mg->setSection(sid);
+#endif
+
+    cnt=new myReductionCounter;
+    cnt->reductionsRemaining=REDUCE_TIME;
+    cnt->reductionNo=0;
+//    mg->setReductionClient(mcp, client, cnt);
+    CkCallback *cb = new CkCallback(CkIndex_Hello::cb_client((CkSectionReductionMsg*)NULL), CkArrayIndex1D(0), thisProxy);
+    mg->setReductionClient(mcp, cb);
+
+    HiMsg *hiMsg = new (1, 0) HiMsg;
+    hiMsg->data[0] = 17;
+    mcp.SayHi(hiMsg);
+  }
+  
+//  void cb_client(CkSectionCookie sid, void *param, int dataSize, void *data)
+  void cb_client(CkSectionReductionMsg *msg)
+  {
+    int dataSize = msg->getSize();
+    void *data = msg->getData();
+    CmiPrintf("RESULT [%d]: %d\n", cnt->reductionNo, *(int *)data); 
+
+    // check correctness
+    int result;
+    if (cnt->reductionNo%3 == 0) {
+      result = 0;
+      for (int i=0; i<SECTIONSIZE; i++) result+=i;
+    }
+    else if (cnt->reductionNo%3 == 2) {
+      result = 1;
+      for (int i=1; i<SECTIONSIZE+1; i++) result*=i;
+    }
+    else {
+      result = SECTIONSIZE+1;
+    }
+    if (*(int *)data != result) {
+      CmiAbort("reduction result is wrong!");
+    }
+  
+    cnt->reductionsRemaining--;
+    if (cnt->reductionsRemaining<=0) {
+      CProxy_main mproxy(mid);
+      mproxy.maindone();
+      cnt->reductionNo++;
+    }
+    else {
+      CkMulticastMgr *mg = CProxy_CkMulticastMgr(mCastGrpId).ckLocalBranch();
+      if (cnt->reductionNo % 32 == 0)
+        mg->rebuild(mcp.ckGetSectionCookie());
+  
+      if (cnt->reductionNo%3 == 0) {
+        HiMsg *hiMsg = new (1, 0) HiMsg;
+        hiMsg->data[0] = 18+cnt->reductionNo;
+        mcp.SayHi(hiMsg);
+      }
+      cnt->reductionNo++;
+    }
+    delete msg;
+  }
+
+  void SayHi(HiMsg *m)
+  {
+//    CkPrintf("[%d] Hi[%d] from element %d\n",CmiMyPe(), m->data[0],thisIndex);
+
+    CkGetSectionCookie(sid, m);
+//CmiPrintf("[%d] SayHi: sid on %d %p\n", CmiMyPe(), sid.pe, sid.val);
+
+    CkMulticastMgr *mg = CProxy_CkMulticastMgr(mCastGrpId).ckLocalBranch();
+
+    int data = thisIndex;
+    CkCallback cb(CkIndex_Hello::cb_client(NULL), CkArrayIndex1D(0), thisProxy);
+    mg->contribute(sizeof(int), &data,CkReduction::sum_int, sid, cb);
+    data = thisIndex+2;
+    mg->contribute(sizeof(int), &data,CkReduction::max_int, sid);
+    data = thisIndex+1;
+    mg->contribute(sizeof(int), &data,CkReduction::product_int, sid);
+    delete m;
+  }
+};
+
+#if 0
 void client(CkSectionCookie sid, void *param, int dataSize, void *data)
 {
   myReductionCounter *c=(myReductionCounter *)param;
@@ -99,74 +222,6 @@ void client(CkSectionCookie sid, void *param, int dataSize, void *data)
     c->reductionNo++;
   }
 }
-
-class Hello : public ArrayElement1D
-{
-private:
-  CkSectionCookie sid;
-  int init;
-
-public:
-  Hello()
-  {
-    CkPrintf("Hello %d created\n",thisIndex);
-    init = 0;
-  }
-
-  Hello(CkMigrateMessage *m) {}
-
-  void start()
-  {
-CmiPrintf("start\n");
-    CkMulticastMgr *mg = CProxy_CkMulticastMgr(mCastGrpId).ckLocalBranch();
-
-    CkArrayIndexMax al[SECTIONSIZE];
-    for (int i=0; i<SECTIONSIZE; i++) {
-      al[i] = CkArrayIndex1D(i);
-    }
-#if 0
-    mcast = new CProxySection_Hello(thisArrayID, al, SECTIONSIZE, mCastGrpId);
 #endif
-    mcp = CProxySection_Hello::ckNew(thisArrayID, al, SECTIONSIZE);
-    mcp.ckDelegate(mCastGrpId);
-
-    mg->setSection(mcp);
-
-#if 0
-    mg->setSection(sid, thisArrayID, al, SECTIONSIZE);
-    sid.create(thisArrayID);
-    for (int i=0; i<SECTIONSIZE; i++) 
-      sid.addMember(CkArrayIndex1D(i));
-    mg->setSection(sid);
-#endif
-
-    myReductionCounter *c=new myReductionCounter;
-    c->reductionsRemaining=REDUCE_TIME;
-    c->reductionNo=0;
-    mg->setReductionClient(mcp, client, c);
-
-    HiMsg *hiMsg = new (1, 0) HiMsg;
-    hiMsg->data[0] = 17;
-    mcp.SayHi(hiMsg);
-  }
-  
-  void SayHi(HiMsg *m)
-  {
-//    CkPrintf("[%d] Hi[%d] from element %d\n",CmiMyPe(), m->data[0],thisIndex);
-
-    CkGetSectionCookie(sid, m);
-//CmiPrintf("[%d] SayHi: sid on %d %p\n", CmiMyPe(), sid.pe, sid.val);
-
-    CkMulticastMgr *mg = CProxy_CkMulticastMgr(mCastGrpId).ckLocalBranch();
-
-    int data = thisIndex;
-    mg->contribute(sizeof(int), &data,CkReduction::sum_int, sid);
-    data = thisIndex+2;
-    mg->contribute(sizeof(int), &data,CkReduction::max_int, sid);
-    data = thisIndex+1;
-    mg->contribute(sizeof(int), &data,CkReduction::product_int, sid);
-    delete m;
-  }
-};
 
 #include "hello.def.h"
