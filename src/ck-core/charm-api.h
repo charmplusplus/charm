a5a286f90d9aa15f9d791e66ea4f9707c621d6e4
Orion Lawlor <olawlor@acm.org>
2003-01-20 20:11:03 +0000

    Added FORTRAN_AS_C macros to make it easier to define fortran functions that are just wrappers around the C version.

--- src/ck-core/charm-api.h
+++ src/ck-core/charm-api.h
@@ -15,14 +15,14 @@ libraries written in Charm for other languages.
 #  define CLINKAGE /*empty*/
 #endif
 
-/*Used to define a "C" entry point*/
+/** Used to define a "C" entry point*/
 #undef CDECL /*<- used by Microsoft Visual C++*/
 #define CDECL CLINKAGE
 
-/*Used to define a Fortran-callable routine*/
+/** Used to define a Fortran-callable routine*/
 #define FDECL CLINKAGE
 
-/*Fortran name mangling:
+/** Fortran name mangling:
 */
 #if CMK_FORTRAN_USES_ALLCAPS
 # define FTN_NAME(caps,nocaps) caps  /*Declare name in all caps*/
@@ -36,6 +36,34 @@ libraries written in Charm for other languages.
 /* # error "Did not set fortran name mangling scheme" */
 # define FTN_NAME(caps,nocaps) NONMANGLED_##nocaps
 #endif
+
+
+/** 
+ * Define a new Fortran-callable API routine, returning void,
+ * that does nothing but map its arguments onto some 
+ * (presumably analogous) C routine. 
+ *
+ *  @param CAPITALNAME Fortran routine name to define, in all capital letters.
+ *  @param Cname C routine to call, in normal case.
+ *  @param lowername Fortran routine name in all lowercase.
+ *  @param routine_args Fortran routine's arguments (e.g., "int *idx")
+ *  @param c_args Arguments to pass to C routine (e.g., "*idx-1")
+ */
+#define FORTRAN_AS_C(CAPITALNAME,Cname,lowername, routine_args,c_args) \
+FDECL void \
+FTN_NAME(CAPITALNAME,lowername) routine_args { \
+	Cname c_args;\
+}
+
+/** 
+ * Like FORTRAN_AS_C, but with a return type as the first parameter.
+ */
+#define FORTRAN_AS_C_RETURN(returnType, CAPITALNAME,Cname,lowername, routine_args,c_args) \
+FDECL returnType \
+FTN_NAME(CAPITALNAME,lowername) routine_args { \
+	return Cname c_args;\
+}
+
 
 #endif /*Def(thisHeader) */
 
