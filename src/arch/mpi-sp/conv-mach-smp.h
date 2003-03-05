4ea227f9715a916c8d063bcce487c5021a8ded58
Gengbin Zheng <gzheng@illinois.edu>
2003-03-05 08:36:10 +0000

    using getusage instead of MPI_Wtimer in mpi-smp to avoid concurrent MPI calls.

--- src/arch/mpi-sp/conv-mach-smp.h
+++ src/arch/mpi-sp/conv-mach-smp.h
@@ -22,5 +22,9 @@
 #define CMK_SHARED_VARS_POSIX_THREADS_SMP                  1
 
 #undef CMK_CCS_AVAILABLE
-#define CMK_CCS_AVAILABLE                                  1
+#define CMK_CCS_AVAILABLE                                  0
 
+#undef CMK_TIMER_USE_SPECIAL
+#undef CMK_TIMER_USE_GETRUSAGE
+#define CMK_TIMER_USE_SPECIAL				   0
+#define CMK_TIMER_USE_GETRUSAGE				   1
