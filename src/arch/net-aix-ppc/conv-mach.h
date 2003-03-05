bdea07d6e7f753bef2e4c48b2158416b9b07bbdb
Gengbin Zheng <gzheng@illinois.edu>
2003-03-05 08:34:50 +0000

    added smp config files for net-rs6k

--- src/arch/net-aix-ppc/conv-mach.h
+++ src/arch/net-aix-ppc/conv-mach.h
@@ -38,6 +38,9 @@
 #define CMK_SHARED_VARS_SUN_THREADS                        0
 #define CMK_SHARED_VARS_UNIPROCESSOR                       0
 
+#define CMK_THREADS_USE_CONTEXT                            0
+#define CMK_STACK_GROWUP                                   1
+
 #define CMK_SIGNAL_NOT_NEEDED                              0
 #define CMK_SIGNAL_USE_SIGACTION                           1
 #define CMK_SIGNAL_USE_SIGACTION_WITH_RESTART              0
