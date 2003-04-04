3f64d1e7c5f40fd0f998831faf3edd9eab24b88c
Theckla Louchios <louchios@uiuc.edu>
2003-04-04 18:16:59 +0000

    Handles the parsed file to generate the sdag code

---
+++ src/xlat-i/CParsedFile.h
@@ -0,0 +1,40 @@
+#include "xi-symbol.h"
+#include "CEntry.h"
+#include "sdag-globals.h"
+#include "EToken.h"
+class Entry;
+
+
+/******************* CParsedFile ***********************/
+class CParsedFile {
+  private:
+    void numberNodes(void);
+    void labelNodes(void);
+    void propagateState(void);
+    void generateEntryList(void);
+    void generateCode(XStr& output);
+    void generateEntries(XStr& output);
+    void generateInitFunction(XStr& output);
+    void generatePupFunction(XStr& output);
+  public:
+    TList<CEntry*> entryList;
+    TList<Entry*> nodeList;
+    CParsedFile(void) {}
+    ~CParsedFile(void){}
+    void print(int indent);
+    void doProcess(XStr& classname, XStr& output) {
+      output << "#define " << classname << "_SDAG_CODE \n";
+      numberNodes();
+      labelNodes();
+      propagateState();
+      generateEntryList();
+      generateCode(output);
+      generateEntries(output);
+      generateInitFunction(output);
+      generatePupFunction(output);
+      output.line_append('\\');
+      output << "\n";
+    }
+
+};
+
