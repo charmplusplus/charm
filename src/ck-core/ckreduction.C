0c4a4435bc3e987078c5de0b65448a049bf20c97
Orion Lawlor <olawlor@acm.org>
2003-04-07 20:52:25 +0000

    Use explicit "pupCkVec" routine instead of operator|, which confuses Visual C++ 6.0

--- src/ck-core/ckreduction.C
+++ src/ck-core/ckreduction.C
@@ -619,7 +619,7 @@ void CkReductionMgr::pup(PUP::er &p)
   p|futureMsgs;
   p|futureRemoteMsgs;
   p|finalMsgs;
-  p|adjVec;
+  pupCkVec(p,adjVec);
   p|nodeProxy;
   p | storedCallback;
   if(p.isUnpacking()){
