a97fde703a8c64041dfb9f4fd963872a9614165d
Orion Lawlor <olawlor@acm.org>
2003-04-11 17:44:19 +0000

    Added this master page, to point to various sub-pages.

---
+++ doc/doxygen/src/main_page.C
@@ -0,0 +1,68 @@
+/* This is the master Doxygen documentation links page--
+   It contains no actual executable source code. */
+/**
+\mainpage Charm Source Code Documentation
+<!-- This HTML is generated from charm/doc/doxygen/main_page.C -->
+
+\section layers Major Runtime Layers:
+
+<table border=2><tr><td>
+<p>Libraries:
+<p>Communication: Barrier, commlib, multicast, 
+sparseReducer, sparseContiguousReducer
+
+<P>Languages: ARMCI, AMPI, TCharm, taskGraph, search
+
+<p>Frameworks: FEM, IFEM, IDXL, Collide, MBlock, AMR
+
+<p>Visualization: liveViz, liveViz3d, netFEM
+ 
+</td></tr><tr><td>
+Charm++:
+<a href="group__CkArray.html">Arrays</a>,
+<a href="group__CkLdb.html">Load balancer</a>,
+<a href="group__CkPerf.html">Tracing</a>,
+<a href="group__CkReduction.html">Reductions</a>,
+<a href="group__CkArrayImpl.html">Array Implementation</a>
+
+</td></tr><tr><td>
+Charm Kernel:
+<a href="group__Ck.html">Groups and Chares</a>,  
+<a href="group__CkRegister.html">Registration</a>, 
+<a href="group__CkQD.html">Quiescence detection</a>, 
+<a href="group__CkFutures.html">Thread support</a>,
+<a href="group__CkInit.html">Startup</a>,
+Translator, SDAG.
+
+</td></tr><tr><td>
+Converse:
+<a href="group__Converse.html">core</a>,
+scheduler, threads, memory allocation,
+conditions, random numbers, converse client/server (CCS).
+
+Converse tracing, parameter marshalling (CPM),
+quiescence.
+
+</td></tr><tr><td>
+Converse Machine Layer:
+<ul>
+<li>net
+  <ul>
+  <li>net-smp
+  <li>gm
+  </ul>
+<li>mpi
+</ul>
+
+</td></tr><tr><td>
+Converse Configuration Layer:
+build system, charmc, configure script, conv-mach.h/.sh.
+
+</td></tr></table>
+
+\section utilities Utility Code
+PUP, CkBitvector, CkDll, CkHashtable, 
+CkImage, CkVec, CkQ, CkStatistics, CkVector3d,
+sockets.
+
+*/
