b5aa5ef07395ccfd04d6ded55f5b6b8f824b2a53
Orion Lawlor <olawlor@acm.org>
2003-04-22 20:32:10 +0000

    Look for intel compiler in /opt, too.

--- src/arch/common/cc-icc.sh
+++ src/arch/common/cc-icc.sh
@@ -6,8 +6,12 @@ CMK_LD='icc -static'
 CMK_LDXX='icpc -static'
 CMK_CF90='ifc '
 CMK_CF90_FIXED="$CMK_CF90 -FI "
-CMK_F90LIBS='-L/usr/local/intel/compiler70/ia32/lib -lintrins -lIEPCF90 -lF90 -lintrins -limf  '
+CMK_SEQ_F90="$CMK_CF90"
+CMK_SEQ_CC="$CMK_CC"
+CMK_SEQ_CXX="$CMK_CXX"
+CMK_SEQ_LD="$CMK_LD"
+CMK_SEQ_LDXX="$CMK_LDXX"
+CMK_F90LIBS='-L/usr/local/intel/compiler70/ia32/lib -L/opt/intel/compiler70/ia32/lib -lintrins -lIEPCF90 -lF90 -lintrins -limf  '
 CMK_MOD_NAME_ALLCAPS=1
 CMK_MOD_EXT="mod"
 CMK_F90_USE_MODDIR=""
-
