a486cc7ded6da0e6117a3ea87c29517537744878
Orion Lawlor <olawlor@acm.org>
2003-04-23 22:08:00 +0000

    Substantially change both policy and implementation:   - Added "idle" global flush criterion.   - Added "bufferMax" local flush criterion. Also substantially cleaned up code.

--- src/libs/ck-libs/commlib/StreamingStrategy.C
+++ src/libs/ck-libs/commlib/StreamingStrategy.C
@@ -1,74 +1,96 @@
 #include "StreamingStrategy.h"
 
-void call_endIteration(void *arg){
-  ((StreamingStrategy *)arg)->periodicFlush();
-  return;
-}
-
-StreamingStrategy::StreamingStrategy(int period){
-  /*
-  streamingMsgBuf = new CkQ<CharmMessageHolder *>[CkNumPes()];
-  streamingMsgCount = new int[CkNumPes()];
-  for (int count=0; count<CkNumPes(); count++)
-    streamingMsgBuf[count] = NULL;
-  */
-  PERIOD = period;
+StreamingStrategy::StreamingStrategy(int periodMs,int bufferMax_)
+	: PERIOD(periodMs), bufferMax(bufferMax_)
+{
+  streamingMsgBuf=NULL;
+  streamingMsgCount=NULL;
 }
 
 void StreamingStrategy::insertMessage(CharmMessageHolder *cmsg) {
-  streamingMsgBuf[cmsg->dest_proc].enq(cmsg);
-  streamingMsgCount[cmsg->dest_proc]++;
+  int pe=cmsg->dest_proc;
+  streamingMsgBuf[pe].enq(cmsg);
+  streamingMsgCount[pe]++;
+  if (streamingMsgCount[pe]>bufferMax) flushPE(pe);
 }
 
 void StreamingStrategy::doneInserting(){
-  ComlibPrintf("[%d] In Streaming strategy\n", CkMyPe());
+  ComlibPrintf("[%d] In Streaming strategy::doneInserting\n", CkMyPe());
   //Do nothing
 }
 
-void StreamingStrategy::periodicFlush(){
+/// Send off all accumulated messages for this PE:
+void StreamingStrategy::flushPE(int pe) {
+  if(streamingMsgCount[pe] == 0)
+     return; //Nothing to do.
   CharmMessageHolder *cmsg, *toBeDeleted = NULL;
-  envelope *env;
-  char **msgComps, *msg;
-  int *sizes, msg_count;
-
-  for (int count=0; count<CkNumPes(); count++) {
-    if(streamingMsgCount[count] == 0)
-      continue;
-    msgComps = new char*[streamingMsgCount[count]];
-    sizes = new int[streamingMsgCount[count]];
-    msg_count = 0;
-    while (!streamingMsgBuf[count].isEmpty()) {
-      cmsg = streamingMsgBuf[count].deq();
-      msg = cmsg->getCharmMessage();
-      env = UsrToEnv(msg);
-      sizes[msg_count] = env->getTotalsize();
-      msgComps[msg_count] = (char *)env;
-      msg_count++;
+  
+  // Build a CmiMultipleSend list of messages to be sent off:
+  int msg_count=streamingMsgCount[pe], msg_pe=0;
+  char **msgComps = new char*[msg_count];
+  int *sizes = new int[msg_count];
+  while (!streamingMsgBuf[pe].isEmpty()) {
+      cmsg = streamingMsgBuf[pe].deq();
+      char *msg = cmsg->getCharmMessage();
+      envelope *env = UsrToEnv(msg);
+      sizes[msg_pe] = env->getTotalsize();
+      msgComps[msg_pe] = (char *)env;
+      msg_pe++;
+      // Link cmsg into the toBeDeleted list:
       cmsg->next = toBeDeleted;
       toBeDeleted = cmsg;
-    }
-    CmiMultipleSend(count, streamingMsgCount[count], sizes, msgComps);
-    delete [] msgComps;
-    delete [] sizes;
-    streamingMsgCount[count] = 0;        
   }
+  if (msg_count!=msg_pe) CkAbort("streamingMsgCount doesn't match streamingMsgBuf!\n");
+  CmiMultipleSend(pe, msg_count, sizes, msgComps);
+  delete [] msgComps;
+  delete [] sizes;
+  streamingMsgCount[pe] = 0;
+  
+  // Traverse the tobeDeleted list:
   cmsg = toBeDeleted;
   while (toBeDeleted) {
     toBeDeleted = toBeDeleted->next;
     delete cmsg;
     cmsg = toBeDeleted;
-  }
-  CcdCallFnAfter(call_endIteration, (void *)this, PERIOD);
+  }      
+}
+
+void StreamingStrategy::periodicFlush(){
+  for (int pe=0; pe<CkNumPes(); pe++) flushPE(pe);
+}
+
+/// This routine is called via CcdCallFnAfter to flush all messages:
+static void call_delayFlush(void *arg){
+  StreamingStrategy *s=(StreamingStrategy *)arg;
+  s->periodicFlush();
+  s->registerFlush(); //Set ourselves up to be called again
+}
+
+void StreamingStrategy::registerFlush(void) {
+  // CkPrintf("[%d] Will call function again every %d ms\n",CkMyPe(),PERIOD);
+  CcdCallFnAfter(call_delayFlush, (void *)this, PERIOD);
+}
+
+/// This routine is called via CcdCallOnCondition to flush all messages:
+static void call_idleFlush(void *arg){
+  StreamingStrategy *s=(StreamingStrategy *)arg;
+  s->periodicFlush();
+}
+
+// When we're finally ready to go, register for timeout and idle flush.
+void StreamingStrategy::beginProcessing(int ignored) {
+  registerFlush();
+  CcdCallOnConditionKeep(CcdPROCESSOR_BEGIN_IDLE,call_idleFlush,(void *)this);
 }
 
 void StreamingStrategy::pup(PUP::er &p){
   p | PERIOD;
+  p | bufferMax;
   if(p.isUnpacking()) {
     streamingMsgBuf = new CkQ<CharmMessageHolder *>[CkNumPes()];
     streamingMsgCount = new int[CkNumPes()];
     for(int count = 0; count < CkNumPes(); count ++)
       streamingMsgCount[count] = 0;
-    periodicFlush();
   }
 }
 
--- src/libs/ck-libs/commlib/StreamingStrategy.h
+++ src/libs/ck-libs/commlib/StreamingStrategy.h
@@ -5,13 +5,31 @@
 class StreamingStrategy : public Strategy {
     CkQ<CharmMessageHolder *> *streamingMsgBuf;
     int *streamingMsgCount;
-    int PERIOD;
+    int PERIOD, bufferMax;
  public:
-    StreamingStrategy(int period);
+    /**
+     Create a streaming strategy, suitable for passing to ComlibManager.
+     These are the criteria for flushing all pending messages:
+       - it's been at least period (in ms) since the last flush, or
+       - the processor just went idle.
+     Thses criteria flush a single PE's pending messages:
+       - more than bufferMax messages to buffered for one PE.
+    */
+    StreamingStrategy(int periodMs=10,int bufferMax=1000);
     StreamingStrategy(CkMigrateMessage *){}
+    
     void insertMessage(CharmMessageHolder *msg);
     void doneInserting();
+    /// Flush all pending messages:
     void periodicFlush();
+    /// Flush all messages destined for this processor:
+    void flushPE(int destPE);
+
+    /// Register self to be flushed again after a delay.
+    void registerFlush(void);
+    
+    virtual void beginProcessing(int ignored);
+
     virtual void pup(PUP::er &p);
     PUPable_decl(StreamingStrategy);
 };
