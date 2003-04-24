e767f1878eae267bc726f13ace360f348321104d
Orion Lawlor <olawlor@acm.org>
2003-04-24 20:52:55 +0000

    Added ability to use 64-bit MPI on IBM SP's.

---
+++ src/arch/mpi-sp/conv-mach-64.sh
@@ -0,0 +1,11 @@
+COMMENT="Enable 64-bit mode (-q64)"
+CMK_CC='mpcc_r -q64 '
+CMK_CXX='mpCC_r -q64 -qstaticinline '
+CMK_LD="$CMK_CC -b64 "
+CMK_LDXX="$CMK_CXX -b64 "
+CMK_CF77='mpxlf_r -q64 '
+CMK_CF90='mpxlf90_r -q64 -qsuffix=f=f90' 
+CMK_CF90_FIXED='mpxlf90_r -q64 ' 
+CMK_C_OPTIMIZE='-O3 -qstrict -Q  '
+CMK_CXX_OPTIMIZE='-O3 -qstrict -Q '
+CMK_AR='ar -X 64 cq'
