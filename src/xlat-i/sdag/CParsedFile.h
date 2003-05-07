7fd3b506d3d55d8fef468fdf33772b1ca444907d
Theckla Louchios <louchios@uiuc.edu>
2003-05-07 18:46:44 +0000

    Modifed to work with the new sdag parser

--- src/xlat-i/sdag/CParsedFile.h
+++ src/xlat-i/sdag/CParsedFile.h
@@ -1,36 +1,28 @@
-/*****************************************************************************
- * $Source$
- * $Author$
- * $Date$
- * $Revision$
- *****************************************************************************/
-
-#ifndef _CParsedFile_H_
-#define _CParsedFile_H_
-
-#include "xi-util.h"
-#include "CList.h"
-#include "COverlap.h"
+#include "xi-symbol.h"
 #include "CEntry.h"
-#include "CParseNode.h"
 #include "sdag-globals.h"
-#include <stdio.h>
+#include "EToken.h"
+class Entry;
 
+
+/******************* CParsedFile ***********************/
 class CParsedFile {
   private:
     void numberNodes(void);
     void labelNodes(void);
     void propagateState(void);
+    void generateConnectEntryList(void);
     void generateEntryList(void);
     void generateCode(XStr& output);
     void generateEntries(XStr& output);
+    void generateConnectEntries(XStr& output);
     void generateInitFunction(XStr& output);
     void generatePupFunction(XStr& output);
   public:
-    TList<COverlap*> overlapList;
     TList<CEntry*> entryList;
-    TList<CParseNode*> nodeList;
-    CParsedFile(void) {}
+    TList<SdagConstruct *> connectEntryList;
+    TList<Entry*> nodeList;
+   CParsedFile(void) {}
     ~CParsedFile(void){}
     void print(int indent);
     void doProcess(XStr& classname, XStr& output) {
@@ -38,6 +30,7 @@ class CParsedFile {
       numberNodes();
       labelNodes();
       propagateState();
+      generateConnectEntryList();
       generateEntryList();
       generateCode(output);
       generateEntries(output);
@@ -46,6 +39,6 @@ class CParsedFile {
       output.line_append('\\');
       output << "\n";
     }
+
 };
 
-#endif
