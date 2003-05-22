21591101e8e4574fb46ef974a63e47951f0e9ec7
Gengbin Zheng <gzheng@illinois.edu>
2003-05-22 21:27:29 +0000

    set CMK_NM to 64 bit mode.

--- src/arch/mpi-sp/conv-mach-64.sh
+++ src/arch/mpi-sp/conv-mach-64.sh
@@ -1,8 +1,8 @@
 COMMENT="Enable 64-bit mode (-q64)"
 CMK_CC='mpcc_r -q64 '
 CMK_CXX='mpCC_r -q64 -qstaticinline '
-CMK_LD="$CMK_CC -b64 "
-CMK_LDXX="$CMK_CXX -b64 "
+CMK_LD="$mpcc_r -q64 "
+CMK_LDXX="mpCC_r -q64 "
 CMK_SEQ_CC='xlc_r -q64'
 CMK_SEQ_LD='xlc_r -q64'
 CMK_SEQ_CXX='xlC_r -qstaticinline -q64'
@@ -14,3 +14,4 @@ CMK_C_OPTIMIZE='-O3 -qstrict -Q  '
 CMK_CXX_OPTIMIZE='-O3 -qstrict -Q '
 CMK_AR='ar -X 64 cq'
 CMK_QT='aix64-xlc'
+CMK_NM='nm -X 64'
