e8fbf5c5ebfe2dc96197f4d2e38aafbf0190d350
Gengbin Zheng <gzheng@illinois.edu>
2003-05-28 07:03:42 +0000

    add -instances=explicit for CC compiler to "Places explicitly instantiated instances into the current object file and gives them global linkage. Does not generate any other needed instances." changed to always statically link libckqt.a

--- src/arch/net-sol/cc-cc64.sh
+++ src/arch/net-sol/cc-cc64.sh
@@ -5,7 +5,7 @@ CMK_CPP_C='cc -E '
 CMK_CC="cc $CMK_CFLAGS"
 CMK_CC_RELIABLE="cc $CMK_CFLAGS"
 CMK_CC_FASTEST="cc $CMK_CFLAGS"
-CMK_CXX="CC -library=Cstd $CMK_CXXFLAGS"
+CMK_CXX="CC -library=Cstd $CMK_CXXFLAGS -instances=explicit "
 CMK_CXXPP='CC -E '
 CMK_CF77='f77'
 CMK_CF90='f90 -C -stackvar -g -xarch=v9a -KPIC'
@@ -14,10 +14,10 @@ CMK_C_OPTIMIZE='-xO5'
 CMK_CXX_DEBUG='-g'
 CMK_CXX_OPTIMIZE='-xO5'
 CMK_LD="cc $CMK_CFLAGS "
-CMK_LDXX="CC -library=Cstd $CMK_CXXFLAGS"
+CMK_LDXX="CC -library=Cstd $CMK_CXXFLAGS -instances=explicit "
 CMK_AR='CC -xar -o'
-CMK_LIBS='-lnsl -lsocket -lckqt'
 CMK_LD_SHARED="-G"
+CMK_LIBS=" -lnsl -lsocket $CHARMLIB/libckqt.a"
 CMK_SEQ_LIBS='-lnsl -lsocket'
 CMK_SEQ_CC="cc $CMK_CFLAGS "
 CMK_SEQ_LD="cc $CMK_CFLAGS "
