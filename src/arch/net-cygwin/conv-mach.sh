fcbafec9375cfa608abd704dfbc349947ea795a7
Gengbin Zheng <gzheng@illinois.edu>
2003-05-29 22:07:14 +0000

    minor changes and disable building so.

--- src/arch/net-cygwin/conv-mach.sh
+++ src/arch/net-cygwin/conv-mach.sh
@@ -1,7 +1,7 @@
 CMK_CPP_CHARM='/lib/cpp -P'
-CMK_CPP_C='gcc -E -I /usr/include/w32api'
-CMK_CC='gcc -I /usr/include/w32api'
-CMK_CXX='g++ -I /usr/include/w32api'
+CMK_CPP_C='gcc -E '
+CMK_CC='gcc '
+CMK_CXX='g++ -Wno-deprecated '
 CMK_CXXPP='g++ -x c++ -E '
 CMK_CF77='f77'
 CMK_CF90='f90'
@@ -11,3 +11,4 @@ CMK_XIOPTS=''
 CMK_F90LIBS='-lvast90 -lg2c'
 CMK_MOD_EXT="vo"
 CMK_POST_EXE='.exe'
+CMK_NO_BUILD_SHARED="true"
