012c5ac0e59badd0b3d82d0ee275ff82c4110cfd
Orion Lawlor <olawlor@acm.org>
2003-06-17 16:09:32 +0000

    Two fixes for the (broken) IBM AIX compiler: 1.) Add "typename" to all templated names, like std::vector<T>
    2.) Add a separate specialization for operator| and std::string,
    because it can't figure out how to specialize std::basic_string<T>.

--- src/util/pup_stl.h
+++ src/util/pup_stl.h
@@ -29,13 +29,13 @@ Orion Sky Lawlor, olawlor@acm.org, 7/22/2002
 /*************** Simple classes ***************/
 
 template <class A,class B> 
-inline void operator|(PUP::er &p,std::pair<A,B> &v)
+inline void operator|(PUP::er &p,typename std::pair<A,B> &v)
 {
   p|v.first;
   p|v.second;
 }
 template <class charType> 
-inline void operator|(PUP::er &p,std::basic_string<charType> &v)
+inline void operator|(PUP::er &p,typename std::basic_string<charType> &v)
 {
   int nChar=v.length();
   p|nChar;
@@ -48,6 +48,21 @@ inline void operator|(PUP::er &p,std::basic_string<charType> &v)
   else /*packing*/ { //Do packing in-place from data
     //Have to cast away constness here
     p((charType *)v.data(),nChar);
+  }
+}
+inline void operator|(PUP::er &p,std::string &v)
+{
+  int nChar=v.length();
+  p|nChar;
+  if (p.isUnpacking()) { //Unpack to temporary buffer
+    char *buf=new char[nChar];
+    p(buf,nChar);
+    v=std::basic_string<char>(buf,nChar);
+    delete[] buf;
+  }
+  else /*packing*/ { //Do packing in-place from data
+    //Have to cast away constness here
+    p((char *)v.data(),nChar);
   }
 }
 
@@ -100,17 +115,17 @@ inline void PUP_stl_map(PUP::er &p,container &c) {
 }
 
 template <class T> 
-inline void operator|(PUP::er &p,std::vector<T> &v)
+inline void operator|(PUP::er &p,typename std::vector<T> &v)
   { PUP_stl_container<std::vector<T>,T>(p,v); }
 template <class T> 
-inline void operator|(PUP::er &p,std::list<T> &v)
+inline void operator|(PUP::er &p,typename std::list<T> &v)
   { PUP_stl_container<std::list<T>,T>(p,v); }
 
 template <class V,class T,class Cmp> 
-inline void operator|(PUP::er &p,std::map<V,T,Cmp> &m)
+inline void operator|(PUP::er &p,typename std::map<V,T,Cmp> &m)
   { PUP_stl_map<std::map<V,T,Cmp>,std::pair<const V,T> >(p,m); }
 template <class V,class T,class Cmp> 
-inline void operator|(PUP::er &p,std::multimap<V,T,Cmp> &m)
+inline void operator|(PUP::er &p,typename std::multimap<V,T,Cmp> &m)
   { PUP_stl_map<std::multimap<V,T,Cmp>,std::pair<const V,T> >(p,m); }
 
 
