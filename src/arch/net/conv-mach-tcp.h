d965018839e36a8c1d013a959bf8e1b1b5959f20
Gengbin Zheng <gzheng@illinois.edu>
2003-07-14 16:19:20 +0000

    make tcp version not use nonblocking, hence use netpoll anmd disable signals.

--- src/arch/net/conv-mach-tcp.h
+++ src/arch/net/conv-mach-tcp.h
@@ -5,3 +5,9 @@
 #if CMK_SMP
 #undef CMK_USE_POLL
 #endif
+
+#define CMK_NETPOLL					    1
+
+#undef CMK_ASYNC_USE_F_SETFL_AND_F_SETOWN
+#undef CMK_ASYNC_NOT_NEEDED
+#define CMK_ASYNC_NOT_NEEDED				    1
