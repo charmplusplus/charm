588dae10cc909630490194ec00ac26c36eb16396
Gengbin Zheng <gzheng@illinois.edu>
2003-07-21 16:21:03 +0000

    added -fPIC

--- src/arch/net-linux/conv-mach-opteron.sh
+++ src/arch/net-linux/conv-mach-opteron.sh
@@ -1,5 +1,5 @@
-CMK_CC='gcc -m64 '
-CMK_CXX='g++ -Wno-deprecated -m64 '
+CMK_CC='gcc -m64 -fPIC '
+CMK_CXX='g++ -Wno-deprecated -m64 -fPIC '
 CMK_SEQ_CC='gcc -m64 -fPIC '
 CMK_SEQ_CXX='g++ -m64 -fPIC '
 CMK_LD="gcc -m64 "
