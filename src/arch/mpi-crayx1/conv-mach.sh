c1c47c25ff08c8105ef6b08f5cca0b1e81289fef
Gengbin Zheng <gzheng@illinois.edu>
2003-07-21 20:12:44 +0000

    charm with mpi on cray x1

---
+++ src/arch/mpi-crayx1/conv-mach.sh
@@ -0,0 +1,15 @@
+CMK_CPP_CHARM='/lib/cpp -P'
+CMK_CPP_C='cc -h ssp -E '
+CMK_CC='cc -h ssp '
+CMK_CXX='CC -h ssp '
+CMK_CXXPP='CC -h ssp -E '
+CMK_CF77='ftn -o ssp'
+CMK_CF90='ftn -o ssp'
+CMK_RANLIB='true'
+CMK_LIBS='-lmpi '
+CMK_LD_LIBRARY_PATH=""
+#CMK_F90LIBS='-L/usr/absoft/lib -L/opt/absoft/lib -lf90math -lfio -lU77 -lf77math '
+#CMK_MOD_NAME_ALLCAPS=1
+#CMK_MOD_EXT="mod"
+#CMK_F90_USE_MODDIR=1
+#CMK_F90_MODINC="-p"
