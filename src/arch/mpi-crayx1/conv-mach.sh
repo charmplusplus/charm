e022572696655d9c1e9e3845118b7035f093c1c4
Gengbin Zheng <gzheng@illinois.edu>
2003-07-22 23:05:16 +0000

    added -lCx to CMK_SEQ_LDXX

--- src/arch/mpi-crayx1/conv-mach.sh
+++ src/arch/mpi-crayx1/conv-mach.sh
@@ -7,7 +7,9 @@ CMK_CF77='ftn -o ssp'
 CMK_CF90='ftn -o ssp'
 CMK_RANLIB='true'
 CMK_LIBS='-lmpi -lCx '
+CMK_SEQ_LDXX="$CMK_CXX -lCx"
 CMK_QT='generic64'
+
 #CMK_F90LIBS='-L/usr/absoft/lib -L/opt/absoft/lib -lf90math -lfio -lU77 -lf77math '
 #CMK_MOD_NAME_ALLCAPS=1
 #CMK_MOD_EXT="mod"
