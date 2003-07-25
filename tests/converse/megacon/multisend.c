5b3f8165fb01d3a46a0deb1bf83b7f8092b1bc5b
Orion Lawlor <olawlor@acm.org>
2003-07-25 21:53:41 +0000

    More extensive and general CmiMultipleSend test:   - Just use CmiRegisterHandlerEx to differentiate handlers,     instead of copy-and-pasting the code.   - Vary the length of each message passed to CmiMultipleSend.

--- tests/converse/megacon/multisend.c
+++ tests/converse/megacon/multisend.c
@@ -3,17 +3,20 @@
 
 void Cpm_megacon_ack();
 
+typedef struct {
+  int me;
+} multisend_info;
+
 typedef struct
 {
   char core[CmiMsgHeaderSizeBytes]; 
-  int hdlrnum;
+  int me;
   double data[10];
 } multisendmsg;
 
-CpvDeclare(int, multisend_first_index);
-CpvDeclare(int, multisend_second_index);
-CpvDeclare(int, multisend_third_index);
-CpvDeclare(int, multisend_recv_index);
+#define nMulti 8
+CpvDeclare(int*, multisend_index);
+CpvDeclare(int, multisend_done_index);
 
 CpvDeclare(int, multisend_replies);
 
@@ -23,111 +26,81 @@ void multisend_fail()
   exit(1);
 }
 
-void multisend_first_handler(multisendmsg *msg)
-{
+static void checkMsg(multisendmsg *msg) {
   int i;
-
-  if(msg->hdlrnum != 1)
-    multisend_fail();
-  for(i=0;i<10;i++) {
-    if(msg->data[i] != (double) (i+1))
+  for(i=0;i<10-msg->me;i++) {
+    if(msg->data[i] != (double) (i+msg->me))
       multisend_fail();
   }
-  CmiSetHandler(msg, CpvAccess(multisend_recv_index));
+}
+
+void multisend_handler(multisendmsg *msg,multisend_info *info)
+{
+  if(msg->me != info->me)
+    multisend_fail();
+  checkMsg(msg);
+  /* Forward message back to master: */
+  CmiSetHandler(msg, CpvAccess(multisend_done_index));
   CmiSyncSendAndFree(0, sizeof(multisendmsg), msg);
 }
 
-void multisend_second_handler(multisendmsg *msg)
+void multisend_done_handler(multisendmsg *msg)
 {
-  int i;
-
-  if(msg->hdlrnum != 2)
-    multisend_fail();
-  for(i=0;i<10;i++) {
-    if(msg->data[i] != (double) (i+2))
-      multisend_fail();
-  }
-  CmiSetHandler(msg, CpvAccess(multisend_recv_index));
-  CmiSyncSendAndFree(0, sizeof(multisendmsg), msg);
-}
-
-void multisend_third_handler(multisendmsg *msg)
-{
-  int i;
-
-  if(msg->hdlrnum != 3)
-    multisend_fail();
-  for(i=0;i<10;i++) {
-    if(msg->data[i] != (double) (i+3))
-      multisend_fail();
-  }
-  CmiSetHandler(msg, CpvAccess(multisend_recv_index));
-  CmiSyncSendAndFree(0, sizeof(multisendmsg), msg);
-}
-
-void multisend_recv_handler(multisendmsg *msg)
-{
-  int i;
+  checkMsg(msg);
+  CmiFree(msg);
 
   CpvAccess(multisend_replies)++;
-  for(i=0;i<10;i++) {
-    if(msg->data[i] != (double) (i+msg->hdlrnum))
-      multisend_fail();
-  }
-  if(CpvAccess(multisend_replies)==3)
+  if(CpvAccess(multisend_replies)==nMulti)
     Cpm_megacon_ack(CpmSend(0));
 }
 
 void multisend_init(void)
 {
-  multisendmsg first, third;
-  multisendmsg *second;
-  int sizes[3];
-  char *msgs[3];
-  int i;
-
+/*
   if(CmiNumPes() < 2) {
     CmiPrintf("Multisend requires at least 2 processors. skipping...\n");
     Cpm_megacon_ack(CpmSend(0));
-  } else {
-    second = (multisendmsg *) CmiAlloc(sizeof(multisendmsg));
-    first.hdlrnum = 1;
-    second->hdlrnum = 2;
-    third.hdlrnum = 3;
-    for(i=0;i<10;i++) {
-      first.data[i] = (double) (i+1);
-      second->data[i] = (double) (i+2);
-      third.data[i] = (double) (i+3);
+  } else 
+*/
+  {
+    int m,i;
+    int sizes[nMulti];
+    char *msgs[nMulti];
+    multisendmsg first; /* Allocate one message on the stack (because you can!) */
+    for (m=0;m<nMulti;m++) {
+      multisendmsg *msg;
+      if (m==0) msg=&first;
+      else msg=(multisendmsg *)CmiAlloc(sizeof(multisendmsg));
+      CmiSetHandler(msg, CpvAccess(multisend_index)[m]);
+      msg->me=m;
+      for (i=0;i<10-m;i++) msg->data[i]=(double)(i+m);
+      sizes[m]=sizeof(multisendmsg)-sizeof(double)*m;
+      msgs[m]=(char *)msg;
     }
-    CmiSetHandler(&first, CpvAccess(multisend_first_index));
-    CmiSetHandler(second, CpvAccess(multisend_second_index));
-    CmiSetHandler(&third, CpvAccess(multisend_third_index));
-    sizes[0] = sizes[1] = sizes[2] = sizeof(multisendmsg);
-    msgs[0] = (char *) &first; 
-    msgs[1] = (char *) second; 
-    msgs[2] = (char *) &third;
-    CmiMultipleSend(1, 3, sizes, msgs);
-    CmiFree(second);
+    CmiMultipleSend(1%CmiNumPes(), nMulti, sizes, msgs);  
+    for (m=0;m<nMulti;m++) 
+      if (m!=0)
+        CmiFree(msgs[m]);
   }
   CpvAccess(multisend_replies) = 0;
 }
 
 void multisend_moduleinit()
 {
-  CpvInitialize(int, multisend_first_index);
-  CpvInitialize(int, multisend_second_index);
-  CpvInitialize(int, multisend_third_index);
-  CpvInitialize(int, multisend_recv_index);
+  int m;
+  CpvInitialize(int*, multisend_index);
+  CpvInitialize(int, multisend_done_index);
   CpvInitialize(int, multisend_replies);
-  CpvAccess(multisend_first_index) = 
-    CmiRegisterHandler((CmiHandler)multisend_first_handler);
-  CpvAccess(multisend_second_index) = 
-    CmiRegisterHandler((CmiHandler)multisend_second_handler);
-  CpvAccess(multisend_third_index) = 
-    CmiRegisterHandler((CmiHandler)multisend_third_handler);
-  CpvAccess(multisend_recv_index) = 
-    CmiRegisterHandler((CmiHandler)multisend_recv_handler);
+  
+  CpvAccess(multisend_index)=(int *)CmiAlloc(nMulti*sizeof(int));
+  for (int m=0;m<nMulti;m++) {
+    multisend_info *i=(multisend_info *)CmiAlloc(sizeof(multisend_info));
+    i->me=m;
+    CpvAccess(multisend_index)[m]=CmiRegisterHandlerEx(
+    	(CmiHandlerEx)multisend_handler,i);
+  }
+
+  CpvAccess(multisend_done_index) = 
+    CmiRegisterHandler((CmiHandler)multisend_done_handler);
 }
-
-
 
