df0dfb2ae7d3cdda10a3fb6c8fd71f21c670b00b
Gengbin Zheng <gzheng@illinois.edu>
2003-08-07 06:13:35 +0000

    switch to gnu old as default malloc for now.

--- src/arch/net-linux/conv-mach-opteron.h
+++ src/arch/net-linux/conv-mach-opteron.h
@@ -8,3 +8,9 @@
 #undef CMK_TIMER_USE_GETRUSAGE
 #define CMK_TIMER_USE_RDTSC				0
 #define CMK_TIMER_USE_GETRUSAGE				1
+
+#undef CMK_MALLOC_USE_GNU_MALLOC
+#undef CMK_MALLOC_USE_OS_BUILTIN
+#define CMK_MALLOC_USE_GNU_MALLOC                          0
+#define CMK_MALLOC_USE_OS_BUILTIN                          0
+#define CMK_MALLOC_USE_GNUOLD_MALLOC		           1
