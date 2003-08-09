ed0bbca58df6b04cd60d6b0567d85cabc316fe6f
Gengbin Zheng <gzheng@illinois.edu>
2003-08-09 01:21:19 +0000

    define HAVE_USR_INCLUDE_MALLOC_H to 1 magically make new gnu malloc works on Opteron.

--- src/arch/net-linux/conv-mach-opteron.sh
+++ src/arch/net-linux/conv-mach-opteron.sh
@@ -1,7 +1,8 @@
-CMK_CC='gcc -m64 -fPIC '
-CMK_CXX='g++ -Wno-deprecated -m64 -fPIC '
-CMK_SEQ_CC='gcc -m64 -fPIC '
-CMK_SEQ_CXX='g++ -m64 -fPIC '
+CMK_DEFS="$CMK_DEFS -DHAVE_USR_INCLUDE_MALLOC_H=1 "
+CMK_CC="gcc -m64 -fPIC $CMK_DEFS"
+CMK_CXX="g++ -Wno-deprecated -m64 -fPIC $CMK_DEFS "
+CMK_SEQ_CC="gcc -m64 -fPIC $CMK_DEFS "
+CMK_SEQ_CXX="g++ -m64 -fPIC $CMK_DEFS "
 CMK_LD="gcc -m64 "
 CMK_LDXX="g++ -m64 "
 CMK_QT='generic64'
