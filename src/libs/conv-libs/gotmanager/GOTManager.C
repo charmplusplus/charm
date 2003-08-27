37fc2a12d1909160acb4e42dcc7010c5c13dc00f
Sameer Kumar <skumar2@uiuc.edu>
2003-08-27 17:24:15 +0000

    global offset table manager for AMPI

---
+++ src/libs/conv-libs/gotmanager/GOTManager.C
@@ -0,0 +1,169 @@
+#include "GOTManager.h"
+
+GOTManager::GOTManager() {
+    createNewGOT();
+}
+
+void GOTManager::createNewGOT(){
+    int count;
+    int off, pos;
+    int relt_size = 0, pltrelt_size = 0;
+    int global_relt_size = 0, global_pltrelt_size = 0;
+    
+    int type, symindx, seg_size = 0, total_size;
+    char *sym_name;
+    int dynamic_pos;
+    int got_pos;
+    int gmon_pos;
+
+    ELF_TYPE_Rel *relt;       //Relocation table
+    ELF_TYPE_Rel *pltrelt;    //Procedure relocation table
+    ELF_TYPE_Sym *symt;       //symbol table
+    char *str_tab;         //String table
+
+    //strcpy(hello, "hello world %d, %d, %d, %u\n");
+
+    /*Point tables and sizes of tables to the correct values
+      from the dynamic segment table*/
+    count = 0;
+    while(_DYNAMIC[count].d_tag != 0){
+
+        if(_DYNAMIC[count].d_tag == DT_REL)
+            relt = (ELF_TYPE_Rel *) _DYNAMIC[count].d_un.d_ptr;
+
+        else if(_DYNAMIC[count].d_tag == DT_RELSZ)
+            relt_size = _DYNAMIC[count].d_un.d_val/ sizeof(ELF_TYPE_Rel);
+
+        else if(_DYNAMIC[count].d_tag == DT_SYMTAB)
+            symt = (ELF_TYPE_Sym *) _DYNAMIC[count].d_un.d_ptr;
+
+        else if(_DYNAMIC[count].d_tag == DT_JMPREL)
+            pltrelt = (ELF_TYPE_Rel *) _DYNAMIC[count].d_un.d_ptr;
+
+        else if(_DYNAMIC[count].d_tag == DT_PLTRELSZ)
+            pltrelt_size = _DYNAMIC[count].d_un.d_val/ sizeof(ELF_TYPE_Rel);
+
+        else if(_DYNAMIC[count].d_tag == DT_STRTAB)
+            str_tab = (char *)_DYNAMIC[count].d_un.d_ptr;
+
+        count ++;
+    }
+
+#if DEBUG_GOT_MANAGER   
+    for(count = 0; count < 600; count ++){
+    }    
+#endif
+
+    /*Compute the size of the got and allocate memory for the new got*/
+    total_size = 3 + relt_size + pltrelt_size;
+    ngot = (ELF_TYPE_Addr *)malloc(total_size * sizeof(ELF_TYPE_Addr));
+
+    /*Compute the Number of global relocation data entries*/
+    for(count = 0; count < relt_size; count ++){
+        type = ELF32_R_TYPE(relt[count].r_info);
+        symindx = ELF32_R_SYM(relt[count].r_info);
+        
+        if(type == R_386_GLOB_DAT){
+            sym_name = str_tab + symt[symindx].st_name;
+            if(strcmp(sym_name, "_DYNAMIC") == 0)
+                dynamic_pos = count;
+            else if(strcmp(sym_name, "__gmon_start__") == 0)
+                gmon_pos = count;
+            else if(strcmp(sym_name, "_GLOBAL_OFFSET_TABLE_") == 0)
+                got_pos = count;
+            else {
+                //Look for data and not function symbols
+                if(ELF32_ST_TYPE(symt[symindx].st_info) == STT_OBJECT) {
+#if DEBUG_GOT_MANAGER   
+                    sym_name = str_tab + symt[symindx].st_name;
+                    printf("%d %s %d %d\n", symindx, sym_name, 
+                           symt[symindx].st_size, symt[symindx].st_value);
+#endif
+                    seg_size += ALIGN8(symt[symindx].st_size);
+                }
+                    
+                global_relt_size ++;
+            }
+        }
+    }
+    
+    /*Compute the Number of global relocation procedure entries*/
+    for(count = 0; count < pltrelt_size; count ++){
+        type = ELF32_R_TYPE(pltrelt[count].r_info);
+        symindx = ELF32_R_SYM(pltrelt[count].r_info);
+
+        //printf("%d %d\n", symindx, type);
+
+        if(type == R_386_JMP_SLOT){
+#if DEBUG_GOT_MANAGER   
+            sym_name = str_tab + symt[symindx].st_name;
+            printf("%d %s %d %d\n", symindx, sym_name, 
+                   symt[symindx].st_size, symt[symindx].st_value);
+#endif
+            global_pltrelt_size ++;
+        }
+    }
+
+#if DEBUG_GOT_MANAGER   
+    printf("\n\n");
+    printf("relt_size = %d, pltrelt_size = %d\n", global_relt_size,
+           global_pltrelt_size);
+    printf("Creating data segment of size %d\n", seg_size);
+#endif
+
+    //Allocating memory for the new data segment
+    new_data_seg = (char *)malloc(seg_size);
+    pos = 3 + pltrelt_size;
+    off = 0;
+
+    //Copying the global offset table to the new global offset table
+    for(count = 0; count < total_size; count ++)
+        ngot[count] =  _GLOBAL_OFFSET_TABLE_[count];
+
+    //Copy Global offset table and global data
+    for(count = 0; count < global_relt_size; count ++){
+        type = ELF32_R_TYPE(relt[count].r_info);
+        symindx = ELF32_R_SYM(relt[count].r_info);
+
+        if(type == R_386_GLOB_DAT){
+            if(count == dynamic_pos) //Dont change pointer to _DYNAMIC
+                pos ++;
+            else if(count == got_pos){
+                ngot[pos ++] = (ELF_TYPE_Addr)ngot;
+            }
+            //A strange wierdness,
+            //this variable is not present in the GOT
+            else if(count == gmon_pos); 
+
+            else {
+                if(ELF32_ST_TYPE(symt[symindx].st_info) == STT_OBJECT) {
+                    ngot[pos ++] = (ELF_TYPE_Addr)
+                        ((char *)new_data_seg + off);
+                    off += ALIGN8(symt[symindx].st_size);
+                    
+                    memcpy((void *)ngot[pos-1], 
+                           (void *)_GLOBAL_OFFSET_TABLE_[pos-1],
+                           symt[symindx].st_size);
+                }
+                else pos ++; //Leave all function pointers alone
+            }
+        }
+    }
+
+#if DEBUG_GOT_MANAGER
+    for(count = 0; count < 3 + global_relt_size + global_pltrelt_size; 
+        count ++)
+        printf("[%d] GOT = %u  NGOT = %u\n", count, 
+               _GLOBAL_OFFSET_TABLE_[count], ngot[count]);
+#endif
+
+}
+
+void * GOTManager::swapGOT(){
+    ELF_TYPE_Addr *oldgot;
+    /*
+    asm("movl %ebx, oldgot");
+    asm("movl ngot, %ebx;");
+    */
+    return (void *)oldgot;
+}
