a493e3a1a08c07cbbb32611eb1d40b9321e7b93e
Orion Lawlor <olawlor@acm.org>
2003-08-29 15:33:15 +0000

    Forgot "inline" keyword in template function.  Without "inline", certain stupid compilers like Compaq cxx can't figure out to instantiate the template, and FEM dies with a link error.

--- src/libs/ck-libs/fem/fem.C
+++ src/libs/ck-libs/fem/fem.C
@@ -140,7 +140,7 @@ FORTRAN_AS_C_RETURN(int,
 
 /// Return a new'd copy of this class, by calling pup.
 template <class T>
-T *clonePointerViaPup(T *old) {
+inline T *clonePointerViaPup(T *old) {
 	int len=PUP::size(*old);
 	char *buf=new char[len];
 	PUP::toMemBuf(*old,buf,len);
