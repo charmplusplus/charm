edc17fa86300d66bfd98a5b3e7004155fda5a2b3
Gengbin Zheng <gzheng@illinois.edu>
2003-09-29 19:08:04 +0000

    untested elan-linux for testing.

---
+++ src/arch/elan-linux-ia64/conv-mach.sh
@@ -0,0 +1,15 @@
+CMK_CPP_CHARM='/lib/cpp -P'
+CMK_CPP_C='gcc -E'
+CMK_CC='gcc '
+CMK_CXX='g++ '
+CMK_CXXPP="$CMK_CXX -x c++ -E  "
+CMK_CF77='f77'
+CMK_CF90='f90'
+CMK_RANLIB='ranlib'
+CMK_LIBS='-lckqt -lelan'
+CMK_LD_SHARED="-shared"
+CMK_LD_LIBRARY_PATH="-Wl,-rpath,$CHARMLIBSO/"
+CMK_QT='generic'
+CMK_XIOPTS='-ansi'
+CMK_F90LIBS='-lUfor -lfor -lFutil'
+CMK_MOD_EXT="mod"
