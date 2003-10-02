fe193174a01d7c35e621bd88b85c1def63d8f42b
Gengbin Zheng <gzheng@illinois.edu>
2003-10-02 14:56:40 +0000

    minor changes in comments of porting context threads.

--- src/conv-core/threads.c
+++ src/conv-core/threads.c
@@ -969,13 +969,14 @@ Use SysV r3 setcontext/getcontext calls instead of
 quickthreads.  This works on lots of architectures (such as
 SUN, IBM SP, O2K, DEC Alpha, IA64, Cray X1, Linux with newer version of 
  glibc such as the one with RH9) 
-where quickthreads' setjmp/alloca/longjmp doesnot work.
-One some machine such as IA64 and Cray X1, the context version is the 
+On some machine such as IA64 and Cray X1, the context version is the 
 only thread package that is working. 
+
+Porting should be easy. To port context threads, one need to set the 
+direction of the thread stack properly in conv-mach.h.
+
 Note: on some machine like Sun and IBM SP, one needs to link with memory gnuold
  to have this context thread working.
-To port context threads, one need to set the direction of the thread stack 
-properly in conv-mach.h.
 
 Written by Gengbin Zheng around April 2001
 */
