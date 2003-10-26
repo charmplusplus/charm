1a69222178f561b75759b930ea9c42162aab6c75
Gengbin Zheng <gzheng@illinois.edu>
2003-10-26 17:52:23 +0000

    added more IA64 specific changes.

--- src/arch/elan-linux-ia64/conv-mach.h
+++ src/arch/elan-linux-ia64/conv-mach.h
@@ -15,9 +15,10 @@
 #define CMK_GETPAGESIZE_AVAILABLE                          1
 
 #define CMK_MALLOC_USE_GNU_MALLOC                          0
-#define CMK_MALLOC_USE_OS_BUILTIN                          1
+#define CMK_MALLOC_USE_OS_BUILTIN                          0
+#define CMK_MALLOC_USE_GNUOLD_MALLOC                       1
 
-#define CMK_MEMORY_PAGESIZE                                8192
+#define CMK_MEMORY_PAGESIZE                                16384
 #define CMK_MEMORY_PROTECTABLE                             1
 
 #define CMK_NODE_QUEUE_AVAILABLE                           0
@@ -28,13 +29,13 @@
 #define CMK_SHARED_VARS_UNIPROCESSOR                       0
 
 #define CMK_SIGNAL_NOT_NEEDED                              0
-#define CMK_SIGNAL_USE_SIGACTION                           1
-#define CMK_SIGNAL_USE_SIGACTION_WITH_RESTART              0
+#define CMK_SIGNAL_USE_SIGACTION                           0
+#define CMK_SIGNAL_USE_SIGACTION_WITH_RESTART              1
 
+#define CMK_THREADS_USE_CONTEXT                            1
+#define CMK_STACK_GROWUP                                   1
 #define CMK_THREADS_USE_PTHREADS                           0
 #define CMK_THREADS_REQUIRE_NO_CPV                         0
-#define CMK_THREADS_USE_CONTEXT                            0
-#define CMK_STACK_GROWUP                                   1
 
 #define CMK_TIMER_USE_RDTSC                                1
 #define CMK_TIMER_USE_GETRUSAGE                            0
@@ -50,19 +51,13 @@
 #define CMK_TYPEDEF_FLOAT4 float
 #define CMK_TYPEDEF_FLOAT8 double
 
-#define CMK_WAIT_NOT_NEEDED                                0
-#define CMK_WAIT_USES_SYS_WAIT_H                           1
-#define CMK_WAIT_USES_WAITFLAGS_H                          0
+#define CMK_NETPOLL                                        1
 
-#define CMK_WHEN_PROCESSOR_IDLE_BUSYWAIT                   0
-#define CMK_WHEN_PROCESSOR_IDLE_USLEEP                     1
+#define CMK_WHEN_PROCESSOR_IDLE_BUSYWAIT                   1
+#define CMK_WHEN_PROCESSOR_IDLE_USLEEP                     0
 
-
-#define CMK_COMPILEMODE_ORIG                               0
-#define CMK_COMPILEMODE_ANSI                               1
-
-#define CMK_WEB_MODE                                       1
 #define CMK_DEBUG_MODE                                     0
+#define CMK_WEB_MODE                                       1
 
 #define CMK_LBDB_ON					   1
 
