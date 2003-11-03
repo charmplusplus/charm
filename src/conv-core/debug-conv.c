ed2f4557d1f862a60511dc5c5c9662710c282851
Rashmi Jyothi <jyothi@uiuc.edu>
2003-11-03 17:44:08 +0000

    cpd relevant changes made; function to start gdb on net-linux

--- src/conv-core/debug-conv.c
+++ src/conv-core/debug-conv.c
@@ -10,6 +10,7 @@ Orion Sky Lawlor, olawlor@acm.org, 4/10/2001
 #include "conv-trace.h"
 #include "queueing.h"
 #include "conv-ccs.h"
+#include <errno.h>
 
 CpvStaticDeclare(int, freezeModeFlag);
 CpvStaticDeclare(int, continueFlag);
@@ -22,6 +23,9 @@ CpvStaticDeclare(void *, debugQueue);
 
 #include <string.h>
 
+
+extern void CpdContinueFromBreakPoint(void);
+
 static void CpdDebugHandler(char *msg)
 {
     char name[128];
@@ -29,11 +33,11 @@ static void CpdDebugHandler(char *msg)
 
     if (strcmp(name, "freeze") == 0) {
       CpdFreeze();
-      CmiPrintf("freeze received\n");
+      //CmiPrintf("freeze received\n");
     }
     else if (strcmp(name, "unfreeze") == 0) {
       CpdUnFreeze();
-      CmiPrintf("unfreeze received\n");
+      //CmiPrintf("unfreeze received\n");
     }
     else if (strncmp(name, "step", strlen("step")) == 0){
       CmiPrintf("step received\n");
@@ -54,13 +58,20 @@ static void CpdDebugHandler(char *msg)
     }
 #endif
     else if (strcmp(name, "quit") == 0){
-      CpdUnFreeze();
+      //CpdUnFreeze();
+      CpdContinueFromBreakPoint(); 
       CsdExitScheduler();
+    }
+    else if (strcmp(name, "startgdb") == 0){
+       //start gdb on processor and attach process to it
+       CpdContinueFromBreakPoint(); 
+       CpdStartGdb();
     }
     else{
       CmiPrintf("bad debugger command:%s received,len=%ld\n",name,strlen(name));
     }
 }
+
 
 /*
  Start the freeze-- call will not return until unfrozen
@@ -103,7 +114,11 @@ void CpdFreezeModeScheduler(void)
 	  int hIdx=CmiGetHandler(msg);
 	  if(_isCcsHandlerIdx(hIdx))
 	  /*A CCS request-- handle it immediately*/
+          {
+            //CmiPrintf("Before handling ccs call in freeze mode\n"); 
 	    CmiHandleMessage(msg);
+            //CmiPrintf("After handling ccs call in freeze mode\n"); 
+          }
 	  else
 	  /*An ordinary message-- queue it up*/
 	    CdsFifo_Enqueue(debugQ, msg);
@@ -118,6 +133,8 @@ void CpdFreezeModeScheduler(void)
     }
 #endif
 }
+
+CpvExtern(char *, displayArgument);
 
 void CpdInit(void)
 {
@@ -138,9 +155,64 @@ void CpdInit(void)
   msgListCleanup();
   msgListCache();
 #endif
+  
+/*  if (CmiGetArgStringDesc(argv, "+DebugDisplay",&displayArgument, "X display for gdb used only in cpd mode"))
+  {
+    if (!displayArgument)
+       CmiPrintf("WARNING> NULL parameter for +DebugDisplay\n***");
+  }
+  else
+  {
+    CmiPrintf("WARNING> x term for gdb needs to be specified as +DebugDisplay by debugger\n***");
+  }*/
 }
 
 
+/* The following code is for only the net-linux version */
+/* Has to be taken care for other versions */
+
+void CpdStartGdb(void)
+{
+  FILE *f;
+  char gdbScript[200];
+  int pid;
+  if (CpvAccess(displayArgument) != NULL)
+  {
+     CmiPrintf("MY NODE IS %d  and process id is %d\n", CmiMyPe(), getpid());
+     sprintf(gdbScript, "/tmp/cpdstartgdb.%d.%d", getpid(), CmiMyPe());
+     f = fopen(gdbScript, "w");
+     fprintf(f,"#!/bin/sh\n");
+     fprintf(f,"cat > /tmp/start_gdb.$$ << END_OF_SCRIPT\n");
+     fprintf(f,"shell /bin/rm -f /tmp/start_gdb.$$\n");
+     //fprintf(f,"handle SIGPIPE nostop noprint\n");
+     fprintf(f,"handle SIGWINCH nostop noprint\n");
+     fprintf(f,"handle SIGWAITING nostop noprint\n");
+     fprintf(f, "attach %d\n", getpid());
+     fprintf(f,"END_OF_SCRIPT\n");
+     fprintf(f, "DISPLAY='%s';export DISPLAY\n",CpvAccess(displayArgument));
+     fprintf(f,"/usr/X11R6/bin/xterm ");
+     fprintf(f," -title 'Node %d ' ",CmiMyPe());
+     fprintf(f," -e /usr/bin/gdb -x /tmp/start_gdb.$$ \n");
+     fprintf(f, "exit 0\n");
+     fclose(f);
+     if( -1 == chmod(gdbScript, 0755))
+     {
+        CmiPrintf("ERROR> chmod on script failed!\n");
+        return;
+     }
+     pid = fork();
+     if (pid < 0)
+        { perror("ERROR> forking to run debugger script\n"); exit(1); }
+     if (pid == 0)
+     {
+         //CmiPrintf("In child process to start script %s\n", gdbScript);
+         if (-1 == execvp(gdbScript, NULL))
+            CmiPrintf ("Error> Could not Execute Debugger Script: %s\n",strerror
+(errno));
+
+      }
+    }
+}
 
 
 
