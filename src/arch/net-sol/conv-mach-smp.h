cf54f6e335b3bb907c6055907d92e7e8f8301e31
Gengbin Zheng <gzheng@illinois.edu>
2003-12-16 08:27:51 +0000

    stick to gnu malloc in smp, due to some report about weird memory behaviour from NAMD users.

--- src/arch/net-sol/conv-mach-smp.h
+++ src/arch/net-sol/conv-mach-smp.h
@@ -7,10 +7,12 @@
 
 #define CMK_SMP						   1
 
+/*
 #undef CMK_MALLOC_USE_GNU_MALLOC
 #undef CMK_MALLOC_USE_OS_BUILTIN
 #define CMK_MALLOC_USE_GNU_MALLOC                          0
 #define CMK_MALLOC_USE_OS_BUILTIN                          1
+*/
 
 #undef CMK_MEMORY_PROTECTABLE
 #define CMK_MEMORY_PROTECTABLE                             0
