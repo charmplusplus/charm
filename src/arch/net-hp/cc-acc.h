0f7d11d32266c7399264d83e69820f9ce1761aa6
Gengbin Zheng <gzheng@illinois.edu>
2003-12-26 06:49:10 +0000

    removed <stdlin.h>

--- src/arch/net-hp/cc-acc.h
+++ src/arch/net-hp/cc-acc.h
@@ -6,7 +6,5 @@
  *****************************************************************************/
 
 
-#include <stdlib.h>
-
 #undef CMK_USE_HP_MAIN_FIX
 #define CMK_USE_HP_MAIN_FIX                                1
