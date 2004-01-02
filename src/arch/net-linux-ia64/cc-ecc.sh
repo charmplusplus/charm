33f1faa11dce5e62654f135d5480d5b9c054f8bc
Gengbin Zheng <gzheng@illinois.edu>
2004-01-02 18:58:28 +0000

    added -fpic to all ecc/ecpc due to some reported compile errors.

--- src/arch/net-linux-ia64/cc-ecc.sh
+++ src/arch/net-linux-ia64/cc-ecc.sh
@@ -1,14 +1,14 @@
-CMK_CPP_C='ecc -E '
-CMK_CC='ecc '
-CMK_CC_RELIABLE='ecc '
-CMK_CC_FASTEST='ecc '
-CMK_CXX='ecpc '
-CMK_CXXPP='ecpc -E '
-CMK_LD='ecc '
-CMK_LDXX='ecpc '
+CMK_CPP_C="ecc -E "
+CMK_CC="ecc -fpic "
+CMK_CC_RELIABLE="ecc "
+CMK_CC_FASTEST="ecc "
+CMK_CXX="ecpc -fpic "
+CMK_CXXPP="ecpc -E "
+CMK_LD="ecc "
+CMK_LDXX="ecpc "
 CMK_LD_LIBRARY_PATH="-Wl,-rpath,$CHARMLIBSO/"
-CMK_SEQ_CC='ecc '
-CMK_SEQ_LD='ecc '
-CMK_SEQ_CXX='ecpc '
-CMK_SEQ_LDXX='ecpc '
+CMK_SEQ_CC="ecc -fpic "
+CMK_SEQ_LD="ecc "
+CMK_SEQ_CXX="ecpc -fpic "
+CMK_SEQ_LDXX="ecpc "
 
