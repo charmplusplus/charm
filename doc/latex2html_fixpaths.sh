7d1227b71dab7ac13156874d8c7110ca2dc402fc
Gengbin Zheng <gzheng@illinois.edu>
2004-01-07 22:33:26 +0000

    fixed paths failed due to different result of pwd - /expand8/home or /home/net. fixed.

--- doc/latex2html_fixpaths.sh
+++ doc/latex2html_fixpaths.sh
@@ -18,7 +18,10 @@ cp -r fig manual/
 for f in `echo manual/*.html`
 do
 	echo "Converting $f"
+	cwd=`pwd`
+	cwd=`echo $cwd | sed -e 's@/home/net@/expand8/home@'`
 	sed -e 's!'`pwd`'/!!g' $f > tmp || die "error running sed on $f"
 	mv $f $f.bak || die "error backing up $f"
-	mv tmp $f || die "error replacing $f"
+	sed -e 's!'$cwd'/!!g' tmp > $f || die "error running sed on $f"
+	#mv tmp $f || die "error replacing $f"
 done
