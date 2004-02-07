bd4863e411e4997b466275fdc83017a878780873
Gengbin Zheng <gzheng@illinois.edu>
2004-02-07 04:38:13 +0000

    configure files for  ChaMPIon/Pro (CMPI) for NCSA's Xeon cluster.

---
+++ src/arch/mpi-linux/conv-mach-cmpi.h
@@ -0,0 +1,6 @@
+#undef CMK_MALLOC_USE_GNU_MALLOC
+#undef CMK_MALLOC_USE_OS_BUILTIN
+#define CMK_MALLOC_USE_GNU_MALLOC                          0
+#define CMK_MALLOC_USE_OS_BUILTIN                          1
+
+#define CMK_DONT_USE_PMPI				1
