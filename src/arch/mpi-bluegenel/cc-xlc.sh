a137e772c40148808f773639f90afe1a96649508
Orion Lawlor <olawlor@acm.org>
2004-02-09 19:21:40 +0000

    Added cc-xlc, to use IBM xlc compilers.   (These are only installed on powerPC machines, like "tryptophan" for now).

---
+++ src/arch/mpi-bluegenel/cc-xlc.sh
@@ -0,0 +1,17 @@
+XLC_PRE=/opt/ibmcmp
+XLC_POST=bin/blrts_
+CMK_CC="$XLC_PRE/vac/6.0/${XLC_POST}xlc "
+CMK_CXX="$XLC_PRE/vacpp/6.0/${XLC_POST}xlC -qstaticinline "
+CMK_LD="$CMK_CC  "
+CMK_LDXX="$CMK_CXX  "
+CMK_CF77="$XLC_PRE/xlf/8.1/${XLC_POST}xlf "
+CMK_CF90="$XLC_PRE/xlf/8.1/${XLC_POST}xlf90  -qsuffix=f=f90" 
+CMK_CF90_FIXED="$XLC_PRE/xlf/8.1/${XLC_POST}xlf90 " 
+CMK_C_OPTIMIZE='-O3 -qstrict -Q  '
+CMK_CXX_OPTIMIZE='-O3 -qstrict -Q '
+CMK_AR='ar cq'
+CMK_NM='nm '
+CMK_QT="aix"
+CMK_SEQ_CXX="$CMK_CXX"
+CMK_SEQ_CC="$CMK_CC"
+CMK_RANLIB="ranlib"
