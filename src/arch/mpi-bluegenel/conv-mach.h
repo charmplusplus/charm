bdec49f5026d7f1c4779db109c3b30e8d694db2b
Orion Lawlor <olawlor@acm.org>
2004-02-10 14:53:30 +0000

    Add CMK_MEMORY_PREALLOCATE_HACK call, to work around a problem with Cth threads on Blue Gene hardware.

--- src/arch/mpi-bluegenel/conv-mach.h
+++ src/arch/mpi-bluegenel/conv-mach.h
@@ -8,6 +8,8 @@
 #ifndef _CONV_MACH_H
 #define _CONV_MACH_H
 
+#define CMK_MEMORY_PREALLOCATE_HACK                        1
+
 #define CMK_CONVERSE_MPI                                   1
 
 #define CMK_NO_SOCKETS					   1
