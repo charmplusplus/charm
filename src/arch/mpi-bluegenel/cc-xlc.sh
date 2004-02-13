d35530a9d527eb19eb778cf1be8b279d07ec98b2
Orion Lawlor <olawlor@acm.org>
2004-02-13 00:09:29 +0000

    Added f90 libraries for linking f90 code.  You'll probably have to use the libxlfmath.a included in this directory, too...

--- src/arch/mpi-bluegenel/cc-xlc.sh
+++ src/arch/mpi-bluegenel/cc-xlc.sh
@@ -1,11 +1,12 @@
 XLC_PRE=/opt/ibmcmp
 XLC_POST=bin/blrts_
+XLC_F=$XLC_PRE/xlf/8.1
 CMK_CC="$XLC_PRE/vac/6.0/${XLC_POST}xlc -qcpluscmt "
 CMK_CXX="$XLC_PRE/vacpp/6.0/${XLC_POST}xlC "
 CMK_LD="$CMK_CC  "
 CMK_LDXX="$CMK_CXX  "
-CMK_CF77="$XLC_PRE/xlf/8.1/${XLC_POST}xlf "
-CMK_CF90="$XLC_PRE/xlf/8.1/${XLC_POST}xlf90  -qsuffix=f=f90" 
+CMK_CF77="$XLC_F/${XLC_POST}xlf "
+CMK_CF90="$XLC_F/${XLC_POST}xlf90  -qsuffix=f=f90" 
 CMK_CF90_FIXED="$XLC_PRE/xlf/8.1/${XLC_POST}xlf90 " 
 CMK_C_OPTIMIZE='-O3 -qstrict -Q  '
 CMK_CXX_OPTIMIZE='-O3 -qstrict -Q '
@@ -15,3 +16,4 @@ CMK_QT="aix"
 CMK_SEQ_CXX="$CMK_CXX"
 CMK_SEQ_CC="$CMK_CC"
 CMK_RANLIB="ranlib"
+CMK_F90LIBS="-L$XLC_F/blrts_lib -lxlf90 -lxlopt -lxl -lxlfmath"
