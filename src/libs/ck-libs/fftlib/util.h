320cf69c9040513fd488a15f691a995fc8cfda29
Yan Shi <yanshi@uiuc.edu>
2004-02-19 23:47:38 +0000

    Added in a pup for complex class.

--- src/libs/ck-libs/fftlib/util.h
+++ src/libs/ck-libs/fftlib/util.h
@@ -23,7 +23,7 @@ public:
   void operator*=(complex a);
   complex operator*(complex a) {
     return complex( re * a.re - im * a.im, re * a.im + im * a.re); }
-
+  void pup(PUP::er &p) {p|re; p|im; }
 };
 
 #endif //__PFFTUTIL_H__
