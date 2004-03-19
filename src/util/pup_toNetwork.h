41df94fe8103b4c2ff9189d83c0df291f4d4088a
Orion Lawlor <olawlor@acm.org>
2004-03-19 16:51:57 +0000

    Moved pup_toNetwork4 implementation to a separate file, and added a new "pup_toNetwork" class that includes the ability to pack 8-byte ints and doubles.

---
+++ src/util/pup_toNetwork.h
@@ -0,0 +1,96 @@
+/*
+Pack/unpack to network-byte-order integer/float/double.
+On-the-wire sizes correspond to Java sizes for 
+int, long, float, and double.
+
+Orion Sky Lawlor, olawlor@acm.org, 11/1/2001
+ */
+#ifndef __OSL_PUP_TONETWORK_H
+#define __OSL_PUP_TONETWORK_H
+
+#include "pup.h"
+
+
+typedef CMK_TYPEDEF_INT4 CMK_NETWORK_INT4;
+#ifdef CMK_PUP_LONG_LONG
+typedef CMK_PUP_LONG_LONG CMK_NETWORK_INT8;
+#else
+typedef long CMK_NETWORK_INT8; /* long is 8 bytes */
+#endif
+
+/// Integer type the same size as "float"
+typedef CMK_NETWORK_INT4 CMK_FLOAT_SIZED_INT;
+/// Integer type the same size as "double"
+typedef CMK_NETWORK_INT8 CMK_DOUBLE_SIZED_INT;
+
+
+class PUP_toNetwork_sizer : public PUP::er {
+	int nBytes;
+	virtual void bytes(void *p,int n,size_t itemSize,PUP::dataType t);
+ public:
+	PUP_toNetwork_sizer(void) :PUP::er(IS_SIZING) {nBytes=0;}
+	int size(void) const {return nBytes;}
+};
+
+class PUP_toNetwork_pack : public PUP::er {
+	unsigned char *buf,*start;
+	inline void w(CMK_NETWORK_INT4 i) {
+		//Write big-endian integer to output stream
+		*buf++=(unsigned char)(i>>24); //High end first
+		*buf++=(unsigned char)(i>>16);
+		*buf++=(unsigned char)(i>>8);
+		*buf++=(unsigned char)(i>>0);  //Low end last
+	}
+	inline void w(CMK_NETWORK_INT8 i) {
+		w(CMK_NETWORK_INT4(i>>32));
+		w(CMK_NETWORK_INT4(i));
+	}
+	//Write floating-point number to stream.
+	inline void w(float f) {
+		w(*(CMK_FLOAT_SIZED_INT *)&f);
+	}
+	//Write floating-point number to stream.
+	inline void w(double f)  {
+		w(*(CMK_DOUBLE_SIZED_INT *)&f);
+	}
+
+	virtual void bytes(void *p,int n,size_t itemSize,PUP::dataType t);
+ public:
+	PUP_toNetwork_pack(void *dest) :PUP::er(IS_PACKING) {
+		start=buf=(unsigned char *)dest;
+	}
+	inline int size(void) const {return buf-start;}
+};
+
+class PUP_toNetwork_unpack : public PUP::er {
+	const unsigned char *buf,*start;
+	inline CMK_NETWORK_INT4 read_int(void) {
+		//Read big-endian integer to output stream
+		CMK_NETWORK_INT4 ret=(buf[0]<<24)|(buf[1]<<16)|(buf[2]<<8)|(buf[3]);
+		buf+=4;
+		return ret;
+	}
+	inline CMK_NETWORK_INT8 read_CMK_NETWORK_INT8(void) {
+		CMK_NETWORK_INT8 hi=0xffFFffFFu&(CMK_NETWORK_INT8)read_int();
+		CMK_NETWORK_INT8 lo=0xffFFffFFu&(CMK_NETWORK_INT8)read_int();
+		return (hi<<32)|(lo);
+	}
+	inline float read_float(void) {
+		CMK_NETWORK_INT4 i=read_int();
+		return *(float *)&i;
+	}
+	inline double read_double(void) {
+		CMK_NETWORK_INT8 i=read_CMK_NETWORK_INT8();
+		return *(double *)&i;
+	}
+
+	virtual void bytes(void *p,int n,size_t itemSize,PUP::dataType t);
+ public:
+	PUP_toNetwork_unpack(const void *src) :PUP::er(IS_UNPACKING) {
+		start=buf=(const unsigned char *)src;
+	}
+	inline int size(void) const {return buf-start;}
+};
+
+#endif
+
--- src/util/pup_util.C
+++ src/util/pup_util.C
@@ -18,7 +18,6 @@ virtual functions are defined here.
 #include <ctype.h>
 #include "charm.h"
 #include "pup.h"
-#include "pup_toNetwork4.h"
 #include "ckhashtable.h"
 
 PUP::er::~er() {}
@@ -141,90 +140,6 @@ void PUP::toDisk::bytes(void *p,int n,size_t itemSize,dataType /*t*/)
 	{/*CkPrintf("writing\n");*/fwrite(p,itemSize,n,F);}
 void PUP::fromDisk::bytes(void *p,int n,size_t itemSize,dataType /*t*/)
 	{fread(p,itemSize,n,F);}
-
-
-/****************** toNetwork4 ********************
-Pack/unpack to 4-byte network-byte-order integer/float.
-This makes it easy to use platform-neutral on-the-wire types.
-*/
-
-void PUP_toNetwork4_sizer::bytes(void *p,int n,size_t itemSize,PUP::dataType t)
-{
-	switch (t) {
-	case PUP::Tchar: //Strings and bytes get copied as-is
-	case PUP::Tuchar:
-	case PUP::Tbyte:
-		nBytes+=n;
-		break;
-	default: //Everything else goes as a network int
-		nBytes+=n*4;
-	}
-}
-
-void PUP_toNetwork4_pack::bytes(void *p,int n,size_t itemSize,PUP::dataType t)
-{
-	int i;
-	switch (t) {
-	case PUP::Tchar: //Strings and bytes get copied as-is
-	case PUP::Tuchar:
-	case PUP::Tbyte:
-		memcpy(buf,p,n);
-		buf+=n;
-		break;
-#define casePUP_toNetwork4_type(enumName,typeName,writeAs) \
-	case PUP::enumName: \
-	        for (i=0;i<n;i++) \
-		  w((writeAs)( ((typeName *)p)[i] ));\
-	        break
-			
-	casePUP_toNetwork4_type(Tfloat,float,float);
-	casePUP_toNetwork4_type(Tdouble,double,float);
-	case PUP::Tushort: //Fallthrough (no special treatment)
-	casePUP_toNetwork4_type(Tshort,short,int);
-	case PUP::Tuint: 
-	casePUP_toNetwork4_type(Tint,int,int);
-       	case PUP::Tulong: 
-       	casePUP_toNetwork4_type(Tlong,long,int);
-       	casePUP_toNetwork4_type(Tbool,CmiBool,int);
-       	case PUP::Tsync:
-       		break; //Ignore
-       	default: 
-       		CkAbort("Unrecognized type passed to PUP_toNetwork4_pack!\n");
-       	}
-#undef casePUP_toNetwork4_type
-}
-
-void PUP_toNetwork4_unpack::bytes(void *p,int n,size_t itemSize,PUP::dataType t)
-{
-	int i;
-	switch (t) {
-	case PUP::Tchar: //Strings and bytes get copied as-is
-	case PUP::Tuchar:
-	case PUP::Tbyte:
-		memcpy(p,buf,n);
-		buf+=n;
-		break;
-#define casePUP_toNetwork4_type(enumName,typeName,readAs) \
-	case PUP::enumName: \
-	        for (i=0;i<n;i++) \
-		  ((typeName *)p)[i]=(typeName)read_##readAs();\
-	        break
-		
-	casePUP_toNetwork4_type(Tfloat,float,float);
-	casePUP_toNetwork4_type(Tdouble,double,float);
-	case PUP::Tushort: //Fallthrough (no special treatment)
-	casePUP_toNetwork4_type(Tshort,short,int);
-	case PUP::Tuint: 
-	casePUP_toNetwork4_type(Tint,int,int);
-       	case PUP::Tulong: 
-       	casePUP_toNetwork4_type(Tlong,long,int);
-       	casePUP_toNetwork4_type(Tbool,CmiBool,int);
-       	case PUP::Tsync:
-       		break; //Ignore
-       	default: 
-       		CkAbort("Unrecognized type passed to PUP_toNetwork4_unpack!\n");
-       	}
-}
 
 
 /****************** Seek support *******************
