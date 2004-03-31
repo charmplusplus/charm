e4285b3c8fd22da5f02011ada774711b012f7560
Gengbin Zheng <gzheng@illinois.edu>
2004-03-31 19:18:31 +0000

    updated for cmpi on tungsten.

--- src/arch/mpi-linux/conv-mach-cmpi.h
+++ src/arch/mpi-linux/conv-mach-cmpi.h
@@ -1,6 +1,10 @@
+/* for ChaMPIon/Pro 1.1.1 (c) 1997 - 2004 MPI Software Technology, Inc. */
+/* on Tungsten.ncsa */
+
 #undef CMK_MALLOC_USE_GNU_MALLOC
 #undef CMK_MALLOC_USE_OS_BUILTIN
 #define CMK_MALLOC_USE_GNU_MALLOC                          0
 #define CMK_MALLOC_USE_OS_BUILTIN                          1
 
-#define CMK_DONT_USE_PMPI				1
+#undef CMK_THREADS_USE_CONTEXT
+#define CMK_THREADS_USE_CONTEXT                            1
