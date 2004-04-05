ce0101b4fcabeb98c16ad718bf940d71efd1b69d
Gengbin Zheng <gzheng@illinois.edu>
2004-04-05 20:08:49 +0000

    try some guess about where the gm.h lives.

--- src/arch/net-linux/conv-mach-gm.sh
+++ src/arch/net-linux/conv-mach-gm.sh
@@ -1,6 +1,19 @@
 #default gm dir
-test -z "$CMK_INCDIR" && CMK_INCDIR="-I /usr/gm/include"
-test -z "$CMK_LIBDIR" && CMK_LIBDIR="-L /usr/gm/lib"
+#guess where the gm.h is installed
+if test -z $CMK_INCDIR 
+then
+  # gm ver 1.0
+  if test -f /usr/gm/include/gm.h
+  then
+    CMK_INCDIR="-I /usr/gm/include"
+    CMK_LIBDIR="-L /usr/gm/lib"
+  # gm ver 2.0
+  elif test -f /opt/gm/include/gm.h
+  then
+    CMK_INCDIR="-I /opt/gm/include"
+    CMK_LIBDIR="-L /opt/gm/lib"
+  fi
+fi
 
 CMK_CPP_C="$CMK_CPP_C -E  $CMK_INCDIR "
 CMK_CC="$CMK_CC $CMK_INCDIR "
