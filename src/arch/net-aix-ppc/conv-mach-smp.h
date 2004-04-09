a15e233860243f0f553218d2f9c034c73412af27
Gengbin Zheng <gzheng@illinois.edu>
2004-04-09 05:37:53 +0000

    use gnuold malloc for smp. The new gnumalloc crashes smp version in NAMD for ibm sp.

--- src/arch/net-aix-ppc/conv-mach-smp.h
+++ src/arch/net-aix-ppc/conv-mach-smp.h
@@ -8,4 +8,7 @@
 #define CMK_SHARED_VARS_UNAVAILABLE                        0
 #define CMK_SHARED_VARS_POSIX_THREADS_SMP                  1
 
+#undef CMK_MALLOC_USE_GNU_MALLOC
+#undef CMK_MALLOC_USE_OS_BUILTIN
+#define CMK_MALLOC_USE_GNUOLD_MALLOC                          1
 
