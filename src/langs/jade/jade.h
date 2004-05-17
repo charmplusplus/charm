bccaa1072bfad2ebef05ed78fa17bb7ab0daf8ae
Jayant DeSouza <jdesouza@uiuc.edu>
2004-05-17 23:01:11 +0000

    Added comments. Divided file into sections. Added macro for _JADE_MIN.

--- src/langs/jade/jade.h
+++ src/langs/jade/jade.h
@@ -1,8 +1,33 @@
 // emacs mode line -*- mode: c++; tab-width: 4 -*-
+
+/** Naming conventions:
+
+  JADE_ is the prefix for any Jade calls exposed to the Jade user as
+  part of the Jade API.
+
+  Jade packages, such as MSA, which are part of the Jade language,
+  should follow the above convention, but for now let it be.
+
+  _JADEIDENT_ is the prefix for any compiler generated symbols not
+  exposed to the user as part of the API.
+
+*/
+
 #ifndef JADE_H
 #define JADE_H
 
-#include "JArray.h"
+// This part of the .h file is for Jade users to read too.
+
+// The Jade user needs to know the MSA API.  But, strictly speaking,
+// there should be a java file with the MSA API.
 #include "msa/msa.h"
+
+// This part is the .h file used by the compiler generated code.  The
+// Jade user should never see it.  I will put it into a separate file
+// when I have the time.
+
+#include "JArray.h"
+
+#define _JADE_MIN(a,b) (((a)<(b))?(a):(b))
 
 #endif
