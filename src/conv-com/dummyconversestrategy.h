e367a952c69cfebed0306498be1fe8527caf5eee
Sameer Kumar <skumar2@uiuc.edu>
2004-05-22 19:00:57 +0000

    *** empty log message ***

---
+++ src/conv-com/dummyconversestrategy.h
@@ -0,0 +1,23 @@
+
+/* Class that calls Krishnan's routers from the new Comlib.
+   Developed to be called from Converse
+   Sameer Kumar 05/14/04
+*/
+   
+
+#ifndef ROUTER_STRATEGY
+#define ROUTER_STRATEGY
+#include "convcomlibmanager.h"
+
+class RouterStrategy : public Strategy {
+ public:
+    RouterStrategy();
+    RouterStrategy(CkMigrateMessage *m): Strategy(m){}
+
+    void insertMessage(MessageHolder *msg);
+    void doneInserting();
+
+    virtual void pup(PUP::er &p);
+    PUPable_decl(RouterStrategy);
+};
+#endif
