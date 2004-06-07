8ed61b2560779dd2a3de17b2159654e6c64f58b5
Gengbin Zheng <gzheng@illinois.edu>
2004-06-07 21:25:06 +0000

    modified into a converse program.

--- examples/bigsim/tools/loadlog/loadlog.C
+++ examples/bigsim/tools/loadlog/loadlog.C
@@ -2,21 +2,20 @@
 #include "blue.h"
 #include "blue_impl.h"
 #include "blue_logs.h"
-#include "loadlog.decl.h"
 
 extern BgTimeLineRec* currTline;
 extern int currTlineIdx;
 
-class Main: public Chare {
-public:
-  Main()
-  {
+int main()
+{
   int totalProcs, numX, numY, numZ, numCth, numWth, numPes;
 
+  // load bg trace summary file
   BgLoadTraceSummary("bgTrace", totalProcs, numX, numY, numZ, numCth, numWth, numPes);
                                                                                 
   int* allNodeOffsets = BgLoadOffsets(totalProcs,numPes);
 
+  // load each individual trace file for each bg proc
   for (int i=0; i<numPes; i++) 
   {
     BgTimeLineRec tline;
@@ -25,13 +24,10 @@ public:
     currTlineIdx = procNum;
     BgReadProc(procNum,numWth,numPes,totalProcs,allNodeOffsets,tline);
                                                                                 
-    // dump bg timeline log to disk
+    // dump bg timeline log to disk in asci format
     BgWriteThreadTimeLine("detail", 0, 0, 0, procNum, tline.timeline);
   }
 
   delete [] allNodeOffsets;
-  CkExit();
-  }
-};
+}
 
-#include "loadlog.def.h"
