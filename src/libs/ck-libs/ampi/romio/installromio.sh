4c8b2918e76def45fccca479df5432bc46385874
Chao Huang <chuang10@uiuc.edu>
2004-06-08 21:23:03 +0000

    Use absolute directory in installing romio. (hopefully this works on lemieux and solaris)

--- src/libs/ck-libs/ampi/romio/installromio.sh
+++ src/libs/ck-libs/ampi/romio/installromio.sh
@@ -1,7 +1,10 @@
 #!/bin/sh 
-CHARMDIR=$PWD/../../../../..
+CHARMDIR=`util/charmdir`
 ARCH=`util/tarch`
 ./configure -mpiincdir=$CHARMDIR/include -mpilib=$CHARMDIR/lib/libmoduleampi.a -mpibindir=$CHARMDIR/bin -cc=$CHARMDIR/bin/mpicc -fc=$CHARMDIR/bin/mpif77 -f90=$CHARMDIR/bin/mpif90 -cflags="-swapglobals $(AMPIROMIOOPTS)" -fflags="$(AMPIROMIOOPTS)"
-cp include/mpio.h include/mpiof.h $CHARMDIR/include
-make
-cp lib/$ARCH/libmpio.a $CHARMDIR/lib/libampiromio.a
+if [ $? == 0 ]; then 
+  cp include/mpio.h include/mpiof.h $CHARMDIR/include
+  make
+  cp lib/$ARCH/libmpio.a $CHARMDIR/lib/libampiromio.a
+fi
+echo "configured"
