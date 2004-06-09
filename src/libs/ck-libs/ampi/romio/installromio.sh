96fcce1df497368f9221c0457a2fbfd8a1e31d3f
Chao Huang <chuang10@uiuc.edu>
2004-06-09 02:39:12 +0000

    Updated a few script in romio

--- src/libs/ck-libs/ampi/romio/installromio.sh
+++ src/libs/ck-libs/ampi/romio/installromio.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
 CHARMDIR=`util/charmdir`
 ARCH=`util/tarch`
-./configure -mpiincdir=$CHARMDIR/include -mpilib=$CHARMDIR/lib/libmoduleampi.a -mpibindir=$CHARMDIR/bin -cc=$CHARMDIR/bin/mpicc -fc=$CHARMDIR/bin/mpif77 -f90=$CHARMDIR/bin/mpif90 -cflags="-swapglobals" -fflags=""
-if [ $? == 0 ]; then 
+./configure -mpiincdir=$CHARMDIR/include -mpilib=$CHARMDIR/lib/libmoduleampi.a -mpibindir=$CHARMDIR/bin -cc=$CHARMDIR/bin/mpicc -fc=$CHARMDIR/bin/mpif77 -f90=$CHARMDIR/bin/mpif90 -cflags="" -fflags=""
+if [ $? = 0 ]; then 
   cp include/mpio.h include/mpiof.h $CHARMDIR/include
   make
   cp lib/$ARCH/libmpio.a $CHARMDIR/lib/libampiromio.a
