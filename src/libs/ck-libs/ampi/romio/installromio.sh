99a3b5acda0fc820babc5e28d3ef86e84a373232
Chao Huang <chuang10@uiuc.edu>
2004-06-09 01:37:02 +0000

    Removed the use of AMPIROMIOOPTS

--- src/libs/ck-libs/ampi/romio/installromio.sh
+++ src/libs/ck-libs/ampi/romio/installromio.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 CHARMDIR=`util/charmdir`
 ARCH=`util/tarch`
-./configure -mpiincdir=$CHARMDIR/include -mpilib=$CHARMDIR/lib/libmoduleampi.a -mpibindir=$CHARMDIR/bin -cc=$CHARMDIR/bin/mpicc -fc=$CHARMDIR/bin/mpif77 -f90=$CHARMDIR/bin/mpif90 -cflags="-swapglobals $(AMPIROMIOOPTS)" -fflags="$(AMPIROMIOOPTS)"
+./configure -mpiincdir=$CHARMDIR/include -mpilib=$CHARMDIR/lib/libmoduleampi.a -mpibindir=$CHARMDIR/bin -cc=$CHARMDIR/bin/mpicc -fc=$CHARMDIR/bin/mpif77 -f90=$CHARMDIR/bin/mpif90 -cflags="-swapglobals" -fflags=""
 if [ $? == 0 ]; then 
   cp include/mpio.h include/mpiof.h $CHARMDIR/include
   make
