512586fefe224a9857c48d178fc2e512272462a6
Orion Lawlor <olawlor@acm.org>
2004-07-22 22:19:27 +0000

    Use FEM_COORD to store node coordinates, instead of FEM_DATA. This is the new standard place to store node coordinates.

--- src/libs/ck-libs/datatransfer/tetmesh_fem.C
+++ src/libs/ck-libs/datatransfer/tetmesh_fem.C
@@ -11,7 +11,7 @@ void readFEM(int m,TetMesh &t) {
 	int nNode=FEM_Mesh_get_length(m,FEM_NODE);
 	int nTet=FEM_Mesh_get_length(m,FEM_ELEM+0);
 	t.allocate(nTet,nNode);
-	FEM_Mesh_data(m,FEM_NODE,FEM_DATA+0,t.getPointArray(), 0,nNode, FEM_DOUBLE,3);
+	FEM_Mesh_data(m,FEM_NODE,FEM_COORD,t.getPointArray(), 0,nNode, FEM_DOUBLE,3);
 	FEM_Mesh_data(m,FEM_ELEM+0,FEM_CONN,t.getTetConn(), 0,nTet, FEM_INDEX_0,4);
 }
 
@@ -19,7 +19,7 @@ void readFEM(int m,TetMesh &t) {
 void writeFEM(int m,TetMesh &t) {
 	int nNode=t.getPoints();
 	int nTet=t.getTets();
-	FEM_Mesh_data(m,FEM_NODE,FEM_DATA+0,t.getPointArray(), 0,nNode, FEM_DOUBLE,3);
+	FEM_Mesh_data(m,FEM_NODE,FEM_COORD,t.getPointArray(), 0,nNode, FEM_DOUBLE,3);
 	FEM_Mesh_data(m,FEM_ELEM+0,FEM_CONN,t.getTetConn(), 0,nTet, FEM_INDEX_0,4);
 }
 
