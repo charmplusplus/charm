e6a2f5efff1b7ac9307d9ae9d131902d9fca82a1
Gengbin Zheng <gzheng@illinois.edu>
2004-08-16 05:03:25 +0000

    added ifort fortran library "-lifport" for system().

--- src/arch/common/conv-mach-ifort.sh
+++ src/arch/common/conv-mach-ifort.sh
@@ -1,7 +1,7 @@
 # for Intel Fortran compiler 8.0 and higher which is renamed to ifort from ifc
 CMK_CF90='ifort -auto '
 CMK_CF90_FIXED="$CMK_CF90 -132 -FI "
-CMK_F90LIBS='-lifcore '
+CMK_F90LIBS="-lifcore -lifport "
 CMK_MOD_NAME_ALLCAPS=
 CMK_MOD_EXT="mod"
 CMK_F90_USE_MODDIR=""
