6935067d6c7ca691b87b67166c032e388529b812
Gengbin Zheng <gzheng@illinois.edu>
2004-08-23 20:33:42 +0000

    refix for the rand() for win32 build.

--- src/libs/ck-libs/parmetis/METISLib/macros.h
+++ src/libs/ck-libs/parmetis/METISLib/macros.h
@@ -16,7 +16,11 @@
 /*************************************************************************
 * The following macro returns a random number in the specified range
 **************************************************************************/
+#if defined(__VC__) || defined(__CYGWIN__) || defined(WIN32) 
+#define RandomInRange(u) ((rand()>>3)%(u))
+#else
 #define RandomInRange(u) ((int)(1.0*(u)*rand()/(RAND_MAX+1.0)))
+#endif
 
 #define amax(a, b) ((a) >= (b) ? (a) : (b))
 #define amin(a, b) ((a) >= (b) ? (b) : (a))
