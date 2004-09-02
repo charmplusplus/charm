169e508b92d8d929b10c6c9e1c517d79227c7635
Gengbin Zheng <gzheng@illinois.edu>
2004-09-02 22:01:25 +0000

    when dumping bglog in detail mode, print out if it is a start event.

--- src/langs/bluegene/bigsim_logs.C
+++ src/langs/bluegene/bigsim_logs.C
@@ -207,7 +207,7 @@ void BgTimeLog::print(int node, int th)
 void BgTimeLog::write(FILE *fp)
 { 
   int i;
-  fprintf(fp,"<<==  %p ep:%d name:%s (srcnode:%d msgID:%d) startTime:%f endTime:%f recvime:%f effRecvTime:%e seqno:%d\n", this, ep, name, msgId.node(), msgId.msgID(), startTime, endTime, recvTime, effRecvTime, seqno);
+  fprintf(fp,"<<==  %p ep:%d name:%s (srcnode:%d msgID:%d) startTime:%f endTime:%f recvime:%f effRecvTime:%e seqno:%d startevent:%d\n", this, ep, name, msgId.node(), msgId.msgID(), startTime, endTime, recvTime, effRecvTime, seqno, isStartEvent());
   for (i=0; i<msgs.length(); i++)
     msgs[i]->write(fp);
   for (i=0; i<evts.length(); i++)
