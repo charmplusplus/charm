05ce4897a4bfa0ecd219226dbb7acf31cc7c7b35
Eric Bohm <ebohm@illinois.edu>
2004-09-08 18:58:55 +0000

    initial creation of test dir from pgms

---
+++ tests/charm++/commtest/pipeBroadcast/benchmark.h
@@ -0,0 +1,32 @@
+# include "benchmark.decl.h"
+
+class TheMain : public Chare {
+ public:
+  TheMain(CkArgMsg *msg);
+  void exit();
+ private:
+  int called;
+};
+
+class MyMess : public CkMcastBaseMsg, public CMessage_MyMess {
+ public:
+  char *data;
+};
+
+class BTest : public CBase_BTest {
+  double startTime;
+  double totalTime;
+  int length;
+  int type;
+  int count;
+  int iter;
+  CkCallback cb;
+  CkCallback cball;
+ public:
+  BTest();
+  BTest(CkMigrateMessage *msg);
+  void sendall();
+  void send();
+  void send(CkReductionMsg *msg);
+  void receive(MyMess *msg);
+};
---
+++ tests/charm++/commtest/pipeBroadcast/test.h
@@ -0,0 +1,22 @@
+# include "test.decl.h"
+
+class TheMain : public Chare {
+ public:
+  TheMain(CkArgMsg *msg);
+  void exit();
+ private:
+  int called;
+};
+
+class MyMess : public CkMcastBaseMsg, public CMessage_MyMess {
+ public:
+  double *data;
+};
+
+class Test : public CBase_Test {
+ public:
+  Test();
+  Test(CkMigrateMessage *msg);
+  void send();
+  void receive(MyMess *msg);
+};
