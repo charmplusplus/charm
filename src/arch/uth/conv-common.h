beb5cc05331d53dc390390988decb34e10339569
Filippo Gioachin <gioachin@illinois.edu>
2004-10-07 02:00:47 +0000

    bugfix, missed semicolon in last checkin by Sameer

--- src/arch/uth/conv-common.h
+++ src/arch/uth/conv-common.h
@@ -1,6 +1,6 @@
 
 #define CMK_MSG_HEADER_BASIC  CMK_MSG_HEADER_EXT
-#define CMK_MSG_HEADER_EXT    { CmiUInt2 hdl,xhdl,info,stratid,root,padding1,padding2,padding3 }
+#define CMK_MSG_HEADER_EXT    { CmiUInt2 hdl,xhdl,info,stratid,root,padding1,padding2,padding3; }
 #define CMK_MSG_HEADER_BLUEGENE    { CmiUInt2 hdl,xhdl,info,stratid,root,padding1,padding2,padding3; int nd, n; double rt; CmiInt2 tID; CmiUInt2 hID; char t; int msgID; int srcPe;}
 
 #define CMK_LBDB_ON					   0
