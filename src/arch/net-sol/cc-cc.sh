d4e5918974ce8645d336e3204d63c7ec4c43af91
Gengbin Zheng <gzheng@illinois.edu>
2004-10-23 19:50:37 +0000

    fixed f90 compiler flag and f90 libs

--- src/arch/net-sol/cc-cc.sh
+++ src/arch/net-sol/cc-cc.sh
@@ -5,26 +5,30 @@ CMK_CC_RELIABLE='cc '
 CMK_CC_FASTEST='cc '
 CMK_CXX='CC -KPIC -library=Cstd -instances=global '
 CMK_CXXPP='CC -E '
-CMK_CF77='f77 -stackvar '
-CMK_CF90='f90 -C -stackvar '
-CMK_CF90_FIXED="$CMK_CF90 -fixed "
 CMK_C_DEBUG='-g'
 CMK_C_OPTIMIZE='-fast'
 CMK_CXX_DEBUG='-g'
 CMK_CXX_OPTIMIZE='-fast'
 CMK_LD='cc '
 CMK_LDXX='CC -library=Cstd -instances=global '
-CMK_AR='CC -xar -o'
-CMK_LD_SHARED="-G"
 CMK_LIBS=" -lnsl -lsocket $CHARMLIB/libckqt.a"
-CMK_NATIVE_LIBS='-lnsl -lsocket'
+CMK_CPP_SUFFIX="cc"
+CMK_XLATPP='charmxlat++ -w -p '
+
+CMK_AR='CC -xar -o'
+CMK_QT='generic_alloca'
+
+# native compiler
 CMK_NATIVE_CC='cc'
 CMK_NATIVE_LD='cc'
 CMK_NATIVE_CXX='CC -library=Cstd'
 CMK_NATIVE_LDXX='CC -library=Cstd'
-CMK_CPP_SUFFIX="cc"
-CMK_XLATPP='charmxlat++ -w -p '
-CMK_QT='generic_alloca'
-CMK_F90LIBS='-lfsu -lsunmath -lfsumai -lfminlai -lfmaxlai -lfminvai -lfmaxvai -lfui -lfai'
-CMK_MOD_EXT="mod"
-CMK_F90_USE_MODDIR=1
+CMK_NATIVE_LIBS='-lnsl -lsocket'
+
+# Sun Forte Developer 7 f90 7.0
+CMK_CF77='f77 -stackvar '
+CMK_CF90='f90 -stackvar '
+CMK_CF90_FIXED="$CMK_CF90 -fixed "
+#CMK_F90LIBS='-lfsu -lsunmath -lfsumai -lfminlai -lfmaxlai -lfminvai -lfmaxvai -lfui -lfai'
+CMK_F90LIBS="-lfui -lfai -lfai2 -lfsumai -lfprodai -lfminlai -lfmaxlai -lfminvai -lfmaxvai -lfsu -lsunmath -lm -lc -lrt"
+
