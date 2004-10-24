6b6a37c73c380ede5022e4a559955586dda2781e
Gengbin Zheng <gzheng@illinois.edu>
2004-10-24 16:51:00 +0000

    define CMK_F90_FIXED to be same as CMK_F90. This is tricky because even without defining FIXED, it takes the default value from conv-mach.sh, not the one in optional conv-machs.

--- src/arch/mpi-linux/cc-cmpi.sh
+++ src/arch/mpi-linux/cc-cmpi.sh
@@ -1,13 +1,17 @@
 CMK_CPP_C="cmpicc -E"
 CMK_CC="cmpicc -icc -gm "
-CMK_CXX="cmpicc -icc -gm "
+CMK_CXX="cmpic++ -icc -gm "
 CMK_CXXPP="cmpicc -E "
-CMK_CF77="cmpifc -ifc "
-CMK_CF90="cmpifc -ifc "
 CMK_LD="cmpicc -icc "
 CMK_LDXX="cmpicc -icc "
 CMK_LIBS="-lckqt -lcmpi "
+
+# fortran compiler
+CMK_CF77="cmpifc -ifc "
+CMK_CF90="cmpif90c -ifc "
+CMK_CF90_FIXED="cmpif90c -ifc "
 CMK_F90LIBS='-lifcore  '
+CMK_MOD_NAME_ALLCAPS=
 CMK_MOD_EXT="mod"
 CMK_F90_USE_MODDIR=
 CMK_F90_MODINC=""
