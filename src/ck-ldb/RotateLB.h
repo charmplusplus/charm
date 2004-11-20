2eeed6792c9cef619fdcbe825487deb3214c6792
Greg Koenig <koenig@uiuc.edu>
2004-11-20 00:21:37 +0000

    These changes add a new Charm++ load balancer strategy called RotateLB.
    RotateLB is a simple load balancer that rotates the objects on each PE
    to the next higher PE, mod the total number of PEs in the computation.
    This provides another simple toy load balancer for learning, but is also
    a useful way of testing the PUP routines for a Charm++ program.

---
+++ src/ck-ldb/RotateLB.h
@@ -0,0 +1,22 @@
+#ifndef _ROTATELB_H_
+#define _ROTATELB_H_
+
+#include "CentralLB.h"
+
+void CreateRotateLB ();
+
+class RotateLB : public CentralLB
+{
+  public:
+    RotateLB (const CkLBOptions &opt);
+    RotateLB (CkMigrateMessage *m) : CentralLB (m) { };
+
+    void work (CentralLB::LDStats *stats, int count);
+
+    void pup (PUP::er &p) { CentralLB::pup(p); }
+
+  private:
+    CmiBool QueryBalanceNow (int step);
+};
+
+#endif /* _ROTATELB_H_ */
