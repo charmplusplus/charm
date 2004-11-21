41860f83d1697e7e90a64ab499cae28465bf698e
Gengbin Zheng <gzheng@illinois.edu>
2004-11-21 06:09:30 +0000

    define CF77 compiler.

--- src/arch/common/conv-mach-ifort.sh
+++ src/arch/common/conv-mach-ifort.sh
@@ -1,4 +1,5 @@
 # for Intel Fortran compiler 8.0 and higher which is renamed to ifort from ifc
+CMK_CF77='ifort -auto '
 CMK_CF90='ifort -auto '
 CMK_CF90_FIXED="$CMK_CF90 -132 -FI "
 CMK_F90LIBS="-L/opt/intel_fc_80/lib/ -lifcore -lifport "
