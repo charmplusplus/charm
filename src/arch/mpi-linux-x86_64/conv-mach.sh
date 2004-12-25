69ac978815c871bc4e4fc0bf39772b47e3df32c9
Gengbin Zheng <gzheng@illinois.edu>
2004-12-25 06:15:41 +0000

    mpi-linux-amd64 for opteron (similar to mpi-linux-ia64)

---
+++ src/arch/mpi-linux-x86_64/conv-mach.sh
@@ -0,0 +1,34 @@
+
+CMK_REAL_COMPILER=`mpiCC -show | cut -d' ' -f1`
+case "$CMK_REAL_COMPILER" in
+g++) CMK_AMD64="-m64 -fPIC" ;;
+esac
+
+CMK_CPP_CHARM="/lib/cpp -P"
+CMK_CPP_C="mpicc -E"
+CMK_CC="mpicc $CMK_AMD64 "
+CMK_CXX="mpiCC $CMK_AMD64 "
+CMK_CXXPP="mpiCC -E $CMK_AMD64 "
+
+CMK_SYSLIBS="-lmpich "
+CMK_LIBS="-lckqt $CMK_SYSLIBS "
+CMK_LD_LIBRARY_PATH="-Wl,-rpath,$CHARMLIBSO/"
+
+CMK_NATIVE_CC="gcc $CMK_AMD64 "
+CMK_NATIVE_LD="gcc $CMK_AMD64 "
+CMK_NATIVE_CXX="g++ $CMK_AMD64 "
+CMK_NATIVE_LDXX="g++ $CMK_AMD64 "
+CMK_NATIVE_LIBS=""
+
+# fortran compiler 
+CMK_CF77="f77"
+CMK_CF90="f90"
+CMK_F90LIBS="-L/usr/absoft/lib -L/opt/absoft/lib -lf90math -lfio -lU77 -lf77math "
+CMK_MOD_NAME_ALLCAPS=1
+CMK_MOD_EXT="mod"
+CMK_F90_USE_MODDIR=1
+CMK_F90_MODINC="-p"
+
+CMK_QT='generic64'
+CMK_RANLIB="ranlib"
+
