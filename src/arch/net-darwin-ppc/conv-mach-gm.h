83f72e73a70cfed64ec893762af96cd336339e60
Gengbin Zheng <gzheng@illinois.edu>
2005-01-04 23:09:30 +0000

    gm config for mac.

---
+++ src/arch/net-darwin-ppc/conv-mach-gm.h
@@ -0,0 +1,9 @@
+
+#undef CMK_USE_GM
+#define CMK_USE_GM                                         1
+
+#undef CMK_NETPOLL
+#define CMK_NETPOLL                                        1
+
+#undef CMK_MULTICAST_LIST_USE_COMMON_CODE
+#define CMK_MULTICAST_LIST_USE_COMMON_CODE		   0
