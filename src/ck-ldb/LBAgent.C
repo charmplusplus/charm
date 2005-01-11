cb7b96acfdae63e289195649b5bf2e2edc788297
Amit Sharma <asharma6@uiuc.edu>
2005-01-11 20:54:10 +0000

    new agent class for load balancer to consult specific knowledge about topology, communication, etc. It returns a prefered list of processors for consideration.

---
+++ src/ck-ldb/LBAgent.C
@@ -0,0 +1,114 @@
+/***********************************
+*	Author : Amit Sharma
+*	Date : 12/04/2004
+************************************/
+
+
+#include <math.h>
+
+//#include "charm.h"
+
+//#include "topology.h"
+#include "Agent.h"
+
+
+int topoAgent :: compare(const void *p,const void *q){
+	return (((Elem*)p)->Cost - ((Elem*)q)->Cost);
+}
+		
+
+Agent::Elem* topoAgent :: my_preferred_procs(int *existing_map,int object,int *trialpes) {
+
+	int *procs_list;
+	
+	//int *preferred_list;
+
+	for(int i=0;i<npes;i++){
+		preferred_list[i].pe = -1;
+		preferred_list[i].Cost = 0;
+	}
+
+	if(trialpes == NULL)
+		*procs_list = -1;
+	else
+		procs_list = trialpes;
+
+	//Before everything...construct a list of comm of this object with all the other objects
+
+	int proc=0;
+	int index=0;
+	int cost=0;
+	int preflistSize = 0;
+
+	if(procs_list[0]!=-1)
+		proc = procs_list[index];
+	
+	/*int j=0;
+	while(procs_list[j]!=-1){
+		CkPrintf(" %d,%d ",j,procs_list[j]);
+		j++;
+	}*/
+		
+	//CkPrintf("in preferred....before loop\n");
+	
+	while(1){
+		
+		cost=0;
+		if(!stats->procs[proc].available){
+			//CkPrintf("some problem\n");
+			if(procs_list[0]==-1){
+				proc++;
+				if(proc == npes)
+					break;
+			}
+			else {
+				index++;
+				if(procs_list[index] == -1)
+					break;
+				proc = procs_list[index];
+			}
+			continue;
+		}
+		
+		//CkPrintf("before cost cal..\n");
+		for(int i=0;i<stats->n_objs;i++){
+			if(existing_map[i]!=-1 && commObjs[object][i]!=0){
+				//CkPrintf("before calling get hop count..proc:%d,existing map:%d\n",proc,existing_map[comm[i].obj]);
+				if(hopCount[proc][existing_map[i]])
+					cost += hopCount[proc][existing_map[i]]*commObjs[object][i];
+				else{
+					hopCount[proc][existing_map[i]]=topo->get_hop_count(proc,existing_map[i]);
+					hopCount[existing_map[i]][proc]=hopCount[proc][existing_map[i]];
+					cost += hopCount[proc][existing_map[i]]*commObjs[object][i];
+				}
+				//CkPrintf("after calling get hop count...\n");
+			}
+		}
+		//CkPrintf("after cost cal..\n");
+		preferred_list[preflistSize].pe = proc;
+		preferred_list[preflistSize].Cost = cost;
+		preflistSize++;
+		
+		if(procs_list[0]==-1){
+			proc++;
+			//CkPrintf("at wrong place\n");
+			if(proc == npes)
+				break;
+		}
+		else {
+			index++;
+			//CkPrintf("right place ,index:%d",index);
+			if(procs_list[index] == -1)
+				break;
+			proc = procs_list[index];
+		}
+
+	}
+
+	//Sort all the elements of the preferred list in increasing order
+	qsort(preferred_list,preflistSize,sizeof(Elem),&compare);
+	//for(int k=0;k<preflistSize;k++)
+		//CkPrintf("pe:%d cost:%d\n",preferred_list[k].pe,preferred_list[k].Cost);
+	
+	return preferred_list;
+}
---
+++ src/ck-ldb/LBAgent.h
@@ -0,0 +1,107 @@
+/***********************************
+*	Author : Amit Sharma
+*	Date : 12/04/2004
+************************************/
+
+#ifndef _AGENT_H
+#define _AGENT_H
+
+#include "CentralLB.h"
+#include "topology.h"
+
+class Agent {
+public:
+	int npes;
+	
+	typedef struct{
+		int pe;
+		int Cost;
+	}Elem;
+	
+	Elem *preferred_list;
+
+//public:
+	Agent(int p): npes(p) { }
+	~Agent() { }
+
+	virtual Elem* my_preferred_procs(int *existing_map,int object,int *trialpes);
+};
+
+class topoAgent : public Agent {
+//protected:
+public:
+	CentralLB::LDStats* stats;
+	//Elem *preferred_list;
+	LBTopology *topo;
+	
+	/*typedef struct{
+		int obj;
+		int bytesComm;
+	}CommObj;
+*/
+	//CommObj
+	int **commObjs;
+	int **hopCount;
+//public:			
+	topoAgent(CentralLB::LDStats* lbDB,void* comlibDB,LBTopology *topology,int p): stats(lbDB), topo(topology), Agent(p){
+		stats->makeCommHash();
+		preferred_list = new Elem[p];
+		commObjs = new int*[stats->n_objs];
+		for(int i=0;i<stats->n_objs;i++){
+			commObjs[i] = new int[stats->n_objs];
+			for(int j=0;j<stats->n_objs;j++)
+				commObjs[i][j] = 0;
+		}
+
+		hopCount = new int*[npes];
+		for(int i=0;i<npes;i++){
+			hopCount[i] = new int[npes];
+			for(int j=0;j<npes;j++)
+				hopCount[i][j] = 0;
+		}
+
+		for(int i=0;i<stats->n_comm;i++){
+			//DO I consider other comm too....i.e. to or from a processor
+			//CkPrintf("in loop..\n");
+			LDCommData &cdata = stats->commData[i];
+			if(cdata.from_proc() || cdata.receiver.get_type() != LD_OBJ_MSG)
+    		continue;
+    	int senderID = stats->getHash(cdata.sender);
+			CmiAssert(senderID < stats->n_objs);
+    	int recverID = stats->getHash(cdata.receiver.get_destObj());
+    	CmiAssert(recverID < stats->n_objs);
+		
+			//Check with Gengbin if 2 different commData have the same pair of processors as role reversed
+			//for(int j=0;j<num_comm;j++)
+				
+			commObjs[senderID][recverID] += cdata.bytes;
+			commObjs[recverID][senderID] += cdata.bytes;
+		}
+		
+		//Pre-calculate the hop counts
+		/*for(int i=0;i<npes;i++){
+			CkPrintf("for proc num :%d calculating hop counts\n",i);
+			for(int j=0;j<npes;j++)
+				if(i!=j && !hopCount[i][j]){
+					hopCount[i][j]=topo->get_hop_count(i,j);
+					hopCount[j][i]=hopCount[i][j];
+				}
+		}*/	
+	}
+	~topoAgent() { }
+	Agent::Elem* my_preferred_procs(int *existing_map,int object,int *trialpes);
+	static int compare(const void *p,const void *q);
+};
+
+class comlibAgent : public Agent {
+	protected:
+		void* comlibstats;
+	public:
+		comlibAgent(int p): Agent(p) { }
+		~comlibAgent() { }
+
+	Agent::Elem* my_preferred_procs(int *existing_map,int object,int *trialpes);
+
+};
+
+#endif
