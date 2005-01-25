2281c7048e5d6686074596eb2f911f10008f4f5e
Gengbin Zheng <gzheng@illinois.edu>
2005-01-25 19:56:58 +0000

    switch back to context threads since QT thread breaks megatest marshall test. thread migration then won't work, need to find a fix

--- src/arch/mpi-linux/cc-cmpi.h
+++ src/arch/mpi-linux/cc-cmpi.h
@@ -6,8 +6,12 @@
 #define CMK_MALLOC_USE_GNU_MALLOC                          0
 #define CMK_MALLOC_USE_OS_BUILTIN                          1
 
-/* keep using QT, context thread migration is broken (megampi) on tungsten
-/*
+/* keep using QT, context thread migration is broken (megampi) on tungsten */
+/* however QT crash megatest marshall test */
 #undef CMK_THREADS_USE_CONTEXT
 #define CMK_THREADS_USE_CONTEXT                            1
-*/
+
+#undef CMK_TYPEDEF_INT8
+#undef CMK_TYPEDEF_UINT8
+#define CMK_TYPEDEF_INT8 long long
+#define CMK_TYPEDEF_UINT8 unsigned long long
