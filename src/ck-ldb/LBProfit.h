88a62ca418fdd10a3c39e8c41b6f1426573c3316
Gengbin Zheng <gzheng@illinois.edu>
2005-03-13 21:07:16 +0000

    a simple class for determining if a new load balancing cycle is profitable or not.

---
+++ src/ck-ldb/LBProfit.h
@@ -0,0 +1,30 @@
+
+/*****************************************************************************
+ * $Source$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *****************************************************************************/
+
+/**
+ * \addtogroup CkLdb
+*/
+/*@{*/
+
+/**
+  a module which determines if a load balancing cycle is profitable
+*/
+
+#include "BaseLB.h"
+
+class LBProfit
+{
+public:
+  virtual int profitable(BaseLB::ProcStats *procArray, int np);
+};
+
+
+/*@}*/
+
+
+
