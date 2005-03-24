e6170ad73d663456108ebc882a0d79d0e7a63c71
Gengbin Zheng <gzheng@illinois.edu>
2005-03-24 17:00:48 +0000

    fixed typedef int8.

--- src/arch/mpi-sp/cc-mpcc32.h
+++ src/arch/mpi-sp/cc-mpcc32.h
@@ -1,2 +1,11 @@
 /* no defines needed. */
 
+#if ! CMK_LONG_LONG_DEFINED
+#error "long long not defined!"
+#endif
+
+#undef CMK_TYPEDEF_INT8
+#undef CMK_TYPEDEF_UINT8
+#define CMK_TYPEDEF_INT8 long long
+#define CMK_TYPEDEF_UINT8 unsigned long long
+
