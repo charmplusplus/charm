6147c78ffaaca1744d6b56282cc2ec617d599c70
Gengbin Zheng <gzheng@illinois.edu>
2005-03-25 01:05:38 +0000

    use CMK_USE_SYNC to protect "sync" system call.

--- src/ck-core/ckmemcheckpoint.C
+++ src/ck-core/ckmemcheckpoint.C
@@ -440,7 +440,9 @@ void CkMemCheckPT::recvData(CkArrayCheckPTMessage *msg)
 void CkMemCheckPT::syncFiles(CkReductionMsg *m)
 {
   delete m;
+#if CMK_HAS_SYNC
   system("sync");
+#endif
   thisProxy[cpStarter].cpFinish();
 }
 
