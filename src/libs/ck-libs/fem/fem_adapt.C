f92b998fa90f7bd34e83e079f5ba53bd7f3eeb2d
Terry L. Wilmarth <wilmarth@uiuc.edu>
2005-04-13 23:33:43 +0000

    Checking in adaptivity module.  Not yet built.

---
+++ src/libs/ck-libs/fem/fem_adapt.C
@@ -0,0 +1,389 @@
+#include "fem_adapt.h"  
+
+// Helpers
+int FEM_Adapt::get_edge_index(int local_node1, int local_node2) 
+{
+  int sum = local_node1 + local_node2;
+  CkAssert(local_node1 != local_node2);
+  if (sum == 1) return 0;
+  else if (sum == 3) return 1;
+  else if (sum == 2) return 2;
+  else {
+    CkAbort("ERROR: local node pair is strange: [%d,%d]\n", local_node1,
+	    local_node2);
+    return -1;
+  }
+}
+
+int FEM_Adapt::find_local_node_index(int e, int n) {
+  int result = theMesh->e2n_getIndex(e, n);
+  if (result < 0) {
+    CkAbort("ERROR: node %d not found on element %d\n", n, e);
+  }
+  return result;
+}
+
+int FEM_Adapt::check_orientation(int e1, int e3, int n, int n1, int n2)
+{
+  int e1_n = find_local_node_index(e1, n);
+  int e1_n1 = find_local_node_index(e1, n1);
+  int e3_n = find_local_node_index(e3, n);
+  int e3_n2 = find_local_node_index(e3, n2);
+  
+  if (((e1_n1 == (e1_n+1)%3) && (e3_n == (e3_n2+1)%3)) ||
+      ((e1_n == (e1_n1+1)%3) && (e3_n2 == (e3_n+1)%3)))
+    return 1;
+  else return 0;
+}
+
+void FEM_Adapt::adj_traverse(int n, int startNode, int stopNode, int startElem,
+			     int stopElem, int *nn, int *ne, int *nodeList,
+			     int *elemList)
+{
+  int elm = startElem, nod = startNode;
+  int nIdx;
+  (*nn) = 0;
+  (*ne) = 0;
+  if (elm == -1) {
+    nodeList[*nn] = nod; (*nn)++;
+  }
+  while ((elm != stopElem) && (elm != -1)) {
+    nodeList[*nn] = nod; (*nn)++;
+    elemList[*ne] = elm; (*ne)++;
+    nIdx = 3 - find_local_node_index(elm,n) - find_local_node_index(elm,nod);
+    nod = theMesh->e2n_getNode(elm, nIdx);
+    elm = theMesh->getElementOnEdge(elm, n, nIdx);
+  }
+  if (elm == stopElem) {
+    nodeList[*nn] = nod; (*nn)++;
+    elemList[*ne] = elm; (*ne)++;
+    nodeList[*nn] = stopNode; (*nn)++;
+  }
+  else {
+    int elm = stopElem;
+    int nod = stopNode;
+    while (elm != -1) {
+      nodeList[*nn] = nod; (*nn)++;
+      elemList[*ne] = elm; (*ne)++;
+      nIdx = 3 - find_local_node_index(elm,n) - find_local_node_index(elm,nod);
+      nod = theMesh->e2n_getNode(elm, nIdx);
+      elm = theMesh->getElementOnEdge(elm, n, nIdx);
+    }
+    nodeList[*nn] = nod; (*nn)++;
+  }
+
+}
+
+// edge_flip and helpers
+int FEM_Adapt::edge_flip(int n1, int n2) 
+{
+  int e1 = theMesh->getElementOnEdge(n1, n2);
+  if (e1 < 0) {
+    CkPrintf("ERROR: edge_flip: no element with edge [%d,%d]\n", n1, n2);
+    return 0;	     
+  }
+  return edge_flip(e1, n1, n2);
+}
+int FEM_Adapt::edge_flip(int e1, int n1, int n2) 
+{
+  int e1_n1 = find_local_node_index(e1, n1);
+  int e1_n2 = find_local_node_index(e1, n2);
+  int shared_edge = get_edge_index(e1_n1, e1_n2);
+  int e2 = theMesh->e2e_getNbr(e1, shared_edge); 
+  if (e2 == -1) { // there is no neighbor; can't flip; 
+    return 1;  // this is a no-op; could return 0 if we want to flag as error
+  }
+  return edge_flip_help(e1, e2, n1, n2, e1_n1, e1_n2);
+}
+int FEM_Adapt::edge_flip_help(int e1, int e2, int n1, int n2, int e1_n1, 
+			      int e1_n2) 
+{
+  int e1_n3 = 3 - e1_n1 - e1_n2;
+  int mod_edge1 = get_edge_index(e1_n2, e1_n3);
+  int e1nbr = theMesh->e2e_getNbr(e1, mod_edge1);
+  int e2_n1 = find_local_node_index(e2, n1);
+  int e2_n2 = find_local_node_index(e2, n2);
+  int e2_n3 = 3 - e2_n1 - e2_n2;
+  int mod_edge2 = get_edge_index(e2_n1, e2_n3);
+  int e2nbr = theMesh->e2e_getNbr(e2, mod_edge2);
+  int n3 = theMesh->e2n_getNode(e1, e1_n3);
+  int n4 = theMesh->e2n_getNode(e2, e2_n3);
+  
+  // Element-to-node updates
+  theMesh->e2n_setIndex(e1, e1_n2, n4);
+  theMesh->e2n_setIndex(e2, e2_n1, n3);
+  // Element-to-element updates
+  theMesh->e2e_replace(e1, e1nbr, e2nbr);
+  theMesh->e2e_replace(e2, e2nbr, e1nbr);
+  theMesh->e2e_replace(e1nbr, e1, e2);
+  theMesh->e2e_replace(e2nbr, e2, e1);
+  // Node-to-node updates
+  theMesh->n2n_remove(n1, n2);
+  theMesh->n2n_remove(n2, n1);
+  theMesh->n2n_add(n3, n4);
+  theMesh->n2n_add(n4, n3);
+  // Node-to-element updates
+  theMesh->n2e_remove(n1, e2);
+  theMesh->n2e_remove(n2, e1);
+  theMesh->n2e_add(n3, e2);
+  theMesh->n2e_add(n4, e1);
+  return 1;
+}
+
+// edge_bisect and helpers  
+int FEM_Adapt::edge_bisect(int n1, int n2) 
+{
+  int e1 = theMesh->getElementOnEdge(n1, n2);
+  if (e1 < 0) {
+    CkPrintf("ERROR: edge_bisect: no element with edge [%d,%d]\n", n1, n2);
+    return -1;	     
+  }
+  return edge_bisect(e1, n1, n2);
+}
+int FEM_Adapt::edge_bisect(int e1, int n1, int n2)
+{
+  int e1_n1 = find_local_node_index(e1, n1);
+  int e1_n2 = find_local_node_index(e1, n2);
+  int shared_edge = get_edge_index(e1_n1, e1_n2);
+  int e2 = theMesh->e2e_getNbr(e1, shared_edge); 
+  return edge_bisect_help(e1, e2, n1, n2, e1_n1, e1_n2);
+}
+int FEM_Adapt::edge_bisect_help(int e1, int e2, int n1, int n2, int e1_n1, 
+				int e1_n2)
+{
+  int e1_n3 = 3 - e1_n1 - e1_n2;
+  int mod_edge1 = get_edge_index(e1_n2, e1_n3);
+  int e1nbr = theMesh->e2e_getNbr(e1, mod_edge1);
+  int n3 = theMesh->e2n_getNode(e1, e1_n3);
+  int n5 = theMesh->newNode();
+  int e3 = theMesh->newElement();
+  if (e2 >= 0) { // neighbor exists
+    int e2_n1 = find_local_node_index(e2, n1);
+    int e2_n2 = find_local_node_index(e2, n2);
+    int e2_n3 = 3 - e2_n1 - e2_n2;
+    int mod_edge2 = get_edge_index(e2_n1, e2_n3);
+    int e2nbr = theMesh->e2e_getNbr(e2, mod_edge2);
+    int n4 = theMesh->e2n_getNode(e2, e2_n3);
+    int e4 = theMesh->newElement();
+  }
+  
+  // Element-to-node updates
+  theMesh->e2n_setIndex(e1, e1_n2, n5);
+  theMesh->e2n_setIndex(e3, e1_n1, n5);
+  theMesh->e2n_setIndex(e3, e1_n2, n2);
+  theMesh->e2n_setIndex(e3, e1_n3, n3);
+  if (e2 >= 0) {
+    theMesh->e2n_setIndex(e2, e2_n2, n5);
+    theMesh->e2n_setIndex(e4, e2_n2, n2);
+    theMesh->e2n_setIndex(e4, e2_n1, n5);
+    theMesh->e2n_setIndex(e4, e2_n3, n4);
+  }
+  // Element-to-element updates
+  theMesh->e2e_replace(e1, e1nbr, e3);
+  theMesh->e2e_replace(e1nbr, e1, e3);
+  if (e2 >= 0) {
+    theMesh->e2e_replace(e2, e2nbr, e4);
+    theMesh->e2e_replace(e2nbr, e2, e4);
+    theMesh->e2e_setAll(e3, e1, e4, e1nbr);
+    theMesh->e2e_setAll(e4, e2, e3, e2nbr);
+  }
+  else {
+    theMesh->e2e_setAll(e3, e1, -1, e1nbr);
+  }
+  // Node-to-node updates
+  theMesh->n2n_replace(n1, n2, n5);
+  theMesh->n2n_replace(n2, n1, n5);
+  theMesh->n2n_add(n3, n5);
+  theMesh->n2n_add(n5, n1);
+  theMesh->n2n_add(n5, n2);
+  theMesh->n2n_add(n5, n3);
+  if (e2 >= 0) {
+    theMesh->n2n_add(n4, n5);
+    theMesh->n2n_add(n5, n4);
+  }
+  // Node-to-element updates
+  theMesh->n2e_replace(n2, e1, e3);
+  theMesh->n2e_add(n3, e3);
+  theMesh->n2e_add(n5, e1);
+  theMesh->n2e_add(n5, e3);
+  if (e2 >= 0) {
+    theMesh->n2e_replace(n2, e2, e4);
+    theMesh->n2e_add(n4, e4);  
+    theMesh->n2e_add(n5, e2);
+    theMesh->n2e_add(n5, e4);
+  }
+  return n5;
+}
+  
+// edge_contraction and helpers
+int FEM_Adapt::edge_contraction(int n1, int n2) 
+{
+  int e1 = theMesh->getElementOnEdge(n1, n2);
+  if (e1 < 0) {
+    CkPrintf("ERROR: edge_bisect: no element with edge [%d,%d]\n", n1, n2);
+    return -1;	     
+  }
+  return edge_contraction(e1, n1, n2);
+}
+int FEM_Adapt::edge_contraction(int e1, int n1, int n2) 
+{
+  int e1_n1 = find_local_node_index(e1, n1);
+  int e1_n2 = find_local_node_index(e1, n2);
+  int shared_edge = get_edge_index(e1_n1, e1_n2);
+  int e2 = theMesh->e2e_getNbr(e1, shared_edge); 
+  return edge_contraction_help(e1, e2, n1, n2, e1_n1, e1_n2);
+}
+int FEM_Adapt::edge_contraction_help(int e1, int e2, int n1, int n2, int e1_n1,
+				     int e1_n2)
+{
+  int e1_n3 = 3 - e1_n1 - e1_n2;
+  int mod_edge1 = get_edge_index(e1_n1, e1_n3);
+  int e1nbr1 = theMesh->e2e_getNbr(e1, mod_edge1);
+  int mod_edge2 = get_edge_index(e1_n2, e1_n3);
+  int e1nbr2 = theMesh->e2e_getNbr(e1, mod_edge2);
+  int n3 = theMesh->e2e_getNode(e1, e1_n3);
+  if (e2 >= 0) {
+    int e2_n1 = find_local_node_index(e2, n1);
+    int e2_n2 = find_local_node_index(e2, n2);
+    int e2_n3 = 3 - e2_n1 - e2_n2;
+    int mod_edge3 = get_edge_index(e2_n1, e2_n3);
+    int e2nbr1 = theMesh->e2e_getNbr(e2, mod_edge3);
+    int mod_edge4 = get_edge_index(e2_n2, e2_n3);
+    int e2nbr2 = theMesh->e2e_getNbr(e2, mod_edge4);
+    int n4 = theMesh->e2e_getNode(e2, e2_n3);
+  }
+
+  int *n2_nbrNodes, *n2_nbrElems;
+  int nnsize, nesize;
+  theMesh->n2n_getAll(n2, &n2_nbrNodes, &nnsize);
+  theMesh->n2e_getAll(n2, &n2_nbrElems, &nesize);
+  
+  // Element-to-node updates
+  for (int i=0; i<nesize; i++) {
+    theMesh->e2n_replace(n2_nbrElems[i], n2, n1);
+    theMesh->n2e_add(n1, n2_nbrElems[i]);
+  }
+  theMesh->n2e_remove(n1, e1); // assume duplicated by loop add above
+  theMesh->n2e_remove(n1, e2); // assume duplicated by loop add above
+  // Element-to-element updates
+  theMesh->e2e_replace(e1nbr1, e1, e1nbr2);
+  theMesh->e2e_replace(e1nbr2, e1, e1nbr1);
+  theMesh->e2e_replace(e2nbr1, e2, e2nbr2);
+  theMesh->e2e_replace(e2nbr2, e2, e2nbr1);
+  // Node-to-node updates
+  for (int i=0; i<nnsize; i++) {
+    theMesh->n2n_replace(n2_nbrNodes[i], n2, n1);
+    theMesh->n2n_add(n1, n2_nbrNodes[i]);
+  }
+  theMesh->n2n_remove(n1, n2);
+  theMesh->n2n_remove(n1, n1); // assume added by loop add above; not necessary
+  theMesh->n2n_remove(n1, n3); // assume duplicated by the loop add above
+  theMesh->n2n_remove(n1, n4); // assume duplicated by the loop add above
+  theMesh->n2n_remove(n3, n2);
+  theMesh->n2n_remove(n4, n2);
+  // Node-to-element updates
+  theMesh->n2e_remove(n1, e1);
+  theMesh->n2e_remove(n1, e2);
+  theMesh->n2e_remove(n3, e1);
+  theMesh->n2e_remove(n4, e2);
+  
+  theMesh->deleteNode(n2);
+  theMesh->deleteElement(e1);
+  theMesh->deleteElement(e2);
+}
+
+// vertex_split and helpers
+int FEM_Adapt::vertex_split(int n, int n1, int n2) 
+{
+  int e1 = theMesh->getElementOnEdge(n, n1);
+  if (e1 < 0) {
+    CkPrintf("ERROR: vertex_split: no element with edge [%d,%d]\n", n, n1);
+    return -1;	     
+  }
+  int e3 = theMesh->getElementOnEdge(n, n2);
+  if (e3 < 0) {
+    CkPrintf("ERROR: vertex_split: no element with edge [%d,%d]\n", n, n2);
+    return -1;	     
+  }
+  return vertex_split(n, n1, n2, e1, e3);
+}
+int FEM_Adapt::vertex_split(int n, int n1, int n2, int e1, int e3)
+{
+  int n_e1 = find_local_node_index(e1, n);
+  int n1_e1 = find_local_node_index(e1, n1);
+  int e2 = theMesh->e2e_getNbr(e1, get_edge_index(n_e1, n1_e1));
+  int n_e3 = find_local_node_index(e3, n);
+  int n2_e3 = find_local_node_index(e3, n2);
+  int e4 = theMesh->e2e_getNbr(e3, get_edge_index(n_e3, n2_e3));
+  if (!check_orientation(e1, e3, n, n1, n2)) {
+    int tmp = e3;
+    e3 = e4;
+    e4 = tmp;
+    n_e3 = find_local_node_index(e3, n);
+    n2_e3 = find_local_node_index(e3, n2);
+  }
+  int np = theMesh->newNode();
+  int e5 = theMesh->newElement();
+  int e6 = theMesh->newElement();
+  int nnCount=0, neCount=0;
+  int np_nodes[50], np_elems[50]; // I certainly hope the mesh is not this bad
+  adj_traverse(n, n1, e2, n2, e4, &nnCount, &neCount, np_nodes, np_elems);
+
+  // Element-to-node updates
+  int nl[3];
+  if ((n_e1 < n1_e1) || ((n_e1 == 2) && (n1_e1 == 0))) {
+    nl[0] = n1; nl[1] = n; nl[2] = np;
+    theMesh->e2n_setAll(e5, nl);
+    nl[0] = e1; nl[1] = e6; nl[2] = e2;
+    theMesh->e2e_setAll(e5, nl);
+  }
+  else {
+    nl[0] = n; nl[1] = n1; nl[2] = np;
+    theMesh->e2n_setAll(e5, nl);
+    nl[0] = e1; nl[1] = e2; nl[2] = e6;
+    theMesh->e2e_setAll(e5, nl);
+  }
+  if ((n_e3 < n2_e3) || ((n_e3 == 2) && (n2_e3 == 0))) {
+    nl[0] = n2; nl[1] = n; nl[2] = np;
+    theMesh->e2n_setAll(e6, nl);
+    nl[0] = e3; nl[1] = e5; nl[2] = e4;
+    theMesh->e2e_setAll(e6, nl);
+  }
+  else {
+    nl[0] = n; nl[1] = n2; nl[2] = np;
+    theMesh->e2n_setAll(e6, nl);
+    nl[0] = e3; nl[1] = e4; nl[2] = e5;
+    theMesh->e2e_setAll(e6, nl);
+  }
+  theMesh->e2n_replace(e2, n, np);
+  theMesh->e2n_replace(e4, n, np);
+  // Element-to-element updates
+  theMesh->e2e_replace(e1, e2, e5);
+  theMesh->e2e_replace(e2, e1, e5);
+  theMesh->e2e_replace(e3, e4, e6);
+  theMesh->e2e_replace(e4, e3, e6);
+  // Node-to-node updates
+  for (i=0; i<nnCount; i++) {
+    theMesh->n2n_add(np, np_nodes[i]);
+    theMesh->n2n_remove(n, np_nodes[i]);
+  }
+  theMesh->n2n_add(n, np);
+  theMesh->n2n_add(n, n1);
+  theMesh->n2n_add(n, n2);
+  theMesh->n2n_add(np, n);
+  // Node-to-element updates
+  for (i=0; i<neCount; i++) { 
+    theMesh->n2e_add(np, np_elems[i]);
+    theMesh->n2e_remove(n, np_elems[i]);
+  }
+  theMesh->n2e_add(n, e5);
+  theMesh->n2e_add(n, e6);
+  theMesh->n2e_add(np, e5);
+  theMesh->n2e_add(np, e6);
+  return np;
+}
+
+// element_bisect and helpers
+void FEM_Adapt::element_bisect(int e1) 
+{
+}
---
+++ src/libs/ck-libs/fem/fem_adapt.h
@@ -0,0 +1,89 @@
+#ifndef __CHARM_FEM_ADAPT_H
+#define __CHARM_FEM_ADAPT_H
+
+#include "fem_mesh.h"
+
+class FEM_Adapt {
+  FEM_Mesh *theMesh;
+
+  /// Initialize FEM_Adapt with a chunk of the mesh
+  FEM_Adapt(FEM_Mesh *m) {
+    theMesh = m;
+  }
+  // Helper methods
+  /// Map a pair of element-local node numberings to an element-local edge 
+  /// numbering
+  /** Given two element-local node numberings (i.e. 0, 1, 2 for triangular 
+      elements), calculate an element-local edge numbering (also 0, 1, or 2
+      for triangular elements) **/
+  int get_edge_index(int local_node1, int local_node2);
+  /// Find an element-local node numbering for a chunk-local node
+  /** Given a chunk-local element number e and a chunk-local node number n,
+      determine the element-local node numbering for node n on element e **/
+  int find_local_node_index(int e, int n);
+  /// Check if e1 and e3 are on the same side of edge path (n1, n, n2)
+  /** Makes use of ordering of nodes in e1 to check is e3 is on the same side
+      of the path of edges (n1, n) and (n, n2) **/
+  int check_orientation(int e1, int e3, int n, int n1, int n2);
+  /// Build adjacency lists for vertex_split operation
+  /** When splitting a node, the adjacency lists for the old and new
+      nodes need to be reconstructed from scratch. In case of gap in
+      mesh, traversal is attempted from both sides of the arc.  Arc
+      goes around node n starting at node startNode and element
+      startElem, and ends at stopElem.  Resulting counts for nodes and
+      elements are stored in nn and ne respectively, and actual nodes
+      and elements are stored in nodeList and elemList
+      respectively **/
+  void adj_traverse(int n, int startNode, int stopNode, int startElem, 
+		    int stopElem, int *nn, int *ne, int *nodeList,
+		    int *elemList);
+
+  /// Perform a Delaunay flip of edge (n1, n2)
+  /** Perform a Delaunay flip of the edge (n1, n2) returning 1 if successful,
+      0 if not.  The convexity of the quadrilateral formed by two faces
+      incident to edge (n1, n2) is assumed. **/
+  virtual int edge_flip(int n1, int n2);
+  /// Helper method to int edge_flip(int n1, int n2)
+  virtual int edge_flip(int e1, int n1, int n2);
+  /// Helper method to int edge_flip(int e1, int n1, int n2)
+  virtual int edge_flip_help(int e1, int e2, int n1, int n2, int e1_n1, 
+			     int e1_n2);
+  
+  /// Bisect edge (n1, n2) and the two adjacent elements
+  /** Given edge e:(n1, n2), determing the two elements e1:(n1,n2,n3) and 
+      e2:(n2,n1,n4) adjacent to e, and bisect e, e1 and e2 by adding a node 
+      n5 to edge e. e1 becomes (n1,n5,n3) and we add e3:(n5,n2,n3), e2 becomes 
+      (n5,n1,n4) and we add e4:(n2,n5,n4); returns new node n5 **/
+  virtual int edge_bisect(int n1, int n2);
+  /// Helper method to int edge_bisect(int n1, int n2);
+  virtual int edge_bisect(int e1, int n1, int n2);
+  /// Helper method to int edge_bisect(int e1, int n1, int n2);
+  virtual int edge_bisect_help(int e1, int e2, int n1, int n2, int e1_n1, 
+			       int e1_n2);
+  
+  /// Contract edge (n1, n2) and the two adjacent elements
+  /** Given and edge e:(n1, n2), determine the two adjacent elements
+      e1:(n1,n2,n3) and e2:(n1,n2,n4). Contract edge e by collapsing node n2 
+      to n1, removing edge e, node n2, elements e1 & e2; return 1 if 
+      successful, 0 if not **/
+  virtual int edge_contraction(int n1, int n2);
+  virtual int edge_contraction(int e1, int n1, int n2);
+  virtual int edge_contraction_help(int e1, int e2, int n1, int n2, int e1_n1,
+				    int e1_n2);
+
+  /// Split a node n into two nodes with an edge in between
+  /** Given a node n and two adjacent nodes n1 and n2, split n into two nodes
+      n and newNode such that the edges to the neighbors n1 and n2
+      expand into two new elements (n, newNode, n1) and (newNode, n, n2);
+      return the id of the newly created node newNode **/
+  virtual int vertex_split(int n, int n1, int n2);
+  virtual int vertex_split(int n, int n1, int n2, int e1, int e3);
+  
+  /// Perform a propagating bisection of element e1 according to marked nodes
+  /** Given and element e1, determined n, e1's marked node, and bisect
+      the edge opposite to n, propagating and bisecting the
+      neighboring element in a similar fashion **/
+  virtual int element_bisect(int e1);
+}
+
+#endif
