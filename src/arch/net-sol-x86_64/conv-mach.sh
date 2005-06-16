fdf38e9b664224c20f30818fd4c459fffabce5f2
Gengbin Zheng <gzheng@illinois.edu>
2005-06-16 18:24:28 +0000

    config files for SunOS on Opteron

---
+++ src/arch/net-sol-x86_64/conv-mach.sh
@@ -0,0 +1,14 @@
+CMK_DEFS=' '
+CMK_CPP_CHARM="/usr/ccs/lib/cpp $CMK_DEFS"
+CMK_CPP_C="gcc -E $CMK_DEFS"
+CMK_CC="cc -xarch=amd64 $CMK_DEFS"
+CMK_CXX="CC -xarch=amd64 $CMK_DEFS "
+CMK_CXXPP="CC -xarch=amd64 -E $CMK_DEFS "
+CMK_CF77='f77'
+CMK_CF90='f90'
+CMK_RANLIB='true'
+CMK_LIBS=' -lnsl -lsocket -lckqt'
+CMK_NATIVE_LIBS=' -lnsl -lsocket'
+CMK_XIOPTS=''
+
+CMK_QT='generic64'
