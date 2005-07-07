0952df3e6f017cbf45b88d7a276a87a5187b4e0d
Gengbin Zheng <gzheng@illinois.edu>
2005-07-07 20:08:51 +0000

    minor change to fix the hardcoded f90 libdir.

--- src/arch/common/conv-mach-ifort.sh
+++ src/arch/common/conv-mach-ifort.sh
@@ -1,13 +1,18 @@
 # for Intel Fortran compiler 8.0 and higher which is renamed to ifort from ifc
-CMK_CF77='ifort -auto '
-CMK_CF90='ifort -auto '
+CMK_CF77="ifort -auto "
+CMK_CF90="ifort -auto "
 CMK_CF90_FIXED="$CMK_CF90 -132 -FI "
-CMK_F90LIBS="-L/opt/intel_fc_80/lib/ -lifcore -lifport "
-CMK_F77LIBS=$CMK_F90LIBS
-# for_main.o is important for main() in f90 code
 F90DIR=`which ifort 2> /dev/null`
-test -x "$F90DIR" && F90MAIN="`dirname $F90DIR`/../lib/for_main.o"
+if test -x "$F90DIR" 
+then
+  F90LIBDIR="`dirname $F90DIR`/../lib"
+  F90MAIN="$F90LIBDIR/for_main.o"
+fi
+# for_main.o is important for main() in f90 code
 CMK_F90MAINLIBS="$F90MAIN "
+CMK_F90LIBS="-L$F90LIBDIR -lifcore -lifport "
+CMK_F77LIBS="$CMK_F90LIBS"
+
 CMK_MOD_NAME_ALLCAPS=
 CMK_MOD_EXT="mod"
 CMK_F90_USE_MODDIR=""
