94822488b02678fc3fc655452199fb92b9004bc5
Isaac Dooley <idooley2@illinois.edu>
2005-07-09 16:29:01 +0000

    Adding one more assumption to the comments.

--- src/libs/ck-libs/fem/fem_mesh_modify.h
+++ src/libs/ck-libs/fem/fem_mesh_modify.h
@@ -9,7 +9,11 @@ The mesh must be in a consistant state before and after these operations:
     - Any shared node must be in the IDXL table for both the local and 
       other chunks.
     - Exactly one ghost layer exists around all chunks. See definition below
+    - All adjacency tables must be correct before any of these calls. 
+      The calls will maintain the adjacency tables, both remotely
+      and locally.
     - FEM_add_element can only be called with a set of existing local or shared nodes
+
 
 A ghost element is one that is adjacent to at least one shared node. A ghost node is any node adjacent to a ghost element, but is itself not a shared node. Thus we have exactly one layer of ghosts.
 
