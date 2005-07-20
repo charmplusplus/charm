1b99c340ef4199b859975b61d10a905325c116e1
Isaac Dooley <idooley2@illinois.edu>
2005-07-20 02:07:15 +0000

    Added some more default parameters.

--- src/libs/ck-libs/fem/fem_mesh_modify.h
+++ src/libs/ck-libs/fem/fem_mesh_modify.h
@@ -35,11 +35,11 @@ extern CProxy_femMeshModify meshMod;
 
 
 // The internal functions which take in a FEM_Mesh*
-int FEM_add_node(FEM_Mesh *m, int* adjacent_nodes, int num_adjacent_nodes, int upcall);
+int FEM_add_node(FEM_Mesh *m, int* adjacent_nodes=0, int num_adjacent_nodes=0, int upcall=0);
 void FEM_remove_node(FEM_Mesh *m, int node);
-void FEM_remove_element(FEM_Mesh *m, int element, int elem_type);
-int FEM_add_element(FEM_Mesh *m, int* conn, int conn_size, int elem_type);
-void FEM_Modify_Lock(FEM_Mesh *m, int* affectedNodes, int numAffectedNodes, int* affectedElts, int numAffectedElts, int elemtype=0);
+void FEM_remove_element(FEM_Mesh *m, int element, int elem_type=0);
+int FEM_add_element(FEM_Mesh *m, int* conn, int conn_size, int elem_type=0);
+void FEM_Modify_Lock(FEM_Mesh *m, int* affectedNodes=0, int numAffectedNodes=0, int* affectedElts=0, int numAffectedElts=0, int elemtype=0);
 void FEM_Modify_Unlock(FEM_Mesh *m);
 
   
