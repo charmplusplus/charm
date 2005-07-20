24df5396099720eb11374a4de8ade0e67d1ecee6
Gengbin Zheng <gzheng@illinois.edu>
2005-07-20 21:19:18 +0000

    follow link to determine fortran lib correctly.

--- src/arch/common/conv-mach-ifort.sh
+++ src/arch/common/conv-mach-ifort.sh
@@ -1,8 +1,13 @@
 # for Intel Fortran compiler 8.0 and higher which is renamed to ifort from ifc
+# does not work for ifc 7.0
 CMK_CF77="ifort -auto "
 CMK_CF90="ifort -auto "
 CMK_CF90_FIXED="$CMK_CF90 -132 -FI "
 F90DIR=`which ifort 2> /dev/null`
+if test -h "$F90DIR"
+then
+  F90DIR=`readlink $F90DIR`
+fi
 if test -x "$F90DIR" 
 then
   F90LIBDIR="`dirname $F90DIR`/../lib"
