3f392a30e6cda44cea16a0332eb0f8f92146b31d
Gengbin Zheng <gzheng@illinois.edu>
2005-08-31 17:27:29 +0000

    updated.

--- src/arch/mpi-darwin-ppc/cc-xlc.sh
+++ src/arch/mpi-darwin-ppc/cc-xlc.sh
@@ -1,20 +1,24 @@
-CMK_CC='xlc_r '
-CMK_CXX='xlc++_r -qstaticinline '
-CMK_LD="$CMK_CC -brtl "
-CMK_LDXX="$CMK_CXX -brtl "
+CMK_CC="xlc_r -qnocommon -qpic "
+#CMK_CXX="xlc++_r -qstaticinline "
+CMK_CXX="xlc++_r -qstaticinline -qnocommon -qpic "
+CMK_LD="$CMK_CC -qnofullpath "
+CMK_LDXX="$CMK_CXX -qnofullpath "
+CMK_C_OPTIMIZE="-O3 -qstrict -Q  "
+CMK_CXX_OPTIMIZE="-O3 -qstrict -Q "
 OPTS_CC=
 OPTS_CXX=
 OPTS_LD=
 OPTS_LDXX=
-CMK_NATIVE_CC='xlc_r '
-CMK_NATIVE_LD='xlc++_r '
-CMK_NATIVE_CXX='xlc++_r -qstaticinline '
-CMK_NATIVE_LDXX='xlc++_r '
-CMK_CF77='xlf_r  '
-CMK_CF90='xlf90_r  -qsuffix=f=f90' 
-CMK_CF90_FIXED='xlf90_r ' 
-CMK_C_OPTIMIZE='-O3 -qstrict -Q  '
-CMK_CXX_OPTIMIZE='-O3 -qstrict -Q '
-CMK_AR='ar cq'
-CMK_NM='nm '
-CMK_QT='aix'
+
+CMK_NATIVE_CC="xlc_r "
+CMK_NATIVE_LD="xlc_r "
+CMK_NATIVE_CXX="xlc++_r -qstaticinline "
+CMK_NATIVE_LDXX="xlc++_r "
+CMK_NATIVE_LIBS="-lstdc++ "
+
+CMK_LD_SHARED=" -qmkshrobj -Wl,-dynamic -Wl,-undefined,dynamic_lookup "
+
+CMK_AR="ar cq"
+CMK_NM="nm "
+CMK_QT=aix
+
