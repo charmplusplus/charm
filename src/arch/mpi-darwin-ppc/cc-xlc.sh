f62e31ff290793a3178083241609482b99435fec
Gengbin Zheng <gzheng@illinois.edu>
2005-08-31 16:40:39 +0000

    Mac specific conf file for xlc++.

---
+++ src/arch/mpi-darwin-ppc/cc-xlc.sh
@@ -0,0 +1,20 @@
+CMK_CC='xlc_r '
+CMK_CXX='xlc++_r -qstaticinline '
+CMK_LD="$CMK_CC -brtl "
+CMK_LDXX="$CMK_CXX -brtl "
+OPTS_CC=
+OPTS_CXX=
+OPTS_LD=
+OPTS_LDXX=
+CMK_NATIVE_CC='xlc_r '
+CMK_NATIVE_LD='xlc++_r '
+CMK_NATIVE_CXX='xlc++_r -qstaticinline '
+CMK_NATIVE_LDXX='xlc++_r '
+CMK_CF77='xlf_r  '
+CMK_CF90='xlf90_r  -qsuffix=f=f90' 
+CMK_CF90_FIXED='xlf90_r ' 
+CMK_C_OPTIMIZE='-O3 -qstrict -Q  '
+CMK_CXX_OPTIMIZE='-O3 -qstrict -Q '
+CMK_AR='ar cq'
+CMK_NM='nm '
+CMK_QT='aix'
