f922528c3c06b9a260efec19a6603b442f6b589a
Gengbin Zheng <gzheng@illinois.edu>
2005-10-09 19:47:48 +0000

    set default f90 compile in conv-mach.sh as xlf90

--- src/arch/mpi-bluegenel/cc-xlc.sh
+++ src/arch/mpi-bluegenel/cc-xlc.sh
@@ -1,21 +1,20 @@
 XLC_PRE=/opt/ibmcmp
 XLC_POST=bin/blrts_
-XLC_F=$XLC_PRE/xlf/9.1
 CMK_CC="$XLC_PRE/vac/7.0/${XLC_POST}xlc -qcpluscmt "
 CMK_CXX="$XLC_PRE/vacpp/7.0/${XLC_POST}xlC "
 CMK_LD="$CMK_CC  "
 CMK_LDXX="$CMK_CXX  "
-CMK_CF77="$XLC_F/${XLC_POST}xlf "
-CMK_CF90="$XLC_F/${XLC_POST}xlf90  -qsuffix=f=f90" 
-CMK_CF90_FIXED="$XLC_PRE/xlf/8.1/${XLC_POST}xlf90 " 
 CMK_C_OPTIMIZE='-O3 -qstrict -Q  '
 CMK_CXX_OPTIMIZE='-O3 -qstrict -Q '
-CMK_AR='ar cq'
-CMK_NM='nm '
 CMK_QT="aix"
+
 CMK_NATIVE_CC="xlc"
 CMK_NATIVE_CXX="xlC"
 CMK_NATIVE_LD="$CMK_NATIVE_CC"
 CMK_NATIVE_LDXX="$CMK_NATIVE_CXX"
-CMK_RANLIB="ranlib"
+
+XLC_F=$XLC_PRE/xlf/9.1
+CMK_CF77="$XLC_F/${XLC_POST}xlf "
+CMK_CF90="$XLC_F/${XLC_POST}xlf90  -qsuffix=f=f90" 
+CMK_CF90_FIXED="$XLC_PRE/xlf/8.1/${XLC_POST}xlf90 " 
 CMK_F90LIBS="-L$XLC_F/blrts_lib -lxlf90 -lxlopt -lxl -lxlfmath"
--- src/arch/mpi-bluegenel/conv-mach.sh
+++ src/arch/mpi-bluegenel/conv-mach.sh
@@ -32,26 +32,38 @@ CMK_CPP_C="$BGL_BIN/powerpc-bgl-blrts-gnu-cpp -E "
 CMK_CC="$BGL_BIN/powerpc-bgl-blrts-gnu-gcc $GCC_OPTS "
 CMK_CXX="$BGL_BIN/powerpc-bgl-blrts-gnu-g++ $GCC_OPTS "
 CMK_CXXPP="$BGL_BIN/powerpc-bgl-blrts-gnu-g++ -E "
-CMK_CF77="$BGL_BIN/powerpc-bgl-blrts-gnu-g77 "
-CMK_CF90='f90'
-CMK_RANLIB="$BGL_BIN/powerpc-bgl-blrts-gnu-ranlib "
-CMK_AR="$BGL_BIN/powerpc-bgl-blrts-gnu-ar q "
 CMK_LD="$CMK_CC "
 CMK_LDXX="$CMK_CXX "
 CMK_LIBS='-lckqt -lmpich.rts -lmsglayer.rts -lrts.rts -ldevices.rts -lrts.rts'
 CMK_LD_LIBRARY_PATH="-Wl,-rpath,$CHARMLIBSO/"
+
+CMK_RANLIB="$BGL_BIN/powerpc-bgl-blrts-gnu-ranlib "
+CMK_AR="$BGL_BIN/powerpc-bgl-blrts-gnu-ar q "
+CMK_NM="$BGL_BIN/powerpc-bgl-blrts-gnu-nm"
+
 #CMK_SEQ_LIBS=''
 #CMK_SEQ_CC="$BGL_BIN/powerpc-bgl-blrts-gnu-gcc -Wno-deprecated "
 #CMK_SEQ_LD="$CMK_SEQ_CC"
 #CMK_SEQ_CXX="$BGL_BIN/powerpc-bgl-blrts-gnu-g++ -Wno-deprecated "
 #CMK_SEQ_LDXX="$CMK_SEQ_CXX"
+
+# native compiler
 CMK_NATIVE_CC='gcc '
 CMK_NATIVE_LD='gcc '
 CMK_NATIVE_CXX='g++ -Wno-deprecated '
 CMK_NATIVE_LDXX='g++'
-CMK_F90LIBS='-L/usr/absoft/lib -L/opt/absoft/lib -lf90math -lfio -lU77 -lf77math '
+
+CMK_QT="generic"
+
+# fortran
+#CMK_CF77="$BGL_BIN/powerpc-bgl-blrts-gnu-g77 "
+#CMK_F90LIBS='-L/usr/absoft/lib -L/opt/absoft/lib -lf90math -lfio -lU77 -lf77math '
+XLC_F=$XLC_PRE/xlf/9.1
+CMK_CF77="$XLC_F/${XLC_POST}xlf "
+CMK_CF90="$XLC_F/${XLC_POST}xlf90  -qsuffix=f=f90" 
+CMK_CF90_FIXED="$XLC_PRE/xlf/8.1/${XLC_POST}xlf90 " 
+CMK_F90LIBS="-L$XLC_F/blrts_lib -lxlf90 -lxlopt -lxl -lxlfmath"
 CMK_MOD_NAME_ALLCAPS=1
 CMK_MOD_EXT="mod"
 CMK_F90_USE_MODDIR=1
 CMK_F90_MODINC="-p"
-CMK_QT="generic"
