492b91bc58a4fcf7dfadbfbe5598796a8a54600c
Gengbin Zheng <gzheng@illinois.edu>
2005-10-25 01:53:08 +0000

    switched to ifort as default fortran compile for icc option. To use old ifc, use option "ifc".

--- src/arch/common/cc-icc.sh
+++ src/arch/common/cc-icc.sh
@@ -13,14 +13,13 @@ CMK_NATIVE_LDXX="$CMK_LDXX"
 # for absoft?
 #CMK_F90LIBS='-L/usr/local/intel/compiler70/ia32/lib -L/opt/intel/compiler70/ia32/lib -lintrins -lIEPCF90 -lPEPCF90 -lF90 -lintrins -limf '
 #CMK_MOD_NAME_ALLCAPS=1
-#CMK_MOD_EXT="mod"
-#CMK_F90_USE_MODDIR=""
 
-# for Intel Fortran compiler ifc
-CMK_CF77="ifc -auto "
-CMK_CF90="ifc -auto "
+# for Intel Fortran compiler 8.0 and higher which is renamed to ifort from ifc
+# does not work for ifc 7.0
+CMK_CF77="ifort -auto "
+CMK_CF90="ifort -auto "
 CMK_CF90_FIXED="$CMK_CF90 -132 -FI "
-F90DIR=`which ifc 2> /dev/null`
+F90DIR=`which ifort 2> /dev/null`
 if test -h "$F90DIR"
 then
   F90DIR=`readlink $F90DIR`
@@ -29,16 +28,12 @@ if test -x "$F90DIR"
 then
   F90LIBDIR="`dirname $F90DIR`/../lib"
   F90MAIN="$F90LIBDIR/for_main.o"
-  if test -f $F90LIBDIR/libifcore.a
-  then
-    CMK_F90LIBS="-L$F90LIBDIR -lifcore -lifport "
-  else
-    CMK_F90LIBS="-L$F90LIBDIR -lintrins -lIEPCF90 -lPEPCF90 -lF90 -lintrins -limf "
-  fi
 fi
 # for_main.o is important for main() in f90 code
 CMK_F90MAINLIBS="$F90MAIN "
+CMK_F90LIBS="-L$F90LIBDIR -lifcore -lifport "
 CMK_F77LIBS="$CMK_F90LIBS"
-CMK_MOD_NAME_ALLCAPS=1
+
+CMK_MOD_NAME_ALLCAPS=
 CMK_MOD_EXT="mod"
 CMK_F90_USE_MODDIR=""
