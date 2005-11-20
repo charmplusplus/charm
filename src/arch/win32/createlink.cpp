2c431415f55f09bd2f439383ae862937dd518e0b
Gengbin Zheng <gzheng@illinois.edu>
2005-11-20 21:16:10 +0000

    a simple program to create windows hard link, which makes it easy to keep track of changes made under tmp/.

---
+++ src/arch/win32/createlink.cpp
@@ -0,0 +1,28 @@
+/*
+unix2nt_cc  -c createlink.cpp -o createlink.o -D_WIN32_WINNT=0x0500
+unix2nt_cc createlink.o
+*/
+
+#include <windows.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  if (argc < 2) {
+    printf("%s srcFile destFile\n", argv[0]);
+    exit(1);
+  }
+  char *src = argv[1];
+  char *dest = argv[2];
+  bool fCreatedLink  = CreateHardLink(dest, src, NULL);
+  if (!fCreatedLink) {
+    int err=GetLastError();
+    printf("CreateHardLink failed errno=%d, WSAerr=%d\n",errno, err);
+    return 1;
+  }
+  else {
+    exit(0);
+  }
+  return 0;
+}
+
