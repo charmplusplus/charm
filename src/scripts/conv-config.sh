b1f8724b2cdadd990b085786bf07125a311c807b
Gengbin Zheng <gzheng@illinois.edu>
2005-11-25 03:56:30 +0000

    define CMK_FPP for preprocessing.

--- src/scripts/conv-config.sh
+++ src/scripts/conv-config.sh
@@ -41,6 +41,7 @@ CMK_LD_SHARED="-shared"
 [ -z "$CMK_NM" ] && CMK_NM='nm '
 [ -z "$CMK_SHARED_SUF" ] && CMK_SHARED_SUF='so'
 
+[ -z "$CMK_FPP" ] && CMK_FPP="$CMK_CF90"
 [ -z "$CMK_CF90_FIXED" ] && CMK_CF90_FIXED="$CMK_CF90"
 [ -z "$CMK_CC_RELIABLE" ] && CMK_CC_RELIABLE="$CMK_CC"
 [ -z "$CMK_CC_FASTEST" ] && CMK_CC_FASTEST="$CMK_CC"
