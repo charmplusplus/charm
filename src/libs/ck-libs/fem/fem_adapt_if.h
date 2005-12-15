b5363e81b0f3ed9fabecb591e4c7c13e50ba1106
Aaron Becker <abecker3@illinois.edu>
2005-12-15 08:29:54 +0000

    Changed functions to inline to avoid linker errors on OSX. Added includes to fix more compile bugs.

--- src/libs/ck-libs/fem/fem_adapt_if.h
+++ src/libs/ck-libs/fem/fem_adapt_if.h
@@ -2,10 +2,12 @@
 #define __CHARM_FEM_ADAPT_IF_H
 
 #include "charm-api.h"
+#include "fem_mesh_modify.h"
+#include "fem_adapt_algs.h"
 
 extern void _registerFEMMeshModify(void);
 
-void FEM_ADAPT_Init(int meshID) {
+inline void FEM_ADAPT_Init(int meshID) {
   const int triangleFaces[6] = {0,1,1,2,2,0};
   FEM_Add_elem2face_tuples(meshID, 0, 2, 3, triangleFaces);
   FEM_Mesh_create_elem_elem_adjacency(meshID);
@@ -18,75 +20,75 @@ void FEM_ADAPT_Init(int meshID) {
   CtvAccess(_adaptAlgs) = meshP->getfmMM()->getfmAdaptAlgs();
   CtvAccess(_adaptAlgs)->FEM_Adapt_Algs_Init(FEM_DATA+0, FEM_BOUNDARY);
 }
-FDECL void FTN_NAME(FEM_ADAPT_INIT,fem_adapt_init)(int *meshID)
+FDECL inline void FTN_NAME(FEM_ADAPT_INIT,fem_adapt_init)(int *meshID)
 {
   FEM_ADAPT_Init(*meshID);
 }
 
 
-void FEM_ADAPT_Refine(int meshID, int qm, int method, double factor,
+inline void FEM_ADAPT_Refine(int meshID, int qm, int method, double factor,
         double *sizes) {
     FEM_Mesh* mesh = FEM_Mesh_lookup(meshID, "FEM_ADAPT_Init");
     mesh->getfmMM()->getfmAdaptAlgs()->FEM_Refine(qm, method, factor, sizes);
 }
-FDECL void FTN_NAME(FEM_ADAPT_REFINE,fem_adapt_refine)(int* meshID, 
+FDECL inline void FTN_NAME(FEM_ADAPT_REFINE,fem_adapt_refine)(int* meshID, 
         int *qm, int *method, double *factor, double *sizes)
 {
   FEM_ADAPT_Refine(*meshID, *qm, *method, *factor, sizes);
 }
 
 
-void FEM_ADAPT_Coarsen(int meshID, int qm, int method, double factor, 
+inline void FEM_ADAPT_Coarsen(int meshID, int qm, int method, double factor, 
         double *sizes) {
     FEM_Mesh* mesh = FEM_Mesh_lookup(meshID, "FEM_ADAPT_Init");
     mesh->getfmMM()->getfmAdaptAlgs()->FEM_Coarsen(qm, method, factor, sizes);
 }
-FDECL void FTN_NAME(FEM_ADAPT_COARSEN,fem_adapt_coarsen)(int* meshID, 
+FDECL inline void FTN_NAME(FEM_ADAPT_COARSEN,fem_adapt_coarsen)(int* meshID, 
         int *qm, int *method, double *factor, double *sizes)
 {
   FEM_ADAPT_Coarsen(*meshID, *qm, *method, *factor, sizes);
 }
 
 
-void FEM_ADAPT_SetElementSizeField(int meshID, int elem, double size) {
+inline void FEM_ADAPT_SetElementSizeField(int meshID, int elem, double size) {
   FEM_Mesh *meshP = FEM_Mesh_lookup(meshID, "FEM_ADAPT_Init");
   meshP->elem[0].setMeshSizing(elem, size);
 }
-FDECL void FTN_NAME(FEM_ADAPT_SETELEMENTSIZEFIELD,fem_adapt_setelementsizefield)(int *meshID, int *elem, double *size)
+FDECL inline void FTN_NAME(FEM_ADAPT_SETELEMENTSIZEFIELD,fem_adapt_setelementsizefield)(int *meshID, int *elem, double *size)
 {
   FEM_ADAPT_SetElementSizeField(*meshID, *elem, *size);
 }
 
 
-void FEM_ADAPT_SetElementsSizeField(int meshID, double *sizes) {
+inline void FEM_ADAPT_SetElementsSizeField(int meshID, double *sizes) {
   FEM_Mesh *meshP = FEM_Mesh_lookup(meshID, "FEM_ADAPT_Init");
   int numElements = meshP->elem[0].size();
   for (int i=0; i<numElements; i++) {
     meshP->elem[0].setMeshSizing(i, sizes[i]);
   }
 }
-FDECL void FTN_NAME(FEM_ADAPT_SETELEMENTSSIZEFIELD,fem_adapt_setelementssizefield)(int *meshID, double *sizes)
+FDECL inline void FTN_NAME(FEM_ADAPT_SETELEMENTSSIZEFIELD,fem_adapt_setelementssizefield)(int *meshID, double *sizes)
 {
   FEM_ADAPT_SetElementsSizeField(*meshID, sizes);
 }
 
 
-void FEM_ADAPT_SetReferenceMesh(int meshID) {
+inline void FEM_ADAPT_SetReferenceMesh(int meshID) {
     FEM_Mesh* mesh = FEM_Mesh_lookup(meshID, "FEM_ADAPT_Init");
     mesh->getfmMM()->getfmAdaptAlgs()->SetReferenceMesh();
 }
-FDECL void FTN_NAME(FEM_ADAPT_SETREFERENCEMESH, fem_adapt_setreferencemesh)(int* meshID)
+FDECL inline void FTN_NAME(FEM_ADAPT_SETREFERENCEMESH, fem_adapt_setreferencemesh)(int* meshID)
 {
     FEM_ADAPT_SetReferenceMesh(*meshID);
 }
 
 
-void FEM_ADAPT_GradateMesh(int meshID, double smoothness)
+void inline FEM_ADAPT_GradateMesh(int meshID, double smoothness)
 {
     FEM_Mesh* mesh = FEM_Mesh_lookup(meshID, "FEM_ADAPT_Init");
     mesh->getfmMM()->getfmAdaptAlgs()->GradateMesh(smoothness);
 }
-FDECL void FTN_NAME(FEM_ADAPT_GRADATEMESH, fem_adapt_gradatemesh)(int* meshID, double* smoothness)
+FDECL inline void FTN_NAME(FEM_ADAPT_GRADATEMESH, fem_adapt_gradatemesh)(int* meshID, double* smoothness)
 {
     FEM_ADAPT_GradateMesh(*meshID, *smoothness);
 }
