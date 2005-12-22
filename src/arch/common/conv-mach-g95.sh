cf20c74b158e4fbfcd753575f5de01ca47397743
Gengbin Zheng <gzheng@illinois.edu>
2005-12-22 17:21:11 +0000

    added -fdollar-ok to allow $ in fortran var name

--- src/arch/common/conv-mach-g95.sh
+++ src/arch/common/conv-mach-g95.sh
@@ -4,7 +4,7 @@ CMK_CC="$CMK_CC -DCMK_G95"
 CMK_CXX="$CMK_CXX -DCMK_G95"
 CMK_CF90=`which g95 2>/dev/null`
 CMK_FPP="/lib/cpp -P -CC"
-CMK_CF90="$CMK_CF90 -fpic"
+CMK_CF90="$CMK_CF90 -fpic -fdollar-ok"
 CMK_CF90_FIXED="$CMK_CF90 -ffixed-form "
 # find f90 librarya:  
 #it can be at g95-install/lib/gcc-lib/i686-pc-linux-gnu/4.0.1
