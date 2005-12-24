ca021d1df114af1da7523351e6a5223c489c9338
Gengbin Zheng <gzheng@illinois.edu>
2005-12-24 16:55:59 +0000

    stub calls for g95 to initialize fortran runtime when is it C main.

--- src/ck-core/main.C
+++ src/ck-core/main.C
@@ -7,10 +7,22 @@
 
 #include "ck.h"
 
+#ifdef CMK_G95
+extern "C" void g95_runtime_start(int argc, char *argv[]);
+extern "C" void g95_runtime_stop();
+#endif
+
 #ifndef __BLUEGENE__
 int main(int argc, char **argv)
 {
+#ifdef CMK_G95
+  g95_runtime_start(argc, argv);
+#endif
   ConverseInit(argc, argv, (CmiStartFn) _initCharm, 0, 0);
+#ifdef CMK_G95
+    // FIXME:  not right place to call, but not calling it does not quite hurt
+  g95_runtime_stop();
+#endif
   return 0;
 }
 #endif
