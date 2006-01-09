4c03fd2e9f184585473206b313efca083feb8a09
Gengbin Zheng <gzheng@illinois.edu>
2006-01-09 05:15:05 +0000

    updated for supporting 64 bit.

--- src/arch/common/conv-mach-g95.sh
+++ src/arch/common/conv-mach-g95.sh
@@ -1,22 +1,36 @@
-# GNU f95
+# g95.org
 
 CMK_CC="$CMK_CC -DCMK_G95"
 CMK_CXX="$CMK_CXX -DCMK_G95"
 
+if test -n "$CMK_MACOSX64" 
+then
+CMK_F90FLAGS="$CMK_F90FLAGS -m64"
+fi
+
+if test -n "$CMK_MACOSX"
+then
+CMK_F90FLAGS="$CMK_F90FLAGS -fno-common"
+fi
+
 CMK_CF90=`which g95 2>/dev/null`
 CMK_FPP="/lib/cpp -P -CC"
-CMK_CF90="$CMK_CF90 -fpic -fno-second-underscore -fdollar-ok"
+CMK_CF90="$CMK_CF90 $CMK_F90FLAGS -fPIC -fno-second-underscore -fdollar-ok"
 CMK_CF90_FIXED="$CMK_CF90 -ffixed-form "
-# find f90 librarya:  
+# find f90 library:
 #it can be at g95-install/lib/gcc-lib/i686-pc-linux-gnu/4.0.1
 F90DIR=`which g95 2> /dev/null`
-if test -h "$F90DIR"
+#F90DIR=$HOME/g95-install/bin/g95
+readlinkcmd=`which readlink 2> /dev/null`
+if test -h "$F90DIR" && test -x "$readlinkcmd"
 then
   F90DIR=`readlink $F90DIR`
+  test `basename $F90DIR` = "$F90DIR" && F90DIR=`which g95 2> /dev/null`
 fi
-F90LIBDIR="`dirname $F90DIR`/../lib/*/*/*"
-F90LIBDIR=`cd $F90LIBDIR && pwd`
-CMK_F90LIBS="-L$F90LIBDIR -lf95 "
+F90DIR="`dirname $F90DIR`"
+F90LIBDIR=`cd $F90DIR/../lib/gcc-lib/*/*; pwd`
+CMK_F90LIBS="-L$F90LIBDIR -lf95 -lgcc_eh"
+
 CMK_MOD_NAME_ALLCAPS=
 CMK_MOD_EXT="mod"
 CMK_F90_USE_MODDIR=1
