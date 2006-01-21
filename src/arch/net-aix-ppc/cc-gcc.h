8a56e3fd2e2ac575741c407ad7f75448f34a5c27
Gengbin Zheng <gzheng@illinois.edu>
2006-01-21 09:10:05 +0000

    for gcc 32 bit.

---
+++ src/arch/net-aix-ppc/cc-gcc.h
@@ -0,0 +1,6 @@
+#undef CMK_DLL_CC
+
+#undef  CMK_COMPILEMODE_ORIG
+#undef  CMK_COMPILEMODE_ANSI
+#define CMK_COMPILEMODE_ORIG                               1
+#define CMK_COMPILEMODE_ANSI                               0
