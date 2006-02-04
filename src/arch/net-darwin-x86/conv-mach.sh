56e3dc66ba19d70bd477531c311264fd75b173b8
Chao Mei <chaomei2@illinois.edu>
2006-02-04 22:47:50 +0000

    Configuration files for building charm on the Intel-iMac machine

---
+++ src/arch/net-darwin-x86/conv-mach.sh
@@ -0,0 +1,26 @@
+CMK_MACOSX=1
+
+CMK_CPP_CHARM="/usr/bin/cpp -P"
+CMK_CPP_C="gcc -E"
+CMK_CC="gcc -Wno-long-double -fPIC -dynamic -fno-common "
+CMK_CXX="g++ -Wno-long-double -fPIC -dynamic -fno-common "
+CMK_CXXPP="g++ -x g++ -E "
+CMK_LDXX="$CMK_CXX -multiply_defined suppress "
+CMK_XIOPTS=""
+CMK_QT="generic-light"
+CMK_LIBS="-lckqt"
+CMK_RANLIB="ranlib"
+
+# Assumes GNU fortran compiler:
+CMK_CF77="g77"
+CMK_CF90="g90"
+
+# setting for shared lib
+# need -lstdc++ for c++ reference, and it needs to be put at very last 
+# of command line.
+# Mac environment varaible
+test -z "$MACOSX_DEPLOYMENT_TARGET" && export MACOSX_DEPLOYMENT_TARGET=10.3
+CMK_SHARED_SUF="dylib"
+CMK_LD_SHARED=" -dynamic -dynamiclib -undefined dynamic_lookup "
+CMK_LD_SHARED_LIBS="-lstdc++"
+CMK_LD_SHARED_ABSOLUTE_PATH=true
