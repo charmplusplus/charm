9e2fcb746fa63af061bc067034e986c511a4d531
Gengbin Zheng <gzheng@illinois.edu>
2006-02-07 08:54:45 +0000

    updated path to mx lib

--- src/arch/net-darwin-ppc/conv-mach-mx.sh
+++ src/arch/net-darwin-ppc/conv-mach-mx.sh
@@ -2,10 +2,19 @@
 #guess where the myriexpress.h is installed
 if test -z "$CMK_INCDIR"
 then
-  if test -f /turing/software/mx/include/myriexpress.h
+  if test -f /turing/software/TIGER/mx/include/myriexpress.h
+  then
+    CMK_INCDIR="-I/turing/software/TIGER/mx/include"
+    CMK_LIBDIR="-L/turing/software/TIGER/mx/lib"
+  elif test -f /turing/software/mx/include/myriexpress.h
   then
     CMK_INCDIR="-I/turing/software/mx/include"
     CMK_LIBDIR="-L/turing/software/mx/lib"
+  else
+    echo
+    echo "Please specify path to mx installation in conv-mach-mx.sh!"
+    echo
+    exit 1
   fi
 fi
 
