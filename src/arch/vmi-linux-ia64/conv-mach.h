c9055ab1a2e7b1fc6a8ceb5c7536dff77c157614
Greg Koenig <koenig@uiuc.edu>
2006-03-05 20:13:24 +0000

    Modify vmi-linux to use Quickthreads on IA-32. conv-mach.h must define Context threads for IA-64 (I think).

--- src/arch/vmi-linux-ia64/conv-mach.h
+++ src/arch/vmi-linux-ia64/conv-mach.h
@@ -5,4 +5,11 @@
 
 #define CMK_MEMORY_PAGESIZE                                16384
 
+#define CMK_THREADS_USE_CONTEXT                            1
+#define CMK_THREADS_COPY_STACK                             0
+#define CMK_THREADS_USE_PTHREADS                           0
+#define CMK_THREADS_ARE_WIN32_FIBERS                       0
+
+#define CMK_THREADS_REQUIRE_NO_CPV                         0
+
 #endif
