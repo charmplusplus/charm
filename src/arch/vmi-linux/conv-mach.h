57423e8badc09802296ac252d228dc8bafdddcfb
Greg Koenig <koenig@uiuc.edu>
2006-03-05 20:11:39 +0000

    Modify vmi-linux to use Quickthreads on IA-32.

--- src/arch/vmi-linux/conv-mach.h
+++ src/arch/vmi-linux/conv-mach.h
@@ -3,4 +3,11 @@
 
 #define CMK_MEMORY_PAGESIZE                                8192
 
+#define CMK_THREADS_USE_CONTEXT                            0
+#define CMK_THREADS_COPY_STACK                             0
+#define CMK_THREADS_USE_PTHREADS                           0
+#define CMK_THREADS_ARE_WIN32_FIBERS                       0
+
+#define CMK_THREADS_REQUIRE_NO_CPV                         0
+
 #endif
