817611d596203c57d16c4a7b008020371d141659
Gengbin Zheng <gzheng@illinois.edu>
2006-03-31 18:54:10 +0000

    let autoconfig decide fortran mod name ext

--- src/arch/net-linux/conv-mach.sh
+++ src/arch/net-linux/conv-mach.sh
@@ -21,12 +21,10 @@ CMK_CF90=`which f90 2>/dev/null`
 if test -n "$CMK_CF90"
 then
 # absoft
-CMK_CF90_FIXED="$CMK_CF90 -W132 "
-CMK_F90LIBS="-L/usr/absoft/lib -L/opt/absoft/lib -lf90math -lfio -lU77 -lf77math "
-CMK_MOD_NAME_ALLCAPS=1
-CMK_MOD_EXT="mod"
-CMK_F90_USE_MODDIR=1
-CMK_F90_MODINC="-p"
+  CMK_CF90_FIXED="$CMK_CF90 -W132 "
+  CMK_F90LIBS="-L/usr/absoft/lib -L/opt/absoft/lib -lf90math -lfio -lU77 -lf77math "
+  CMK_F90_USE_MODDIR=1
+  CMK_F90_MODINC="-p"
 else
 # gnu f95
   CMK_CF90=`which f95 2>/dev/null`
@@ -36,8 +34,6 @@ else
     CMK_CF90="$CMK_CF90 -fpic -fautomatic -fdollar-ok "
     CMK_CF90_FIXED="$CMK_CF90 -ffixed-form "
     CMK_F90LIBS="-lgfortran "
-    CMK_MOD_NAME_ALLCAPS=
-    CMK_MOD_EXT="mod"
     CMK_F90_USE_MODDIR=1
     CMK_F90_MODINC="-I"
   fi
