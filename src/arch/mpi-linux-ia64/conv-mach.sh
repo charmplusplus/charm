a14867b046cfdde10eb180d83fb7cb40ada1a6d9
Gengbin Zheng <gzheng@illinois.edu>
2006-03-31 22:05:14 +0000

    remove manual definition of CMK_MOD_EXT.

--- src/arch/mpi-linux-ia64/conv-mach.sh
+++ src/arch/mpi-linux-ia64/conv-mach.sh
@@ -3,18 +3,18 @@ CMK_CPP_C='gcc -E'
 CMK_CC="mpicc $CMK_INCDIR"
 CMK_CXX="mpiCC $CMK_INCDIR"
 CMK_CXXPP="$CMK_CXX -E "
-CMK_CF77="mpif77"
-CMK_CF90="mpif90"
+CMK_XIOPTS=''
+CMK_RANLIB='ranlib'
+CMK_LIBS='-lckqt -lpmpich -lmpich'
+
 CMK_NATIVE_CC="gcc "
 CMK_NATIVE_LD="gcc "
 CMK_NATIVE_CXX="g++ "
 CMK_NATIVE_LDXX="g++ "
-CMK_RANLIB='ranlib'
-CMK_LIBS='-lckqt -lpmpich -lmpich'
-CMK_XIOPTS=''
+
+CMK_CF77="mpif77"
+CMK_CF90="mpif90"
 CMK_F90LIBS='-L/usr/absoft/lib -lf90math -lfio -lU77 -lf77math '
-CMK_MOD_NAME_ALLCAPS=1
-CMK_MOD_EXT="mod"
 CMK_F90_USE_MODDIR=1
 CMK_F90_MODINC="-p"
 
