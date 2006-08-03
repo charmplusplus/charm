10bcb53732416d71edb213036e53665901c058d7
Sayantan Chakravorty <sayantan_chak@yahoo.com>
2006-08-03 17:18:18 +0000

    Added ParFUM_desharing and ParFUM_deghosting methods to import.h and .C

--- src/libs/ck-libs/ParFUM/import.C
+++ src/libs/ck-libs/ParFUM/import.C
@@ -1,1 +1,12 @@
 #include "import.h"
+void ParFUM_desharing(int meshid){
+	FEM_Mesh 	*mesh = (FEM_chunk::get("ParFUM_desharing"))->getMesh("ParFUM_desharing");
+	mesh->clearSharedNodes();
+}
+
+
+void ParFUM_deghosting(int meshid){
+	FEM_Mesh 	*mesh = (FEM_chunk::get("ParFUM_desharing"))->getMesh("ParFUM_desharing");
+	mesh->clearGhostNodes();
+	mesh->clearGhostElems();
+}
--- src/libs/ck-libs/ParFUM/import.h
+++ src/libs/ck-libs/ParFUM/import.h
@@ -1,7 +1,9 @@
 #ifndef __PARFUM_IMPORT_H
 #define __PARFUM_IMPORT_H
 #include "ParFUM_internals.h"
-
+void ParFUM_desharing(int meshid);
+void ParFUM_deghosting(int meshid);
+void ParFUM_generateGlobalNodeNumbers(int fem_mesh);
 #endif
 
 
