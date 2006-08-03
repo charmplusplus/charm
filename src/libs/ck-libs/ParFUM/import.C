9a97735853862ed3901ee239a8b0bc9db0e7fd7f
Sayantan Chakravorty <sayantan_chak@yahoo.com>
2006-08-03 17:56:21 +0000

    minor compilation problems in import.C fixed

--- src/libs/ck-libs/ParFUM/import.C
+++ src/libs/ck-libs/ParFUM/import.C
@@ -43,22 +43,22 @@ void ParFUM_recreateSharedNodes(int meshid) {
   for (int i=0; i<rank; i++) {
     std::vector<int> remoteSharedNodes, localSharedNodes;
     double *recvNodeCoords;
-    MPI_Status *status;
+    MPI_Status status;
     int source, length;
     // Probe for a coordinate message from any source; extract source and msg length
-    MPI_Probe(MPI_ANY_SOURCE, coord_msg_tag, comm, status);
+    MPI_Probe(MPI_ANY_SOURCE, coord_msg_tag, comm, &status);
     source = status.MPI_SOURCE;
     length = status.MPI_LENGTH;
     // Receive whatever data was available according to probe
     recvNodeCoords = (double *)malloc(length*sizeof(double));
     MPI_Recv((void*)recvNodeCoords, length, MPI_DOUBLE, source, 
-	      coord_msg_tag, comm);
+	      coord_msg_tag, comm, &status);
     // Match coords between local nodes and received coords
     // FIX ME: this is the dumb super-slow brute force algorithm
     int recvNodeCount = length/3;
     for (int j=0; j<numNodes; j++) {
       for (int k=0; k<recvNodeCount; k++) {
-	if (coordEqual(nodeCoords[j*3], recvNodeCoords[k*3])) {
+	if (coordEqual(&nodeCoords[j*3], &recvNodeCoords[k*3])) {
 	  localSharedNodes.push_back(j); 
 	  remoteSharedNodes.push_back(k);
 	  break;
@@ -81,15 +81,15 @@ void ParFUM_recreateSharedNodes(int meshid) {
   }
   for (int i=rank+1; i<comm_size; i++) {  // recv shared node lists
     int *sharedNodes;
-    MPI_Status *status;
+    MPI_Status status;
     int source, length;
     // Probe for a shared node list from any source; extract source and msg length
-    MPI_Probe(MPI_ANY_SOURCE, sharedlist_msg_tag, comm, status);
+    MPI_Probe(MPI_ANY_SOURCE, sharedlist_msg_tag, comm, &status);
     source = status.MPI_SOURCE;
     length = status.MPI_LENGTH;
     // Recv the shared node list the probe revealed was available
     sharedNodes = (int *)malloc(length*sizeof(int));
-    MPI_Recv((void*)sharedNodes, length, MPI_INT, source, sharedlist_msg_tag, comm);
+    MPI_Recv((void*)sharedNodes, length, MPI_INT, source, sharedlist_msg_tag, comm, &status);
     // Store the shared node list in the data structure
     sharedNodeCounts[source] = length;
     sharedNodeLists[source] = sharedNodes;
