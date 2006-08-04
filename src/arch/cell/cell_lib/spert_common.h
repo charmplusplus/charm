59459c45c9cb8d20a048dd2a11cdb7ce71ef222e
David Kunzman <kunzman2@illinois.edu>
2006-08-04 19:21:36 +0000

    Removed the defines for the SPE offsets.

--- src/arch/cell/cell_lib/spert_common.h
+++ src/arch/cell/cell_lib/spert_common.h
@@ -39,32 +39,6 @@ extern void* calloc_aligned(size_t size, char alignment);
 extern void free_aligned(void* ptr);
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// Defines from bpa_map.def ////////////////////////////////////////////////////////////////////////////////
-
-#define BP_BASE 0x20000000000ULL
-
-#define DMA_LSA                 0x3004
-#define DMA_EA                  0x3008
-#define DMA_EA_HI               DMA_EA
-#define DMA_EA_LO               (DMA_EA_HI + 4)
-#define DMA_STBRC               0x3010  /* Addr for writing
-                                           64-bit packed data */
-#define DMA_Size                0x3010
-#define DMA_Tag                 0x3012
-#define DMA_BRCMD               0x3014  /* Addr for writing
-                                           32-bit packed cmd */
-#define DMA_RclassID            0x3015
-#define DMA_CMD                 0x3016
-#define DMA_CMDStatus           DMA_CMD
-#define DMA_QStatus             0x3104
-#define DMA_QueryType           0x3204
-#define DMA_QueryMask           0x321C
-#define DMA_TagStatus           0x322C
-#define DMA_TAGSTATUS_INTR_ANY  1L
-#define DMA_TAGSTATUS_INTR_ALL  2L
-
-
 #ifdef __cplusplus
 }
 #endif
