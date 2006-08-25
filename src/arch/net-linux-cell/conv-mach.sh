48cc602c4582c75707b0d7c4e633228f89cbbdeb
David Kunzman <kunzman2@illinois.edu>
2006-08-25 04:36:19 +0000

    Modified and added some settings.

--- src/arch/net-linux-cell/conv-mach.sh
+++ src/arch/net-linux-cell/conv-mach.sh
@@ -1,17 +1,25 @@
-CELL_SDK_DIR="$HOME/cellsim"
+CELL_SDK_DIR="/opt/IBM/cell-sdk-1.1"
 
 CMK_CPP_CHARM='/usr/lib/cpp'
 CMK_CPP_C='ppu32-gcc -E'
 CMK_CXXPP='ppu32-g++ -E'
-CMK_CC="ppu32-gcc -fPIC -w -I$CELL_SDK_DIR/sysroot/usr/include"
-CMK_CXX="ppu32-g++ -fPIC -w  -I$CELL_SDK_DIR/sysroot/usr/include"
-CMK_LD="$CMK_CC -L$CELL_SDK_DIR/sysroot/usr/lib "
-CMK_LDXX="$CMK_CXX -L$CELL_SDK_DIR/sysroot/usr/lib "
+CMK_CC="ppu32-gcc -fPIC -w -I$CELL_SDK_DIR/sysroot/usr/include -L$CELL_SDK_DIR/sysroot/usr/lib"
+CMK_CXX="ppu32-g++ -fPIC -w  -I$CELL_SDK_DIR/sysroot/usr/include -L$CELL_SDK_DIR/sysroot/usr/lib"
+CMK_LD="$CMK_CC -L$CELL_SDK_DIR/sysroot/usr/lib"
+CMK_LDXX="$CMK_CXX -L$CELL_SDK_DIR/sysroot/usr/lib"
 
 CMK_SEQ_CC=$CMK_CC
 CMK_SEQ_CXX=$CMK_CXX
 CMK_SEQ_LD=$CMK_LD
 CMK_SEQ_LDXX=$CMK_LDXX
+
+CMK_SPE_CC="spu-gcc -W -Wall -Winline -Wno-main -Wl,-N -I$CELL_SDK_DIR/sysroot/usr/spu/include -L$CELL_SDK_DIR/sysroot/usr/spu/lib"
+CMK_SPE_CXX="spu-g++ -W -Wall -Winline -Wno-main -Wl,-N -I$CELL_SDK_DIR/sysroot/usr/spu/include -L$CELL_SDK_DIR/sysroot/usr/spu/lib"
+CMK_SPE_LD="spu-gcc -Wl,-N -L$CELL_SDK_DIR/sysroot/usr/spu/lib"
+CMK_SPE_LDXX="spu-g++ -Wl,-N -L$CELL_SDK_DIR/sysroot/usr/spu/lib"
+CMK_SPERT_LIBS="-lcellspu $CELL_SDK_DIR/sysroot/usr/spu/lib/libc.a $CELL_SDK_DIR/sysroot/usr/spu/lib/libsim.a"
+CMK_SPE_AR='spu-ar'
+CMK_PPU_EMBEDSPU='ppu32-embedspu'
 
 CMK_RANLIB='ppu-ranlib'
 CMK_AR='ppu-ar -r'
