f2f00187c1ffc06aee4737544ba819be2bf76d7c
David Kunzman <kunzman2@illinois.edu>
2006-08-25 04:39:32 +0000

    Modified file to execute conv-config.sh instead of conv-mach.sh.  Added and modified the contents of Makefile.cell for xlc support (compilers are no longer hard-coded).

--- src/arch/net-linux-cell/special.sh
+++ src/arch/net-linux-cell/special.sh
@@ -10,19 +10,26 @@ do
 done
 
 #make library
-. ./conv-mach.sh
+export CHARMINC=../include
+. ./conv-config.sh
 if test ! -f $CELL_SDK_DIR/sysroot/usr/include/libspe.h
 then
   echo "Please define CELL_SDK_DIR in charm/src/arch/net-linux-cell/conv-mach.sh!"
   exit 1
 fi
 
+
 cat > Makefile.cell << EOF
 CELL_SDK_DIR=$CELL_SDK_DIR
-SPU_CC = spu-gcc \$(OPTS)
-SPU_CXX = spu-g++ \$(OPTS)
-SPU_AR = spu-ar
-PPU_EMBEDSPU = ppu32-embedspu
+PPU_CC = $CMK_CC \$(OPTS)
+PPU_CXX = $CMK_CXX \$(OPTS)
+SPU_CC = $CMK_SPE_CC \$(OPTS)
+SPU_CXX = $CMK_SPE_CXX \$(OPTS)
+SPU_LD = $CMK_SPE_LD
+SPU_LDXX = $CMK_SPE_LDXX
+SPU_AR = $CMK_SPE_AR
+PPU_EMBEDSPU = $CMK_PPU_EMBEDSPU
+SPERT_LIBS = $CMK_SPERT_LIBS
 EOF
 
 cd cell_lib && make install
