836162880b694f8cf0cc1439c49b6e8731cf1319
David Kunzman <kunzman2@illinois.edu>
2006-09-21 20:11:38 +0000

    Removed startX variable.

--- examples/charm++/cell/jacobi/jacobi_shared.cpp
+++ examples/charm++/cell/jacobi/jacobi_shared.cpp
@@ -17,10 +17,13 @@ void funcLookup(int funcIndex,
 
   switch (funcIndex) {
 
+    case SPE_FUNC_INDEX_INIT: break;   // Not needed, but not invalid (i.e. - don't let the default case get it)
+    case SPE_FUNC_INDEX_CLOSE: break;  // Not needed, but not invalid (i.e. - don't let the default case get it)
+
     case FUNC_DoCalculation: doCalculation((float*)writeOnlyPtr, (float*)readOnlyPtr); break;
 
     default:
-      //sim_printf("!!! WARNING !!! :: SPE Received Invalid funcIndex (%d)... Ignoring...\n", funcIndex);
+      printf("SPE_%d :: !!!!! WARNING !!!!! :: SPE Received Invalid funcIndex (%d)... Ignoring...\n", (int)getSPEID(), funcIndex);
       break;
   }
 }
@@ -31,24 +34,43 @@ void doCalculation(volatile float* matrixTmp, volatile float* matrix) {
   float maxError = 0.0f;
   int x, y;
 
-  // Update matrixTmp with new values
   register int isNorthwestern = (matrix[DATA_BUFFER_COLS - 1] == 1.0f);
-  register int startX = ((isNorthwestern) ? (2) : (1));
-  for (y = 1; y < (DATA_BUFFER_ROWS - 1); y++) {
-    for (x = startX; x < (DATA_BUFFER_COLS - 1); x++) {
 
-      matrixTmp[GET_DATA_I(x,y)] = (matrix[GET_DATA_I(x    , y    )] +
-                                    matrix[GET_DATA_I(x - 1, y    )] +
-                                    matrix[GET_DATA_I(x + 1, y    )] +
-                                    matrix[GET_DATA_I(x    , y - 1)] +
-                                    matrix[GET_DATA_I(x    , y + 1)]
-				   ) / 5.0f;
+  // Peel off the first iteration of the y loop since the starting x values changes depending on
+  //   the value of isNorthwestern
+  for (x = ((__builtin_expect(isNorthwestern, 0)) ? (2) : (1)); x < (DATA_BUFFER_COLS - 1); x++) {
+    register int index = GET_DATA_I(x,1);
 
-      float tmp = matrixTmp[GET_DATA_I(x,y)] - matrix[GET_DATA_I(x,y)];
-      if (tmp < 0.0f) tmp *= -1.0f;
-      if (maxError < tmp) maxError = tmp;
+    matrixTmp[index] = (matrix[index] +
+                        matrix[GET_DATA_I(x - 1, 1)] +
+                        matrix[GET_DATA_I(x + 1, 1)] +
+                        matrix[GET_DATA_I(x    , 0)] +
+                        matrix[GET_DATA_I(x    , 2)]
+                       ) / 5.0f;
+
+    register float tmp = matrixTmp[index] - matrix[index];
+    // NOTE: Values should rise so matrixTmp[index] should be > matrix[index]
+    if (__builtin_expect(tmp < 0.0f, 0)) tmp *= -1.0f;
+    if (__builtin_expect(maxError < tmp, 0)) maxError = tmp;
+  }
+
+  // Calculate the rest of the values (same regardless of isNorthwestern)
+  for (y = 2; y < (DATA_BUFFER_ROWS - 1); y++) {
+    for (x = 1; x < (DATA_BUFFER_COLS - 1); x++) {
+      register int index = GET_DATA_I(x,y);
+
+      matrixTmp[index] = (matrix[index] +
+                          matrix[GET_DATA_I(x - 1, y    )] +
+                          matrix[GET_DATA_I(x + 1, y    )] +
+                          matrix[GET_DATA_I(x    , y - 1)] +
+                          matrix[GET_DATA_I(x    , y + 1)]
+		         ) / 5.0f;
+
+      register float tmp = matrixTmp[index] - matrix[index];
+      // NOTE: Values should rise so matrixTmp[index] should be > matrix[index]
+      if (__builtin_expect(tmp < 0.0f, 0)) tmp *= -1.0f;
+      if (__builtin_expect(maxError < tmp, 0)) maxError = tmp;
     }
-    startX = 1;
   }
 
   // NOTE : Since the writeOnlyPtr is being used, the data buffer is initially allocated
@@ -59,7 +81,7 @@ void doCalculation(volatile float* matrixTmp, volatile float* matrix) {
   //   stores to the LS are probably faster than DMAing the matrixTmp buffer down to the LS.  ;)
   matrixTmp[GET_DATA_I(0, DATA_BUFFER_ROWS - 1)] = 0.0f;                     // unused corner (here for completeness)
   matrixTmp[GET_DATA_I(DATA_BUFFER_COLS - 1, DATA_BUFFER_ROWS - 1)] = 0.0f;  // unused corner (here for completeness)
-  if (isNorthwestern) {
+  if (__builtin_expect(isNorthwestern, 0)) {
     matrixTmp[GET_DATA_I(1, 1)] = 1.0f;  // Hold this single element constant across iterations
     matrixTmp[GET_DATA_I(DATA_BUFFER_COLS - 1, 0)] = 1.0f;  // northwest flag
   } else {
