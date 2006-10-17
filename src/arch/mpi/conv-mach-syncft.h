4161d3b5c1f6230b2d2b0b690eef22e9785f7df6
Gengbin Zheng <gzheng@illinois.edu>
2006-10-17 05:26:03 +0000

    a syncft (in-mem) checkpoint configure file only for testing purpose.

---
+++ src/arch/mpi/conv-mach-syncft.h
@@ -0,0 +1,15 @@
+
+#undef CMK_MSG_HEADER_EXT_
+//#undef CMK_MSG_HEADER_EXT
+//#undef CMK_MSG_HEADER_BLUEGENE
+/* expand the header to store the restart phase counter(pn) */
+#define CMK_MSG_HEADER_EXT_   CmiUInt2 rank, root, hdl,xhdl,info, stratid, pn,d7; unsigned char cksum, magic;  CmiUInt2 padding;
+//#define CMK_MSG_HEADER_EXT    { CMK_MSG_HEADER_EXT_ }
+//#define CMK_MSG_HEADER_BLUEGENE    { CmiUInt2 d0,d1,d2,d3,d4,d5,hdl,xhdl,pn,info; int nd, n; double rt; CmiInt2 tID; CmiUInt2 hID; char t; int msgID; int srcPe;}
+//#define CMK_MSG_HEADER_BLUEGENE  { CMK_MSG_HEADER_EXT_ CMK_BLUEGENE_FIELDS }
+
+#define CmiGetRestartPhase(m)       ((((CmiMsgHeaderExt*)m)->pn));
+
+#define __FAULT__					   1
+
+#define CMK_MEM_CHECKPOINT				   1
