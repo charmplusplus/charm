0bacb0e478e282856ab7bdc5e10ce86007c907d2
Gengbin Zheng <gzheng@illinois.edu>
2006-11-28 20:20:16 +0000

    a slightly more complicated test

--- examples/bigsim/tools/buildlog/buildlog.C
+++ examples/bigsim/tools/buildlog/buildlog.C
@@ -17,32 +17,46 @@ int main()
   newlog->setName("Method0");
   newlog->setTime(curTime, curTime+0.5);
   curTime = 1.4;
-    // send a message to 1
+    // send first message to 1
   BgMsgID msgID = BgMsgID(0, 0);      // (mype, seqno)
   BgMsgEntry *msgEntry = new BgMsgEntry(0, 1024, curTime, curTime+0.001, 1, 0);
   newlog->addMsg(msgEntry);
+  curTime += 0.003;
+    // send second message to 1
+  msgID = BgMsgID(0, 1);      // (mype, seqno)
+  msgEntry = new BgMsgEntry(0, 1024, curTime, curTime+0.001, 1, 0);
+  newlog->addMsg(msgEntry);
+
   tlinerecs[0].logEntryInsert(newlog);
+
 
   // PE 1
   curTime = 1.0;
-  newlog = new BgTimeLog(BgMsgID(0,0));   // receive the message
-  newlog->setTime(curTime, curTime+1.0);
-  newlog->setEP(1);
-  newlog->setName("Method1");
+  newlog = new BgTimeLog(BgMsgID(0,0));   // receive the first message with ID of (0,0)
+  newlog->setTime(curTime, curTime+1.0);  // set start/end time of execution
+  newlog->setEP(1);                       // function handle
+  newlog->setName("Method1");             // human-readable name
   tlinerecs[1].logEntryInsert(newlog);
   curTime += 2.0;
 
   BgTimeLog *prevlog = newlog;
-  newlog = new BgTimeLog();   // no msg, depend on the previous timelog
+  newlog = new BgTimeLog();   // no msg for this event, depend on the previous timelog
   newlog->setTime(curTime, curTime+1.0);
   newlog->setEP(2);
   newlog->setName("Method2");
   newlog->addBackwardDep(prevlog);
   tlinerecs[1].logEntryInsert(newlog);
+  curTime += 1.5;
+
+  newlog = new BgTimeLog(BgMsgID(0,1));   // receive the second message with ID of (0,1)
+  newlog->setTime(curTime, curTime+2.0);
+  newlog->setEP(3);
+  newlog->setName("Method3");
+  tlinerecs[1].logEntryInsert(newlog);
 
   // start output
     // write summary file for totalPEs processor traces, running on 
-       1 REAL processor
+    // 1 REAL processor
   BgWriteTraceSummary(totalPEs, 1, totalPEs, 1, 1);
 
     // write all timelines
