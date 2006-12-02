fb3afc3ea138e416a342ec306dbd0c4494415014
David Kunzman <kunzman2@illinois.edu>
2006-12-02 15:30:10 +0000

    Modified macros to be branchless.

--- src/arch/cell/cell_lib/spert_common.h
+++ src/arch/cell/cell_lib/spert_common.h
@@ -26,8 +26,18 @@ extern "C" {
 
 // SIZEOF_16 : Returns the size of the structure s rounded up to the nearest multiple of 16.
 //   NOTE: All of the values in this macro are constants so a good compiler should be able to reduce all of this to a constant.
-#define SIZEOF_16(s)   ( (((sizeof(s) & 0x0000000F)) == (0x00)) ? (int)(sizeof(s)) : (int)((sizeof(s) & 0xFFFFFFF0) + (0x10)) )
-#define ROUNDUP_16(s)  ( ((((s) & 0x0000000F)) == (0x00)) ? (int)(s) : (int)(((s) & 0xFFFFFFF0) + (0x10)) )
+
+#define ROUNDUP_16(s)   (s + ((16 - (s & 15)) & 15))
+#define SIZEOF_16(s)    (sizeof(s) + ((16 - (sizeof(s) & 15)) & 15))
+
+#define ROUNDUP_128(s)  (s + ((128 - (s & 127)) & 127))
+#define SIZEOF_128(s)   (sizeof(s) + ((128 - (sizeof(s) & 127)) & 127))
+
+#define ROUNDUP(s,p2)   (s + ((p2 - (s & (p2 - 1))) & (p2 - 1)))
+#define SIZEOF(s,p2)    (sizeof(s) + ((p2 - (sizeof(s) & (p2 - 1))) & (p2 - 1)))
+
+//#define SIZEOF_16(s)   ( (((sizeof(s) & 0x0000000F)) == (0x00)) ? (int)(sizeof(s)) : (int)((sizeof(s) & 0xFFFFFFF0) + (0x10)) )
+//#define ROUNDUP_16(s)  ( ((((s) & 0x0000000F)) == (0x00)) ? (int)(s) : (int)(((s) & 0xFFFFFFF0) + (0x10)) )
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
