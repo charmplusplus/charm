07c246857e4bf9b9794b3b174cbf2c45b6e4e526
Isaac Dooley <idooley2@illinois.edu>
2007-02-17 18:00:35 +0000

    Now element insertion appears to work

--- src/libs/ck-libs/ParFUM-Tops/ParFUM_TOPS.C
+++ src/libs/ck-libs/ParFUM-Tops/ParFUM_TOPS.C
@@ -3,6 +3,20 @@
 *
 *      Author: Isaac Dooley
 
+
+
+Assumptions: 
+
+TopNode is just the index into the nodes
+TopElem is just the signed index into the elements negatives are ghosts
+
+
+
+
+
+Notes:
+
+
 mesh_modify.C:  FEM_Mesh *m=FEM_Mesh_lookup(mesh,"FEM_Print_Mesh_Summary");
 mesh_modify.C:  return FEM_add_node(FEM_Mesh_lookup(mesh,"FEM_add_node"), adjacent_nodes, num_adjacent_nodes, chunks, numChunks, forceShared);
 mesh_modify.C:  return FEM_remove_node(FEM_Mesh_lookup(mesh,"FEM_remove_node"), node);
@@ -21,27 +35,26 @@ TopModel* topModel_Create(){
   int which_mesh=FEM_Mesh_default_read();
   FEM_Mesh_allocate_valid_attr(which_mesh, FEM_NODE);
   FEM_Mesh_allocate_valid_attr(which_mesh, FEM_ELEM+0);
-  
-  // Allocate an ID attribute for the elements and nodes
-  
-  //	FEM_GLOBALNO
-  
-  return FEM_Mesh_lookup(which_mesh,"TopModel::TopModel()");
+  FEM_Mesh_allocate_valid_attr(which_mesh, FEM_ELEM+1);
+  FEM_Mesh *mesh = FEM_Mesh_lookup(which_mesh,"TopModel::TopModel()");
+
+  // Don't Allocate the Global Number attribute for the elements and nodes  
+  // It will be automatically created upon calls to void FEM_Entity::setGlobalno(int r,int g) {
+
+  return mesh;
 }
 
 TopNode topModel_InsertNode(TopModel* m, double x, double y, double z){
-  // TODO : insert a node here
-  int which = FEM_add_node_local_nolock(m);
-  m->node.set_coord(which,x,y,z);
-
-  TopNode a;
-  return a;
+  int newNode = FEM_add_node_local_nolock(m);
+  m->node.set_coord(newNode,x,y,z);
+  return newNode;
 }
 
 
 /** Set id of a node */
-void topNode_SetId(TopModel*, TopNode, TopID id){
+void topNode_SetId(TopModel* m, TopNode n, TopID id){
 	// just set the nodal id attribute to id
+  m->node.setGlobalno(n,id);
 }
 
 /** Set attribute of a node */
@@ -50,18 +63,23 @@ void topNode_SetAttrib(TopModel*, TopNode, NodeAtt*){
 }
 
 /** Insert an element */
-TopElement topModel_InsertElem(TopModel*, TopElemType, TopNode*){
-
+TopElement topModel_InsertElem(TopModel*m, TopElemType type, TopNode* nodes){
+  assert(type == FEM_TRIANGULAR);
+  int conn[3];
+  conn[0] = nodes[0];
+  conn[1] = nodes[1];
+  conn[2] = nodes[2];
+  int newEl = FEM_add_element_local(m, conn, 3, 0, 0, 0);
+  return newEl;
 }
 
 /** Set id of an element */
-void topElement_SetId(TopModel*, TopElement, TopID id){
-
+void topElement_SetId(TopModel* m, TopElement e, TopID id){
+  m->elem[0].setGlobalno(e,id);
 }
 
 /** Set attribute of an element */
 void topElement_SetAttrib(TopModel*, TopElement, ElemAtt*){
-
 
 }
 
