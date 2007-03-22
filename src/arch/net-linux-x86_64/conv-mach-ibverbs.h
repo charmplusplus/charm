4ceb06620640677f0aedc0d3b69b9cfc3e7dc20e
Sayantan Chakravorty <sayantan_chak@yahoo.com>
2007-03-22 03:21:36 +0000

    Added the conv-mach files for the ibverbs (infiniband openib libibverbs based build).

---
+++ src/arch/net-linux-x86_64/conv-mach-ibverbs.h
@@ -0,0 +1,6 @@
+#undef CMK_USE_IBVERBS
+#define CMK_USE_IBVERBS				1
+
+
+#undef CMK_NETPOLL
+#define CMK_NETPOLL						1
---
+++ src/arch/net-linux-x86_64/conv-mach-ibverbs.sh
@@ -0,0 +1,13 @@
+#default ibverbs path for openib
+if test -z "$CMK_INCDIR"
+then
+  #openib-1.1
+  if test -f /usr/local/ofed/include/infiniband/verbs.h
+  then
+    CMK_INCDIR="-I /usr/local/ofed/include/infiniband/"
+    CMK_LIBDIR="-L /usr/local/ofed/lib64"
+	fi
+fi
+
+CMK_LIBS="$CMK_LIBS -l ibverbs"
+
