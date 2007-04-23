8b9d77e750bbc4cc2b35defcb21c40e1117bb1e8
Isaac Dooley <idooley2@illinois.edu>
2007-04-23 02:03:48 +0000

    This version works with CUDA

---
+++ src/libs/ck-libs/ParFUM-Tops/ParFUM_TOPS_CUDA.cu
@@ -0,0 +1,14 @@
+/**
+
+   @file
+   @brief Implementation of ParFUM-TOPS layer, except for Iterators
+
+   @author Isaac Dooley, Aaron Becker
+
+*/
+
+#include "ParFUM_TOPS_CUDA.h"
+
+
+#ifdef CUDA
+#endif
