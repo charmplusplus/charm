171f89ff56e31e26b1b406f32f9ee79f6d027b6b
Filippo Gioachin <gioachin@illinois.edu>
2007-09-15 19:45:01 +0000

    Added definition of CMK_64BIT

--- src/arch/mpi-sp/cc-mpcc32.h
+++ src/arch/mpi-sp/cc-mpcc32.h
@@ -9,3 +9,8 @@
 #define CMK_TYPEDEF_INT8 long long
 #define CMK_TYPEDEF_UINT8 unsigned long long
 
+#ifdef CMK_64BIT
+#undef CMK_64BIT
+#endif
+#define CMK_64BIT    0
+
--- src/arch/mpi-sp/cc-mpcc64.h
+++ src/arch/mpi-sp/cc-mpcc64.h
@@ -1,2 +1,8 @@
 /* no defines needed. */
 
+#ifdef CMK_64BIT
+#undef CMK_64BIT
+#endif
+#define CMK_64BIT    1
+
+
