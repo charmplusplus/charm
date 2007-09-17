bcbcfba3478f88c1043186ee57b36614a12f453b
Gengbin Zheng <gzheng@illinois.edu>
2007-09-17 22:23:45 +0000

    change OS malloc to be default.

--- src/arch/mpi-linux-x86_64/conv-mach-vmi.h
+++ src/arch/mpi-linux-x86_64/conv-mach-vmi.h
@@ -1,5 +1,5 @@
 
 #undef CMK_MALLOC_USE_GNU_MALLOC
 #undef CMK_MALLOC_USE_OS_BUILTIN
-#define CMK_MALLOC_USE_GNU_MALLOC                          1
-#define CMK_MALLOC_USE_OS_BUILTIN                          0
+#define CMK_MALLOC_USE_GNU_MALLOC                          0
+#define CMK_MALLOC_USE_OS_BUILTIN                          1
