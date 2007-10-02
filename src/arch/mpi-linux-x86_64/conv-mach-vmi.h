e668303e86fab3f6b49e8616658aebb03441ad2e
Gengbin Zheng <gzheng@illinois.edu>
2007-10-02 08:06:13 +0000

    use context thread for vmi

--- src/arch/mpi-linux-x86_64/conv-mach-vmi.h
+++ src/arch/mpi-linux-x86_64/conv-mach-vmi.h
@@ -4,5 +4,7 @@
 #define CMK_MALLOC_USE_GNU_MALLOC                          0
 #define CMK_MALLOC_USE_OS_BUILTIN                          1
 
+#undef CMK_THREADS_USE_CONTEXT
+#define CMK_THREADS_USE_CONTEXT                            1
 
 #define CMK_VMI						   1
