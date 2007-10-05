d0058e273c287e3a4aa5015d25a2f07ce577625e
Gengbin Zheng <gzheng@illinois.edu>
2007-10-05 08:07:26 +0000

    refixed the previous fix.

--- src/ck-core/debug-charm.C
+++ src/ck-core/debug-charm.C
@@ -3,7 +3,8 @@
  *****************************************************************************/
 
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 #include <sys/stat.h>		// for chmod
