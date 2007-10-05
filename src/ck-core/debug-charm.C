44451325a15e222f1e8049c87f9ccf25fffd64ef
Gengbin Zheng <gzheng@illinois.edu>
2007-10-05 03:14:48 +0000

    comment out stdlib.h which somehow the intel compiler does not like the sequence of the header files

--- src/ck-core/debug-charm.C
+++ src/ck-core/debug-charm.C
@@ -3,7 +3,7 @@
  *****************************************************************************/
 
 #include <stdio.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 #include <sys/stat.h>		// for chmod
