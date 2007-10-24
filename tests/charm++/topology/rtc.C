5b35f8f9dfe3b3601a1d0ce6575a87283c963f92
Abhinav Bhatele <bhatele@illinois.edu>
2007-10-24 20:16:36 +0000

    added the torus contiguity metric to see if we have a complete torus

--- tests/charm++/topology/rtc.C
+++ tests/charm++/topology/rtc.C
@@ -13,6 +13,7 @@ class Main : public Chare
       mainProxy = thishandle;
       CkPrintf("Testing TopoManager .... Abhinav\n");
       TopoManager tmgr;
+      CkPrintf("Torus Size [%d] [%d] [%d]\n", tmgr.getDimNX(), tmgr.getDimNY(), tmgr.getDimNZ());
       int x, y, z, t;
       if(CkMyPe()==0) {
         for(int i=0; i<CkNumPes(); i++) {
@@ -20,6 +21,8 @@ class Main : public Chare
           CkPrintf("Processor %d ---> x %d y %d z %d t %d\n", i, x, y, z, t);
         }
       }
+      int size = tmgr.getDimNX() * tmgr.getDimNY() * tmgr.getDimNZ();
+      CkPrintf("Torus Contiguity Metric %d : %d [%f] \n", size, CkNumPes()/2, (float)(CkNumPes())/(2*size) );
       CkExit();
     };
     
