cd12ac47fe7a7b7362f95bc911564ae85f4b0e1e
Gengbin Zheng <gzheng@illinois.edu>
2007-11-20 19:45:34 +0000

    expanded default stack size

--- tests/converse/megacon/fibthr.c
+++ tests/converse/megacon/fibthr.c
@@ -4,7 +4,7 @@ void Cpm_megacon_ack();
 
 /* an accumulator datatype, which can have one pending thread */
 
-#define STACKSIZE_DEFAULT 0
+#define STACKSIZE_DEFAULT  102400
 
 typedef struct accum
 {
