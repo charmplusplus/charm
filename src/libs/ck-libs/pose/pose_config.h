d3e8930bb0e88705aebcc0b19192bcee3e9d0fad
Terry L. Wilmarth <wilmarth@uiuc.edu>
2008-01-10 22:17:23 +0000

    Turned off memory temporal (and thereby serialized checkpointing) by default.  To turn back on, uncomment MEM_TEMPORAL in pose_config.h.

--- src/libs/ck-libs/pose/pose_config.h
+++ src/libs/ck-libs/pose/pose_config.h
@@ -29,7 +29,7 @@
 #define MSG_RECYCLING 1
 
 /// Uncomment to use temporally-blocked memory management
-#define MEM_TEMPORAL
+//#define MEM_TEMPORAL
 
 /// Uncomment to make use of the Streaming Communication Library optimizations
 //#define POSE_COMM_ON 1
