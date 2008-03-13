3cf73b6b0ecca4aafcb59faf98f622c5b9323043
Terry L. Wilmarth <wilmarth@uiuc.edu>
2008-03-13 22:01:09 +0000

    Got rid of absoft for net-linux-ppc, replaced with xlf.

--- src/arch/net-linux-ppc/conv-mach.sh
+++ src/arch/net-linux-ppc/conv-mach.sh
@@ -20,11 +20,12 @@ CMK_F77LIBS="-lg2c "
 CMK_CF90=`which f90 2>/dev/null`
 if test -n "$CMK_CF90"
 then
-# absoft
-  CMK_CF90_FIXED="$CMK_CF90 -W132 "
-  CMK_F90LIBS="-L/usr/absoft/lib -L/opt/absoft/lib -lf90math -lfio -lU77 -lf77math "
+# xlf
+  CMK_CF90="$CMK_CF90 -qthreaded -qlanglvl=90std -qwarn64 -qspill=32648 -qsuppress=1513-029:1518-012:1518-059 -qsuffix=f=f90:cpp=F90 "
+  CMK_CF90_FIXED="$CMK_CF90 -qsuffix=f=f:cpp=F -qfixed=132 "
+  CMK_F90LIBS="-L/opt/ibmcmp/xlf/11.1/bin/../../../xlsmp/1.7/lib -L/opt/ibmcmp/xlf/11.1/lib -lxl -lxlf90 -lxlfmath -lxlopt -lxlsmp"
   CMK_F90_USE_MODDIR=1
-  CMK_F90_MODINC="-p"
+  CMK_F90_MODINC="-I"
 else
 # gnu f95
   CMK_CF90=`which f95 2>/dev/null`
