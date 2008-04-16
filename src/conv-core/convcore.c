3bcb241bb417dbebdecf06cf855d1bdc9521a7dd
Gengbin Zheng <gzheng@illinois.edu>
2008-04-16 08:38:02 +0000

    fixed a compialtion error in previous checkin

--- src/conv-core/convcore.c
+++ src/conv-core/convcore.c
@@ -2744,11 +2744,14 @@ void ConverseCommonInit(char **argv)
   CIdleTimeoutInit(argv);
   
 #if CMK_SHARED_VARS_POSIX_THREADS_SMP /*Used by the net-*-smp versions*/
-	if(CmiMyRank() == 0){
-		if(CmiGetArgFlagDesc(argv,"+CmiNoProcForComThread","Is there an extra processor for the communication thread on each node(only for net-smp-*) ?")){
-			_Cmi_noprocforcommthread=1;
-		}
+  if(CmiMyRank() == 0){
+	if(CmiGetArgFlagDesc(argv,"+CmiNoProcForComThread","Is there an extra processor for the communication thread on each node(only for net-smp-*) ?")){
+		_Cmi_noprocforcommthread=1;
 	}
+    }
+#if CMK_MULTICORE
+    _Cmi_noprocforcommthread = 1;
+#endif
 #endif
 	
 #ifndef CMK_OPTIMIZE
