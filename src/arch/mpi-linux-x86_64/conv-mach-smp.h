b4a1a47a1be99a958a30b076b33ea559ca73460c
Gengbin Zheng <gzheng@illinois.edu>
2008-05-16 15:35:43 +0000

    for the default mpich on ubuntu, it looks like mpich replaces malloc which is not a thread safe one. Have to switch to locked protected gnu malloc as default for SMP.

--- src/arch/mpi-linux-x86_64/conv-mach-smp.h
+++ src/arch/mpi-linux-x86_64/conv-mach-smp.h
@@ -28,3 +28,8 @@
 /*#define  CMK_USE_MFENCE                                    1 */
 #define  CMK_PCQUEUE_LOCK                                  1 
 #define CMK_USE_TLS_THREAD                                 0
+
+#undef CMK_MALLOC_USE_GNU_MALLOC
+#undef CMK_MALLOC_USE_OS_BUILTIN
+#define CMK_MALLOC_USE_GNU_MALLOC                          1
+#define CMK_MALLOC_USE_OS_BUILTIN                          0
