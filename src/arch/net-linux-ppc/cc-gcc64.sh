cc232179de75bf15195829ef3b6f2715c4622452
Gengbin Zheng <gzheng@illinois.edu>
2008-06-01 01:39:41 +0000

    fixed various issues including correct 64 bit fortran compiler (xlf90_r), etc for turing linux.

--- src/arch/net-linux-ppc/cc-gcc64.sh
+++ src/arch/net-linux-ppc/cc-gcc64.sh
@@ -2,8 +2,8 @@ CMK_64bit=" -m64 "
 
 CMK_CPP_CHARM="/lib/cpp -P"
 CMK_CPP_C="gcc -E"
-CMK_CC="gcc $CMK_64bit"
-CMK_CXX="g++ $CMK_64bit"
+CMK_CC="gcc $CMK_64bit -fPIC "
+CMK_CXX="g++ $CMK_64bit -fPIC "
 CMK_CXXPP="$CMK_CXX -x c++ -E "
 CMK_RANLIB="ranlib"
 CMK_LIBS="-lckqt"
@@ -19,14 +19,14 @@ CMK_QT="generic64-light"
 # fortran compiler Absoft or gnu f95
 CMK_CF77="g77 $CMK_64bit"
 CMK_F77LIBS="-lg2c "
-CMK_CF90=`which xlf90 2>/dev/null`
+CMK_CF90=`which xlf90_r 2>/dev/null`
 if test -n "$CMK_CF90"
 then
 # xlf
   bindir=`dirname $CMK_CF90`
   libdir="$bindir/../lib64"
   CMK_64bit=" -q64 "
-  CMK_CF90="$CMK_CF90 $CMK_64bit -qthreaded -qlanglvl=90std -qwarn64 -qspill=32648 -qsuppress=1513-029:1518-012:1518-059 -qsuffix=f=f90:cpp=F90 "
+  CMK_CF90="$CMK_CF90 $CMK_64bit -qpic=large -qthreaded -qlanglvl=90std -qwarn64 -qspill=32648 -qsuppress=1513-029:1518-012:1518-059 -qsuffix=f=f90:cpp=F90 "
   CMK_CF90_FIXED="$CMK_CF90 $CMK_64bit -qsuffix=f=f:cpp=F -qfixed=132 "
   CMK_F90LIBS="-L/opt/ibmcmp/xlf/11.1/bin/../../../xlsmp/1.7/lib64 -L$libdir -lxl -lxlf90 -lxlfmath -lxlopt -lxlsmp"
   CMK_F90_USE_MODDIR=1
@@ -37,8 +37,8 @@ else
   if test -n "$CMK_CF90"
   then
     CMK_FPP="/lib/cpp -P -CC"
-    CMK_CF90="$CMK_CF90 -fpic -fautomatic -fdollar-ok $CMK_64bit"
-    CMK_CF90_FIXED="$CMK_CF90 -ffixed-form $CMK_64bit"
+    CMK_CF90="$CMK_CF90 -m64 -fpic -fautomatic -fdollar-ok $CMK_64bit"
+    CMK_CF90_FIXED="$CMK_CF90 -m64 -ffixed-form $CMK_64bit"
     CMK_F90LIBS="-lgfortran "
     CMK_F90_USE_MODDIR=1
     CMK_F90_MODINC="-I"
