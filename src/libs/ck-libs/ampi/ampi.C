6821fa5a456d3a57b14bdf5302fdb3d9a8eb8e2e
Aaron Becker <abecker3@illinois.edu>
2008-09-18 20:15:31 +0000

    fixed MPI_Attr_get for the case of predefined tags

--- src/libs/ck-libs/ampi/ampi.C
+++ src/libs/ck-libs/ampi/ampi.C
@@ -941,8 +941,7 @@ int ampiParent::getAttr(MPI_Comm comm, int keyval, void *attribute_val, int *fla
 	*flag = false;
 	if (kv_is_builtin(keyval)) { /* Allow access to special builtin flags */
 	  *flag=true;
-	  // *(int *)attribute_val = *kv_builtin_storage;  // looks like all kv_builtin_storage are of integer type
-          *(void **)attribute_val = kv_builtin_storage;
+          *(int **)attribute_val = kv_builtin_storage;  // all default tags are ints
 	  return 0;
 	}
 	if(keyval<0 || keyval >= kvlist.size() || (kvlist[keyval]==NULL))
