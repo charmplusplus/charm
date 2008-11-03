8644d268b96d3e7ba131758f6105bc16763671e7
Ryan Mokos <mokos@illinois.edu>
2008-11-03 22:43:44 +0000

    Added bgReadProcWindow for BigSim incremental log reading functionality.

--- src/langs/bluegene/bigsim_read.C
+++ src/langs/bluegene/bigsim_read.C
@@ -49,6 +49,71 @@ int BgReadProc(int procNum, int numWth ,int numPes, int totalProcs, int* allNode
   return fileNum;
 }
 
+// This version only reads in a part (window) of the time line
+int BgReadProcWindow(int procNum, int numWth ,int numPes, int totalProcs, int* allNodeOffsets, BgTimeLineRec& tlinerec,
+		     int& fileLoc, int& totalTlineLength, int firstLog, int numLogs) {
+
+  int firstLogToRead, numLogsToRead, tLineLength;
+
+  /*Right now works only for cyclicMapInfo - needs a more general scheme*/
+  int nodeNum = procNum/numWth;
+  int numNodes = totalProcs/numWth;
+  int fileNum = nodeNum%numPes;
+  int arrayID=0, fileOffset;
+  char fName[20];
+
+  currTline = &tlinerec;
+  
+  for(int i=0;i<fileNum;i++)
+    arrayID += (numNodes/numPes + ((i < numNodes%numPes)?1:0))*numWth;
+  
+  arrayID += (nodeNum/numPes)*numWth + procNum%numWth;
+  fileOffset = allNodeOffsets[arrayID];
+
+  //  CmiPrintf("   BgReadProc: fileOffset=%d arrayID=%d nodeNum=%d numNodes=%d fileNum=%d\n", fileOffset, arrayID, nodeNum, numNodes, fileNum);
+
+  sprintf(fName,"bgTrace%d",fileNum);
+  FILE* f = fopen(fName,"r");
+  if (f == NULL) {
+    printf("Error> Open failed with %s. \n", fName);
+    return -1;
+  }
+  PUP::fromDisk pd(f);
+  PUP::machineInfo machInfo;
+  pd((char *)&machInfo, sizeof(machInfo));
+  if (!machInfo.valid()) CmiAbort("Invalid machineInfo on disk file!\n");
+  PUP::xlater p(machInfo, pd);
+
+  //  CmiPrintf("BgReadProc: procNum=%d, ftell=%d\n", procNum, ftell(f));
+
+  if (firstLog == 0) {
+    fseek(f, fileOffset, SEEK_SET);
+  } else {
+    fseek(f, fileLoc, 0);
+  }
+  firstLogToRead = firstLog;
+  if (numLogs < 1) {
+    numLogsToRead = 0x7FFFFFFF;
+  } else {
+    numLogsToRead = numLogs;
+  }
+  if (firstLog != 0) {
+    tLineLength = totalTlineLength;
+  } else {
+    tLineLength = -1;
+  }
+  //  CmiPrintf("   BgReadProc: procNum=%d, ftell=%d\n", procNum, ftell(f));
+  tlinerec.winPup(p, firstLogToRead, numLogsToRead, tLineLength);
+  if (firstLog == 0) {
+    totalTlineLength = tLineLength;
+  }
+  //  CmiPrintf("      BgReadProc: procNum=%d, ftell=%d\n", procNum, ftell(f));
+  fileLoc = ftell(f);
+  fclose(f);
+  currTline = NULL;
+
+  return fileNum;
+}
 
 // user has to remember to free allProcOffsets
 int* BgLoadOffsets(int totalProcs, int numPes){
