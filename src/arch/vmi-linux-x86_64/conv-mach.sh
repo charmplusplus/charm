c4136684eab99d8e26cddd66efce2e05690e12e6
Gengbin Zheng <gzheng@illinois.edu>
2008-12-26 18:15:14 +0000

    remove -lexpat

--- src/arch/vmi-linux-x86_64/conv-mach.sh
+++ src/arch/vmi-linux-x86_64/conv-mach.sh
@@ -12,7 +12,7 @@ CMK_CXXPP="$CMK_CC -x c++ -E -m64 -DNO_LOCK "
 CMK_LD="$CMK_CC -rdynamic -pthread -Wl,-rpath,$VMI_DIR/lib $VMI_LIBDIR "
 CMK_LDXX="$CMK_CXX -rdynamic -pthread -Wl,-rpath,$VMI_DIR/lib $VMI_LIBDIR "
 CMK_RANLIB='ranlib'
-CMK_LIBS='-lckqt -lvmi20 -lcurl -ldl -lexpat -lssl -lcrypto'
+CMK_LIBS='-lckqt -lvmi20 -lcurl -ldl -lssl -lcrypto'
 CMK_QT='generic64'
 CMK_XIOPTS=''
 
