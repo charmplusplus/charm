063695b057d0f347764822e399e687e864a0c552
Gengbin Zheng <gzheng@illinois.edu>
2009-02-06 18:22:59 +0000

    for AIX, avoid new. Looks like new does not go to malloc, and so isomalloc may not work

--- examples/ampi/Cjacobi3D/jacobi.C
+++ examples/ampi/Cjacobi3D/jacobi.C
@@ -142,7 +142,11 @@ int main(int ac, char** av)
 
   MPI_Bcast(&niter, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
+#if CMK_AIX
+  cp = (chunk*)malloc(sizeof(chunk));
+#else
   cp = new chunk;
+#endif
 #if defined(AMPI) && ! defined(NO_PUP)
   MPI_Register((void*)&cp, (MPI_PupFn) chunk_pup);
 #endif
