3c7baba0d3eaa6089a100ad4fb359f765f38b720
Gengbin Zheng <gzheng@illinois.edu>
2009-02-09 16:26:04 +0000

    removed duplicated of definition of CMK_MACHINE_PROGRESS_DEFINED in previous checkin

--- src/arch/lapi/conv-common.h
+++ src/arch/lapi/conv-common.h
@@ -28,6 +28,5 @@
 
 #define NODE_0_IS_CONVHOST                                 1
 
-#define CMK_MACHINE_PROGRESS_DEFINED                       1
 #define CMK_IMMEDIATE_MSG				   1
 #define CMK_MACHINE_PROGRESS_DEFINED                       1
