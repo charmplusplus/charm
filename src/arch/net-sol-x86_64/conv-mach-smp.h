a925c343ea49d254056329a6715fd0dae9b0fc51
Gengbin Zheng <gzheng@illinois.edu>
2009-02-18 20:26:16 +0000

    fixed smp configurations

--- src/arch/net-sol-x86_64/conv-mach-smp.h
+++ src/arch/net-sol-x86_64/conv-mach-smp.h
@@ -7,12 +7,10 @@
 
 #define CMK_SMP						   1
 
-/*
 #undef CMK_MALLOC_USE_GNU_MALLOC
 #undef CMK_MALLOC_USE_OS_BUILTIN
 #define CMK_MALLOC_USE_GNU_MALLOC                          0
 #define CMK_MALLOC_USE_OS_BUILTIN                          1
-*/
 
 #undef CMK_MEMORY_PROTECTABLE
 #define CMK_MEMORY_PROTECTABLE                             0
@@ -25,6 +23,7 @@
 #define CMK_SHARED_VARS_UNAVAILABLE                        0
 #define CMK_SHARED_VARS_POSIX_THREADS_SMP                  1
 
-#undef CMK_SYNCHRONIZE_ON_TCP_CLOSE
-#define CMK_SYNCHRONIZE_ON_TCP_CLOSE                       1
+#undef CMK_THREADS_USE_CONTEXT
+#define CMK_THREADS_USE_CONTEXT                            1
 
+#define CMK_PCQUEUE_LOCK                                   1
