8854b4cc711c4a298d448bd0545caf423fd4c37e
Gengbin Zheng <gzheng@illinois.edu>
2009-02-18 03:14:27 +0000

    move old cc file to cc8, and make cc10 default

--- src/arch/common/cc-icc.sh
+++ src/arch/common/cc-icc.sh
@@ -1,27 +1,37 @@
+
+ICC_ver=`icc -v 2>&1`
+ICC_ver=`echo $ICC_ver | awk '{ print $2; }' | awk 'BEGIN {FS="."}; { print $1; }'`
+test -z "$ICC_ver" && echo "ICC compiler not found!" && exit 1
+
+if test $ICC_ver  -eq 10
+then
+
+# for version 10 
+ICCOPTS="-xT"
+
 CMK_CPP_C='icc -E '
-CMK_DEPRECATED="-cxxlib-icc"
-CMK_CC="icc -fpic "
-CMK_CXX="icpc -fpic "
-CMK_CXXPP='icpc -E '
-CMK_LD='icc -i_dynamic '
-CMK_LDXX='icpc -i_dynamic '
+CMK_CC="icc -fpic $ICCOPTS "
+CMK_CXX="icpc -fpic $ICCOPTS "
+CMK_CXXPP="icpc -E $ICCOPTS "
+
+CMK_LD="icc -shared-intel $ICCOPTS "
+CMK_LDXX="icpc -shared-intel $ICCOPTS "
+
 CMK_LD_LIBRARY_PATH="-Wl,-rpath,$CHARMLIBSO/"
 CMK_NATIVE_CC="$CMK_CC"
 CMK_NATIVE_CXX="$CMK_CXX"
 CMK_NATIVE_LD="$CMK_LD"
 CMK_NATIVE_LDXX="$CMK_LDXX"
-CPPFLAGS="$CPPFLAGS -fpic "
-LDFLAGS="$LDFLAGS -i_dynamic "
-
-# for absoft?
-#CMK_F90LIBS='-L/usr/local/intel/compiler70/ia32/lib -L/opt/intel/compiler70/ia32/lib -lintrins -lIEPCF90 -lPEPCF90 -lF90 -lintrins -limf '
-#CMK_MOD_NAME_ALLCAPS=1
+CPPFLAGS="$CPPFLAGS -fpic $ICCOPTS "
+LDFLAGS="$LDFLAGS -shared-intel $ICCOPTS "
 
 # for Intel Fortran compiler 8.0 and higher which is renamed to ifort from ifc
 # does not work for ifc 7.0
-CMK_CF77="ifort -auto "
-CMK_CF90="ifort -auto "
-CMK_CF90_FIXED="$CMK_CF90 -132 -FI "
+CMK_CF77="ifort -auto -fPIC "
+CMK_CF90="ifort -auto -fPIC "
+#CMK_CF90_FIXED="$CMK_CF90 -132 -FI "
+#FOR 64 bit machine
+CMK_CF90_FIXED="$CMK_CF90 -164 -FI "
 F90DIR=`which ifort 2> /dev/null`
 if test -h "$F90DIR"
 then
@@ -38,3 +48,16 @@ CMK_F90LIBS="-L$F90LIBDIR -lifcore -lifport "
 CMK_F77LIBS="$CMK_F90LIBS"
 
 CMK_F90_USE_MODDIR=""
+
+
+# native compiler for compiling charmxi, etc
+CMK_SEQ_CC="$CMK_NATIVE_CC"
+CMK_SEQ_CXX="$CMK_NATIVE_CXX"
+CMK_SEQ_LD="$CMK_NATIVE_LD"
+CMK_SEQ_LDXX="$CMK_NATIVE_LDXX"
+
+else      # version other than 10
+
+. $CHARMINC/cc-icc8.sh
+
+fi
