c9322fd5ea71e74664d4ecf3a85198c62fca06c1
David Kunzman <kunzman2@illinois.edu>
2009-02-19 19:22:38 +0000

    Make member variables private again.  Added CkIndex_Jacobi as a friend class to Jacobi.

--- examples/charm++/cell/jacobi/jacobi.h
+++ examples/charm++/cell/jacobi/jacobi.h
@@ -30,12 +30,16 @@ class NorthSouthGhost : public CMessage_NorthSouthGhost {
 
 class Jacobi : public CBase_Jacobi {
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  //  Member Variables
-  public:
+  // Declare CkIndex_Jacobi as a friend so the accelerated member functions
+  //   can access the member variables of the class
+  friend class CkIndex_Jacobi;
+
+  private:
+
+    /// Member Variables ///
     float* matrix;     // Pointer to the first buffer (the read buffer per iteration)
     float* matrixTmp;  // Pointer to the second buffer (the write buffer per iteration)
-  private:
+
     int ghostCount;
     int ghostCountNeeded;
     int iterCount;
@@ -50,18 +54,17 @@ class Jacobi : public CBase_Jacobi {
     NorthSouthGhost* futureNorthMsg;
     NorthSouthGhost* futureSouthMsg;
 
+    /// Member Functions ///
+    void attemptCalculation();
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  // Constructor(s) / Destructor
   public:
+
+    /// Constructor(s) / Destructor ///
     Jacobi();
     Jacobi(CkMigrateMessage *msg);
     ~Jacobi();
 
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  // Member Functions
-  public:
+    /// Entry Methods ///
     void startIteration();
 
     void northData(int size, float* ghostData, int iterRef);
@@ -74,8 +77,6 @@ class Jacobi : public CBase_Jacobi {
     void eastData_msg(EastWestGhost*);
     void westData_msg(EastWestGhost*);
 
-    void attemptCalculation();
-    void doCalculation();
     void doCalculation_post();
 };
 
