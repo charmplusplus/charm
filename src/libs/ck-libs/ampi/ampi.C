8eb5db43761ff68ba54c80c04e36a5a4a1e8d125
Gengbin Zheng <gzheng@illinois.edu>
2009-02-27 06:12:07 +0000

    correct the dependence if ampi_barrier_end to the beginning of ampi_barrier.

--- src/libs/ck-libs/ampi/ampi.C
+++ src/libs/ck-libs/ampi/ampi.C
@@ -2502,9 +2502,9 @@ int AMPI_Barrier(MPI_Comm comm)
   if(getAmpiParent()->isInter(comm)) CkAbort("MPI_Barrier not allowed for Inter-communicator!");
 #if CMK_BLUEGENE_CHARM
   void *barrierLog;		// store current log in timeline
-  _TRACE_BG_TLINE_END(&barrierLog); 
   //TRACE_BG_AMPI_BARRIER_START(barrierLog);
   TRACE_BG_AMPI_BREAK(NULL, "AMPI_Barrier", NULL, 0);
+  _TRACE_BG_TLINE_END(&barrierLog); 
 #endif
   //HACK: Use collective operation as a barrier.
   AMPI_Allreduce(NULL,NULL,0,MPI_INT,MPI_SUM,comm);
