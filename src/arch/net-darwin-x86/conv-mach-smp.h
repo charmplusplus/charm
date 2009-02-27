87d0857d44970bdeb4650f182dc177205ad5977b
Gengbin Zheng <gzheng@illinois.edu>
2009-02-27 20:43:31 +0000

    turn CMK_PCQUEUE_LOCK on

--- src/arch/net-darwin-x86/conv-mach-smp.h
+++ src/arch/net-darwin-x86/conv-mach-smp.h
@@ -9,4 +9,6 @@
 #define CMK_SHARED_VARS_UNAVAILABLE                        0
 #define CMK_SHARED_VARS_POSIX_THREADS_SMP                  1
 
-#define CMK_MACOSX_PTHREAD_HACK                             1
+#define CMK_MACOSX_PTHREAD_HACK                            1
+
+#define CMK_PCQUEUE_LOCK				   1
