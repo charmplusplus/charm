bfbb71aa4879b93599f680c4f0300951e2ad079d
Gengbin Zheng <gzheng@illinois.edu>
2009-03-03 00:17:35 +0000

    switch to memory os as default again, this time with a different isomalloc initialization scheme that I just implemented.

--- src/arch/net-linux-x86_64/conv-mach-smp.h
+++ src/arch/net-linux-x86_64/conv-mach-smp.h
@@ -9,12 +9,12 @@
 #define CMK_SHARED_VARS_UNAVAILABLE                        0
 #define CMK_SHARED_VARS_POSIX_THREADS_SMP                  1
 
-/*
 #undef CMK_MALLOC_USE_GNU_MALLOC
 #undef CMK_MALLOC_USE_OS_BUILTIN
 #define CMK_MALLOC_USE_GNU_MALLOC                          0
 #define CMK_MALLOC_USE_OS_BUILTIN                          1
-*/
+
+#define CMK_MMAP_PROBE                                     1
 
 /*#define CMK_PCQUEUE_LOCK				   1 */
 /*#define CMK_USE_MFENCE 					   1 */
