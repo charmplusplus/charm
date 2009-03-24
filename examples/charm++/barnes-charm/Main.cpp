26f9894e6eaf0b070c241be70446b71ee6b74079
Pritish Jetley <pjetley2@illinois.edu>
2009-03-24 01:45:39 +0000

    barnes charm code

---
+++ examples/charm++/barnes-charm/Main.cpp
@@ -0,0 +1,113 @@
+#include "Main.h"
+
+CProxy_Main mainChare;
+CProxy_TreePiece pieces;
+CProxy_ParticleChunk chunks;
+
+int log8floor(int arg){
+  int ret = -1;
+  for(; arg > 0; arg >>= 3){
+    ret++;
+  }
+  return (ret);
+}
+
+Main::Main(CkArgMsg *m){
+  int c;
+
+  while ((c = getopt(m->argc, m->argv, "h")) != -1) {
+    switch(c) {
+      case 'h': 
+        Help(); 
+        exit(-1); 
+        break;
+      default:
+        fprintf(stderr, "Only valid option is \"-h\".\n");
+        exit(-1);
+        break;
+    }
+  }
+
+  // parameters
+  initparam(m->argv, defv);
+  startrun();
+  initoutput();
+  tab_init();
+
+  // FIXME - get value (from command line?)
+  int numTreePieces;
+  // create top portion of global tree
+  int depth = log8floor(numTreePieces);
+  nodeptr = createTopLevelTree(depth);
+
+  CProxy_BlockMap myMap=CProxy_BlockMap::ckNew(); 
+  CkArrayOptions opts(numTreePieces); 
+
+  opts.setMap(myMap);
+  CProxy_TreePiece pieces = CProxy_TreePiece::ckNew(numTreePieces,opts);
+  treeProxy = pieces;
+
+  // create holders, treepieces
+  // slavestart for holders
+
+
+  SlaveStart();
+
+}
+
+void
+Main::Help () 
+{
+   printf("There are a total of twelve parameters, and all of them have default values.\n");
+   printf("\n");
+   printf("1) infile (char*) : The name of an input file that contains particle data.  \n");
+   printf("    The  of the file is:\n");
+   printf("\ta) An int representing the number of particles in the distribution\n");
+   printf("\tb) An int representing the dimensionality of the problem (3-D)\n");
+   printf("\tc) A double representing the current time of the simulation\n");
+   printf("\td) Doubles representing the masses of all the particles\n");
+   printf("\te) A vector (length equal to the dimensionality) of doubles\n");
+   printf("\t   representing the positions of all the particles\n");
+   printf("\tf) A vector (length equal to the dimensionality) of doubles\n");
+   printf("\t   representing the velocities of all the particles\n");
+   printf("\n");
+   printf("    Each of these numbers can be separated by any amount of whitespace.\n");
+   printf("\n");
+   printf("2) nbody (int) : If no input file is specified (the first line is blank), this\n");
+   printf("    number specifies the number of particles to generate under a plummer model.\n");
+   printf("    Default is 16384.\n");
+   printf("\n");
+   printf("3) seed (int) : The seed used by the random number generator.\n");
+   printf("    Default is 123.\n");
+   printf("\n");
+   printf("4) outfile (char*) : The name of the file that snapshots will be printed to. \n");
+   printf("    This feature has been disabled in the SPLASH release.\n");
+   printf("    Default is NULL.\n");
+   printf("\n");
+   printf("5) dtime (double) : The integration time-step.\n");
+   printf("    Default is 0.025.\n");
+   printf("\n");
+   printf("6) eps (double) : The usual potential softening\n");
+   printf("    Default is 0.05.\n");
+   printf("\n");
+   printf("7) tol (double) : The cell subdivision tolerance.\n");
+   printf("    Default is 1.0.\n");
+   printf("\n");
+   printf("8) fcells (double) : The total number of cells created is equal to \n");
+   printf("    fcells * number of leaves.\n");
+   printf("    Default is 2.0.\n");
+   printf("\n");
+   printf("9) fleaves (double) : The total number of leaves created is equal to  \n");
+   printf("    fleaves * nbody.\n");
+   printf("    Default is 0.5.\n");
+   printf("\n");
+   printf("10) tstop (double) : The time to stop integration.\n");
+   printf("    Default is 0.075.\n");
+   printf("\n");
+   printf("11) dtout (double) : The data-output interval.\n");
+   printf("    Default is 0.25.\n");
+   printf("\n");
+   printf("12) NPROC (int) : The number of processors.\n");
+   printf("    Default is 1.\n");
+}
+#include "barnes.def.h"
---
+++ examples/charm++/barnes-charm/Main.h
@@ -0,0 +1,41 @@
+
+#include "barnes.decl.h"
+extern CProxy_Main mainChare;
+extern CProxy_TreePiece pieces;
+extern CProxy_ParticleChunk chunks;
+
+class Main : public CBase_Main {
+
+  char *defv[] = {                 /* DEFAULT PARAMETER VALUES              */
+    /* file names for input/output                                         */
+    "in=",                        /* snapshot of initial conditions        */
+    "out=",                       /* stream of output snapshots            */
+
+    /* params, used if no input specified, to make a Plummer Model         */
+    "nbody=16384",                /* number of particles to generate       */
+    "seed=123",                   /* random number generator seed          */
+
+    /* params to control N-body integration                                */
+    "dtime=0.025",                /* integration time-step                 */
+    "eps=0.05",                   /* usual potential softening             */
+    "tol=1.0",                    /* cell subdivision tolerence            */
+    "fcells=2.0",                 /* cell allocation parameter             */
+    "fleaves=0.5",                 /* leaf allocation parameter             */
+
+    "tstop=0.075",                 /* time to stop integration              */
+    "dtout=0.25",                 /* data-output interval                  */
+
+    "NPROC=1",                    /* number of processors                  */
+  };
+
+  Help();
+  public:
+  Main(CkArgMsg *m);
+  Main(CkMigrateMessage *m);
+};
+
+class ParticleChunk : public CBase_ParticleChunk {
+  public:
+  ParticleChunk(CkArgMsg *m);
+  ParticleChunk(CkMigrateMessage *m);
+};
