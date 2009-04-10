d6da0849241210ce7cae80a3f11ddefb514f7449
Gengbin Zheng <gzheng@illinois.edu>
2009-04-10 20:13:44 +0000

    a few tweaks like switching to mfence

--- src/arch/mpi-linux-x86_64/conv-mach-smp.h
+++ src/arch/mpi-linux-x86_64/conv-mach-smp.h
@@ -17,19 +17,19 @@
 
 #define CMK_LINUX_PTHREAD_HACK                             0
 
-#undef CMK_SYNCHRONIZE_ON_TCP_CLOSE
-#define CMK_SYNCHRONIZE_ON_TCP_CLOSE                       1
-
 #undef CMK_TIMER_USE_GETRUSAGE
 #undef CMK_TIMER_USE_SPECIAL
 #define CMK_TIMER_USE_GETRUSAGE                            1
 #define CMK_TIMER_USE_SPECIAL                              0
 
-/*#define  CMK_USE_MFENCE                                    1 */
-#define  CMK_PCQUEUE_LOCK                                  1 
-#define CMK_USE_TLS_THREAD                                 1
-
+/*
 #undef CMK_MALLOC_USE_GNU_MALLOC
 #undef CMK_MALLOC_USE_OS_BUILTIN
 #define CMK_MALLOC_USE_GNU_MALLOC                          1
 #define CMK_MALLOC_USE_OS_BUILTIN                          0
+*/
+
+/*#define  CMK_USE_MFENCE                                    1 */
+/*#define  CMK_PCQUEUE_LOCK                                  1 */
+#define CMK_USE_TLS_THREAD                                 1
+
