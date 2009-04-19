9da86f6daccf94e54f26a35568c8b838550f5bfc
Gengbin Zheng <gzheng@illinois.edu>
2009-04-19 00:58:07 +0000

    call fclose()

--- src/conv-core/isomalloc.c
+++ src/conv-core/isomalloc.c
@@ -55,6 +55,7 @@ static int read_randomflag(void)
   fp = fopen("/proc/sys/kernel/randomize_va_space", "r");
   if (fp != NULL) {
     fscanf(fp, "%d", &random_flag);
+    fclose(fp);
   }
   else {
     random_flag = -1;
