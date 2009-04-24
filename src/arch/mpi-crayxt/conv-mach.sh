9aae32f190ffa0916aad5f97355963370c83dae1
Gengbin Zheng <gzheng@illinois.edu>
2009-04-24 15:18:47 +0000

    tweak compiler options for SEQ, and fortran compilers

--- src/arch/mpi-crayxt/conv-mach.sh
+++ src/arch/mpi-crayxt/conv-mach.sh
@@ -1,6 +1,8 @@
 #CMK_DEFS="-I/opt/xt-mpt/1.5.47/mpich2-64/T/include "
 #CMK_LD_DEFS="-lrca "
 
+PGCC=`CC -V 2>/dev/null | grep pgCC`
+
 CMK_CPP_CHARM="/lib/cpp -P"
 CMK_CPP_C="cc -E $CMK_DEFS "
 CMK_CXXPP="CC -E $CMK_DEFS "
@@ -13,10 +15,17 @@ CMK_LIBS="-lckqt -lrca"
 CMK_LD_LIBRARY_PATH="-Wl,-rpath,$CHARMLIBSO/"
 
 # compiler for compiling sequential programs
+if test -n "$PGCC"
+then
+# gcc is needed for building QT
 CMK_SEQ_CC="gcc -fPIC"
-CMK_SEQ_LD="gcc -fPIC"
-CMK_SEQ_CXX="g++ "
-CMK_SEQ_LDXX="g++ "
+CMK_SEQ_CXX="pgCC -fPIC "
+else
+CMK_SEQ_CC="gcc -fPIC"
+CMK_SEQ_CXX="g++ -fPIC "
+fi
+CMK_SEQ_LD="$CMK_SEQ_CC "
+CMK_SEQ_LDXX="$CMK_SEQ_CXX "
 CMK_SEQ_LIBS=""
 
 # compiler for native programs
@@ -34,7 +43,8 @@ CMK_CF77="ftn "
 CMK_CF90="ftn "
 CMK_F90LIBS=""
 CMK_F90_USE_MODDIR=1
-CMK_F90_MODINC="-p"
+CMK_F90_MODINC="-I"
+CMK_MOD_EXT="mod"
 
 CMK_NO_BUILD_SHARED=true
 
