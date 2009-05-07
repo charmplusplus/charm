d660c83be4d753ef2721dd3d99e42fc3476f2e51
Gengbin Zheng <gzheng@illinois.edu>
2009-05-07 20:29:55 +0000

    make sure xlc generate 32 bit code by using -q32

--- src/arch/common/cc-xlc.sh
+++ src/arch/common/cc-xlc.sh
@@ -1,5 +1,6 @@
-CMK_CC='xlc_r '
-CMK_CXX='xlC_r -qstaticinline '
+CMK_DEFS="$CMK_DEFS -q32"
+CMK_CC="xlc_r -qcpluscmt $CMK_DEFS "
+CMK_CXX="xlC_r -qstaticinline $CMK_DEFS"
 CMK_C_OPTIMIZE='-O3 -qstrict -Q  '
 CMK_CXX_OPTIMIZE='-O3 -qstrict -Q '
 CMK_PIC='-qpic=small'
@@ -10,11 +11,11 @@ CMK_LDXX="$CMK_CXX -brtl "
 CMK_AR='ar cq'
 CMK_NM='nm '
 
-CMK_NATIVE_CC='xlc_r '
-CMK_NATIVE_LD='xlc_r '
-CMK_NATIVE_CXX='xlC_r -qstaticinline '
-CMK_NATIVE_LDXX='xlC_r '
+CMK_NATIVE_CC='xlc_r -q32 '
+CMK_NATIVE_LD='xlc_r -q32 '
+CMK_NATIVE_CXX='xlC_r -q32 -D_H_UNISTD -DYY_NEVER_INTERACTIVE=1 -qstaticinline '
+CMK_NATIVE_LDXX='xlC_r -q32 '
 
-CMK_CF77='xlf_r  '
-CMK_CF90='xlf90_r  -qsuffix=f=f90' 
-CMK_CF90_FIXED='xlf90_r ' 
+CMK_CF77='xlf_r -q32  '
+CMK_CF90='xlf90_r  -qsuffix=f=f90 -q32 ' 
+CMK_CF90_FIXED='xlf90_r -q32 ' 
