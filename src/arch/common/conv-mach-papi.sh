983b84d054cf469762e1e6a8b0d6119394a188d3
Gengbin Zheng <gzheng@illinois.edu>
2009-06-10 19:49:40 +0000

    guess default directory

--- src/arch/common/conv-mach-papi.sh
+++ src/arch/common/conv-mach-papi.sh
@@ -1,1 +1,21 @@
 CMK_USE_PAPI=true
+
+#default papi dir
+#guess where the papi.h is installed
+if test -z "$CMK_INCDIR"
+then
+  # abe.ncsa
+  if test -f /usr/apps/tools/papi/include/papi.h
+  then
+    CMK_INCDIR="-I /usr/apps/tools/papi/include"
+    if test -n "$CMK_CC64"
+    then
+      CMK_LIBDIR="-L /usr/apps/tools/papi/lib64"
+    else
+      CMK_LIBDIR="-L /usr/apps/tools/papi/lib"
+    fi
+  fi
+fi
+
+
+CMK_LIBS="$CMK_LIBS -lpapi"
