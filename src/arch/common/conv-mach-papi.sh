281eca5b00be59ee990466a7ecebde6d1fcaf305
Gengbin Zheng <gzheng@illinois.edu>
2009-06-10 20:26:20 +0000

    added -Wl,-rpath to papi libdir for loading the papi.so

--- src/arch/common/conv-mach-papi.sh
+++ src/arch/common/conv-mach-papi.sh
@@ -10,10 +10,13 @@ then
     CMK_INCDIR="-I /usr/apps/tools/papi/include"
     if test -n "$CMK_CC64"
     then
-      CMK_LIBDIR="-L /usr/apps/tools/papi/lib64"
+      PAPI_LIBDIR="/usr/apps/tools/papi/lib64"
     else
-      CMK_LIBDIR="-L /usr/apps/tools/papi/lib"
+      PAPI_LIBDIR="/usr/apps/tools/papi/lib"
     fi
+    CMK_LIBDIR="-L $PAPI_LIBDIR"
+    CMK_LD="$CMK_LD -Wl,-rpath,$PAPI_LIBDIR"
+    CMK_LDXX="$CMK_LDXX -Wl,-rpath,$PAPI_LIBDIR"
   fi
 fi
 
