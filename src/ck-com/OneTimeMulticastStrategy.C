41f4a75fb35123ad3bd7c130fa36e36b3b444d5d
Isaac Dooley <idooley2@illinois.edu>
2009-06-24 19:55:46 +0000

    Adding some new multicast strategies.

--- src/ck-com/OneTimeMulticastStrategy.C
+++ src/ck-com/OneTimeMulticastStrategy.C
@@ -11,7 +11,6 @@
 
 CkpvExtern(CkGroupID, cmgrID);
 
-
 OneTimeMulticastStrategy::OneTimeMulticastStrategy()
   : Strategy(), CharmStrategy() {
   //  ComlibPrintf("OneTimeMulticastStrategy constructor\n");
@@ -41,7 +40,7 @@ void OneTimeMulticastStrategy::insertMessage(CharmMessageHolder *cmsg){
   localMulticast(cmsg);
   
   // The remote multicast method will send the message to the remote PEs, as specified in multMsg
-  remoteMulticast(multMsg);
+  remoteMulticast(multMsg, true);
    
   delete cmsg;    
 }
@@ -58,44 +57,85 @@ void OneTimeMulticastStrategy::localMulticast(CharmMessageHolder *cmsg) {
 
 
 
+
+
 /** 
-    Forward multicast message to all other processors containing destination objects. 
+    Forward multicast message to our successor processors in the spanning tree. 
     Uses CmiSyncListSendAndFree for delivery to this strategy's OneTimeMulticastStrategy::handleMessage method.
 */
-void OneTimeMulticastStrategy::remoteMulticast(ComlibMulticastMsg * multMsg) {
+void OneTimeMulticastStrategy::remoteMulticast(ComlibMulticastMsg * multMsg, bool rootPE) {
+
   envelope *env = UsrToEnv(multMsg);
-
-  // double StartTime = CmiWallTimer();
-
-  int npes = multMsg->nPes;
+    
+  int npes;
+  int *pelist;   
+  
+  /// The index into the PE list in the message
+  int myIndex = -10000; 
+  const int totalDestPEs = multMsg->nPes;
+  const int myPe = CkMyPe();
+  
+  // Find my index in the list of all destination PEs
+  if(rootPE){
+    myIndex = -1;
+  } else {
+    for (int i=0; i<totalDestPEs; ++i) {
+      if(multMsg->indicesCount[i].pe == myPe){
+	myIndex = i;
+	break;
+      }
+    }
+  }
+  
+  CkAssert(myIndex != -10000); // Sanity check
+    
+  determineNextHopPEs(multMsg, myIndex, pelist, npes );
   
   if(npes == 0) {
     CmiFree(env);
-    return;    
-  }
-
-  // ComlibPrintf("[%d] remoteMulticast Sending to %d PEs: \n", CkMyPe(), npes);
- 
-  int *pelist = new int[npes];
-  for (int i=0; i<npes; ++i) {
-    pelist[i] = multMsg->indicesCount[i].pe;
-    //  ComlibPrintf("[%d]   %d messages to pe %d\n", CkMyPe(), multMsg->indicesCount[i].count, multMsg->indicesCount[i].pe);
+    return;
   }
   
   CmiSetHandler(env, CkpvAccess(comlib_handler));
-
-  ((CmiMsgHeaderBasic *) env)->stratid = getInstance();
+  ((CmiMsgHeaderBasic *) env)->stratid = getInstance();  
+  CkPackMessage(&env);
 
   //Collect Multicast Statistics
   RECORD_SENDM_STATS(getInstance(), env->getTotalsize(), pelist, npes);
-    
-  CkPackMessage(&env);
-     
+  
+
+#if DEBUG
+  for(int i=0;i<npes;i++){
+    CkPrintf("[%d] Multicast to %d  rootPE=%d\n", CkMyPe(), pelist[i], (int)rootPE);
+  }
+#endif
+
+  //  if(npes > 0)
   CmiSyncListSendAndFree(npes, pelist, env->getTotalsize(), (char*)env);
-  //CmiSyncBroadcastAndFree(env->getTotalsize(), (char*)env);
+  
+  delete[] pelist;
+}
 
-  //	traceUserBracketEvent( 2201, StartTime, CmiWallTimer()); 
 
+/** 
+    Fill in pelist and npes to which the multicast message will be forwarded from this PE.
+    Caller should delete pelist if npes>0.
+*/
+void OneTimeMulticastStrategy::determineNextHopPEs(ComlibMulticastMsg * multMsg, int myIndex, int * &pelist, int &npes ) {
+  if(myIndex==-1){
+    // We are at a root node of the spanning tree. 
+    // We will forward the message to all other PEs in the destination list.
+    npes = multMsg->nPes;
+    
+    pelist = new int[npes];
+    for (int i=0; i<npes; ++i) {
+      pelist[i] = multMsg->indicesCount[i].pe;
+    }
+  } else {
+    // We are at a leaf node of the spanning tree. 
+    npes = 0;
+  }
+  
 }
 
 
@@ -106,26 +146,99 @@ void OneTimeMulticastStrategy::remoteMulticast(ComlibMulticastMsg * multMsg) {
     Deliver the message to all local elements 
 */
 void OneTimeMulticastStrategy::handleMessage(void *msg){
-  // ComlibPrintf("[%d] In handleMulticastMessage\n", CkMyPe());
-
-  //	double StartTime = CmiWallTimer();
-  
   envelope *env = (envelope *)msg;
+  CkUnpackMessage(&env);
+  
+  ComlibMulticastMsg* multMsg = (ComlibMulticastMsg*)EnvToUsr(env);
+  
+  // Don't use msg after this point. Instead use the unpacked env
+  
   RECORD_RECV_STATS(getInstance(), env->getTotalsize(), env->getSrcPe());
   
-  // Extract the list of elements to be delivered locally
-
+  // Deliver to objects marked as local in the message
   int localElems;
   envelope *newenv;
-  CkArrayIndexMax *local_idx_list;    
-    
-  CkUnpackMessage(&env);
+  CkArrayIndexMax *local_idx_list;  
   sinfo.unpack(env, localElems, local_idx_list, newenv);
-
   ComlibMulticastMsg *newmsg = (ComlibMulticastMsg *)EnvToUsr(newenv);  
-
   deliverToIndices(newmsg, localElems, local_idx_list );
-    
+  
+  // Forward on to other processors if necessary
+  remoteMulticast( multMsg, false);
+
+}
+
+
+
+
+
+/** 
+    Fill in pelist and npes to which the multicast message will be forwarded from this PE.
+    Caller should delete pelist if npes>0.
+*/
+void OneTimeRingMulticastStrategy::determineNextHopPEs(ComlibMulticastMsg * multMsg, int myIndex, int * &pelist, int &npes ) {
+  const int totalDestPEs = multMsg->nPes;
+  const int myPe = CkMyPe();
+
+  if(myIndex == totalDestPEs-1){
+    // Final PE won't send to anyone
+    npes = 0;
+    return;
+  } else {
+    // All non-final PEs will send to next PE in list
+    npes = 1;
+    pelist = new int[1];
+    pelist[0] = multMsg->indicesCount[myIndex+1].pe;
+  }
+
+}
+
+
+
+
+/** 
+    Fill in pelist and npes to which the multicast message will be forwarded from this PE.
+    Caller should delete pelist if npes>0.
+*/
+void OneTimeTreeMulticastStrategy::determineNextHopPEs(ComlibMulticastMsg * multMsg, int myIndex, int * &pelist, int &npes ) {
+  const int totalDestPEs = multMsg->nPes;
+  const int myPe = CkMyPe();
+  
+  // The logical indices start at 0 = root node. Logical index i corresponds to the entry i+1 in the array of PEs in the message
+  
+  // All non-final PEs will send to next PE in list
+  int sendLogicalIndexStart = degree*(myIndex+1) + 1;       // inclusive
+  int sendLogicalIndexEnd = sendLogicalIndexStart + degree - 1;   // inclusive
+  
+  if(sendLogicalIndexEnd-1 >= totalDestPEs){
+    sendLogicalIndexEnd = totalDestPEs;
+  }
+
+  int numSend = sendLogicalIndexEnd - sendLogicalIndexStart + 1;
+  if(numSend <= 0){
+    npes = 0;
+    return;
+  }
+  
+
+#if DEBUG
+  if(numSend > 0)
+    CkPrintf("Tree logical index %d sending to logical %d to %d (totalDestPEs excluding root=%d)  numSend=%d\n",
+	     myIndex+1, sendLogicalIndexStart, sendLogicalIndexEnd, totalDestPEs, numSend);
+#endif
+
+  npes = numSend;
+  pelist = new int[npes];
+  
+  for(int i=0;i<numSend;i++){
+    CkAssert(sendLogicalIndexStart-1+i < totalDestPEs);
+    pelist[i] = multMsg->indicesCount[sendLogicalIndexStart-1+i].pe;
+#if DEBUG
+    CkPrintf("Tree logical index %d sending to PE %d\n", myIndex+1, pelist[i]);
+#endif
+    CkAssert(pelist[i] < CkNumPes());
+  }
+  
 }
 
 
--- src/ck-com/OneTimeMulticastStrategy.h
+++ src/ck-com/OneTimeMulticastStrategy.h
@@ -13,31 +13,35 @@
 #include "ComlibSectionInfo.h"
 
 /**
-   The simplest multicast strategy. This strategy extracts the array section information, and packs the section information and the user message into a single message. The original message is delivered locally, and the new message is sent using CmiSyncListSendAndFree to all other processors containing destination objects.
+   The simplest multicast strategy. This strategy extracts the array section information, and packs the section information and the user message into a single message. The original message is delivered locally, and the new message is sent using CmiSyncListSendAndFree to all other processors containing destination objects. 
 
    This strategy is simpler than those which are derived from the MulticastStrategy class because those maintain a persistant record of previous array section information extracted from the messages, and those provide multiple implementations of the multicast tree (such as ring or multiring or all to all). Those strategies ought to be used when multiple multicasts are sent to the same array section. If an array section is not reused, then this strategy ought to be used.
+
+   A class can be created which inherits from this class, but provides its own determineNextHopPEs method to specify any type of desired spanning tree. For example, OneTimeRingMulticastStrategy forwards the multicast messages in a ring while OneTimeTreeMulticastStrategy forwards messages along a tree of specified degree. In the future, topology aware (both network and core/cpu/node) strategies should be added.
 
    The local messages are delivered through the array manager using the CharmStrategy::deliverToIndices methods. If a destination chare is remote, the array manager will forward it on to the pe that contains the chare.
    
 */
 class OneTimeMulticastStrategy: public Strategy, public CharmStrategy {
  private:
-
-  ComlibSectionInfo sinfo; // This is used to create the multicast messages themselves
   
-  void remoteMulticast(ComlibMulticastMsg *cmsg);
+  ComlibSectionInfo sinfo; // This is used to create the multicast messages themselves
+
+  void remoteMulticast(ComlibMulticastMsg * multMsg, bool rootPE);
   void localMulticast(CharmMessageHolder *cmsg);
   
  public:
 
+  virtual void determineNextHopPEs(ComlibMulticastMsg * multMsg, int myIndex, int * &pelist, int &npes );
+  
  OneTimeMulticastStrategy(CkMigrateMessage *m): Strategy(m), CharmStrategy(m){}
-    
+  
   OneTimeMulticastStrategy();
   ~OneTimeMulticastStrategy();
-
+  
   void insertMessage(MessageHolder *msg) {insertMessage((CharmMessageHolder*)msg);}
   void insertMessage(CharmMessageHolder *msg);
-
+  
   ///Called by the converse handler function
   void handleMessage(void *msg);    
 
@@ -46,6 +50,57 @@ class OneTimeMulticastStrategy: public Strategy, public CharmStrategy {
   PUPable_decl(OneTimeMulticastStrategy);
 
 };
+
+
+
+
+
+/**
+   A OneTimeMulticastStrategy that sends along a ring
+*/
+class OneTimeRingMulticastStrategy: public OneTimeMulticastStrategy {
+  
+ public:
+  void determineNextHopPEs(ComlibMulticastMsg * multMsg, int myIndex, int * &pelist, int &npes );
+
+ OneTimeRingMulticastStrategy(CkMigrateMessage *m): OneTimeMulticastStrategy(m) {}
+ OneTimeRingMulticastStrategy(): OneTimeMulticastStrategy() {}
+  ~OneTimeRingMulticastStrategy() {}
+  
+  void pup(PUP::er &p){ OneTimeMulticastStrategy::pup(p); }
+  
+  PUPable_decl(OneTimeRingMulticastStrategy);
+
+};
+
+
+
+/**
+   A OneTimeMulticastStrategy that sends along a tree of arbitrary degree
+*/
+class OneTimeTreeMulticastStrategy: public OneTimeMulticastStrategy {
+ private:
+  int degree;
+  
+ public:
+  
+  void determineNextHopPEs(ComlibMulticastMsg * multMsg, int myIndex, int * &pelist, int &npes );
+  
+ OneTimeTreeMulticastStrategy(CkMigrateMessage *m): OneTimeMulticastStrategy(m) {}
+ OneTimeTreeMulticastStrategy(int treeDegree=4): OneTimeMulticastStrategy(), degree(treeDegree) {}
+  ~OneTimeTreeMulticastStrategy() {}
+  
+  void pup(PUP::er &p){ 
+    OneTimeMulticastStrategy::pup(p); 
+    p | degree;
+  }
+  
+  PUPable_decl(OneTimeTreeMulticastStrategy);
+  
+};
+
+
+
 #endif
 
 /*@}*/
