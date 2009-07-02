bebf63d2403ed581648f2b0a0de3725513bc1267
Gengbin Zheng <gzheng@illinois.edu>
2009-07-02 19:45:16 +0000

    use -fpic instead of -fPIC to be consistent with icc options

--- src/arch/common/conv-mach-ifort.sh
+++ src/arch/common/conv-mach-ifort.sh
@@ -1,7 +1,7 @@
 # for Intel Fortran compiler 8.0 and higher which is renamed to ifort from ifc
 # does not work for ifc 7.0
-CMK_CF77="ifort -auto -fPIC "
-CMK_CF90="ifort -auto -fPIC "
+CMK_CF77="ifort -auto -fpic "
+CMK_CF90="ifort -auto -fpic "
 CMK_CF90_FIXED="$CMK_CF90 -132 -FI "
 F90DIR=`which ifort 2> /dev/null`
 if test -h "$F90DIR"
