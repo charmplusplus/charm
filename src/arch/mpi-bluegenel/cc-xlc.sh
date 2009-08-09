03bbe1a48940493b2f5f5e0d8bd2ee48b6c50b24
Abhinav Bhatele <bhatele@illinois.edu>
2009-08-09 23:27:37 +0000

    updated compilers and cleanup

--- src/arch/mpi-bluegenel/cc-xlc.sh
+++ src/arch/mpi-bluegenel/cc-xlc.sh
@@ -1,7 +1,7 @@
 XLC_PRE=/opt/ibmcmp
 XLC_POST=bin/blrts_
-CMK_CC="$XLC_PRE/vac/bg/8.0/${XLC_POST}xlc -qcpluscmt "
-CMK_CXX="$XLC_PRE/vacpp/bg/8.0/${XLC_POST}xlC "
+CMK_CC="$XLC_PRE/vac/bg/9.0/${XLC_POST}xlc -qcpluscmt"
+CMK_CXX="$XLC_PRE/vacpp/bg/9.0/${XLC_POST}xlC"
 CMK_LD="$CMK_CC  "
 CMK_LDXX="$CMK_CXX  "
 CMK_C_OPTIMIZE='-O3 -qstrict -Q  '
@@ -13,8 +13,8 @@ CMK_NATIVE_CXX="xlC"
 CMK_NATIVE_LD="$CMK_NATIVE_CC"
 CMK_NATIVE_LDXX="$CMK_NATIVE_CXX"
 
-XLC_F=$XLC_PRE/xlf/bg/10.1
+XLC_F=$XLC_PRE/xlf/bg/11.1
 CMK_CF77="$XLC_F/${XLC_POST}xlf "
-CMK_CF90="$XLC_F/${XLC_POST}xlf90  -qsuffix=f=f90" 
-CMK_CF90_FIXED="$XLC_PRE/xlf/8.1/${XLC_POST}xlf90 " 
+CMK_CF90="$XLC_F/${XLC_POST}xlf90 -qsuffix=f=f90"
+CMK_CF90_FIXED="$XLC_F/${XLC_POST}xlf90 "
 CMK_F90LIBS="-L$XLC_F/blrts_lib -lxlf90 -lxlopt -lxl -lxlfmath"
