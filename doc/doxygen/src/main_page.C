093b5b5171765d1e4b60eb2332599adf9e4a4637
Isaac Dooley <idooley2@illinois.edu>
2009-09-11 20:37:45 +0000

    Improving the main Doxygen Page.

--- doc/doxygen/src/main_page.C
+++ doc/doxygen/src/main_page.C
@@ -6,72 +6,83 @@
 
 \section layers Major Runtime Layers:
 
-<table border=2><tr><td>
-<p>Libraries:
-<p>Communication: Barrier, commlib, multicast, 
-sparseReducer, sparseContiguousReducer
+<table border=2>
 
-<P>Languages: ARMCI, AMPI, TCharm, taskGraph, search
 
-<p>Frameworks: <a href="group__ParFUM.shtml">ParFUM</a>, IFEM, IDXL, Collide, MBlock, AMR
-
-<p>Visualization: liveViz, liveViz3d, netFEM
- 
-</td></tr><tr><td>
+<tr><td>
 Charm++:
-<a href="group__CkArray.shtml">Arrays</a>,
-<a href="group__CkLdb.shtml">Load balancer</a>,
-<a href="group__Comlib.shtml">Comlib</a>,
-<a href="group__CkPerf.shtml">Tracing</a>,
-<a href="group__CkReduction.shtml">Reductions</a>,
-<a href="group__CkArrayImpl.shtml">Array Implementation</a>
-
-</td></tr><tr><td>
-Charm Kernel:
-<a href="group__Ck.shtml">Groups and Chares</a>,  
-<a href="group__CkRegister.shtml">Registration</a>, 
-<a href="group__CkQD.shtml">Quiescence detection</a>, 
-<a href="group__CkFutures.shtml">Thread support</a>,
-<a href="group__CkInit.shtml">Startup</a>,
-<a href="group__CkEnvelope.shtml">Message Envelope</a>,
-Translator, SDAG.
-
-</td></tr><tr><td>
-Converse:
-<a href="group__Converse.shtml">core</a>,
-scheduler, threads, memory allocation,
-conditions, random numbers, converse client/server (CCS).
-
-Converse tracing, parameter marshalling (CPM),
-quiescence.
-
-</td></tr><tr><td>
-<a href="group__Machine.shtml">Converse Machine Layer</a>:
-<ul>
-<li><a href="group__NET.shtml">net</a>
-  <ul>
-  <li>smp
-  <li>gm
-  <li>tcp
-  <li>udp
-  </ul>
-<li>mpi
-<li>elan
-<li>lapi
-<li>vmi
-<li>shmem
-<li>sp3
-<li>t3e
-<li>uth (user-level threads)
-<li>ncube2
-<li>origin
+<p><ul>
+<li> <a href="group__CkArray.shtml">Arrays</a>,
+<li> Load balancer,
+<li> <a href="group__Comlib.shtml">Comlib</a>,
+<li> <a href="group__CkPerf.shtml">Tracing</a>,
+<li>Reductions,
+<li> <a href="group__CkArrayImpl.shtml">Array Implementation</a>
+<li> <a href="group__Ck.shtml">Groups and Chares</a>,  
+<li> <a href="group__CkRegister.shtml">Registration</a>, 
+<li> <a href="group__CkQD.shtml">Quiescence detection</a>, 
+<li> <a href="group__CkFutures.shtml">Futures</a>,
+<li> <a href="group__CkInit.shtml">Startup</a>,
+<li> <a href="group__CkEnvelope.shtml">Message Envelope</a>,
+<li> Translator
+<li> SDAG
 </ul>
+</td></tr>
+ 
+ <tr><td>
+ Converse:
+<p><ul>
+<li> <a href="group__Converse.shtml">core</a>,
+<li> <a href="group__Scheduler.shtml">scheduler</a>, 
+<li> threads, 
+<li> memory allocation,
+<li> conditions, 
+<li> random numbers, 
+<li> converse client/server (CCS).
+<li> Converse tracing, 
+<li> parameter marshalling (CPM),
+<li> quiescence.
+</ul>
+ </td></tr>
+ 
+ 
+ 
+ <tr><td>
+ <a href="group__Machine.shtml">Converse Machine Layers</a>:
+ <p><ul>
+ <li><a href="group__NET.shtml">net</a>
+ <li> ...
+ </ul>
+ </td></tr>
+ 
+ 
+ <tr><td>
+ Converse Configuration Layer:
+<p><ul>
+<li>build system
+<li>charmc
+<li>configure script
+<li>conv-mach.h/.sh
+</ul>
+ </td></tr>
+ 
+ 
+ 
+ 
+ 
+<tr><td>
+<p>Libraries:
+<p>Communication: Barrier, commlib, multicast, sparseReducer, sparseContiguousReducer
+<P>Languages: ARMCI, AMPI, TCharm, taskGraph, search
+<p>Frameworks: <a href="group__ParFUM.shtml">ParFUM</a>, IFEM, IDXL, Collide, MBlock, AMR
+<p>Visualization: liveViz, liveViz3d, netFEM
+</td></tr>
+ 
+ 
 
-</td></tr><tr><td>
-Converse Configuration Layer:
-build system, charmc, configure script, conv-mach.h/.sh.
-
-</td></tr></table>
+ 
+ 
+ </table>
 
 \section utilities Utility Code
 PUP, CkBitVector, CkDll, CkHashtable, 
