11e6260d30d397527301538dfd130b2f26ce4018
Gengbin Zheng <gzheng@illinois.edu>
2009-09-29 02:39:11 +0000

    disable immediate msg since it is not working right now, will fix later.

--- src/arch/mpi-win32/conv-mach.h
+++ src/arch/mpi-win32/conv-mach.h
@@ -57,7 +57,8 @@
 #define CMK_WHEN_PROCESSOR_IDLE_BUSYWAIT                   1
 #define CMK_WHEN_PROCESSOR_IDLE_USLEEP                     0
 
-
+#undef  CMK_IMMEDIATE_MSG
+#define CMK_IMMEDIATE_MSG				   0
 
 #define CMK_WEB_MODE                                       0
 
