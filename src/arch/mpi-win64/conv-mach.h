fdc35b85fef5cb68c7c8206929e5e0d20c0ea804
Gengbin Zheng <gzheng@illinois.edu>
2009-09-29 05:53:05 +0000

    change CMK_SHARED_VARS_NT_THREADS to CMK_SHARED_VARS_UNAVAILABLE to avoid a separate comm thread for non-smp.

--- src/arch/mpi-win64/conv-mach.h
+++ src/arch/mpi-win64/conv-mach.h
@@ -8,8 +8,8 @@
 #ifndef _CONV_MACH_H
 #define _CONV_MACH_H
 
-#define CMK_64BIT                                          1
 #define CMK_CONVERSE_MPI                                   1
+#define CMK_64BIT                                          1
 
 #define CMK_GETPAGESIZE_AVAILABLE                          0
 
@@ -19,20 +19,16 @@
 #define CMK_MEMORY_PAGESIZE                                8192
 #define CMK_MEMORY_PROTECTABLE                             0
 
-#define CMK_IS_HETERO                                      1
-
 #define CMK_NODE_QUEUE_AVAILABLE                           0
 
 #define CMK_SHARED_VARS_EXEMPLAR                           0
-#define CMK_SHARED_VARS_UNAVAILABLE                        0
+#define CMK_SHARED_VARS_UNAVAILABLE                        1
 #define CMK_SHARED_VARS_UNIPROCESSOR                       0
-#define CMK_SHARED_VARS_NT_THREADS                         1
+#define CMK_SHARED_VARS_NT_THREADS                         0
 
 #define CMK_SIGNAL_NOT_NEEDED                              1
 #define CMK_SIGNAL_USE_SIGACTION                           0
 #define CMK_SIGNAL_USE_SIGACTION_WITH_RESTART              0
-
-#define CMK_SYNCHRONIZE_ON_TCP_CLOSE                       0
 
 #define CMK_THREADS_USE_JCONTEXT                           0
 #define CMK_THREADS_REQUIRE_NO_CPV                         0
@@ -59,8 +55,6 @@
 #define CMK_WEB_MODE                                       0
 
 #define CMK_LBDB_ON					   1
-
-#define CMK_COMPILEMODE_ANSI				   1
 
 #undef  CMK_IMMEDIATE_MSG
 #define CMK_IMMEDIATE_MSG				   0
