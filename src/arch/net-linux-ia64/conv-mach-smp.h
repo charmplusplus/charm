362218ee2bd60aed0824bf7d3f47ca45f24acaae
Gengbin Zheng <gzheng@illinois.edu>
2009-10-28 03:30:56 +0000

    switch to memory fence for IA64

--- src/arch/net-linux-ia64/conv-mach-smp.h
+++ src/arch/net-linux-ia64/conv-mach-smp.h
@@ -11,10 +11,12 @@
 
 #undef CMK_MALLOC_USE_GNU_MALLOC
 #undef CMK_MALLOC_USE_OS_BUILTIN
+#undef CMK_MALLOC_USE_GNUOLD_MALLOC
 #define CMK_MALLOC_USE_GNU_MALLOC                          0
 #define CMK_MALLOC_USE_OS_BUILTIN                          1
+#define CMK_MALLOC_USE_GNUOLD_MALLOC                       0
 
-#define CMK_PCQUEUE_LOCK                                   1
+#define CMK_PCQUEUE_LOCK                                   0
 /*Replaced by CMK_NOT_USE_TLS_THREAD as the default case*/
 /*#define CMK_USE_TLS_THREAD                                 1*/
 
