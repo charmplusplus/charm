9c1b930f66fd1873cdbd30a7b48bd0477ef5a2d4
Ramprasad Venkataraman <ramv@illinois.edu>
2009-11-10 23:29:01 +0000

    Fix broken build. bzero aliases memset on systems where its not available

--- src/ck-core/charm++.h
+++ src/ck-core/charm++.h
@@ -10,8 +10,14 @@
 
 #include <stdlib.h>
 #include <memory.h>
-#include "charm.h"
 
+#if CMK_HAS_STRINGS_H
+  #include <strings.h>            /* defines bzero */
+#else
+  #define bzero(s,n)   memset(s,0,n)
+#endif
+
+#include "charm.h"
 #include "middle.h"
 
 class CMessage_CkArgMsg {
