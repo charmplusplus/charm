765550275bf0c8d7d86fc5cae6bad50a5a6b3053
Sameer Kumar <sameerk@login1.surveyor.alcf.anl.gov>
2009-11-26 14:36:49 -0600

    Added a comment.

--- src/arch/bluegenep/conv-mach-smp.h
+++ src/arch/bluegenep/conv-mach-smp.h
@@ -9,5 +9,6 @@
 #define CMK_SHARED_VARS_UNAVAILABLE                        0
 #define CMK_SHARED_VARS_POSIX_THREADS_SMP                  1
 
+/* Right now only comm thread (no multicore) and tls thread version with gcc works on Blue Gene*/
 #define CMK_MULTICORE                                      0
 #define CMK_USE_TLS_THREAD                                 1 
