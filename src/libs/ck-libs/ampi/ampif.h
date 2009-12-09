5ff01673f2df509fdf38239cb5aed98d241c8b95
Gengbin Zheng <gzheng@honest4.ncsa.uiuc.edu>
2009-12-09 16:12:59 -0600

    a new fix for MPI_IN_PLACE for the fortran API (which is tested to work for ifort, don't know about others)

--- src/libs/ck-libs/ampi/ampif.h
+++ src/libs/ck-libs/ampi/ampif.h
@@ -89,7 +89,7 @@
        integer, parameter :: MPI_KEYVAL_INVALID = -1
        integer, parameter :: MPI_INFO_NULL = -1
 
-       integer*8, parameter ::  MPI_IN_PLACE = -1
+       integer, pointer   :: MPI_IN_PLACE
 
        integer, parameter :: MPI_BOTTOM = 0
        integer, parameter :: MPI_UNDEFINED = -32766
