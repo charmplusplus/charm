5a6cfb3dbd5029933f5dcd6ab29d4f90b66474b5
Chao Mei <chaomei09@gmail.com>
2010-02-09 17:49:39 -0600

    Revert the default mode to polling mode in non-smp case because the CmiNodeLock is not defined as a real lock in non-smp case.

--- src/arch/lapi/machine.c
+++ src/arch/lapi/machine.c
@@ -1853,8 +1853,8 @@ void ConverseInit(int argc, char **argv, CmiStartFn fn, int usched, int initret)
     if(CmiGetArgFlag(argv,"+poll")) CsvAccess(lapiInterruptMode) = 0;
     if(CmiGetArgFlag(argv,"+nopoll")) CsvAccess(lapiInterruptMode) = 1;    
 #else
-    /* To make the interrupt mode as default in the non-smp case */
-    CsvAccess(lapiInterruptMode) = 1;    
+    /* To make the interrupt mode as default in the non-smp case -THERE ARE PROBLEMS --Chao Mei */
+    CsvAccess(lapiInterruptMode) = 0;    
     if(CmiGetArgFlag(argv,"+poll")) CsvAccess(lapiInterruptMode) = 0;
     if(CmiGetArgFlag(argv,"+nopoll")) CsvAccess(lapiInterruptMode) = 1;  
 
