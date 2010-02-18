15a72fe7d3b58dc05e5c0f0955a1609dfe69d91f
Gengbin Zheng <gzheng@gzheng-desktop.(none)>
2010-02-18 00:01:07 -0600

    remove stdout, which can be a reason for crashing (I guess some malloc in fflush() is mistakenly treated as isomalloc)

--- tests/ampi/migration/test.C
+++ tests/ampi/migration/test.C
@@ -28,26 +28,26 @@ int main(int argc,char **argv)
 	int destination_pe = (CkMyPe() + 1) % CkNumPes();
 	CkPrintf("Trying to migrate partition %d from pe %d to %d\n",
 		 rank, CkMyPe(), destination_pe);
-	fflush(stdout);
+	//fflush(stdout);
 	CkAssert(destination_pe >= 0);
 	int migrate_test = CkMyPe();
 	printf("Entering TCHARM_Migrate_to, "
                "FEM_My_partition is %d, "
 	       "CkMyPe() is %d, migrate_test is %d\n",
 	       rank, CkMyPe(), migrate_test);
-	fflush(stdout);
+	//fflush(stdout);
 	AMPI_Migrateto(destination_pe);
 	printf("Leaving TCHARM_Migrate_to, "
                "FEM_My_partition is %d, "
 	       "CkMyPe() is %d, migrate_test is %d\n",
 	       rank, CkMyPe(), migrate_test);
-	fflush(stdout);
+	//fflush(stdout);
       }
 
       MPI_Barrier(MPI_COMM_WORLD);
 
       CkPrintf("Done with step %d\n", step);
-      fflush(stdout);
+      //fflush(stdout);
     }
 
 
