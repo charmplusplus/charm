40552ca50b75701945f25181558d8e8a8616dfd8
Isaac Dooley <idooley2@bp-login1.(none)>
2010-05-04 11:32:25 -0500

    Adding fortran interface

---
+++ src/ck-cp/controlPointsf.h
@@ -0,0 +1,17 @@
+
+              
+interface
+
+subroutine controlPointTimingStamp() 
+end subroutine
+
+subroutine gotoNextPhase()
+end subroutine
+
+integer function controlPoint(lb, ub)
+     integer :: lb
+integer :: ub
+end function
+
+end interface
+
