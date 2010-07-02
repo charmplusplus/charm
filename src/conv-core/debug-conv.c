f8ecb7bc608d3ced1a746712e2e9e9645be58db5
Filippo Gioachin <gioachin@uiuc.edu>
2010-07-02 16:42:40 -0500

    For now make conditional available only in non-bigsim

--- src/conv-core/debug-conv.c
+++ src/conv-core/debug-conv.c
@@ -317,10 +317,10 @@ void CpdInit(void)
 
   CpvInitialize(void *, debugQueue);
   CpvAccess(debugQueue) = CdsFifo_Create();
+#endif
 
   CpvInitialize(void *, conditionalQueue);
   CpvAccess(conditionalQueue) = CdsFifo_Create();
-#endif
   
   CcsRegisterHandler("ccs_debug", (CmiHandler)CpdDebugHandler);
   CcsSetMergeFn("ccs_debug", CcsMerge_concat);
