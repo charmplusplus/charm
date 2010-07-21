689788d9f0da875fde02c2a970b472e738ea9672
Gengbin Zheng <gzheng@illinois.edu>
2010-07-21 18:24:17 -0500

    fixed dependence of all ci files to charmxi. In old setting, changing charmxi does not recompile ci files.

--- src/xlat-i/xi-main.C
+++ src/xlat-i/xi-main.C
@@ -168,7 +168,7 @@ int main(int argc, char *argv[])
       if(loc != std::string::npos)
           ciFileBaseName = ciFileBaseName.substr(loc+1);
       m->genDepends(ciFileBaseName);
-      cout << ciFileBaseName << ".stamp: " << fname << endl;
+      cout << ciFileBaseName << ".stamp: " << fname << " charmxi" << endl;
   }
   else
       m->generate();
