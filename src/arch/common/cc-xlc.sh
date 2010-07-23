63fd46a13fea0b152e6346c02e51c7a23e3004ef
Gengbin Zheng <gzheng@bp-login1.(none)>
2010-07-23 11:36:09 -0500

    keep up to date (for blueprint)

--- src/arch/common/cc-xlc.sh
+++ src/arch/common/cc-xlc.sh
@@ -8,9 +8,6 @@ CMK_PIC='-qpic=small'
 CMK_LD="$CMK_CC -brtl "
 CMK_LDXX="$CMK_CXX -brtl "
 
-CMK_AR='ar cq'
-CMK_NM='nm '
-
 CMK_NATIVE_CC='xlc_r -q32 '
 CMK_NATIVE_LD='xlc_r -q32 '
 CMK_NATIVE_CXX='xlC_r -q32 -D_H_UNISTD -DYY_NEVER_INTERACTIVE=1 '
@@ -19,3 +16,10 @@ CMK_NATIVE_LDXX='xlC_r -q32 '
 CMK_CF77='xlf_r -q32  '
 CMK_CF90='xlf90_r  -qsuffix=f=f90 -q32 ' 
 CMK_CF90_FIXED='xlf90_r -q32 ' 
+
+if test "$isAIX" = "true"
+then
+  AR_OPTS="-X 32"
+fi
+CMK_AR="ar $AR_OPTS cq"
+CMK_NM="nm $AR_OPTS"
