94c8b723437e5bab88baee937ebe2cf31436fd37
Phil Miller <mille121@illinois.edu>
2011-04-26 22:28:25 -0500

    IO Framework: Initial implementation of striped output forwarding
    For bulk output to files of a known size, a striping mechanism that
    makes some number of PEs responsible for a portion each. There are
    still many limitations, but the basics are there and a test is
    working.

--- src/libs/ck-libs/io/ckio.C
+++ src/libs/ck-libs/io/ckio.C
@@ -1,12 +1,61 @@
 #include <ckio.h>
+#include <errno.h>
+#include <algorithm>
 
 namespace Ck { namespace IO {
+    Manager::Manager() : nextToken(0) {
+      __sdag_init();
+      run();
+    }
 
-  Manager::Manager() {
+    void Manager::prepareOutput(const char *name, size_t bytes,
+				CkCallback ready, CkCallback complete,
+				Options opts) {
+      thisProxy[0].prepareOutput_central(name, bytes, ready, complete, opts);
+    }
 
-  }
+    void Manager::write(Token token, const char *data, size_t bytes, size_t offset) {
+      Options &opts = files[token].opts;
+      do {
+	size_t stripe = offset / opts.peStripe;
+	int pe = opts.basePE + stripe * opts.skipPEs;
+	size_t bytesToSend = std::min(bytes, opts.peStripe - offset % opts.peStripe);
+	thisProxy[pe].write_forwardData(token, data, bytesToSend, offset);
+	data += bytesToSend;
+	offset += bytesToSend;
+	bytes -= bytesToSend;
+      } while (bytes > 0);
+    }
 
+    void Manager::write_forwardData(Token token, const char *data, size_t bytes,
+				    size_t offset) {
+      CkAssert(offset + bytes <= files[token].bytes);
+      // XXX: CkAssert(this is the right processor to receive this data)
 
+      while (bytes > 0) {
+	ssize_t ret = pwrite(files[token].fd, data, bytes, offset);
+	if (ret < 0)
+	  if (errno == EINTR)
+	    continue;
+	  else {
+	    CkPrintf("Output failed on PE %d: %s", CkMyPe(), strerror(errno));
+	    CkAbort("Giving up");
+	  }
+	bytes -= ret;
+	data += ret;
+	offset += ret;
+      }
+    }
 
+    void Manager::prepareInput(const char *name, CkCallback ready, Options opts) {
+      CkAbort("not yet implemented");
+    }
+
+    void Manager::read(Token token, void *data, size_t bytes, size_t offset,
+		       CkCallback complete) {
+      CkAbort("not yet implemented");
+    }
   }
 }
+
+#include "CkIO.def.h"
--- src/libs/ck-libs/io/ckio.h
+++ src/libs/ck-libs/io/ckio.h
@@ -1,14 +1,67 @@
 #ifndef CK_IO_H
 #define CK_IO_H
 
-#include "CkIO.decl.h"
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <fcntl.h>
+#include <pup_stl.h>
 
 namespace Ck { namespace IO {
   /// Identifier for a file to be accessed
   typedef int Token;
 
-  class Options {
+  struct Options {
+    Options()
+      : peStripe(-1), writeStripe(-1), activePEs(-1), basePE(-1), skipPEs(-1)
+      { }
 
+    /// How much contiguous data (in bytes) should be assigned to each active PE
+    size_t peStripe;
+    /// How much contiguous data (in bytes) should a PE gather before writing it out
+    size_t writeStripe;
+    /// How many PEs should participate in this activity
+    int activePEs;
+    /// Which PE should be the first to participate in this activity
+    int basePE;
+    /// How should active PEs be spaced out?
+    int skipPEs;
+
+    void pup(PUP::er &p) {
+      p|peStripe;
+      p|writeStripe;
+      p|activePEs;
+      p|basePE;
+      p|skipPEs;
+    }
+  };
+
+    struct FileReadyMsg;
+  }
+}
+
+#include "CkIO.decl.h"
+#include <map>
+
+namespace Ck { namespace IO {
+  struct FileReadyMsg : public CMessage_FileReadyMsg {
+    Token token;
+    FileReadyMsg(const Token &tok) : token(tok) {}
+  };
+
+  struct FileInfo {
+    std::string name;
+    Options opts;
+    size_t bytes;
+    int fd;
+
+    FileInfo(std::string name_, size_t bytes_, Options opts_)
+      : name(name_), opts(opts_), bytes(bytes_), fd(-1)
+      { }
+    FileInfo()
+      : bytes(-1), fd(-1)
+      { }
   };
 
   /// Class to mediate IO operations between Charm++ application code
@@ -20,15 +73,33 @@ namespace Ck { namespace IO {
   public:
     Manager();
 
+    Manager_SDAG_CODE;
+
+    /// Application-facing methods, invoked locally on the calling PE
     void prepareOutput(const char *name, size_t bytes,
 		       CkCallback ready, CkCallback complete,
 		       Options opts = Options());
-    void write(Token token, const void *data, size_t bytes, size_t offset);
+    void write(Token token, const char *data, size_t bytes, size_t offset);
 
     void prepareInput(const char *name, CkCallback ready,
 		      Options opts = Options());
     void read(Token token, void *data, size_t bytes, size_t offset,
 	      CkCallback complete);
+
+
+    /// Internal methods, used for interaction among IO managers across the system
+    void write_forwardData(Token token, const char *data, size_t bytes, size_t offset);
+
+  private:
+    int filesOpened;
+    Token nextToken;
+    std::map<Token, FileInfo> files;
+    CkCallback nextReady;
+
+    int lastActivePE(const Options &opts) {
+      return opts.basePE + (opts.activePEs-1)*opts.skipPEs;
+    }
+
   };
 
   }}
