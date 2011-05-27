e9e3eabb883304476842192827170a0f38f52476
Eric Bohm <ebohm@illinois.edu>
2011-05-26 22:10:45 -0500

    new test to demonstrate a race condition

---
+++ tests/charm++/ckAllocSysMsgTest/ckAllocSysMsgTest.h
@@ -0,0 +1,30 @@
+class main : public CBase_main
+{
+ public:
+  main(CkArgMsg *msg);
+  void startBatching();
+  void reportInArr();
+  void reportInGrp();
+  void nextBatch();
+  void done();
+ private:
+  int reportedArr,reportedGrp, completeBatches, arrSize, nBatches, batchSize;
+
+};
+
+class RaceMeArr : public CBase_RaceMeArr
+{
+ public:
+ RaceMeArr(int nElements_):nElements(nElements_){}
+  void recvMsg();
+  RaceMeArr(CkMigrateMessage *m) {};
+ private:
+  int nElements;
+};
+
+class RaceMeGrp : public Group
+{
+ public:
+  RaceMeGrp(){}
+  void recvMsg();
+};
