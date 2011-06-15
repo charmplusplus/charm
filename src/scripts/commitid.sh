b1e44b1c4964724efcd23aa376e3bfb59d8a6797
Gengbin Zheng <gzheng@illinois.edu>
2011-06-15 15:49:14 -0500

    bash=>sh

--- src/scripts/commitid.sh
+++ src/scripts/commitid.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 # Get a string describing the current code, and compare it to the
 # recorded value in VERSION. Copy over that if it's changed, so that
 # Make sees a more recent file.
 
 VOLD=""
-if [ -r VERSION ]
+if test -r VERSION
 then
     VOLD=`cat VERSION`
 fi
@@ -15,7 +15,7 @@ git describe --long --always > VERSION.new || touch VERSION.new
 
 VNEW=`cat VERSION.new`
 
-if [ "$VOLD" != "$VNEW" ]
+if test -n "$VNEW" -a "$VOLD" != "$VNEW"
 then
     cp VERSION.new VERSION
     echo Copying VERSION.new = $VNEW over VERSION = $VOLD
