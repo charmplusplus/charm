8908b92e6c203975a7a4a72e094d6a6f3602e25d
Gengbin Zheng <gzheng@illinois.edu>
2011-07-12 16:30:47 -0500

    updated to point to right ampiCC scripts.

--- src/libs/ck-libs/ampi/romio/config.sh
+++ src/libs/ck-libs/ampi/romio/config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
 CHARMDIR=`util/charmdir`
-./configure -mpiincdir=$CHARMDIR/include -mpilib=$CHARMDIR/lib/libmoduleampi.a -mpibindir=$CHARMDIR/bin -cc=$CHARMDIR/bin/mpicc -fc=$CHARMDIR/bin/mpif77 -f90=$CHARMDIR/bin/mpif90
+./configure -mpiincdir=$CHARMDIR/include -mpilib=$CHARMDIR/lib/libmoduleampi.a -mpibindir=$CHARMDIR/bin -cc=$CHARMDIR/bin/ampicc -fc=$CHARMDIR/bin/ampif77 -f90=$CHARMDIR/bin/ampif90
