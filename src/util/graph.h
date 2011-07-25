881ea4cfe0b878e9bbb4f11491539eda116e687c
Gengbin Zheng <gzheng@illinois.edu>
2011-07-24 22:27:01 -0500

    move graph.[hc] to converse util directory

---
+++ src/util/graph.h
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * $Source$
+ * $Author$
+ * $Date$
+ * $Revision$
+ *****************************************************************************/
+
+/**
+ * \addtogroup CkLdb
+*/
+/*@{*/
+
+#ifndef _GRAPH_H
+#define _GRAPH_H
+
+typedef struct {
+  int index;
+  float weight;
+  int firstEdge;
+  int numEdges;
+} VertexRecord;
+
+
+typedef struct {
+  int V, E;
+  VertexRecord * vertices;
+  int * edges;
+  int currentVertex; /* needed during construction of graph */
+  int currentEdge; /* needed during construction of graph */
+} Graph;
+
+
+Graph * generateRandomGraph(int numNodex);
+
+#endif
+
+/*@}*/
