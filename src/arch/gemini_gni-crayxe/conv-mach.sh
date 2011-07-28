4c8c9543e91614afec698b46d2d9cb663a307534
Yanhua Sun <suny@arthur-login1.ccs.ornl.gov>
2011-07-28 11:44:04 -0400

    change compiler to cc

--- src/arch/gemini_gni-crayxe/conv-mach.sh
+++ src/arch/gemini_gni-crayxe/conv-mach.sh
@@ -4,15 +4,15 @@ UGNI_CFLAGS=`pkg-config --cflags cray-ugni`
 UGNI_LIBS=`pkg-config --libs cray-ugni`
 
 CMK_CPP_CHARM='/lib/cpp -P'
-CMK_CPP_C="gcc -E"
-CMK_CC="gcc $PMI_CFLAGS $UGNI_CFLAGS "
-CMK_CXX="g++ $PMI_CFLAGS $UGNI_CFLAGS"
+CMK_CPP_C="cc -E"
+CMK_CC="cc $PMI_CFLAGS $UGNI_CFLAGS "
+CMK_CXX="CC $PMI_CFLAGS $UGNI_CFLAGS"
 CMK_CXXPP="$CMK_CXX -x c++ -E  "
 CMK_LD="eval $CMK_CC "
 CMK_RANLIB='ranlib'
 CMK_LIBS='-lckqt'
 CMK_LD_LIBRARY_PATH="-rpath $CHARMLIBSO/ $PMI_LIBS $UGNI_LIBS"
-CMK_QT='gcc'
+CMK_QT='cc'
 CMK_XIOPTS='-ansi'
 
 # fortran compilers
