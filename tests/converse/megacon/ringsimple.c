a88e3869b84f14ca91e724271bee33d56925e536
Gengbin Zheng <gzheng@illinois.edu>
2011-08-17 13:38:14 -0700

    minor change

--- tests/converse/megacon/ringsimple.c
+++ tests/converse/megacon/ringsimple.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <converse.h>
-#define entries    10
+#define entries    100
 void Cpm_megacon_ack();
 
 typedef struct
 {
   char core[CmiMsgHeaderSizeBytes]; 
   int hops, ringno;
-  int data[entries];
+  int data[10];
 } ringmsg;
 
 CpvDeclare(int, ringsimple_hop_index);
@@ -22,21 +22,23 @@ void ringsimple_hop(ringmsg *msg)
 {
   int thispe = CmiMyPe();
   int nextpe = (thispe+1) % CmiNumPes();
+  // CmiPrintf("[%d] ringsimple #%d hop send to %d hop: %d\n", thispe, msg->ringno, nextpe, msg->hops);
   int i;
-  for (i=0; i<entries; i++)
+  for (i=0; i<10; i++)
     if (msg->data[i] != i) ringsimple_fail();
   if (msg->hops) {
     msg->hops--;
     CmiSyncSendAndFree(nextpe, sizeof(ringmsg), msg);
   } else {
     Cpm_megacon_ack(CpmSend(0));
+    CmiFree(msg);
   }
 }
 
 void ringsimple_init(void)
 {
   int i; ringmsg msg={{0},1000,0,{0}};
-  for (i=0; i<entries; i++) msg.data[i] = i;
+  for (i=0; i<10; i++) msg.data[i] = i;
   CmiSetHandler(&msg, CpvAccess(ringsimple_hop_index));
   for (i=0; i<entries; i++) {
     msg.ringno = i;
