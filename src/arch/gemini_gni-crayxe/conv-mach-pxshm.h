b4e01f4f1f0cc53653d814085c4e073c2479afe2
Gengbin Zheng <gzheng@illinois.edu>
2011-11-11 23:16:08 -0800

    always compile with pxshm as default

--- src/arch/gemini_gni-crayxe/conv-mach-pxshm.h
+++ src/arch/gemini_gni-crayxe/conv-mach-pxshm.h
@@ -1,3 +1,6 @@
+#ifndef _CONV_MACH_PXSHM_
+#define _CONV_MACH_PXSHM_
+
 #undef CMK_USE_PXSHM
 #define CMK_USE_PXSHM 			1
 
@@ -11,3 +14,5 @@
 #define CMK_WHEN_PROCESSOR_IDLE_USLEEP  0
 
 #define PXSHM_LOCK                      1
+
+#endif
