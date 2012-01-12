6c818c5d3dc2a172e1561156553bd9bf35089e3c
Yanhua Sun <sun51@hopper01.(none)>
2012-01-12 10:23:46 -0800

    enable immediate message in gemini

--- src/arch/gemini_gni/conv-common.h
+++ src/arch/gemini_gni/conv-common.h
@@ -33,7 +33,7 @@
 
 #define NODE_0_IS_CONVHOST                                 1
 
-#define CMK_IMMEDIATE_MSG				   0
+#define CMK_IMMEDIATE_MSG				   1
 #define CMK_MACHINE_PROGRESS_DEFINED                       1
 
 #define CMK_LB_CPUTIMER					   0
