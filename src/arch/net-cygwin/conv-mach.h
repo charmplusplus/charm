26c9efcdd3f7ce8a8b60861733fb99e81644d209
Gengbin Zheng <gzheng@illinois.edu>
2012-02-22 11:40:22 -0600

    added signal back

--- src/arch/net-cygwin/conv-mach.h
+++ src/arch/net-cygwin/conv-mach.h
@@ -38,9 +38,9 @@
 #define CMK_SHARED_VARS_UNIPROCESSOR                       0
 #define CMK_SHARED_VARS_NT_THREADS                         0
 
-#define CMK_SIGNAL_NOT_NEEDED                              1
+#define CMK_SIGNAL_NOT_NEEDED                              0
 #define CMK_SIGNAL_USE_SIGACTION                           0
-#define CMK_SIGNAL_USE_SIGACTION_WITH_RESTART              0
+#define CMK_SIGNAL_USE_SIGACTION_WITH_RESTART              1
 
 #define CMK_THREADS_REQUIRE_NO_CPV                         0
 #define CMK_THREADS_COPY_STACK                             0
