6387b521f1be517674624ac04727b7bc597a63aa
Chao Mei <chaomei2@illinois.edu>
2012-02-29 11:46:24 -0600

    Minor fix to get rid of warnings

--- NodeHelper.C
+++ NodeHelper.C
@@ -24,7 +24,7 @@ static volatile int exitFlag = 0;
 static int HelperOnCore(){
 #if CMK_OS_IS_LINUX
 	char fname[64];
-	sprintf(fname, "/proc/%ld/task/%ld/stat", getpid(), syscall(SYS_gettid));
+	sprintf(fname, "/proc/%d/task/%ld/stat", getpid(), syscall(SYS_gettid));
 	FILE *ifp = fopen(fname, "r");
 	if(ifp == NULL) return -1;
 	fseek(ifp, 0, SEEK_SET);
