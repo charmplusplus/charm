a83d76bb9a3cb41efe8c3035c2fedfa9aa5d7e8a
Eric Bohm <ebohm@illinois.edu>
2012-03-21 18:05:18 +0000

    xlc path tinkering

--- src/arch/pami-bluegeneq/cc-xlc.sh
+++ src/arch/pami-bluegeneq/cc-xlc.sh
@@ -3,7 +3,7 @@ XLC_PRE=$XLC_TYPICAL_PRE
 
 XLC_TYPICAL_POST_BG=vacpp/bg/12.1/bin/bg
 XLC_TYPICAL_POST=vacpp/bg/12.1/bin
-XLC_POST=$XLC_TYPICAL_POST
+XLC_POST=$XLC_TYPICAL_POST_BG
 
 # if no floor set, use typical floor path
 if test -n "$BGQ_XLC_PRE"
