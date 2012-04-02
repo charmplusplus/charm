b7c875c2f4c86e311f9fc17624b7ed206f7bc86e
Harshitha <gplkrsh2@illinois.edu>
2012-04-02 12:24:12 -0500

    Adding Communication aware refinement based strategy called CommAwareRefineLB. Made RefineSwapLB and CommAwareRefineLB to be built by default

---
+++ src/ck-ldb/CommAwareRefineLB.h
@@ -0,0 +1,43 @@
+/**
+ * \addtogroup CkLdb
+*/
+/*@{*/
+
+#ifndef _COMMAWARELB_H_
+#define _COMMAWARELB_H_
+
+#include "CentralLB.h"
+#include "CommAwareRefineLB.decl.h"
+
+void CreateCommAwareRefineLB();
+BaseLB * AllocateCommAwareRefineLB();
+
+class CommAwareRefineLB : public CentralLB {
+
+public:
+  struct HeapData {
+    double load;
+    int    pe;
+    int    id;
+
+  };
+
+  CommAwareRefineLB(const CkLBOptions &);
+  CommAwareRefineLB(CkMigrateMessage *m):CentralLB(m) {
+    lbname = "CommAwareRefineLB";
+  }
+  void work(LDStats* stats);
+private:
+	enum           HeapCmp {GT = '>', LT = '<'};
+    	void           Heapify(HeapData*, int, int, HeapCmp);
+	void           HeapSort(HeapData*, int, HeapCmp);
+	void           BuildHeap(HeapData*, int, HeapCmp);
+	CmiBool        Compare(double, double, HeapCmp);
+	HeapData*      BuildCpuArray(BaseLB::LDStats*, int, int *);  
+	HeapData*      BuildObjectArray(BaseLB::LDStats*, int, int *);      
+	CmiBool        QueryBalanceNow(int step);
+};
+
+#endif /* _COMMAWARELB_H_ */
+
+/*@}*/
