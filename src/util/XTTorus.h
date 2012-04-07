f7d9b1b62e2ff7997fbe377e5eda1b32f84b30f9
Nikhil Jain <nikhil@illinois.edu>
2012-04-07 02:57:22 -0500

    Bug fix in coordinatesToRank of Cray Torus

--- src/util/XTTorus.h
+++ src/util/XTTorus.h
@@ -225,7 +225,7 @@ class XTTorusManager {
     }
 
     inline int coordinatesToRank(int x, int y, int z, int t) {
-      if(coords2pid[x+origin.x][y+origin.y][z+origin.z][t+origin.t] == -1) {
+      if(coords2pid[x+origin.x][y+origin.y][z+origin.z][t+origin.t] == -1 && ((t+origin.t)>=CmiNumCores)) {
 				return coords2pid[x+origin.x][y+origin.y][z+origin.z][t+origin.t-CmiNumCores()]; 
 			} else {
 				return coords2pid[x+origin.x][y+origin.y][z+origin.z][t+origin.t];
