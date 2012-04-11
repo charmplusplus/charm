0a697232232e3fc483118a23e69ef535f64f8c74
Chao Mei <chaomei2@illinois.edu>
2012-04-11 14:03:22 -0500

    NodeHelper: move to corresponding directories

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-make clean -f Makefile.template
-make CHARMDIR="$HOME/curcvs/charm/net-linux-x86_64-smp-opt" -f Makefile.template
-
-make clean -f Makefile.template
-make CHARMDIR="$HOME/curcvs/charm/net-linux-x86_64-smp-dbg" -f Makefile.template
