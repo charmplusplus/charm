b00c27f30fdf672f32cabec8daf2a4730da89e7b
Ramprasad Venkataraman <ramv@illinois.edu>
2012-07-30 18:56:25 -0500

    doc: more regex to cleanup html manual markup

--- doc/l2h_sanitize_markup.sh
+++ doc/l2h_sanitize_markup.sh
@@ -21,19 +21,21 @@ do
 	cwd=`echo $cwd | sed -e 's@/home/net@/expand/home@'`
 	sed -e 's!'`pwd`'/!!g' $f > tmp || die "error running sed on $f"
 	mv $f $f.bak || die "error backing up $f"
-    # Munge through the markup and... 
+  # Munge through the markup and... 
 	# Relativize all paths
-    # Replace placeholder with script tag
-    # Replace div.alltt with pre tag
+	# Replace placeholder with script tag
+	# Replace div.alltt with pre tag
 	# Delete tt tag that is no longer supported in html5
 	# Remove matching closing tags
-    # and also remove the closing div matching the div.alltt
+	# and also remove the closing div matching the div.alltt
+	# Remove all br tags in between pre tags
 	# and finally delete the line if it just has whitespace
 	sed -e 's!'$cwd'/!!g' \
 	    -e 's|replace_with_script|script|g' \
-		-e 's|<DIV CLASS="alltt"[^>]*>|<pre>|g' \
+		-e 's|<DIV CLASS="alltt"[^>]*>|<pre><code>|g' \
 		-e 's|<TT>||g' \
-		-e '/<\/TT>/{N;s|<\/TT>||g;/\n<\/DIV>/{s|<\/DIV>|</pre>|g}}' \
+		-e '/<\/TT>/{N;s|<\/TT>||g;/\n<\/DIV>/{s|<\/DIV>|</code></pre>|g}}' \
+		-e '/<pre>/,/<\/pre>/s|<BR>||g' \
 		-e '/^\w*$/d' \
 	tmp > $f || die "error running sed on $f"
 	rm $f.bak
