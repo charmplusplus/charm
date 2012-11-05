e397910e641646b6b2a8b2f111c4cac24384aa21
Gengbin Zheng <gzheng@h2ologin3.ncsa.illinois.edu>
2012-11-05 15:59:18 -0600

    use LOADEDMODULES env var instead of module shell command to check loaded hughpages module.

--- src/arch/gemini_gni/conv-mach-hugepages.sh
+++ src/arch/gemini_gni/conv-mach-hugepages.sh
@@ -1,5 +1,7 @@
+##!/bin/sh -l
+
 # Check that some hugepages module is loaded
-if module list 2>&1 | grep -q craype-hugepages; then
+if echo $LOADEDMODULES | grep -q craype-hugepages; then
     true
 else
     echo 'Must have a craype-hugepages module loaded (e.g. module load craype-hugepages8M)' >&2
