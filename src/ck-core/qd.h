434c0061ef750c8aa85c85b1d2e040bdbeb27f34
Jonathan Lifflander <avunda@wirelessprvnat-172-16-212-37.near.illinois.edu>
2013-01-28 22:14:48 -0600

    qd: use compile-time flag CMK_IMMEDIATE_MSG instead of nonsensical runtime flag CK_MSG_IMMEDIATE

--- src/ck-core/qd.h
+++ src/ck-core/qd.h
@@ -92,14 +92,14 @@ class QdState {
     void enq(QdCallback *c) { callbacks->enq((void *) c); }
     void create(int n=1) { 
         mCreated += n; 
-#if CK_MSG_IMMEDIATE
+#if CMK_IMMEDIATE_MSG
         sendCount(0, n);
 #endif
     }
     void sendCount(int flag, int count);     // send msg to rank 0 for counting
     void process(int n=1) { 
          mProcessed += n; 
-#if CK_MSG_IMMEDIATE
+#if CMK_IMMEDIATE_MSG
         sendCount(1, n);
 #endif
     }
