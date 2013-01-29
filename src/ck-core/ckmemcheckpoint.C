5300d7f16842df2bb11c974f0c41792824aee8a4
Xiang Ni <xiangni2@illinois.edu>
2013-01-29 15:23:20 -0600

    don't detect failure when checkpointing

--- src/ck-core/ckmemcheckpoint.C
+++ src/ck-core/ckmemcheckpoint.C
@@ -80,6 +80,7 @@ CpvDeclare(int, _crashedNode);
 
 // static, so that it is accessible from Converse part
 int CkMemCheckPT::inRestarting = 0;
+int CkMemCheckPT::inCheckpointing = 0;
 int CkMemCheckPT::inLoadbalancing = 0;
 double CkMemCheckPT::startTime;
 char *CkMemCheckPT::stage;
@@ -481,6 +482,7 @@ void CkMemCheckPT::doItNow(int starter, CkCallback &cb)
 {
   checkpointed = 1;
   cpCallback = cb;
+  inCheckpointing = 1;
   cpStarter = starter;
   if (CkMyPe() == cpStarter) {
     startTime = CmiWallTimer();
@@ -708,6 +710,7 @@ void CkMemCheckPT::cpFinish()
 // for debugging, report checkpoint info
 void CkMemCheckPT::report()
 {
+  inCheckpointing = 0;
 #if !CMK_CHKP_ALL
   int objsize = 0;
   int len = ckTable.length();
@@ -1423,6 +1426,12 @@ int CkInRestarting()
 }
 
 extern "C"
+int CkInCheckpointing()
+{
+  return CkMemCheckPT::inCheckpointing;
+}
+
+extern "C"
 void CkSetInLdb(){
 #if CMK_MEM_CHECKPOINT
 	CkMemCheckPT::inLoadbalancing = 1;
@@ -1553,7 +1562,7 @@ void pingCheckHandler()
 {
 #if CMK_MEM_CHECKPOINT
   double now = CmiWallTimer();
-  if (lastPingTime > 0 && now - lastPingTime > 4 && !CkInLdb()) {
+  if (lastPingTime > 0 && now - lastPingTime > 4 && !CkInLdb() && !CkInRestarting() && !CkInCheckpointing()) {
     int i, pe, buddy;
     // tell everyone the buddy dies
     CkMemCheckPT *obj = CProxy_CkMemCheckPT(ckCheckPTGroupID).ckLocalBranch();
