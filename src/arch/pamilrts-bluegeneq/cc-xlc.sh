d9f98d217ddf25514f40aba0326d96b5902b2a74
Yanhua Sun <sun51@illinois.edu>
2013-04-18 20:07:40 +0000

    fix the ZLIB path problem on BGQ (cherry picked from commit df206d1897961b913c312ced89da23aee7841c32)

--- src/arch/pamilrts-bluegeneq/cc-xlc.sh
+++ src/arch/pamilrts-bluegeneq/cc-xlc.sh
@@ -1,3 +1,8 @@
+BGQ_ZLIB=/soft/libraries/alcf/current/xl/ZLIB/
+BGQ_BIN=$BGQ_FLOOR/gnu-linux/bin
+BGQ_INC="-I$BGQ_INSTALL/comm/sys/include -I$BGQ_INSTALL/spi/include -I$BGQ_INSTALL -I$BGQ_INSTALL/spi/include/kernel/cnk -I$BGQ_ZLIB/include"
+BGQ_LIB="-L$BGQ_INSTALL/comm/sys-fast/lib -lpami -L$BGQ_INSTALL/spi/lib -L$BGQ_ZLIB/lib -lSPI -lSPI_cnk -lpthread -lrt" 
+CMK_SYSLIBS="$BGQ_LIB"
 CMK_CC="bgxlc_r -qcpluscmt -qhalt=e $BGQ_INC -qnokeyword=__int128"
 CMK_CXX="bgxlC_r -qhalt=e $BGQ_INC -qnokeyword=__int128"
 CMK_LD="$CMK_CC"
@@ -17,4 +22,5 @@ CMK_NATIVE_LD="$CMK_NATIVE_CC"
 CMK_NATIVE_LDXX="$CMK_NATIVE_CXX"
 CMK_RANLIB="ranlib"
 CMK_F90LIBS="-lxlf90 -lxlopt -lxl -lxlfmath"
+
 
