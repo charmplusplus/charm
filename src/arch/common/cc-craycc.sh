0f111ab4a543f4e575cc61c0ffa0fa926953cdb2
Eric Bohm <ebohm@illinois.edu>
2013-05-20 15:55:24 -0500

    Blank cc-craycc files. With craycc, the default is to optimize and attempts to use advanced flags can lead to a lot of weird edge case bugs. This just provides a substrate onto which optimizations, or customizations, can be grafted if necessary. It also provides symmetry so you can put this compiler name in the build line, just like others, without using a custom suffix. This is known to work correctly with 8.1.7.

---
+++ src/arch/common/cc-craycc.sh
@@ -0,0 +1,7 @@
+#This is deliberately vacuous.
+
+#conv-mach.sh already does the right thing. This is just a placeholder so you can choose craycc 
+
+#NOTE: craycc is -O2 by default and is otherwise fairly aggressive. 
+#      Further tinkering should be done only as necessary and tested thoroughly.  
+
