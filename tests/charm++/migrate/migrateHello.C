2dd678e16114f3595c8258f5147507d87507dcac
Yanhua Sun <sun22@llnl.gov>
2013-07-16 10:47:33 -0700

    minor fix for migration overhead when iteration is odd

--- tests/charm++/migrate/migrateHello.C
+++ tests/charm++/migrate/migrateHello.C
@@ -2,25 +2,23 @@
 #include "migrateHello.decl.h"
 
 /*readonly*/ CProxy_Main mainProxy;
-/*readonly*/ int nElements;
+/*readonly*/ int numIterations;
 
 /*mainchare*/
 class Main : public CBase_Main
 {
-    int counter;
     CProxy_MigrateHello arr;
 public:
   Main(CkArgMsg* m)
   {
-      counter = 0;
     //Process command-line arguments
-    nElements=5;
-    if(m->argc >1 ) nElements=atoi(m->argv[1]);
+    numIterations=1000;
+    if(m->argc >1 ) numIterations=atoi(m->argv[1]);
     delete m;
 
     //Start the computation
     CkPrintf("Running MigrateHello on %d processors for %d iterations \n",
-	     CkNumPes(),nElements);
+	     CkNumPes(),numIterations);
     mainProxy = thisProxy;
     arr =  CProxy_MigrateHello::ckNew(1);
     arr[0].SayHi(0);
@@ -42,9 +40,9 @@ public:
   
   void SayHi(int hiNo)
   {
-      if(hiNo == 0)
+      if(hiNo <2 )
           startTimer = CkWallTimer();
-      else if(hiNo >= nElements)
+      else if(hiNo >= numIterations)
       {
           double time = CkWallTimer() - startTimer;
           CkPrintf(" migration cost total : %f sec single migration cost: %f us\n", time, time/(hiNo-1)*1000000); 
