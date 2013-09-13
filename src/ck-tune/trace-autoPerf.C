ccba879cfd7b217921fcf0a783464bf203cf4857
Yanhua Sun <sun51@illinois.edu>
2013-09-13 11:01:13 -0500

    add Jacobi3d example without ft and with correct boundary constrain so that parallel version performs same work with sequential version

--- src/ck-tune/trace-autoPerf.C
+++ src/ck-tune/trace-autoPerf.C
@@ -164,9 +164,9 @@ void TraceAutoPerfBOC::globalPerfAnalyze(CkReductionMsg *msg )
     double overheadPercentage = data->overheadTotalTime/totalTime;
     double utilPercentage = data->utilTotalTime/totalTime;
     //DEBUG_PRINT ( 
-    CkPrintf("Utilization(%):  \t(min:max:avg):(%.1f:\t  %.1f:\t  %.1f)\n", data->utilMin*100, data->utilMax*100, utilPercentage*100 );
-    CkPrintf("Idle(%):         \t(min:max:avg):(%.1f:\t  %.1f:\t  %.1f) \n", data->idleMin*100,  data->idleMax*100, idlePercentage*100);
-    CkPrintf("Overhead(%):     \t(min:max:avg):(%.1f:\t  %.1f:\t  %.1f) \n", data->overheadMin*100, data->overheadMax*100, overheadPercentage*100);
+    CkPrintf("Utilization(%):  \t(min:max:avg):(%.1f:\t  %.1f:\t  %.1f) time:%f\n", data->utilMin*100, data->utilMax*100, utilPercentage*100, data->utilTotalTime);
+    CkPrintf("Idle(%):         \t(min:max:avg):(%.1f:\t  %.1f:\t  %.1f) time:%f \n", data->idleMin*100,  data->idleMax*100, idlePercentage*100, data->idleTotalTime);
+    CkPrintf("Overhead(%):     \t(min:max:avg):(%.1f:\t  %.1f:\t  %.1f) time:%f \n", data->overheadMin*100, data->overheadMax*100, overheadPercentage*100, data->overheadTotalTime);
     CkPrintf("Grainsize(ms):\t(avg:max)\t: (%.3f:    %.3f) \n", data->utilTotalTime/data->numInvocations*1000, data->grainsizeMax*1000);
     CkPrintf("Invocations:  \t%lld\n", data->numInvocations);
     //)
