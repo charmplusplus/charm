dd31f914eb761434a630d9105aa4de963ed9eb87
Bilge Acun <acun2@illinois.edu>
2013-09-18 23:38:43 -0500

    Matrix multiplication bug fixes initial shift-by-i operation in the Cannon's algorithm is added to the algorithm for both A and B matrices. initialization of the C matrix through the beta variable was causing wrong data accumulation in C, beta variable is removed.

--- examples/charm++/matmul/matmul.C
+++ examples/charm++/matmul/matmul.C
@@ -4,15 +4,14 @@
 CProxy_Main mainProxy;
 
 void example_dgemm(int M, int N, int K, double alpha,
-                   double *A, double *B,
-                   double beta, double *C) {
+                   double *A, double *B, double *C) {
   for (int i = 0; i < M; ++i) {
     for (int j = 0; j < N; ++j) {
       double sum = 0.0;
       for (int k = 0; k < K; ++k) {
         sum += A[i*K + k] * B[k*N + j];
       }
-      C[N*i + j] = beta*C[N*i + j] + alpha*sum;
+      C[N*i + j] = C[N*i + j] + alpha*sum;
     }
   }
 }
@@ -32,20 +31,20 @@ public:
 
     mainProxy = thisProxy;
 
-    a = CProxy_Block::ckNew(blockSize, numBlocks, numBlocks, numBlocks);
-    b = CProxy_Block::ckNew(blockSize, numBlocks, numBlocks, numBlocks);
-    c = CProxy_Block::ckNew(blockSize, numBlocks, numBlocks, numBlocks);
+    a = CProxy_Block::ckNew(true, blockSize, numBlocks, numBlocks, numBlocks);
+    b = CProxy_Block::ckNew(true, blockSize, numBlocks, numBlocks, numBlocks);
+    c = CProxy_Block::ckNew(false, blockSize, numBlocks, numBlocks, numBlocks);
 
     startTime = CkWallTimer();
 
     a.pdgemmSendInput(c, true);
     b.pdgemmSendInput(c, false);
-    c.pdgemmRun(1.0, 0.0, CkCallback(CkReductionTarget(Main, done), thisProxy));
+    c.pdgemmRun(1.0, CkCallback(CkReductionTarget(Main, done), thisProxy));
   }
 
   void done() {
     double endTime = CkWallTimer();
-    CkPrintf("Matrix multiply of %u blocks with %u elements each (%u^2) finished in %f seconds\n",
+    CkPrintf("Matrix multiply of %u blocks with %u elements each (%u^2) finished in %f seconds\n", 
              numBlocks, blockSize, numBlocks*blockSize, endTime - startTime);
     CkExit();
   }
@@ -56,16 +55,22 @@ class Block : public CBase_Block {
   double* data;
   Block_SDAG_CODE
   public:
-  Block(unsigned int blockSize_, unsigned int numBlocks_)
+  Block(bool randomInit, unsigned int blockSize_, unsigned int numBlocks_)
     : blockSize(blockSize_), numBlocks(numBlocks_)
   {
     unsigned int elems = blockSize * blockSize;
     data = new double[elems];
-    for (int i = 0; i < elems; ++i)
-      data[i] = drand48();
+    if(randomInit)
+      for (int i = 0; i < elems; ++i)
+        data[i] = drand48();
+    else
+      for (int i = 0; i < elems; ++i)
+        data[i] = 0;
+
   }
 
   Block(CkMigrateMessage*) {}
+
 };
 
 #include "matmul.def.h"
