282d5bd81e9f95163d87076bd73370708a04d040
Nikhil Jain <nikhil@illinois.edu>
2013-10-14 07:11:44 +0000

    topomanager: give correct coordinate on BGQ when using partitions
    TopoManager: Makefile updates

---
+++ src/util/BGQTorus.C
@@ -0,0 +1,169 @@
+#include "BGQTorus.h"
+
+#if CMK_BLUEGENEQ
+
+#include "spi/include/kernel/process.h"
+#include "spi/include/kernel/location.h"
+#include <firmware/include/personality.h>
+
+int *bgq_localNodes = NULL;
+CmiNodeLock bgq_lock;
+int bgq_isLocalSet = 0;
+
+void bgq_topo_init() {
+  static init_done = 0;
+  if (!init_done) {
+    bgq_lock = CmiCreateLock();
+    init_done = 1;
+  }
+}
+
+void bgq_topo_free() { 
+  CmiLock(bgq_lock);
+  if(bgq_localNodes) free(bgq_localNodes);
+  bgq_isLocalSet = 0;
+  CmiUnlock(bgq_lock);
+}
+
+void bgq_topo_reset() {
+  CmiLock(bgq_lock);
+  bgq_isLocalSet = 0;
+  CmiUnlock(bgq_lock);
+}
+
+BGQTorusManager::BGQTorusManager() {
+  order[0] = 5;
+  order[1] = 4;
+  order[2] = 3;
+  order[3] = 2;
+  order[4] = 1;
+  order[5] = 0;
+
+  int numPes = CmiNumPesGlobal();
+  procsPerNode = Kernel_ProcessCount();
+  thdsPerProc = CmiMyNodeSize();
+  hw_NT = procsPerNode*thdsPerProc;
+
+  Personality_t pers;
+  Kernel_GetPersonality(&pers, sizeof(pers));
+
+  hw_NA = pers.Network_Config.Anodes;
+  hw_NB = pers.Network_Config.Bnodes;
+  hw_NC = pers.Network_Config.Cnodes;
+  hw_ND = pers.Network_Config.Dnodes;
+  hw_NE = pers.Network_Config.Enodes;
+
+  mapping = getenv("RANK_ORDER");
+  if(mapping != NULL) {
+    sscanf(mapping,"%d %d %d %d %d %d",&order[5],&order[4],&order[3],&order[2],&order[1],&order[0]);
+  }
+  //printf("Mapping %d %d %d %d %d %d\n",order[0],order[1],order[2],order[3],order[4],order[5]);
+
+  rn_NA = hw_NA;
+  rn_NB = hw_NB;
+  rn_NC = hw_NC;
+  rn_ND = hw_ND;
+  rn_NE = hw_NE;
+
+  int max_t = 0;
+  if(rn_NA * rn_NB * rn_NC * rn_ND * rn_NE != numPes/hw_NT) {
+    rn_NA = rn_NB = rn_NC = rn_ND =rn_NE =0;
+    int rn_NT=0;
+    int min_a, min_b, min_c, min_d, min_e, min_t;
+    min_a = min_b = min_c = min_d = min_e = min_t = (~(-1));
+    int tmp_t, tmp_a, tmp_b, tmp_c, tmp_d, tmp_e;
+    uint64_t numentries;
+    BG_CoordinateMapping_t *coord;
+
+    int nranks=numPes/thdsPerProc;
+    coord = (BG_CoordinateMapping_t *) malloc(sizeof(BG_CoordinateMapping_t)*nranks);
+    Kernel_RanksToCoords(sizeof(BG_CoordinateMapping_t)*nranks, coord, &numentries);
+
+    for(int c = 0; c < nranks; c++) {
+      tmp_a = coord[c].a;
+      tmp_b = coord[c].b;
+      tmp_c = coord[c].c;
+      tmp_d = coord[c].d;
+      tmp_e = coord[c].e;
+      tmp_t = coord[c].t;
+
+      if(tmp_a > rn_NA) rn_NA = tmp_a;
+      if(tmp_a < min_a) min_a = tmp_a;
+      if(tmp_b > rn_NB) rn_NB = tmp_b;
+      if(tmp_b < min_b) min_b = tmp_b;
+      if(tmp_c > rn_NC) rn_NC = tmp_c;
+      if(tmp_c < min_c) min_c = tmp_c;
+      if(tmp_d > rn_ND) rn_ND = tmp_d;
+      if(tmp_d < min_d) min_d = tmp_d;
+      if(tmp_e > rn_NE) rn_NE = tmp_e;
+      if(tmp_e < min_e) min_e = tmp_e;
+      if(tmp_t > rn_NT) rn_NT = tmp_t;
+      if(tmp_t < min_t) min_t = tmp_t;
+    }
+    rn_NA = rn_NA - min_a + 1;
+    rn_NB = rn_NB - min_b + 1;
+    rn_NC = rn_NC - min_c + 1;
+    rn_ND = rn_ND - min_d + 1;
+    rn_NE = rn_NE - min_e + 1;
+    procsPerNode = rn_NT - min_t + 1;
+    hw_NT = procsPerNode * thdsPerProc;
+    free(coord);
+  }
+
+  dimA = rn_NA;
+  dimB = rn_NB;
+  dimC = rn_NC;
+  dimD = rn_ND;
+  dimE = rn_NE;
+  dimA = dimA * hw_NT;	// assuming TABCDE
+
+  dims[0] = rn_NA;
+  dims[1] = rn_NB;
+  dims[2] = rn_NC;
+  dims[3] = rn_ND;
+  dims[4] = rn_NE;
+  dims[5] = hw_NT;
+
+  torus[0] = ((rn_NA % 4) == 0)? true:false;
+  torus[1] = ((rn_NB % 4) == 0)? true:false;
+  torus[2] = ((rn_NC % 4) == 0)? true:false;
+  torus[3] = ((rn_ND % 4) == 0)? true:false;
+  torus[4] = true;
+
+  populateLocalNodes();
+}
+
+void BGQTorusManager::populateLocalNodes() {
+  if(CmiNumPartitions() == 1) return;
+
+  CmiLock(bgq_lock);
+  if(bgq_isLocalSet) {
+    CmiUnlock(bgq_lock);
+    return;
+  }
+
+  if(bgq_localNodes == NULL)
+    bgq_localNodes = (int *)malloc(CmiNumNodesGlobal()*sizeof(int));
+
+  CmiAssert(bgq_localNodes != NULL);
+
+  for(int i = 0; i < CmiNumNodesGlobal(); i++)
+    bgq_localNodes[i] = -1;
+
+  for(int i = 0; i < CmiNumNodes(); i++) {
+    int a, b, c, d, e, t;
+    int global;
+
+    rankToCoordinates(CmiNodeFirst(i), a, b, c, d, e, t);
+    global = CmiNodeOf(coordinatesToRank(a, b, c, d, e, t));
+
+    bgq_localNodes[global] = i;
+  }
+
+  bgq_isLocalSet = 1;
+
+  CmiUnlock(bgq_lock);
+}
+
+#endif
+
--- src/util/TopoManager.C
+++ src/util/TopoManager.C
@@ -433,6 +433,10 @@ void TopoManager::printAllocation(FILE *fp)
 extern "C" void craynid_init();
 extern "C" void craynid_reset();
 extern "C" void craynid_free();
+#elif CMK_BLUEGENEQ
+extern void bgq_topo_init();
+extern void bgq_topo_reset();
+extern void bgq_topo_free();
 #endif
 
 CmiNodeLock _topoLock = 0;
@@ -444,6 +448,8 @@ extern "C" void TopoManager_init()
     _topoLock = CmiCreateLock();
 #if XT4_TOPOLOGY || XT5_TOPOLOGY || XE6_TOPOLOGY
     craynid_init();
+#elif CMK_BLUEGENEQ
+    bgq_topo_init();
 #endif
   }
 }
@@ -452,6 +458,8 @@ extern "C" void TopoManager_reset() {
   CmiLock(_topoLock);
 #if XT4_TOPOLOGY || XT5_TOPOLOGY || XE6_TOPOLOGY
   craynid_reset();
+#elif CMK_BLUEGENEQ
+  bgq_topo_reset();
 #endif
   if(_tmgr) delete _tmgr;
   _tmgr = new TopoManager;
@@ -464,6 +472,8 @@ extern "C" void TopoManager_free() {
   _tmgr = NULL;
 #if XT4_TOPOLOGY || XT5_TOPOLOGY || XE6_TOPOLOGY
   craynid_free();
+#elif CMK_BLUEGENEQ
+  bgq_topo_free();
 #endif
   CmiUnlock(_topoLock);
 }
@@ -577,124 +587,3 @@ extern "C" void TopoManager_createPartitions(int scheme, int numparts, int *node
   }
 }
 
-#if CMK_BLUEGENEQ
-
-#include "spi/include/kernel/process.h"
-#include "spi/include/kernel/location.h"
-#include <firmware/include/personality.h>
-
-BGQTorusManager::BGQTorusManager() {
-  order[0] = 5;
-  order[1] = 4;
-  order[2] = 3;
-  order[3] = 2;
-  order[4] = 1;
-  order[5] = 0;
-
-  int numPes = CmiNumPes();
-  procsPerNode = Kernel_ProcessCount();
-  thdsPerProc = CmiMyNodeSize();
-  hw_NT = procsPerNode*thdsPerProc;
-
-  if(CmiNumPartitions() > 1) {
-    dimA = rn_NA = numPes/hw_NT;
-    dimB = dimC = dimD = dimE = 1;
-    rn_NB = rn_NC = rn_ND = rn_NE = 1;
-    torus[0] = torus[1] = torus[2] = torus[3] = torus[4] = 0;
-    dims[0] = rn_NA;
-    dims[1] = rn_NB;
-    dims[2] = rn_NC;
-    dims[3] = rn_ND;
-    dims[4] = rn_NE;
-    dims[5] = hw_NT;
-    return;
-  }
-
-  Personality_t pers;
-  Kernel_GetPersonality(&pers, sizeof(pers));
-
-  hw_NA = pers.Network_Config.Anodes;
-  hw_NB = pers.Network_Config.Bnodes;
-  hw_NC = pers.Network_Config.Cnodes;
-  hw_ND = pers.Network_Config.Dnodes;
-  hw_NE = pers.Network_Config.Enodes;
-
-  mapping = getenv("RANK_ORDER");
-  if(mapping != NULL) {
-    sscanf(mapping,"%d %d %d %d %d %d",&order[5],&order[4],&order[3],&order[2],&order[1],&order[0]);
-  }
-  //printf("Mapping %d %d %d %d %d %d\n",order[0],order[1],order[2],order[3],order[4],order[5]);
-
-  rn_NA = hw_NA;
-  rn_NB = hw_NB;
-  rn_NC = hw_NC;
-  rn_ND = hw_ND;
-  rn_NE = hw_NE;
-
-  int max_t = 0;
-  if(rn_NA * rn_NB * rn_NC * rn_ND * rn_NE != numPes/hw_NT) {
-    rn_NA = rn_NB = rn_NC = rn_ND =rn_NE =0;
-    int rn_NT=0;
-    int min_a, min_b, min_c, min_d, min_e, min_t;
-    min_a = min_b = min_c = min_d = min_e = min_t = (~(-1));
-    int tmp_t, tmp_a, tmp_b, tmp_c, tmp_d, tmp_e;
-    uint64_t numentries;
-    BG_CoordinateMapping_t *coord;
-
-    int nranks=numPes/thdsPerProc;
-    coord = (BG_CoordinateMapping_t *) malloc(sizeof(BG_CoordinateMapping_t)*nranks);
-    Kernel_RanksToCoords(sizeof(BG_CoordinateMapping_t)*nranks, coord, &numentries);
-
-    for(int c = 0; c < nranks; c++) {
-      tmp_a = coord[c].a;
-      tmp_b = coord[c].b;
-      tmp_c = coord[c].c;
-      tmp_d = coord[c].d;
-      tmp_e = coord[c].e;
-      tmp_t = coord[c].t;
-
-      if(tmp_a > rn_NA) rn_NA = tmp_a;
-      if(tmp_a < min_a) min_a = tmp_a;
-      if(tmp_b > rn_NB) rn_NB = tmp_b;
-      if(tmp_b < min_b) min_b = tmp_b;
-      if(tmp_c > rn_NC) rn_NC = tmp_c;
-      if(tmp_c < min_c) min_c = tmp_c;
-      if(tmp_d > rn_ND) rn_ND = tmp_d;
-      if(tmp_d < min_d) min_d = tmp_d;
-      if(tmp_e > rn_NE) rn_NE = tmp_e;
-      if(tmp_e < min_e) min_e = tmp_e;
-      if(tmp_t > rn_NT) rn_NT = tmp_t;
-      if(tmp_t < min_t) min_t = tmp_t;
-    }
-    rn_NA = rn_NA - min_a + 1;
-    rn_NB = rn_NB - min_b + 1;
-    rn_NC = rn_NC - min_c + 1;
-    rn_ND = rn_ND - min_d + 1;
-    rn_NE = rn_NE - min_e + 1;
-    procsPerNode = rn_NT - min_t + 1;
-    hw_NT = procsPerNode * thdsPerProc;
-  }
-
-  dimA = rn_NA;
-  dimB = rn_NB;
-  dimC = rn_NC;
-  dimD = rn_ND;
-  dimE = rn_NE;
-  dimA = dimA * hw_NT;	// assuming TABCDE
-
-  dims[0] = rn_NA;
-  dims[1] = rn_NB;
-  dims[2] = rn_NC;
-  dims[3] = rn_ND;
-  dims[4] = rn_NE;
-  dims[5] = hw_NT;
-
-  torus[0] = ((rn_NA % 4) == 0)? true:false;
-  torus[1] = ((rn_NB % 4) == 0)? true:false;
-  torus[2] = ((rn_NC % 4) == 0)? true:false;
-  torus[3] = ((rn_ND % 4) == 0)? true:false;
-  torus[4] = true;
-
-}
-
-#endif
