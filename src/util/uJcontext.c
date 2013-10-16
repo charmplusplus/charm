9e7d9e60b047231b5a8ce907396c9b618405db13
Nikhil Jain <nikhil@illinois.edu>
2013-10-16 00:56:06 -0500

    cleanup: do not initialize int with a character

--- src/util/uJcontext.c
+++ src/util/uJcontext.c
@@ -32,7 +32,7 @@
 
 /* Return an approximation of the top of the stack */
 static void *getStack(void) {
-	int x = 'A';
+	int x = 0;
         void *p=&x;
 	return p;
 }
@@ -122,7 +122,7 @@ int setJcontext (const uJcontext_t *u)
 #elif 0 /* Blue Gene/Light gcc PPC assembly version: */
 			asm __volatile__ ("mr 1,%0" :: "r"(new_sp));
 #else /* Portable alloca version */
-			char *old_sp;
+			char *old_sp = NULL;
 			register CmiInt8 allocLen;
 			old_sp =  (char *)&old_sp;
 			allocLen=old_sp-new_sp;
