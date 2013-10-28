ae146996e51e3463a263596cc37c6c60c54185da
YanhuaSun <sun51@illinois.edu>
2013-10-28 12:09:46 -0500

    fix g++ compiling hilbert.C

--- src/util/hilbert.h
+++ src/util/hilbert.h
@@ -3,8 +3,9 @@
 
 #include <vector>
 using namespace std;
+
 /*
- *  map a point with index of i on Hilbert walk in dim dimensions to its coordinate
+ *  map a point with index of i on Hilbert walk in dim dimensions to its coordinate, return this coord
  */
 extern vector<int> int_to_Hilbert(int i, int dim) ;
 
@@ -12,4 +13,5 @@ extern vector<int> int_to_Hilbert(int i, int dim) ;
  *  map a point with coordinate 'coords' to its linearized index on Hilbert walk , return this value 
  */
 extern int Hilbert_to_int(const vector<int>& coords, int dim);
+
 #endif
