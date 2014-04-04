854353d947d4d0ef9db65913a67033b7a9aa5551
Gengbin Zheng <gzheng@illinois.edu>
2014-04-04 11:53:03 -0500

    only report error on PE0 for GNI_SetBIConfig
    Change-Id: I6923ad4b201001d67b6a398aa0cdaaca3490cfbc

--- src/util/ckBIconfig.h
+++ src/util/ckBIconfig.h
@@ -15,7 +15,7 @@ static inline uint16_t ck_get_GNI_BIConfig()
   gni_bi_desc_t gni_bi_desc;
   uint32_t gni_device_id = 0;
   gni_return_t gni_rc = GNI_GetBIConfig(gni_device_id, &gni_bi_desc);
-  if (gni_rc != GNI_RC_SUCCESS) {
+  if (gni_rc != GNI_RC_SUCCESS && CkMyPe() == 0) {
     CmiPrintf("Error, unable to retrieve BI config, rc=%d\n",gni_rc);
   }
   return(gni_bi_desc.current_bw);
@@ -26,7 +26,7 @@ static inline void ck_set_GNI_BIConfig(uint16_t biValue)
   uint16_t modes = GNI_BI_FLAG_APPLY_NOW;
   uint32_t gni_device_id = 0;
   uint16_t gni_rc = GNI_SetBIConfig(gni_device_id, biValue, 0, modes);
-  if (gni_rc != GNI_RC_SUCCESS) {
+  if (gni_rc != GNI_RC_SUCCESS && CkMyPe() == 0) {
     CmiPrintf("Error, unable to set BI config, rc=%d\n",gni_rc);
   }
 }
