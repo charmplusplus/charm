991c551934f18ee8117818dc477ecebd1f37620c
Bilge Acun <acun2@illinois.edu>
2014-04-22 14:56:47 -0500

    Bug #470: Fixing the ft header for fault tolerence support for netlrts changes include, replacement of d4,d5 fields with a single size field adding the required rank field remaning the unused field d8 to d4
    Change-Id: Ie4becc180935df3d25742ed6f0d89a6c43991549

--- src/arch/netlrts/conv-mach-syncft.h
+++ src/arch/netlrts/conv-mach-syncft.h
@@ -5,7 +5,7 @@
 //#undef CMK_MSG_HEADER_BIGSIM_
 /* expand the header to store the restart phase counter(pn) */
 #define CMK_MSG_HEADER_BASIC   CMK_MSG_HEADER_EXT
-#define CMK_MSG_HEADER_EXT_    CmiUInt2 d0,d1,d2,d3,d4,d5,hdl,pn,d8,stratid,root,xhdl,info,dd,redID,pad2;
+#define CMK_MSG_HEADER_EXT_    CmiUInt2 d0,d1,d2,d3,hdl,pn,d4,stratid,xhdl,info,dd,redID,pad2,rank; CmiInt4 root, size;
 //#define CMK_MSG_HEADER_EXT    { CMK_MSG_HEADER_EXT_ }
 //#define CMK_MSG_HEADER_BIGSIM_    { CmiUInt2 d0,d1,d2,d3,d4,d5,hdl,xhdl,pn,info; int nd, n; double rt; CmiInt2 tID; CmiUInt2 hID; char t; int msgID; int srcPe;}
 //#define CMK_MSG_HEADER_BIGSIM_  { CMK_MSG_HEADER_EXT_ CMK_BIGSIM_FIELDS }
