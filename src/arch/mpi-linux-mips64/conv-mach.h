3a5e4cc02620889971ee4c3773d858718772f4a8
Phil Miller <mille121@illinois.edu>
2014-05-13 11:50:26 -0500

    arch/*linux*: All non-ancient systems have stdint.h, so drop their custom integer type definitions
    Change-Id: I3fae74c19031a48dceac52ca54de16bccf8fef5d

--- src/arch/mpi-linux-mips64/conv-mach.h
+++ src/arch/mpi-linux-mips64/conv-mach.h
@@ -32,12 +32,6 @@
 #define CMK_TIMER_USE_SPECIAL                              1
 #define CMK_TIMER_USE_TIMES                                0
 
-#define CMK_TYPEDEF_INT2 short
-#define CMK_TYPEDEF_INT4 int
-#define CMK_TYPEDEF_INT8 long long
-#define CMK_TYPEDEF_UINT2 unsigned short
-#define CMK_TYPEDEF_UINT4 unsigned int
-#define CMK_TYPEDEF_UINT8 unsigned long long
 #define CMK_TYPEDEF_FLOAT4 float
 #define CMK_TYPEDEF_FLOAT8 double
 
