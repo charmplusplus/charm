aefd5c13c31842a6a3bf02e9138785aa5af616e2
Phil Miller <mille121@illinois.edu>
2014-09-30 14:29:12 -0500

    charmxi: Support multi-dimensional arrays for readonly variables
    Change-Id: Ia43fc6e651674ed492e5e194618479fe17f9f996

--- tests/charm++/simplearrayhello/hello.C
+++ tests/charm++/simplearrayhello/hello.C
@@ -4,6 +4,8 @@
 /*readonly*/ CProxy_Main mainProxy;
 /*readonly*/ int nElements;
 
+/* readonly */ float values[3][3];
+
 /*mainchare*/
 class Main : public CBase_Main
 {
@@ -21,6 +23,10 @@ public:
     mainProxy = thisProxy;
 
     CProxy_Hello arr = CProxy_Hello::ckNew(nElements);
+
+    for (int i = 0; i < 3; ++i)
+      for (int j = 0; j < 3; ++j)
+	values[i][j] = 3*i + j;
 
     arr[0].SayHi(17);
   };
@@ -45,6 +51,10 @@ public:
   
   void SayHi(int hiNo)
   {
+    for (int i = 0; i < 3; ++i)
+      for (int j = 0; j < 3; ++j)
+	CkAssert(values[i][j] == 3*i + j);
+
     CkPrintf("Hi[%d] from element %d\n",hiNo,thisIndex);
     if (thisIndex < nElements-1)
       //Pass the hello on:
