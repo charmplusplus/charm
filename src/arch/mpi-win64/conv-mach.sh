7544d1200735a10421694e1b9b40dbb6b0cc0f91
Michael Robson <mprobson@illinois.edu>
2014-11-05 13:00:40 -0600

    Feature #574 win: Copy binaries to <build>/bin not /usr/bin
    Change-Id: I0e383dbc7b5426bb4ebd1df80923845b5b320deb

--- src/arch/mpi-win64/conv-mach.sh
+++ src/arch/mpi-win64/conv-mach.sh
@@ -7,10 +7,10 @@ else
 fi
 HPC_SDK=`cygpath -d "$HPC_SDK"`
 
-CMK_CC="unix2nt_cc -D_CRT_SECURE_NO_DEPRECATE -I `cygpath -u "$HPC_SDK\Inc"`"
+CMK_CC="$CHARMBIN/unix2nt_cc -D_CRT_SECURE_NO_DEPRECATE -I `cygpath -u "$HPC_SDK\Inc"`"
 CMK_CPP_CHARM="/usr/bin/cpp -P"
 CMK_CPP_C="$CMK_CC -E"
-CMK_CXX="unix2nt_cc -D_CRT_SECURE_NO_DEPRECATE  -I `cygpath -u "$HPC_SDK\Inc"`"
+CMK_CXX="$CHARMBIN/unix2nt_cc -D_CRT_SECURE_NO_DEPRECATE  -I `cygpath -u "$HPC_SDK\Inc"`"
 CMK_CXXPP=$CMK_CC
 CMK_LD="$CMK_CC -L `cygpath -u "$HPC_SDK\Lib\amd64"` -lmsmpi"
 CMK_LDXX="$CMK_CXX -L `cygpath -u "$HPC_SDK\Lib\amd64"` -lmsmpi"
@@ -22,7 +22,7 @@ CMK_SEQ_LDXX="$CMK_LDXX"
 
 CMK_CF77="f77"
 CMK_CF90="f90"
-CMK_AR="unix2nt_ar "
+CMK_AR="$CHARMBIN/unix2nt_ar "
 CMK_RANLIB="echo "
 CMK_LIBS=""
 CMK_XIOPTS=""
