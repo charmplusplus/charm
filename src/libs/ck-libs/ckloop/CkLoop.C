d464551bd241a5becd658a94f4ffcb2b106ec265
Harshitha <gplkrsh2@illinois.edu>
2015-10-06 15:26:59 -0500

    Issue #855: Fix CkLoop compilation for windows
    Change-Id: I3e22d74b55691510e370609eef61c89a2919e689

--- src/libs/ck-libs/ckloop/CkLoop.C
+++ src/libs/ck-libs/ckloop/CkLoop.C
@@ -14,7 +14,7 @@
 /*====Beginning of pthread-related variables and impelementation====*/
 //__thread is not portable, but it works almost everywhere if pthread works
 //After C++11, this should be thread_local
-#if !CMK_SMP
+#if !CMK_SMP && !defined(_WIN32)
 static __thread pthread_cond_t thdCondition; //the signal var of each pthread to be notified
 static __thread pthread_mutex_t thdLock; //the lock associated with the condition variables
 #endif
