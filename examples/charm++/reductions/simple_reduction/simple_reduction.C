3b19d01b33fe1e1c5f2d54c9323bd3a05c044871
Ronak Buch <rabuch2@illinois.edu>
2015-11-09 20:20:54 -0600

    Improve command line argument handling of simple_reduction example
    Change-Id: I617d33152f180ef9c016788961737a8521fba136

--- examples/charm++/reductions/simple_reduction/simple_reduction.C
+++ examples/charm++/reductions/simple_reduction/simple_reduction.C
@@ -17,14 +17,15 @@ main::main(CkArgMsg* m)
     //Start the computation
 
     mainProxy = thishandle;
-    if(m->argc<2)
+    if(m->argc < 4)
       {
+        CkPrintf("%s [numChares] [starting_index_one] [starting_index_two]\n", m->argv[0]);
 	CkExit();
       }
     units=atoi(m->argv[1]);
     dOne=atof(m->argv[2]);
     dTwo=atof(m->argv[3]);
-    
+
     arr = CProxy_RedExample::ckNew(units);
 
     CkPrintf("simple_reduction for %d pes on %d units for %f and %f\n",
