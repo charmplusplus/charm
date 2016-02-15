fb9ecf750f183615d2cae3916a3dc108bc622c14
Michael Robson <mprobson@illinois.edu>
2016-02-15 12:50:37 -0600

    Bug #788: enable cuda out of tree build for all layers
    This propogrates the change in b4a496 to work
    everywhere. It also partially adresses Cleanup #512
    since we are removing duplicated special.sh and
    keeping them in a single file in common.
    net-linux-cell still has its own version of
    special.sh and that should overwrite the common
    version during the gather stage due to system_ln
    passing the -f flag.
    
    Change-Id: I21ee6cd62ece09e225bdf1147e6155d1a608391f

--- src/arch/common/special.sh
+++ src/arch/common/special.sh
@@ -6,8 +6,11 @@ export CHARMINC=../include
 if test -n "$BUILD_CUDA"
 then
   echo "---------------------- special.sh for cuda executing ----------------"
+  if [ `cat .gdir` == "gni" ] ; then
+    export CRAY_CUDA_PROXY=1
+  fi
 
-  ./gathertree ../../src/arch/cuda .
+  ./gathertree $SRCBASE/arch/cuda .
 
 # make links
   test ! -f "../include/cuda-hybrid-api.h" && ./system_ln "../tmp/hybridAPI/cuda-hybrid-api.h" ../include && test ! -f "../include/wr.h" && ./system_ln "../tmp/hybridAPI/wr.h" ../include && test ! -f "../include/wrqueue.h" && ./system_ln "../tmp/hybridAPI/wrqueue.h" ../include
