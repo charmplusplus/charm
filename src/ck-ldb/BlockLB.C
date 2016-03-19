7d869e36fc3fdc4d8af34eefc95ec455a340abdc
Harshitha <gplkrsh2@illinois.edu>
2016-03-18 23:34:04 -0500

    Bug #1016: Fix BlockLB to distribute objects evenly.
    When the number of objects are not divisible by number of PEs the excess objects
    are sent to PE 0. This fix distributes the objects evenly
    
    Change-Id: I93fa3b6ef2e1178ead86f167ea81b90c5270d23e

--- src/ck-ldb/BlockLB.C
+++ src/ck-ldb/BlockLB.C
@@ -1,6 +1,7 @@
 
 #include "BlockLB.decl.h"
 #include "BlockLB.h"
+#include <math.h>
 
 
 CreateLBFunc_Def (BlockLB, "Allocate objects in blocks to the remaining valid PE")
@@ -51,18 +52,26 @@ void BlockLB::work (LDStats *stats)
     CmiAbort ("BlockLB: no available processors!");
   }
 
-	int objsPerProcessor = stats->n_objs/validProcs;
+	int objsPerProcessor_floor = stats->n_objs/validProcs;
+  int remainder = stats->n_objs % validProcs;
+  int markidx = remainder * (objsPerProcessor_floor + 1);
+
+
+  int validDest;
   // Rotate each object to the next higher processor.
   for (obj = 0; obj < stats->n_objs; obj++) {
     odata = &(stats->objData[obj]);
 		const LDObjid& objID = odata->objID();
     if (odata->migratable) {
 			int idx = objID.id[0];
-			int validDest = idx/objsPerProcessor;
-			validDest = validDest % validProcs;
+      if (idx < markidx) {
+        validDest = idx / (objsPerProcessor_floor + 1);
+      } else {
+        validDest = ((idx - markidx) / objsPerProcessor_floor) + remainder;
+      }
 			dest = mapValidToAbsolute[validDest];
 			stats->to_proc[obj] = dest;
-	//		printf("Index %d object shows up in BlockLB moved to %d\n",objID.id[0],stats->to_proc[obj]);
+			//printf("Index %d object shows up in BlockLB moved to %d\n",objID.id[0],stats->to_proc[obj]);
     }
   }
 
