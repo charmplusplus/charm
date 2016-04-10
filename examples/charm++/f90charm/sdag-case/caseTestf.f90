96056d45376cfd07bf42e0ecd3a07cc3ff4ef9ea
Gengbin Zheng <zhenggb@gmail.com>
2016-04-10 12:04:50 -0500

    f90charm: Add example of SDAG in f90charm chares
    Co-authored-by: Evan Ramos <evan@hpccharm.com>
    
    Change-Id: Ide7d86bf0c42bae79e045adb31230bca4e1ec942

---
+++ examples/charm++/f90charm/sdag-case/caseTestf.f90
@@ -0,0 +1,49 @@
+      MODULE  CaseTestMod
+
+      Type CaseTestArray
+      END TYPE
+
+      Type CaseTestArrayPtr
+        TYPE (CaseTestArray), POINTER :: obj
+        integer*8 aid
+      END TYPE
+      END MODULE
+
+      SUBROUTINE CaseTestArray_allocate(objPtr, aid, index)
+        USE CaseTestMod
+        IMPLICIT NONE
+        TYPE(CaseTestArrayPtr) objPtr
+        integer*8 aid
+        integer index
+
+        allocate(objPtr%obj)
+        objPtr%aid = aid
+        ! initialize Chare data here in constructor
+      END SUBROUTINE
+
+      SUBROUTINE CaseTestArray_pup(p, objPtr, aid)
+        USE CaseTestMod
+        IMPLICIT NONE
+        INCLUDE 'pupf.h'
+        INTEGER :: p
+        TYPE(CaseTestArrayPtr),target :: objPtr
+        integer*8 aid
+
+      END SUBROUTINE
+
+      SUBROUTINE CaseTestArray_resumefromsync(objPtr, aid, index)
+        USE CaseTestMod
+        TYPE(CaseTestArrayPtr) objPtr
+        integer*8 aid
+        integer index
+
+      END SUBROUTINE
+
+      SUBROUTINE f90charmmain()
+        USE CaseTestMod
+        IMPLICIT NONE
+        integer*8  aid
+
+        call CaseTestArray_CkNew(1, aid)
+        call SendTo_CaseTestArray_run(aid, 0)
+      END SUBROUTINE
