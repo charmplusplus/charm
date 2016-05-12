4a1956a64cb806daf0d0a8ed8b852fba22e63a50
Phil Miller <mille121@illinois.edu>
2016-05-11 20:13:35 -0500

    Delete machine layers for obsolete IA-64 processors
    Change-Id: I14c0860a49faf0d5be4f80743f8c840e39b43a9d

--- src/arch/net-linux-ia64/cc-ecc.h
+++
@@ -1,9 +0,0 @@
-#undef  CMK_COMPILEMODE_ORIG
-#undef  CMK_COMPILEMODE_ANSI
-#define CMK_COMPILEMODE_ORIG                               0
-#define CMK_COMPILEMODE_ANSI                               1
-
-#undef CMK_TIMER_USE_GETRUSAGE
-#undef CMK_TIMER_USE_RDTSC
-#define CMK_TIMER_USE_GETRUSAGE                            1
-#define CMK_TIMER_USE_RDTSC                                0
