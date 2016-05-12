fe974b8f50b7f10017863ce5831429157de46e6f
Phil Miller <mille121@illinois.edu>
2016-05-11 20:33:49 -0500

    Delete archaic support/references to GCC3 and KAI compilers
    Change-Id: I586a399239d5bd3f99715f3cba1d7f76d5147336

--- src/arch/common/cc-gcc3.h
+++
@@ -1,7 +0,0 @@
-#undef CMK_DLL_CC
-#define CMK_DLL_CC  "g++3 -shared -O3 -o "
-
-#undef  CMK_COMPILEMODE_ORIG
-#undef  CMK_COMPILEMODE_ANSI
-#define CMK_COMPILEMODE_ORIG                               0
-#define CMK_COMPILEMODE_ANSI                               1
