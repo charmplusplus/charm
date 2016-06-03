e82575a78ecca6708d704eff4f69629f535b6d1b
Phil Miller <mille121@illinois.edu>
2016-06-03 13:10:31 -0500

    Bug #1028: Look for the testrun script inside the build tree
    Fixes win64 builds that don't symlink bin/ and other directories from
    the root of the source tree into the build tree.
    
    Change-Id: I04744fc6b5aff1d66e0efca0bf9937b51b9e7587

--- tests/common.mk
+++ tests/common.mk
@@ -1,5 +1,9 @@
+ifneq ($(wildcard ../../bin/.),)
+	run = ../../bin/testrun $(1) $(TESTOPTS)
+else
 ifneq ($(wildcard ../../../bin/.),)
 	run = ../../../bin/testrun $(1) $(TESTOPTS)
 else
 	run = ../../../../bin/testrun $(1) $(TESTOPTS)
+endif
 endif
