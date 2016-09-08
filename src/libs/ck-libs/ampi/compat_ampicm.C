8df8f1535b47254fff1066e83051770e1756b3f4
Sam White <white67@illinois.edu>
2016-09-07 21:31:40 -0500

    AMPI cleanup: silence unused variable compiler warnings
    Change-Id: I962065d9b7c6b8ea2be1821f65eeb130bab697e6

--- src/libs/ck-libs/ampi/compat_ampicm.C
+++ src/libs/ck-libs/ampi/compat_ampicm.C
@@ -1,4 +1,5 @@
 int AMPI_Main_cpp(int argc,char **argv)
 {
+	(void)argc; (void)argv;
 	return 0;
 }
--- src/libs/ck-libs/ampi/compat_ampim.C
+++ src/libs/ck-libs/ampi/compat_ampim.C
@@ -2,5 +2,6 @@
 
 CDECL int AMPI_Main(int argc,char **argv)
 {
+	(void)argc; (void)argv;
 	return 0;
 }
