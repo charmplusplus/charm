ba9fa6e9ccd65ab35156bab529e997ecc383d807
Sam White <white67@illinois.edu>
2017-01-03 16:40:56 -0600

    mpi-bluegeneq: add support for building on bgclang's MPI wrappers
    Users can build on Mira with './build charm++ mpi-bluegeneq clang' after
    executing 'soft add +mpiwrapper-bgclang'.
    
    Change-Id: I97525b27336e5371bb15c26508d1a6fefbf1905c

---
+++ src/arch/mpi-bluegeneq/cc-clang.sh
@@ -0,0 +1,15 @@
+CMK_CPP_CHARM="$BGQ_BIN/powerpc64-bgq-linux-cpp -P"
+CMK_CPP_C="mpic++ -E "
+CMK_CXX="mpic++ -Wno-deprecated-declarations "
+CMK_CC="mpicc -Wno-deprecated-declarations "
+CMK_CXXPP="mpic++ -E "
+CMK_LD="$CMK_CC"
+CMK_LDXX="$CMK_CXX"
+
+CMK_C_OPTIMIZE='-O3 -ffast-math '
+CMK_CXX_OPTIMIZE='-O3 -ffast-math '
+OPTS_LD="$OPTS_LD "
+OPTS_CC="$OPTS_CC -x c "
+CMK_QT="generic64-light"
+
+CMK_USING_BGCLANG="1"
