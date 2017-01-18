77d6631c60ad6dd5d7e2663a80ec9f84436a60fb
Phil Miller <mille121@illinois.edu>
2017-01-18 15:28:01 -0600

    Use logical operators for conditional compilation, not bitwise
    Change-Id: I9e3ab3678cd76dd25ba5411647dbc651b91b1aae

--- src/conv-core/converse.h
+++ src/conv-core/converse.h
@@ -1899,7 +1899,7 @@ extern int _immRunning;
 #define CmiMemoryAtomicDecrement(...) ImplSelect2(__VA_ARGS__, CmiMemoryAtomicDecrementMemOrder, CmiMemoryAtomicDecrementSimple, Dummy)(__VA_ARGS__)
 #define CmiMemoryAtomicFetchAndInc(...) ImplSelect3(__VA_ARGS__, CmiMemoryAtomicFetchAndIncMemOrder, CmiMemoryAtomicFetchAndIncSimple, Dummy)(__VA_ARGS__)
 
-#if __GNUC__ & __STDC_VERSION__ >= 201112L & !__STDC_NO_ATOMICS__
+#if __GNUC__ && __STDC_VERSION__ >= 201112L && !__STDC_NO_ATOMICS__
 #define CmiMemoryAtomicIncrementMemOrder(someInt, MemModel) __atomic_fetch_add(&(someInt),1, MemModel);
 #define CmiMemoryAtomicDecrementMemOrder(someInt, MemModel) __atomic_fetch_sub(&(someInt),1, MemModel);
 #define CmiMemoryAtomicFetchAndIncMemOrder(input,output, MemModel) (output) = __atomic_fetch_add(&(input),1, MemModel);
