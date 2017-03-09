18b59468a97c2fb74e9e4cb8a7709159a2299c40
Matthias Diener <mdiener@illinois.edu>
2017-03-09 10:35:07 -0600

    icc: remove broken CPU detection and icc8 support
    Change-Id: Idf6085b2fa3e0eb867f7c86b118350d4899e1e3e

--- src/arch/common/cc-iccstatic.sh
+++ src/arch/common/cc-iccstatic.sh
@@ -1,19 +1,6 @@
-# test version
-ICC_ver=`icc --version | awk -F'.' '{print $1; exit}' | awk '{print $NF}'`
-test -z "$ICC_ver" && echo "ICC compiler not found!" && exit 1
-#echo version:$ICC_ver
-
 ICC_STATIC=true
 
-if test $ICC_ver  -ge 10
-then
-  ICCOPTS="-static-intel"
-elif test $ICC_ver -eq 9
-then
-  ICCOPTS="-i-static"
-else
-  ICCOPTS="-static-libcxa"
-fi
+ICCOPTS="-static-intel"
 
 CMK_CPP_C='icc -E '
 CMK_CC='icc '
@@ -34,10 +21,4 @@ CMK_SEQ_CXX="$CMK_NATIVE_CXX"
 CMK_SEQ_LD="$CMK_NATIVE_LD"
 CMK_SEQ_LDXX="$CMK_NATIVE_LDXX"
 
-#CMK_CF90='ifc -auto '
-#CMK_CF90_FIXED="$CMK_CF90 -132 -FI "
-#CMK_F90LIBS='-L/usr/local/intel/compiler70/ia32/lib -L/opt/intel/compiler70/ia32/lib -lintrins -lIEPCF90 -lF90 -lintrins -limf  '
-#CMK_F90_USE_MODDIR=""
-
 . $CHARMINC/conv-mach-ifort.sh
-
