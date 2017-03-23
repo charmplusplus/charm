592408b82b300ddc74dc40489e17ac156b6bf413
Sam White <white67@illinois.edu>
2017-03-23 10:21:21 -0500

    Create netlrts-linux-arm7 build target from net-linux-arm7
    Change-Id: I992da9d9ca38fd49549bf57d81ab1291ceeeed89

---
+++ src/arch/netlrts-linux-arm7/conv-mach.h
@@ -0,0 +1,63 @@
+
+#ifndef _CONV_MACH_H
+#define _CONV_MACH_H
+
+#define CMK_NETPOLL         1
+
+#define CMK_ARM					  1 
+
+#define CMK_ASYNC_NOT_NEEDED                               0
+#define CMK_ASYNC_USE_FIOASYNC_AND_FIOSETOWN               0
+#define CMK_ASYNC_USE_FIOASYNC_AND_SIOCSPGRP               0
+#define CMK_ASYNC_USE_FIOSSAIOSTAT_AND_FIOSSAIOOWN         0
+#define CMK_ASYNC_USE_F_SETFL_AND_F_SETOWN                 1
+
+#define CMK_DLL_CC  "g++ -shared -O3 -o "
+
+#define CMK_GETPAGESIZE_AVAILABLE                          1
+
+#define CMK_MALLOC_USE_GNU_MALLOC                          0
+#define CMK_MALLOC_USE_OS_BUILTIN                          1
+
+#define CMK_MEMORY_PAGESIZE                                8192
+#define CMK_MEMORY_PROTECTABLE                             0
+
+
+#define CMK_SSH_IS_A_COMMAND                               1
+#define CMK_SSH_NOT_NEEDED                                 0
+
+#define CMK_SHARED_VARS_UNAVAILABLE                        1
+#define CMK_SHARED_VARS_UNIPROCESSOR                       0
+
+#define CMK_THREADS_USE_CONTEXT                            0
+#define CMK_THREADS_USE_JCONTEXT                           0
+#define CMK_THREADS_USE_PTHREADS                           0
+#define CMK_THREADS_ARE_WIN32_FIBERS                       0
+
+#define CMK_SIGNAL_NOT_NEEDED                              0
+#define CMK_SIGNAL_USE_SIGACTION                           0
+#define CMK_SIGNAL_USE_SIGACTION_WITH_RESTART              1
+
+#define CMK_THREADS_REQUIRE_NO_CPV                         0
+#define CMK_THREADS_COPY_STACK                             0
+
+#define CMK_TIMER_USE_RDTSC                                0
+#define CMK_TIMER_USE_GETRUSAGE                            1
+#define CMK_TIMER_USE_SPECIAL                              0
+#define CMK_TIMER_USE_TIMES                                0
+
+#define CMK_64BIT   0 
+#define CMK_32BIT   1
+#define CMK_WHEN_PROCESSOR_IDLE_BUSYWAIT                   0
+#define CMK_WHEN_PROCESSOR_IDLE_USLEEP                     1
+
+
+#define CMK_DEBUG_MODE					   0 
+#define CMK_WEB_MODE                                       1
+
+#define CMK_LBDB_ON					   1
+
+
+#endif
+
+
---
+++ src/arch/netlrts-linux-arm7/conv-mach.sh
@@ -0,0 +1,54 @@
+#CMK_DEFS="$CMK_DEFS -DHAVE_USR_INCLUDE_MALLOC_H=1 "
+CMK_ARM7=" "
+
+CMK_PIC="-fPIC"
+CMK_CPP_CHARM="/lib/cpp -P"
+CMK_CPP_C="gcc -E $CMK_ARM7 "
+CMK_CC="gcc $CMK_ARM7 "
+CMK_CXX="g++ $CMK_ARM7 "
+CMK_CXXPP="$CMK_CXX -x c++ -E $CMK_ARM7 "
+CMK_XIOPTS=""
+CMK_LD="$CMK_CC $CMK_ARM7 "
+CMK_LDXX="$CMK_CXX $CMK_ARM7 "
+CMK_LD_SHARED="-shared"
+CMK_LD_LIBRARY_PATH="-Wl,-rpath,$CHARMLIBSO/"
+CMK_LIBS="-lckqt"
+CMK_RANLIB="ranlib"
+CMK_WARNINGS_ARE_ERRORS="-Werror"
+CMK_CXX_OPTIMIZE="-O3"
+
+# native compiler for compiling charmxi, etc
+CMK_NATIVE_CC="$CMK_CC $CMK_ARM7 -fPIC $CMK_DEFS "
+CMK_NATIVE_CXX="$CMK_CXX $CMK_ARM7 -fPIC $CMK_DEFS "
+CMK_NATIVE_LD="$CMK_CC $CMK_ARM7 -fPIC "
+CMK_NATIVE_LDXX="$CMK_CXX $CMK_ARM7 -fPIC "
+
+# native compiler for compiling charmxi, etc
+CMK_SEQ_CC="$CMK_NATIVE_CC"
+CMK_SEQ_CXX="$CMK_NATIVE_CXX"
+CMK_SEQ_LD="$CMK_NATIVE_LD"
+CMK_SEQ_LDXX="$CMK_NATIVE_LDXX"
+
+CMK_QT='generic64-light' 
+CMK_CF90=`which f95 2>/dev/null`
+if test -n "$CMK_CF90"
+then
+#    CMK_FPP="/lib/cpp -P -CC"
+#    CMK_CF90="$CMK_CF90 -fpic -fautomatic -fdollar-ok "
+#    CMK_CF90_FIXED="$CMK_CF90 -ffixed-form "
+#    CMK_F90LIBS="-lgfortran "
+#    CMK_F90_USE_MODDIR=1
+#    CMK_F90_MODINC="-I"
+#    CMK_MOD_NAME_ALLCAPS=
+#    CMK_MOD_EXT="mod"
+    . $CHARMINC/conv-mach-gfortran.sh
+else
+    CMK_CF77="g77 "
+    CMK_CF90="f90 "
+    CMK_CF90_FIXED="$CMK_CF90 -W132 "
+    CMK_F90LIBS="-L/usr/absoft/lib -L/opt/absoft/lib -lf90math -lfio -lU77 -lf77math "
+    CMK_F77LIBS="-lg2c "
+    CMK_F90_USE_MODDIR=1
+    CMK_F90_MODINC="-p"
+fi
+
