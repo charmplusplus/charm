2815fa37c1fe800786e801f088038dd10b5592e7
Matthias Diener <mdiener@illinois.edu>
2017-06-12 11:44:43 -0500

    AMPI: retire the charmdir script
    Change-Id: Ifa8a9d0c7d2900a8f5603453d50e38b113c3de36

--- src/libs/ck-libs/ampi/romio/config.sh
+++ src/libs/ck-libs/ampi/romio/config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
-CHARMDIR=`util/charmdir`
+CHARMDIR="$( cd -P "$( dirname "$0" )/../../../../../" && pwd )"
 ./configure -mpiincdir=$CHARMDIR/include -mpilib=$CHARMDIR/lib/libmoduleampi.a -mpibindir=$CHARMDIR/bin -cc=$CHARMDIR/bin/ampicc -fc=$CHARMDIR/bin/ampif77 -f90=$CHARMDIR/bin/ampif90
