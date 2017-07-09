ef691dee0c156e502f7f3ae8c1ad39c086e74661
Matthias Diener <mdiener@illinois.edu>
2017-07-09 13:20:58 -0500

    ROMIO: Simplify compilation
    - no more dummy romio lib (only link against romio when it is available)
    - can compile romio when running “make AMPI” in tmp/
    - AMPI files can now be built in parallel
    - no need for config.sh/install.sh
    
    Change-Id: I636c2165023f4de8c7b2511a057d4cf476ea948c

--- src/libs/ck-libs/ampi/ampiromio.c
+++
@@ -1,2 +0,0 @@
-/* dummy file for creating libampiromio.a when ROMIO is not built */
-typedef int avoid_empty_translation_unit;
--- src/libs/ck-libs/ampi/romio/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-CHARMDIR="$( cd -P "$( dirname "$0" )/../../../../../" && pwd )"
-ARCH=`util/tarch`
-if test -f include/mpio.h ; then
-  cp include/mpio.h include/mpiof.h $CHARMDIR/include && make && cp lib/$ARCH/libmpio.a $CHARMDIR/lib/libampiromio.a ;
-fi
