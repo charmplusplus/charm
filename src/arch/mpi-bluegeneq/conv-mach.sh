2de94c2ce78df3dc344a69a3c25f98ba539c59b2
Anirudh Manoj <anirudh@hpccharm.com>
2017-07-17 16:27:29 -0500

    Move common code to cc-bluegene.sh
    Change-Id: I12decfea355c5b567f4823bbb1f863cd74c06f98

--- src/arch/mpi-bluegeneq/conv-mach.sh
+++ src/arch/mpi-bluegeneq/conv-mach.sh
@@ -1,19 +1,4 @@
-BGQ_TYPICAL_FLOOR=/bgsys/drivers/ppcfloor
-
-# if no floor set, use typical floor path
-if test -z "$BGQ_FLOOR"
-then
-  BGQ_FLOOR=$BGQ_TYPICAL_FLOOR
-fi
-
-# if no install path (for experimental) set, use floor
-if test -z "$BGQ_INSTALL"
-then
-  BGQ_INSTALL=$BGQ_TYPICAL_FLOOR
-fi
-
-BGQ_BIN=$BGQ_FLOOR/gnu-linux/bin
-BGQ_ZLIB=/soft/libraries/alcf/current/xl/ZLIB/
+. $CHARMINC/cc-bluegene.sh
 
 BGQ_INC="-I$BGQ_ZLIB/include"
 BGQ_LIB="-L$BGQ_ZLIB/lib -lpthread -lrt"
@@ -37,38 +22,17 @@ then
   fi
 fi
 
-OPTS_CPP="$OPTS_CPP"
-GCC_OPTS="-Wno-deprecated -mminimal-toc $BGQ_INC"
-OPTS_LD="$OPTS_LD"
+GCC_OPTS="$GCC_OPTS -mminimal-toc $BGQ_INC"
 
-CMK_CPP_CHARM="$BGQ_BIN/powerpc64-bgq-linux-cpp -P"
-CMK_CPP_C="$BGQ_BIN/powerpc64-bgq-linux-cpp -E "
 CMK_CXX="mpixlcxx_r -qhalt=e $BGQ_INC -qnokeyword=__int128 -qtls=local-exec -DCMK_USING_XLC=1"
 CMK_CC="mpixlc_r -qcpluscmt -qhalt=e $BGQ_INC -qnokeyword=__int128 -qtls=local-exec"
+CMK_GCXX="mpicxx $GCC_OPTS "
+
+CMK_LD="$CMK_CC"
+CMK_LDXX="$CMK_CXX"
+
 CMK_CF77="mpixlf77_r "
 CMK_CF90="mpixlf90_r  -qsuffix=f=f90"
 CMK_CF90_FIXED="mpixlf90_r "
-CMK_RANLIB="$BGQ_BIN/powerpc64-bgq-linux-ranlib "
-CMK_AR="$BGQ_BIN/powerpc64-bgq-linux-ar q "
-CMK_SYSLIBS="$BGQ_LIB"
-CMK_LIBS='-lckqt'
-CMK_LD="$CMK_CC"
-CMK_LDXX="$CMK_CXX"
-CMK_C_OPTIMIZE='-O3'
-CMK_CXX_OPTIMIZE='-O3'
-CMK_ENABLE_C11='-qlanglvl=extc1x'
-CMK_ENABLE_CPP11='-qlanglvl=extended0x'
-CMK_LD_LIBRARY_PATH="-Wl,-rpath,$CHARMLIBSO/"
-CMK_NATIVE_CC='gcc '
-CMK_NATIVE_LD='gcc '
-CMK_NATIVE_CXX='g++ -Wno-deprecated '
-CMK_NATIVE_LDXX='g++'
-CMK_F90LIBS="-lxlf90 -lxlopt -lxl -lxlfmath"
-CMK_MOD_NAME_ALLCAPS=1
-CMK_MOD_EXT="mod"
-CMK_F90_USE_MODDIR=1
-CMK_F90_MODINC="-I"
-CMK_QT="aix"
-CMK_NM='nm '
 
-CMK_BLUEGENEQ="1"
+CMK_SYSLIBS="$BGQ_LIB"
--- src/arch/pami-bluegeneq/conv-mach.sh
+++ src/arch/pami-bluegeneq/conv-mach.sh
@@ -1,19 +1,5 @@
-BGQ_TYPICAL_FLOOR=/bgsys/drivers/ppcfloor
+. $CHARMINC/cc-bluegene.sh
 
-# if no floor set, use typical floor path
-if test -z "$BGQ_FLOOR"
-then
-  BGQ_FLOOR=$BGQ_TYPICAL_FLOOR
-fi
-
-# if no install path (for experimental) set, use floor
-if test -z "$BGQ_INSTALL"
-then
-  BGQ_INSTALL=$BGQ_TYPICAL_FLOOR
-fi
-
-BGQ_ZLIB=/soft/libraries/alcf/current/xl/ZLIB/
-BGQ_BIN=$BGQ_FLOOR/gnu-linux/bin
 if test -d "$BGQ_INSTALL/comm/include"
 then
   BGQ_INC="-I$BGQ_INSTALL/comm/include -I$BGQ_INSTALL/spi/include -I$BGQ_INSTALL -I$BGQ_INSTALL/spi/include/kernel/cnk -I$BGQ_ZLIB/include"
@@ -30,44 +16,15 @@ then
  exit 1
 fi
 
-GCC_OPTS="-Wno-deprecated "
-OPTS_CPP="$OPTS_CPP"
-OPTS_LD="$OPTS_LD"
-
-CMK_C_OPTIMIZE='-O3'
-CMK_CXX_OPTIMIZE='-O3'
-CMK_ENABLE_C11='-qlanglvl=extc1x'
-CMK_ENABLE_CPP11='-qlanglvl=extended0x'
-
-CMK_CPP_CHARM="$BGQ_BIN/powerpc64-bgq-linux-cpp -P"
-CMK_CPP_C="$BGQ_BIN/powerpc64-bgq-linux-cpp -E "
 CMK_CXX="bgxlC_r -qhalt=e -qnokeyword=__int128 -qtls=local-exec -DCMK_USING_XLC=1"
 CMK_CC="bgxlc_r -qcpluscmt -qhalt=e -qnokeyword=__int128 -qtls=local-exec"
-CMK_CF77="bgxlf_r "
-CMK_CF90="bgxlf90_r  -qsuffix=f=f90"
-CMK_CF90_FIXED="bgxlf90_r "
 
 CMK_LD="$CMK_CC"
 CMK_LDXX="$CMK_CXX"
 
-CMK_NATIVE_CC='gcc '
-CMK_NATIVE_LD='gcc '
-CMK_NATIVE_CXX='g++ -Wno-deprecated '
-CMK_NATIVE_LDXX='g++'
+CMK_CF77="bgxlf_r "
+CMK_CF90="bgxlf90_r  -qsuffix=f=f90"
+CMK_CF90_FIXED="bgxlf90_r "
 
-CMK_RANLIB="$BGQ_BIN/powerpc64-bgq-linux-ranlib "
-CMK_AR="$BGQ_BIN/powerpc64-bgq-linux-ar q "
-CMK_NM='nm '
-CMK_QT="aix"
-
-CMK_LD_LIBRARY_PATH="-Wl,-rpath,$CHARMLIBSO/"
-CMK_LIBS='-lckqt'
 CMK_SYSINC="$BGQ_INC"
 CMK_SYSLIBS="$BGQ_LIB"
-CMK_F90LIBS="-lxlf90 -lxlopt -lxl -lxlfmath"
-CMK_MOD_NAME_ALLCAPS=1
-CMK_MOD_EXT="mod"
-CMK_F90_USE_MODDIR=1
-CMK_F90_MODINC="-I"
-
-CMK_BLUEGENEQ="1"
