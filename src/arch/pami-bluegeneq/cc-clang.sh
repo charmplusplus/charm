7b5d1ff237396a9967bb81698028056b45b142af
Evan Ramos <evan@hpccharm.com>
2017-10-31 12:23:27 -0500

    Separate flags from compiler variables for bgclang
    Change-Id: Ic92889e2803096e83d9b0e014b96b58587425031

--- src/arch/pami-bluegeneq/cc-clang.sh
+++ src/arch/pami-bluegeneq/cc-clang.sh
@@ -1,13 +1,19 @@
 CMK_CPP_CHARM="$BGQ_BIN/powerpc64-bgq-linux-cpp -P"
-CMK_CPP_C="bgclang++ -E "
-CMK_CXX="bgclang++ -Wno-deprecated-declarations "
-CMK_CC="bgclang -Wno-deprecated-declarations "
+CMK_CPP_C='bgclang++'
+CMK_CXX='bgclang++'
+CMK_CC='bgclang'
 CMK_LD="$CMK_CC"
 CMK_LDXX="$CMK_CXX"
 
+CMK_CPP_C_FLAGS='-E'
+
+CMK_CC_FLAGS='-Wno-deprecated-declarations'
+CMK_CXX_FLAGS='-Wno-deprecated-declarations'
+CMK_LD_FLAGS=''
+CMK_LDXX_FLAGS=''
+
 CMK_C_OPTIMIZE='-O3 -ffast-math '
 CMK_CXX_OPTIMIZE='-O3 -ffast-math '
-OPTS_LD="$OPTS_LD "
 CMK_QT="generic64-light"
 
 CMK_COMPILER='bgclang'
