f86061dbc709de707adb36d6feab4d951f53056e
Matthias Diener <mdiener@illinois.edu>
2018-01-17 15:56:01 -0600

    AMPI: update chkpt test to use new migration interface
    Change-Id: I623129577ff2c8a5ff3508cf8f4759ab3ecad405

--- tests/ampi/chkpt/hello.c
+++ tests/ampi/chkpt/hello.c
@@ -8,15 +8,10 @@ int main(int argc, char **argv) {
   int i;
   double a[2]={.1,.3},b[2]={.5,.7};
   MPI_Status sts;
-  MPI_Info hints;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
   MPI_Comm_size(MPI_COMM_WORLD,&size);
-
-  /* Set up MPI_Info hints for AMPI_Migrate() */
-  MPI_Info_create(&hints);
-  MPI_Info_set(hints, "ampi_checkpoint", "to_file=log");
 
   for(step=0;step<6;step++){
     leftnbr = (myrank+size-1)%size;
@@ -25,7 +20,7 @@ int main(int argc, char **argv) {
     MPI_Recv(b,2,MPI_DOUBLE,leftnbr,0,MPI_COMM_WORLD,&sts);
     if(myrank==0) printf("[%d]step %d,a={%f,%f},b={%f,%f}\n",myrank,step,a[0],a[1],b[0],b[1]);
     if(step==2){
-      AMPI_Migrate(hints);
+      AMPI_Migrate(AMPI_INFO_CHKPT_IN_MEMORY);
     }
   }
 
