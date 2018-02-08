fa6aa941d7d494ce88061bb14439f99517c3cbc4
Sam White <white67@illinois.edu>
2018-02-08 15:50:03 -0600

    AMPI cleanup: make chkpt test checkpoint to file as 'make test' expects
    Change-Id: Ic30aaf4e9fd5c21118e3a77a49953c816029ca2a

--- tests/ampi/chkpt/hello.c
+++ tests/ampi/chkpt/hello.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+#define CHKPT_TO_FILE 1 // define to 0 for in-memory checkpointing
+
 int main(int argc, char **argv) {
   int myrank,size,leftnbr,rightnbr;
   int step=0;
@@ -13,6 +15,14 @@ int main(int argc, char **argv) {
   MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
   MPI_Comm_size(MPI_COMM_WORLD,&size);
 
+  MPI_Info chkpt_info;
+  MPI_Info_create(&chkpt_info);
+#if CHKPT_TO_FILE
+  MPI_Info_set(chkpt_info, "ampi_checkpoint", "to_file=log");
+#else
+  MPI_Info_set(chkpt_info, "ampi_checkpoint", "in_memory");
+#endif
+
   for(step=0;step<6;step++){
     leftnbr = (myrank+size-1)%size;
     rightnbr = (myrank+1)%size;
@@ -20,10 +30,11 @@ int main(int argc, char **argv) {
     MPI_Recv(b,2,MPI_DOUBLE,leftnbr,0,MPI_COMM_WORLD,&sts);
     if(myrank==0) printf("[%d]step %d,a={%f,%f},b={%f,%f}\n",myrank,step,a[0],a[1],b[0],b[1]);
     if(step==2){
-      AMPI_Migrate(AMPI_INFO_CHKPT_IN_MEMORY);
+      AMPI_Migrate(chkpt_info);
     }
   }
 
+  MPI_Info_free(&chkpt_info);
   MPI_Finalize();
   return 0;
 }
