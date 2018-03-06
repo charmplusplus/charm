ba80d8c4073514f7ddc7bfe5d1ffc0dad14eafca
Ronak Buch <rabuch2@illinois.edu>
2018-03-06 14:36:32 -0600

    Feature #1353: Add support for easily specifying compiler versions for gcc and clang when building
    Change-Id: Ibf9da82d6a889171f29cbac5b0d8b3ce53306506

--- src/arch/common/cc-clang.sh
+++ src/arch/common/cc-clang.sh
@@ -1,10 +1,10 @@
 # Assumes Clang C/C++ compiler:
 CMK_CPP_CHARM="cpp -P"
-CMK_CPP_C="clang"
-CMK_CC="clang"
-CMK_LD="clang"
-CMK_CXX="clang++"
-CMK_LDXX="clang++"
+CMK_CPP_C="clang$CMK_COMPILER_SUFFIX"
+CMK_CC="clang$CMK_COMPILER_SUFFIX"
+CMK_LD="clang$CMK_COMPILER_SUFFIX"
+CMK_CXX="clang++$CMK_COMPILER_SUFFIX"
+CMK_LDXX="clang++$CMK_COMPILER_SUFFIX"
 
 CMK_CPP_C_FLAGS="-E"
 
