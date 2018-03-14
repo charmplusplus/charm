43c51a4b11744bc3fea64f18439217ac2a92ee5d
Seonmyeong Bak <sbak5@illinois.edu>
2018-03-14 01:32:58 -0500

    Bug #1798: uFcontext thread support for 64-bit ARM systems
    Added separate configuration directories at 'src/arch' for ARMv8
    systems.
    
    Turned on CMK_64BIT and off CMK_32BIT.
    
    For ARMv8 systems, the name of the target should end with arm8.
    
    Other further configuration changes for ARMv8 should be added
    in the separate directories.
    
    This patch doens't resolve uFcontext issue on ARM systems but avoid
    compile issues.
    
    Change-Id: Icd22beaf2a8ca3574dc4ebbb298cba29d785f50f

--- src/arch/multicore-arm8/conv-mach.h
+++ src/arch/multicore-arm8/conv-mach.h
@@ -3,8 +3,12 @@
 #define _CONV_MACH_H
 
 #define CMK_NETPOLL         1
+#define CMK_MULTICORE                                      1
 
 #define CMK_ARM					  1 
+
+#undef CMK_IMMEDIATE_MSG
+#define CMK_IMMEDIATE_MSG                                  0
 
 #define CMK_ASYNC_NOT_NEEDED                               0
 #define CMK_ASYNC_USE_FIOASYNC_AND_FIOSETOWN               0
@@ -26,8 +30,9 @@
 #define CMK_SSH_IS_A_COMMAND                               1
 #define CMK_SSH_NOT_NEEDED                                 0
 
-#define CMK_SHARED_VARS_UNAVAILABLE                        1
+#define CMK_SHARED_VARS_UNAVAILABLE                        0
 #define CMK_SHARED_VARS_UNIPROCESSOR                       0
+#define CMK_SHARED_VARS_POSIX_THREADS_SMP                  1
 
 #define CMK_THREADS_USE_CONTEXT                            1
 #define CMK_THREADS_USE_JCONTEXT                           0
@@ -47,8 +52,30 @@
 #define CMK_TIMER_USE_SPECIAL                              0
 #define CMK_TIMER_USE_TIMES                                0
 
-#define CMK_64BIT   0 
-#define CMK_32BIT   1
+#if CMK_HAS_STDINT_H
+#include <stdint.h>
+#define CMK_TYPEDEF_INT2 int16_t
+#define CMK_TYPEDEF_INT4 int32_t
+#define CMK_TYPEDEF_INT8 int64_t
+#define CMK_TYPEDEF_UINT2 uint16_t
+#define CMK_TYPEDEF_UINT4 uint32_t
+#define CMK_TYPEDEF_UINT8 uint64_t
+#else
+#define CMK_TYPEDEF_INT2 short
+#define CMK_TYPEDEF_INT4 int
+#define CMK_TYPEDEF_UINT2 unsigned short
+#define CMK_TYPEDEF_UINT4 unsigned int
+#if CMK_LONG_LONG_DEFINED
+#define CMK_TYPEDEF_INT8 long long
+#define CMK_TYPEDEF_UINT8 unsigned long long
+#else
+#error "No definition for a 64-bit integer"
+#endif
+#endif
+
+
+#define CMK_64BIT   1 
+#define CMK_32BIT   0
 #define CMK_WHEN_PROCESSOR_IDLE_BUSYWAIT                   0
 #define CMK_WHEN_PROCESSOR_IDLE_USLEEP                     1
 
@@ -62,3 +89,5 @@
 #endif
 
 
+
+#define CMK_SMP						   1
--- src/arch/netlrts-linux-arm8/conv-mach.h
+++ src/arch/netlrts-linux-arm8/conv-mach.h
@@ -47,8 +47,8 @@
 #define CMK_TIMER_USE_SPECIAL                              0
 #define CMK_TIMER_USE_TIMES                                0
 
-#define CMK_64BIT   0 
-#define CMK_32BIT   1
+#define CMK_64BIT   1 
+#define CMK_32BIT   0
 #define CMK_WHEN_PROCESSOR_IDLE_BUSYWAIT                   0
 #define CMK_WHEN_PROCESSOR_IDLE_USLEEP                     1
 
