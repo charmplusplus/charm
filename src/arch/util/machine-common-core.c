charmplusplus/charm

commit 1d8d260abd63dce83f9b9e94905b8733e5712c4a

Rename all machine.c and memory.c to machine.C and memory.C
This redefines these files as C++.

Change-Id: I63ac7dc495569d193b1690ceb02e28aa09df2bf0

src/arch/util/machine-common-core.c
@@ -237,7 +237,7 @@ CpvDeclare(unsigned , networkProgressCount);
 int networkProgressPeriod;
 
 #if CMK_CCS_AVAILABLE
-extern int ccsRunning;
+CMI_EXTERNC_VARIABLE int ccsRunning;
 #endif
 
 /* ===== Beginning of Common Function Declarations ===== */
@@ -309,6 +309,7 @@ void ConverseExit(void);
 /* Functions providing incoming network messages */
 void *CmiGetNonLocal(void);
 #if CMK_NODE_QUEUE_AVAILABLE
+CMI_EXTERNC
 void *CmiGetNonLocalNodeQ(void);
 #endif
 /* Utiltiy functions */
@@ -822,7 +823,8 @@ if (  MSG_STATISTIC)
 #include "TopoManager.h"
 CMI_EXTERNC
 void createCustomPartitions(int numparts, int *partitionSize, int *nodeMap);
-extern void setDefaultPartitionParams(void);
+CMI_EXTERNC
+void setDefaultPartitionParams(void);
 
 void create_topoaware_partitions(void) {
   int i, j, numparts_bak;
@@ -1430,8 +1432,10 @@ if (  MSG_STATISTIC)
     ConverseRunPE(initret);
 }
 
-extern void ConverseCommonInit(char **argv);
-extern void CthInit(char **argv);
+CMI_EXTERNC
+void ConverseCommonInit(char **argv);
+CMI_EXTERNC
+void CthInit(char **argv);
 static void ConverseRunPE(int everReturn) {
     CmiState cs;
     char** CmiMyArgv;
@@ -1572,7 +1576,8 @@ static INLINE_KEYWORD void AdvanceCommunication(int whenidle) {
 #endif
 }
 
-extern void ConverseCommonExit(void);
+CMI_EXTERNC
+void ConverseCommonExit(void);
 
 static void CommunicationServer(int sleepTime) {
 #if CMK_SMP 

src/arch/gni-crayxc/conv-mach.h
@@ -14,7 +14,7 @@
 #define CMK_MEMORY_PROTECTABLE                             0
 
 /* defines which version of memory handlers should be used.
-   used in conv-core/machine.c */
+   used in conv-core/machine.C */
 #define CMK_MALLOC_USE_GNU_MALLOC                          0
 #define CMK_MALLOC_USE_OS_BUILTIN                          1
 
@@ -56,7 +56,7 @@
 #define CMK_USE_SPINLOCK                                   1
 
 /* Specifies what kind of timer to use, and the correspondent headers will be
-   included in convcore.c. If none is selected, then the machine.c file needs to
+   included in convcore.c. If none is selected, then the machine.C file needs to
    implement the timer primitives. */
 #define CMK_TIMER_USE_RTC                                  0
 #define CMK_TIMER_USE_RDTSC                                0
@@ -69,7 +69,7 @@
 
 /* Specifies what the processor will do when it is idle, either sleep (1) or go
    into busy waiting mode (0). In convcore.c there are a few files included if
-   sleeping mode, but the real distinct implementation is in the machine.c
+   sleeping mode, but the real distinct implementation is in the machine.C
    file. */
 #define CMK_WHEN_PROCESSOR_IDLE_BUSYWAIT                   1
 #define CMK_WHEN_PROCESSOR_IDLE_USLEEP                     0

src/arch/gni-crayxe/conv-mach.h
@@ -15,7 +15,7 @@
 #define CMK_MEMORY_PROTECTABLE                             0
 
 /* defines which version of memory handlers should be used.
-   used in conv-core/machine.c */
+   used in conv-core/machine.C */
 #define CMK_MALLOC_USE_GNU_MALLOC                          0
 #define CMK_MALLOC_USE_OS_BUILTIN                          1
 
@@ -57,7 +57,7 @@
 #define CMK_USE_SPINLOCK                                   1
 
 /* Specifies what kind of timer to use, and the correspondent headers will be
-   included in convcore.c. If none is selected, then the machine.c file needs to
+   included in convcore.c. If none is selected, then the machine.C file needs to
    implement the timer primitives. */
 #define CMK_TIMER_USE_RTC                                  0
 #define CMK_TIMER_USE_RDTSC                                0
@@ -69,7 +69,7 @@
 
 /* Specifies what the processor will do when it is idle, either sleep (1) or go
    into busy waiting mode (0). In convcore.c there are a few files included if
-   sleeping mode, but the real distinct implementation is in the machine.c
+   sleeping mode, but the real distinct implementation is in the machine.C
    file. */
 #define CMK_WHEN_PROCESSOR_IDLE_BUSYWAIT                   1
 #define CMK_WHEN_PROCESSOR_IDLE_USLEEP                     0

src/arch/gni/machine-cmidirect.c
@@ -4,7 +4,7 @@
 */
 
 /*
-  included in machine.c
+  included in machine.C
   Yanhua Sun, 2/5/2012
 */
 

src/arch/gni/machine-onesided.c
@@ -1,5 +1,5 @@
 /*
-   included in machine.c
+   included in machine.C
    Justin Miron
  */
 

src/arch/gni/machine-persistent.c
@@ -4,7 +4,7 @@
 */
 
 /*
-  included in machine.c
+  included in machine.C
   Gengbin Zheng, 9/6/2011
 */
 

src/arch/gni/machine.C
@@ -756,7 +756,8 @@ static IndexPool  persistPool;
 #define CHARM_MAGIC_NUMBER               126
 
 #if CMK_ERROR_CHECKING
-extern unsigned char computeCheckSum(unsigned char *data, int len);
+CMI_EXTERNC
+unsigned char computeCheckSum(unsigned char *data, int len);
 static int checksum_flag = 0;
 #define CMI_SET_CHECKSUM(msg, len)      \
         if (checksum_flag)  {   \

src/arch/mpi/machine-onesided.c
@@ -1,5 +1,5 @@
 /*
-  included in machine.c
+  included in machine.C
   Nitin Bhat 03/07/2017
 */
 

commit 9513b238a2d1c691c2529211ef0e81ce1b9c1867

Fix comm thread tracing broken in 1d8d260ab
Change-Id: I8f22d6972de691f4399e8e6374f50e33bbb66de3

src/arch/util/machine-common-core.c
@@ -77,7 +77,8 @@ static int CmiNodesDim;
 /* ###End of Broadcast related definitions ### */
 
 #if CMK_SMP_TRACE_COMMTHREAD
-extern double TraceTimerCommon(void);
+CMI_EXTERNC
+double TraceTimerCommon(void);
 #endif
 
 static void handleOneBcastMsg(int size, char *msg);