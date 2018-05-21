e8e5ab8f519f0a36d885ca7e39cec24fe7736274
Evan Ramos <evan@hpccharm.com>
2018-05-21 16:08:38 -0500

    AMPI: Add Fortran privatization test
    Change-Id: Icadc128575a1174c79c856cfbfc27980590d09ad

---
+++ tests/ampi/privatization/test-f90-tlsglobals.f90
@@ -0,0 +1,26 @@
+      subroutine mpi_main
+
+        implicit none
+        include 'mpif.h'
+        integer :: ierr
+
+        call mpi_init(ierr)
+
+        call privatization_test_framework()
+
+        call mpi_finalize(ierr)
+
+      end subroutine mpi_main
+
+
+      subroutine perform_test_batch(failed, rank, my_wth)
+
+        implicit none
+        integer failed, rank, my_wth
+        integer global_variable
+        common /globals/ global_variable
+        !$omp threadprivate(/globals/)
+
+        call test_privatization(failed, rank, my_wth, global_variable)
+
+      end subroutine perform_test_batch
