bb7213169ba60ed96ca7cff34aecc49eccc53c4b
Juan Galvez <jjgalvez@illinois.edu>
2018-08-31 11:21:25 -0500

    mpi-win: Don't let windows.h include winsock.h
    winsock.h conflicts with winsock2.h included in sockRoutines
    
    Change-Id: I915c21caab0c61afbca99ce2b91bef775fcec868

--- src/arch/mpi/machine.C
+++ src/arch/mpi/machine.C
@@ -18,6 +18,9 @@
 
 /*Support for ++debug: */
 #if defined(_WIN32)
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 #include <wincon.h>
 #include <sys/types.h>
