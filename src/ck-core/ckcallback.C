charmplusplus/charm

commit ede3f6d854de03f1836c887530b3626cdc139640

Sections #1967: cleanup API by taking section creation args as std::vector's
Also replace internal storage of section objects with std::vector's, and
update the documentation and examples to match.

Define a macro CMK_CKSECTIONINFO_STL now for applications to check.

Change-Id: I0ee0dcf70f62528cc7b2cd06f3881e5409296966

src/ck-core/ckcallback.C
@@ -183,12 +183,12 @@ CkCallback::CkCallback(int ep,CProxySection_ArrayBase &sectElt,bool doInline) {
       d.section.ep=ep; 
       CkSectionID secID=sectElt.ckGetSectionID(0); 
       d.section.sinfo = secID._cookie.info;
-      d.section._elems = secID._elems;
-      d.section._nElems = secID._nElems;
-      d.section.pelist = secID.pelist;
-      d.section.npes = secID.npes;
-      secID._elems = NULL;
-      secID.pelist = NULL;
+      d.section._elems = secID._elems.data();
+      d.section._nElems = secID._elems.size();
+      d.section.pelist = secID.pelist.data();
+      d.section.npes = secID.pelist.size();
+      secID._elems.clear();
+      secID.pelist.clear();
       d.section.hasRefnum = false;
       d.section.refnum = 0;
 }
@@ -200,10 +200,10 @@ CkCallback::CkCallback(int ep, CkSectionID &id) {
       type=bcastSection;
       d.section.ep=ep;
       d.section.sinfo = id._cookie.info;
-      d.section._elems = id._elems;
-      d.section._nElems = id._nElems;
-      d.section.pelist = id.pelist;
-      d.section.npes = id.npes;
+      d.section._elems = id._elems.data();
+      d.section._nElems = id._elems.size();
+      d.section.pelist = id.pelist.data();
+      d.section.npes = id.pelist.size();
 }
 
 CkCallback::CkCallback(ArrayElement *p, int ep,bool doInline) {
@@ -394,8 +394,8 @@ void CkCallback::send(void *msg) const
                 CkSectionInfo sinfo(d.section.sinfo);
                 CkSectionID secID(sinfo, d.section._elems, d.section._nElems, d.section.pelist, d.section.npes);
 		CkBroadcastMsgSection(d.section.ep, msg, secID);
-                secID._elems = NULL;
-                secID.pelist = NULL;
+                secID._elems.clear();
+                secID.pelist.clear();
 		break;
              }
 	case replyCCS: { /* Send CkDataMsg as a CCS reply */

src/ck-core/cksection.h
@@ -112,7 +112,8 @@ class CkArrayIndex5D;
 class CkArrayIndex6D;
 
 #define CKSECTIONID_CONSTRUCTOR(index) \
-  CkSectionID(const CkArrayID &aid, const CkArrayIndex##index *elems, const int nElems, int factor=USE_DEFAULT_BRANCH_FACTOR);
+  CkSectionID(const CkArrayID &aid, const CkArrayIndex##index *elems, const int nElems, int factor=USE_DEFAULT_BRANCH_FACTOR); \
+  CkSectionID(const CkArrayID &aid, const std::vector<CkArrayIndex##index> &elems, int factor=USE_DEFAULT_BRANCH_FACTOR);
 
 #define USE_DEFAULT_BRANCH_FACTOR 0
 
@@ -125,28 +126,31 @@ public:
   /// Minimal section info (cookie)
   CkSectionInfo _cookie;
   /// The list of array indices that are section members (array sections)
-  CkArrayIndex *_elems;
-  /// The number of section members
-  int _nElems;
+  std::vector<CkArrayIndex> _elems;
   /**
    * \brief A list of PEs that host section members.
    * - For group sections these point to the PEs in the section
    * - For array sections these point to the processors the array elements are on
    * @note For array sections, currently not saved when pupped across processors
    */
-  int *pelist;
-  /// The number of PEs that host section members
-  int npes;
+  std::vector<int> pelist;
   /// Branching factor in the spanning tree, can be negative
   int bfactor;
 
-  CkSectionID(): _elems(NULL), _nElems(0), pelist(NULL), npes(0),
-                  bfactor(USE_DEFAULT_BRANCH_FACTOR) {}
+  CkSectionID(): bfactor(USE_DEFAULT_BRANCH_FACTOR) {}
   CkSectionID(const CkSectionID &sid);
-  CkSectionID(CkSectionInfo &c, CkArrayIndex *e, int n, int *_pelist, int _npes,
+  CkSectionID(CkSectionInfo &c, const CkArrayIndex *e, int n, const int *_pelist, int _npes,
+              int factor=USE_DEFAULT_BRANCH_FACTOR): _cookie(c), bfactor(factor)
+  {
+    _elems.assign(e, e+n);
+    pelist.assign(_pelist, _pelist+n);
+  }
+  CkSectionID(CkSectionInfo &c, const std::vector<CkArrayIndex>& e, const std::vector<int>& _pelist,
               int factor=USE_DEFAULT_BRANCH_FACTOR): _cookie(c), _elems(e),
-              _nElems(n), pelist(_pelist), npes(_npes), bfactor(factor)  {}
+              pelist(_pelist), bfactor(factor)  {}
   CkSectionID(const CkGroupID &gid, const int *_pelist, const int _npes,
+              int factor=USE_DEFAULT_BRANCH_FACTOR);
+  CkSectionID(const CkGroupID &gid, const std::vector<int>& _pelist,
               int factor=USE_DEFAULT_BRANCH_FACTOR);
   CKSECTIONID_CONSTRUCTOR(1D)
   CKSECTIONID_CONSTRUCTOR(2D)
@@ -157,11 +161,10 @@ public:
   CKSECTIONID_CONSTRUCTOR(Max)
 
   inline CkGroupID get_aid() const { return _cookie.get_aid(); }
+  inline int nElems() const { return _elems.size(); }
+  inline int nPes() const { return pelist.size(); }
   void operator=(const CkSectionID &);
-  ~CkSectionID() {
-    delete [] _elems;
-    delete [] pelist;
-  }
+  ~CkSectionID() = default;
   void pup(PUP::er &p);
 };
 PUPmarshall(CkSectionID)

doc/charm++/sections.tex
@@ -31,12 +31,12 @@ of all the array section members through either explicit enumeration, or an inde
 For example, for a 3D array:
 
 \begin{alltt}
-  CkVec<CkArrayIndex3D> elems;    // add array indices
+  std::vector<CkArrayIndex3D> elems;  // add array indices
   for (int i=0; i<10; i++)
     for (int j=0; j<20; j+=2)
       for (int k=0; k<30; k+=2)
-         elems.push_back(CkArrayIndex3D(i, j, k));
-  CProxySection_Hello proxy = CProxySection_Hello::ckNew(helloArrayID, elems.getVec(), elems.size());
+         elems.emplace_back(i, j, k);
+  CProxySection_Hello proxy = CProxySection_Hello::ckNew(helloArrayID, elems);
 \end{alltt}
 
 Alternatively, one can do the same thing by providing the index range [lbound:ubound:stride]
@@ -53,9 +53,8 @@ For user-defined array index other than CkArrayIndex1D to CkArrayIndex6D,
 one needs to use the generic array index type: CkArrayIndex.
 
 \begin{alltt}
-  CkArrayIndex *elems;    // add array indices
-  int numElems;
-  CProxySection_Hello proxy = CProxySection_Hello::ckNew(helloArrayID, elems, numElems);
+  std::vector<CkArrayIndex> elems;  // add array indices
+  CProxySection_Hello proxy = CProxySection_Hello::ckNew(helloArrayID, elems);
 \end{alltt}
 
 \subsection{Group sections}
@@ -246,12 +245,11 @@ Note: cross section logic also works for groups with analogous characteristics.
 Given three arrays declared thusly:
 
 \begin{alltt}
-	  CkArrayID *aidArr= new CkArrayID[3];
-	  CProxy\_multisectiontest\_array1d *Aproxy= new CProxy\_multisectiontest\_array1d[3];
-	  for(int i=0;i<3;i++)
+	  std::vector<CkArrayID> aidArr(3);
+	  for (int i=0; i<3; i++)
 	    \{
-	      Aproxy[i]=CProxy\_multisectiontest\_array1d::ckNew(masterproxy.ckGetGroupID(),ArraySize);	  
-	      aidArr[i]=Aproxy[i].ckGetArrayID();
+	      CProxy\_multisectiontest\_array1d Aproxy = CProxy\_multisectiontest\_array1d::ckNew(masterproxy.ckGetGroupID(), ArraySize);
+	      aidArr[i] = Aproxy.ckGetArrayID();
 	    \}
 \end{alltt}
 
@@ -259,23 +257,19 @@ One can make a section including the  lower half elements of all three
 arrays as follows:
 
 \begin{alltt}
-	  int aboundary=ArraySize/2;
-	  int afloor=aboundary;
-	  int aceiling=ArraySize-1;
-	  int asectionSize=aceiling-afloor+1;
+	  int aboundary = ArraySize/2;
+	  int afloor = aboundary;
+	  int aceiling = ArraySize-1;
+	  int asectionSize = aceiling-afloor+1;
 	  // cross section lower half of each array
-	  CkArrayIndex **aelems= new CkArrayIndex*[3];
-	  aelems[0]= new CkArrayIndex[asectionSize];
-	  aelems[1]= new CkArrayIndex[asectionSize];
-	  aelems[2]= new CkArrayIndex[asectionSize];
-	  int *naelems=new int[3];
-	  for(int k=0;k<3;k++)
+	  std::vector<std::vector<CkArrayIndex> > aelems(3);
+	  for (int k=0; k<3; k++)
 	    \{
-	      naelems[k]=asectionSize;
-	      for(int i=afloor,j=0;i<=aceiling;i++,j++)
-	        aelems[k][j]=CkArrayIndex1D(i);
+	      aelems[k].resize(asectionSize);
+	      for (int i=afloor,j=0; i<=aceiling; i++,j++)
+	        aelems[k][j] = CkArrayIndex1D(i);
 	    \}
-	  CProxySection\_multisectiontest\_array1d arrayLowProxy(3,aidArr,aelems,naelems);
+	  CProxySection\_multisectiontest\_array1d arrayLowProxy(aidArr, aelems);
 \end{alltt}
 
 The resulting cross section proxy, as in the example \uw{arrayLowProxy},

examples/charm++/hello/xarraySection/hello.C
@@ -42,27 +42,23 @@ public:
     int afloor = 0, aceiling = sectionSize-1;
 
     // Allocate space
-    std::vector<CProxy_Hello> arrayOfArrays(numArrays);
     std::vector<CkArrayID> arrID(numArrays);
-    std::vector<int> nelems(numArrays);
-    std::vector<CkArrayIndex *> elems(numArrays);
+    std::vector<std::vector<CkArrayIndex> > elems(numArrays);
 
     // Create a list of array section members
     for(int k=0; k < numArrays; k++)
     {
         // Create the array
-        arrayOfArrays[k] = CProxy_Hello::ckNew(k, mcastMgrGID, numElements);
+        CProxy_Hello array = CProxy_Hello::ckNew(k, mcastMgrGID, numElements);
         // Store the AID
-        arrID[k]  = arrayOfArrays[k].ckGetArrayID();
+        arrID[k]  = array.ckGetArrayID();
         // Create a list of section member indices in this array
-        nelems[k] = sectionSize;
-        elems[k]  = new CkArrayIndex[sectionSize];
+        elems[k].resize(sectionSize);
         for(int i=afloor,j=0; i <= aceiling; i++,j++)
             elems[k][j] = CkArrayIndex1D(i);
     }
     // Create the x-array-section
-    sectionProxy = CProxySection_Hello(numArrays, arrID.data(), elems.data(), nelems.data());
-    for (auto elem : elems) delete [] elem;
+    sectionProxy = CProxySection_Hello(arrID, elems);
 
     // Delegate the section comm to the CkMulticast library
     sectionProxy.ckSectionDelegate(mcastMgr);

src/ck-core/charm++.h
@@ -1010,10 +1010,10 @@ public:
   inline CkSectionID &ckGetSectionID() {return _sid[0]; }
   inline CkSectionID &ckGetSectionID(int i) {return _sid[i]; }
   inline CkGroupID ckGetGroupIDn(int i) const {return _sid[i]._cookie.get_aid();}
-  inline int *ckGetElements() const {return _sid[0].pelist;}
-  inline int *ckGetElements(int i) const {return _sid[i].pelist;}
-  inline int ckGetNumElements() const { return _sid[0].npes; }
-  inline int ckGetNumElements(int i) const { return _sid[i].npes; }
+  inline int *ckGetElements() const {return _sid[0].pelist.data();}
+  inline int *ckGetElements(int i) const {return _sid[i].pelist.data();}
+  inline int ckGetNumElements() const { return _sid[0].pelist.size(); }
+  inline int ckGetNumElements(int i) const { return _sid[i].pelist.size(); }
   inline int ckGetBfactor() const { return _sid[0].bfactor; }
   void pup(PUP::er &p) {
     CProxy_Group::pup(p);

src/ck-core/ck.C
@@ -256,13 +256,13 @@ void CkDelegateMgr::GroupSend(CkDelegateData *pd,int ep,void *m,int onPE,CkGroup
 void CkDelegateMgr::GroupBroadcast(CkDelegateData *pd,int ep,void *m,CkGroupID g)
   { CkBroadcastMsgBranch(ep,m,g); }
 void CkDelegateMgr::GroupSectionSend(CkDelegateData *pd,int ep,void *m,int nsid,CkSectionID *s)
-  { CkSendMsgBranchMulti(ep,m,s->_cookie.get_aid(),s->npes,s->pelist); }
+  { CkSendMsgBranchMulti(ep,m,s->_cookie.get_aid(),s->pelist.size(),s->pelist.data()); }
 void CkDelegateMgr::NodeGroupSend(CkDelegateData *pd,int ep,void *m,int onNode,CkNodeGroupID g)
   { CkSendMsgNodeBranch(ep,m,onNode,g); }
 void CkDelegateMgr::NodeGroupBroadcast(CkDelegateData *pd,int ep,void *m,CkNodeGroupID g)
   { CkBroadcastMsgNodeBranch(ep,m,g); }
 void CkDelegateMgr::NodeGroupSectionSend(CkDelegateData *pd,int ep,void *m,int nsid,CkSectionID *s)
-  { CkSendMsgNodeBranchMulti(ep,m,s->_cookie.get_aid(),s->npes,s->pelist); }
+  { CkSendMsgNodeBranchMulti(ep,m,s->_cookie.get_aid(),s->pelist.size(),s->pelist.data()); }
 void CkDelegateMgr::ArrayCreate(CkDelegateData *pd,int ep,void *m,const CkArrayIndex &idx,int onPE,CkArrayID a)
 {
 	CProxyElement_ArrayBase ap(a,idx);
@@ -402,14 +402,19 @@ void CProxy::pup(PUP::er &p) {
 
 /**** Array sections */
 #define CKSECTIONID_CONSTRUCTOR_DEF(index) \
-CkSectionID::CkSectionID(const CkArrayID &aid, const CkArrayIndex##index *elems, const int nElems, int factor): _nElems(nElems), bfactor(factor) { \
+CkSectionID::CkSectionID(const CkArrayID &aid, const CkArrayIndex##index *elems, const int nElems, int factor): bfactor(factor) { \
+  _elems.assign(elems, elems+nElems);  \
   _cookie.get_aid() = aid;	\
   _cookie.get_pe() = CkMyPe();	\
-  _elems = new CkArrayIndex[nElems];	\
-  for (int i=0; i<nElems; i++) _elems[i] = elems[i];	\
-  pelist = NULL;	\
-  npes  = 0;	\
-}
+} \
+CkSectionID::CkSectionID(const CkArrayID &aid, const std::vector<CkArrayIndex##index> &elems, int factor): bfactor(factor) { \
+  _elems.resize(elems.size()); \
+  for (int i=0; i<_elems.size(); ++i) { \
+    _elems[i] = static_cast<CkArrayIndex>(elems[i]); \
+  } \
+  _cookie.get_aid() = aid;	\
+  _cookie.get_pe() = CkMyPe();	\
+} \
 
 CKSECTIONID_CONSTRUCTOR_DEF(1D)
 CKSECTIONID_CONSTRUCTOR_DEF(2D)
@@ -419,60 +424,34 @@ CKSECTIONID_CONSTRUCTOR_DEF(5D)
 CKSECTIONID_CONSTRUCTOR_DEF(6D)
 CKSECTIONID_CONSTRUCTOR_DEF(Max)
 
-CkSectionID::CkSectionID(const CkGroupID &gid, const int *_pelist, const int _npes, int factor): _nElems(0), _elems(NULL), npes(_npes), bfactor(factor) {
-  pelist = new int[npes];
-  for (int i=0; i<npes; i++) pelist[i] = _pelist[i];
+CkSectionID::CkSectionID(const CkGroupID &gid, const int *_pelist, const int _npes, int factor): bfactor(factor) {
+  _cookie.get_aid() = gid;
+  pelist.assign(_pelist, _pelist+_npes);
+}
+
+CkSectionID::CkSectionID(const CkGroupID &gid, const std::vector<int>& _pelist, int factor): pelist(_pelist), bfactor(factor) {
   _cookie.get_aid() = gid;
 }
 
 CkSectionID::CkSectionID(const CkSectionID &sid) {
-  int i;
   _cookie = sid._cookie;
-  _nElems = sid._nElems;
+  pelist = sid.pelist;
+  _elems = sid._elems;
   bfactor = sid.bfactor;
-  if (_nElems > 0) {
-    _elems = new CkArrayIndex[_nElems];
-    for (i=0; i<_nElems; i++) _elems[i] = sid._elems[i];
-  } else _elems = NULL;
-  npes = sid.npes;
-  if (npes > 0) {
-    pelist = new int[npes];
-    for (i=0; i<npes; ++i) pelist[i] = sid.pelist[i];
-  } else pelist = NULL;
 }
 
 void CkSectionID::operator=(const CkSectionID &sid) {
-  int i;
   _cookie = sid._cookie;
-  _nElems = sid._nElems;
+  pelist = sid.pelist;
+  _elems = sid._elems;
   bfactor = sid.bfactor;
-  if (_nElems > 0) {
-    _elems = new CkArrayIndex[_nElems];
-    for (i=0; i<_nElems; i++) _elems[i] = sid._elems[i];
-  } else _elems = NULL;
-  npes = sid.npes;
-  if (npes > 0) {
-    pelist = new int[npes];
-    for (i=0; i<npes; ++i) pelist[i] = sid.pelist[i];
-  } else pelist = NULL;
 }
 
 void CkSectionID::pup(PUP::er &p) {
-    p | _cookie;
-    p | bfactor;
-    p(_nElems);
-    if (_nElems > 0) {
-      if (p.isUnpacking()) _elems = new CkArrayIndex[_nElems];
-      for (int i=0; i< _nElems; i++) p | _elems[i];
-      npes = 0;
-      pelist = NULL;
-    } else {
-      // If _nElems is zero, than this section describes processors instead of array elements
-      _elems = NULL;
-      p(npes);
-      if (p.isUnpacking()) pelist = new int[npes];
-      p(pelist, npes);
-    }
+  p | _cookie;
+  p | pelist;
+  p | _elems;
+  p | bfactor;
 }
 
 /**** Tiny random API routines */

src/ck-core/ckarray.C
@@ -551,24 +551,33 @@ CProxyElement_ArrayBase::CProxyElement_ArrayBase(const ArrayElement *e)
 	:CProxy_ArrayBase(e), _idx(e->ckGetArrayIndex())
 	{}
 
-CProxySection_ArrayBase::CProxySection_ArrayBase(const CkArrayID &aid, const CkArrayIndex *elems, const int nElems, int factor) :CProxy_ArrayBase(aid), _nsid(1) {
-    _sid = new CkSectionID(aid, elems, nElems, factor);
+CProxySection_ArrayBase::CProxySection_ArrayBase(const CkArrayID &aid, const CkArrayIndex *elems, const int nElems, int factor) :CProxy_ArrayBase(aid) {
+    _sid.emplace_back(aid, elems, nElems, factor);
 }
 
+CProxySection_ArrayBase::CProxySection_ArrayBase(const CkArrayID &aid, const std::vector<CkArrayIndex> &elems, int factor) :CProxy_ArrayBase(aid) {
+    _sid.emplace_back(aid, elems, factor);
+}
 
-CProxySection_ArrayBase::CProxySection_ArrayBase(const int n, const CkArrayID *aid, CkArrayIndex const * const *elems, const int *nElems, int factor) :CProxy_ArrayBase(aid[0]), _nsid(n) {
-    if (_nsid == 1) _sid = new CkSectionID(aid[0], elems[0], nElems[0], factor);
-    else if (_nsid > 1) {
-    _sid = new CkSectionID[n];
-    for (int i=0; i<n; ++i) _sid[i] = CkSectionID(aid[i], elems[i], nElems[i], factor);
-    } else _sid = NULL;
+CProxySection_ArrayBase::CProxySection_ArrayBase(const int n, const CkArrayID *aid, CkArrayIndex const * const *elems, const int *nElems, int factor) :CProxy_ArrayBase(aid[0]) {
+    _sid.resize(n);
+    for (int i=0; i<_sid.size(); i++) {
+      _sid[i] = CkSectionID(aid[i], elems[i], nElems[i], factor);
+    }
+}
+
+CProxySection_ArrayBase::CProxySection_ArrayBase(const std::vector<CkArrayID> &aid, const std::vector<std::vector<CkArrayIndex> > &elems, int factor) :CProxy_ArrayBase(aid[0]) {
+    _sid.resize(aid.size());
+    for (int i=0; i<_sid.size(); i++) {
+      _sid[i] = CkSectionID(aid[i], elems[i], factor);
+    }
 }
 
 
 void CProxySection_ArrayBase::ckAutoDelegate(int opts){
-    if(_nsid < 1)
+    if(_sid.empty())
       CmiAbort("Auto Delegation before setting up CkSectionID\n");
-    CkArray *ckarr = CProxy_CkArray(_sid->get_aid()).ckLocalBranch();
+    CkArray *ckarr = CProxy_CkArray(_sid[0].get_aid()).ckLocalBranch();
     if(ckarr->isSectionAutoDelegated()){
     	CkMulticastMgr *mCastGrp = CProxy_CkMulticastMgr(ckarr->getmCastMgr()).ckLocalBranch();
     	ckSectionDelegate(mCastGrp, opts);
@@ -577,9 +586,9 @@ void CProxySection_ArrayBase::ckAutoDelegate(int opts){
 
 
 void CProxySection_ArrayBase::setReductionClient(CkCallback *cb) {
-    if(_nsid < 1)
+    if(_sid.empty())
       CmiAbort("setReductionClient before setting up CkSectionID\n");
-    CkArray *ckarr = CProxy_CkArray(_sid->get_aid()).ckLocalBranch();
+    CkArray *ckarr = CProxy_CkArray(_sid[0].get_aid()).ckLocalBranch();
     if(ckarr->isSectionAutoDelegated()){
       CkMulticastMgr *mCastGrp = CProxy_CkMulticastMgr(ckarr->getmCastMgr()).ckLocalBranch();
       mCastGrp->setReductionClient(*this, cb);
@@ -591,9 +600,9 @@ void CProxySection_ArrayBase::setReductionClient(CkCallback *cb) {
 
 
 void CProxySection_ArrayBase::resetSection(){
-    if(_nsid < 1)
+    if(_sid.empty())
       CmiAbort("resetSection before setting up CkSectionID\n");
-    CkArray *ckarr = CProxy_CkArray(_sid->get_aid()).ckLocalBranch();
+    CkArray *ckarr = CProxy_CkArray(_sid[0].get_aid()).ckLocalBranch();
     if(ckarr->isSectionAutoDelegated()){
       CkMulticastMgr *mCastGrp = CProxy_CkMulticastMgr(ckarr->getmCastMgr()).ckLocalBranch();
       mCastGrp->resetSection(*this);
@@ -742,13 +751,7 @@ void CProxyElement_ArrayBase::pup(PUP::er &p)
 void CProxySection_ArrayBase::pup(PUP::er &p)
 {
   CProxy_ArrayBase::pup(p);
-  p | _nsid;
-  if (p.isUnpacking()) {
-    if (_nsid == 1) _sid = new CkSectionID;
-    else if (_nsid > 1) _sid = new CkSectionID[_nsid];
-    else _sid = NULL;
-  }
-  for (int i=0; i<_nsid; ++i) _sid[i].pup(p);
+  p | _sid;
 }
 
 /*
@@ -1207,18 +1210,18 @@ void CkBroadcastMsgSection(int entryIndex, void *msg, CkSectionID sID, int opts 
 void CProxySection_ArrayBase::ckSend(CkArrayMessage *msg, int ep, int opts)
 {
 	if (ckIsDelegated()) //Just call our delegateMgr
-	  ckDelegatedTo()->ArraySectionSend(ckDelegatedPtr(), ep, msg, _nsid, _sid, opts);
+	  ckDelegatedTo()->ArraySectionSend(ckDelegatedPtr(), ep, msg, _sid.size(), _sid.data(), opts);
 	else {
 	  // send through all
-	  for (int k=0; k<_nsid; ++k) {
-	    for (int i=0; i< _sid[k]._nElems-1; i++) {
+	  for (int k=0; k<_sid.size(); ++k) {
+	    for (int i=0; i< _sid[k]._elems.size()-1; i++) {
 	      CProxyElement_ArrayBase ap(_sid[k]._cookie.get_aid(), _sid[k]._elems[i]);
 	      void *newMsg=CkCopyMsg((void **)&msg);
 	      ap.ckSend((CkArrayMessage *)newMsg,ep,opts);
 	    }
-	    if (_sid[k]._nElems > 0) {
-	      void *newMsg= (k<_nsid-1) ? CkCopyMsg((void **)&msg) : msg;
-	      CProxyElement_ArrayBase ap(_sid[k]._cookie.get_aid(), _sid[k]._elems[_sid[k]._nElems-1]);
+	    if (!_sid[k]._elems.empty()) {
+	      void *newMsg= (k<_sid.size()-1) ? CkCopyMsg((void **)&msg) : msg;
+	      CProxyElement_ArrayBase ap(_sid[k]._cookie.get_aid(), _sid[k]._elems[_sid[k]._elems.size()-1]);
 	      ap.ckSend((CkArrayMessage *)newMsg,ep,opts);
 	    }
 	  }

commit 45973b455c5e1bbc1ac9db0b4a7c9b8ae4fede9a

Partially fix CkCallbacks to section multicasts
This fixes CkCallbacks to section multicasts, which were broken by
commit ede3f6d854de03f1836c887530b3626cdc139640, and may have been
broken before then.

CkCallback's to sections will still break when migrated, since they
contain bare pointers to the section info. That issue is redmine #235.

Change-Id: If1830106d0f947e105e90aa6cbbab8219a059d35

src/ck-core/ckcallback.C
@@ -187,8 +187,6 @@ CkCallback::CkCallback(int ep,CProxySection_ArrayBase &sectElt,bool doInline) {
       d.section._nElems = secID._elems.size();
       d.section.pelist = secID.pelist.data();
       d.section.npes = secID.pelist.size();
-      secID._elems.clear();
-      secID.pelist.clear();
       d.section.hasRefnum = false;
       d.section.refnum = 0;
 }
@@ -204,6 +202,8 @@ CkCallback::CkCallback(int ep, CkSectionID &id) {
       d.section._nElems = id._elems.size();
       d.section.pelist = id.pelist.data();
       d.section.npes = id.pelist.size();
+      d.section.hasRefnum = false;
+      d.section.refnum = 0;
 }
 
 CkCallback::CkCallback(ArrayElement *p, int ep,bool doInline) {
@@ -394,8 +394,6 @@ void CkCallback::send(void *msg) const
                 CkSectionInfo sinfo(d.section.sinfo);
                 CkSectionID secID(sinfo, d.section._elems, d.section._nElems, d.section.pelist, d.section.npes);
 		CkBroadcastMsgSection(d.section.ep, msg, secID);
-                secID._elems.clear();
-                secID.pelist.clear();
 		break;
              }
 	case replyCCS: { /* Send CkDataMsg as a CCS reply */

src/ck-core/ckcallback.h
@@ -193,10 +193,18 @@ public:
 	      return (d.cfn.fn == other.d.cfn.fn &&
 		  d.cfn.onPE == other.d.cfn.onPE &&
 		  d.cfn.param == other.d.cfn.param);
+	    case bcastSection:
+	      return (d.section._elems == other.d.section._elems &&
+		d.section.pelist && other.d.section.pelist &&
+		d.section.sinfo == other.d.section.sinfo &&
+		d.section._nElems == other.d.section._nElems &&
+		d.section.npes == other.d.section.npes &&
+		d.section.ep == other.d.section.ep &&
+		((d.section.hasRefnum && other.d.section.hasRefnum) &&
+		 (d.section.refnum == other.d.section.refnum)));
 	    case ignore:
 	    case ckExit:
 	    case invalid:
-	    case bcastSection:
 	      return true;
 	    default:
 	      CkAbort("Inconsistent CkCallback type");
@@ -337,10 +345,6 @@ public:
 
 	~CkCallback() {
 	  thread_destroy();
-          if (bcastSection == type) {
-            if (d.section._elems != NULL) delete [] d.section._elems;
-            if (d.section.pelist != NULL) delete [] d.section.pelist;
-          }
 	}
 	
 	bool isInvalid(void) const {return type==invalid;}

src/ck-core/cksection.h
@@ -44,6 +44,10 @@ public:
     int pe;
     /// Counter tracking the last reduction that has traversed this section (used by CkMulticast)
     int redNo;
+
+    bool operator==(CkSectionInfoStruct &other) {
+      return (val == other.val && aid == other.aid && pe == other.pe && redNo == other.redNo);
+    }
   };
 
   CkSectionInfoStruct info;
@@ -153,7 +157,7 @@ public:
               int factor=USE_DEFAULT_BRANCH_FACTOR): _cookie(c), bfactor(factor)
   {
     _elems.assign(e, e+n);
-    pelist.assign(_pelist, _pelist+n);
+    pelist.assign(_pelist, _pelist+_npes);
   }
   CkSectionID(CkSectionInfo &c, const std::vector<CkArrayIndex>& e, const std::vector<int>& _pelist,
               int factor=USE_DEFAULT_BRANCH_FACTOR): _cookie(c), _elems(e),