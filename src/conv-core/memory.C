48fafa7ae5622265a119a07af6878303dbb8c3e7
Sam White <white67@illinois.edu>
2018-09-27 15:51:57 -0500

    Reduce memory.C's life-raft allocation from 32 to 16 KB
    Change-Id: Ic98487b7e0c553bf2d73d04d5d8411de849bb11a

--- src/conv-core/memory.C
+++ src/conv-core/memory.C
@@ -1027,7 +1027,7 @@ void CmiOutOfMemoryInit(void) {
 #if CMK_MEMORY_PREALLOCATE_HACK
   memory_preallocate_hack();
 #endif
-  MEM_LOCK_AROUND( memory_lifeRaft=(char *)mm_malloc(65536/2); )
+  MEM_LOCK_AROUND( memory_lifeRaft=(char *)mm_malloc(16384); )
   }
 }
 
