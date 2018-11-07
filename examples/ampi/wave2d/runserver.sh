9eec3cb21ef00c45a3bcca28787bbfc980037ad7
Sam White <white67@illinois.edu>
2018-11-07 10:53:22 -0600

    WIP: add a hacky liveViz interface to AMPI and use it in wave2d
    - AMPI initializes liveViz internally, and the user needs to give it
      a buffer that it will continuously update, while liveViz periodically
      copies those values in the background.
    
    - Currently hangs after displaying the initial liveViz image on my Mac.
    
    Change-Id: I499ed8ba6c9cd17e5c987310b6fea1eada7bbd7a

--- examples/ampi/wave2d/runserver.sh
+++ examples/ampi/wave2d/runserver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Starting up server:"
-./charmrun +p 4 ./wave2d +vp 64 ++server ++server-port 1234
+./charmrun +p 4 ./wave2d +vp 256 ++server ++server-port 1234
 echo "Server Exited"
 
--- examples/ampi/wave2d/wave2d.C
+++ examples/ampi/wave2d/wave2d.C
@@ -5,20 +5,16 @@
 #include <cmath>
 #include "mpi.h"
 
-#if USE_LIVEVIZ
-#include "liveViz.h"
-#endif
-
+#define CHECKPT_FREQ    200
+#define LIVE_VIZ_BUFFER_UPDATE_FREQ 5
 #define NumIters        5000
 #define TotalDataWidth  800
 #define TotalDataHeight 800
 #define NumInitPerturbs 5
 
-#define nbor(a,b) (a*comm_dim+b)
-#define mod(a,b)  (((a)+b)%b)
+enum : int { left=0, right, up, down }; // used as an MPI tag
 
-struct indexStruct { int x, y; };
-enum { left=0, right, up, down };
+void update_live_viz_buffer(int w, int h, double* p, unsigned char* i);
 
 int main(int argc, char **argv) {
   int my_rank, comm_size, num_wths, flag;
@@ -37,26 +33,32 @@ int main(int argc, char **argv) {
   }
   else if (my_rank == 0) {
     MPI_Comm_get_attr(MPI_COMM_WORLD, AMPI_NUM_WTHS, &num_wths, &flag);
-    printf("Running wave2d on %d VPs on %d processors\n", comm_size, num_wths);
+    printf("Running wave2d on %d VPs on %d PEs\n", comm_size, num_wths);
   }
 
-#if USE_LIVEVIZ
-  // Setup liveviz
-  CkArrayOptions opts(comm_size);
-  CkCallback cb(CkIndex_Wave::requestNextFrame(NULL), arrayProxy);
-  liveVizConfig cfg(liveVizConfig::pix_color, true);
-  liveVizInit(cfg, arrayProxy, cb, opts);
-#endif
+  // Set up neighbors
+  MPI_Comm comm2D;
+  const int ndims = 2;
+  int my_cart_rank, reorder, left_nbor, right_nbor, up_nbor, down_nbor;
+  int dims[ndims], coord[ndims], wrap_around[ndims];
+  dims[0] = comm_dim; dims[1] = comm_dim;
+  wrap_around[0] = 0; wrap_around[1] = 0;
+  reorder = 1;
 
-  const int my_x = my_rank / comm_dim;
-  const int my_y = my_rank % comm_dim;
+  MPI_Dims_create(comm_size, ndims, dims);
+  MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, wrap_around, reorder, &comm2D);
+  MPI_Cart_coords(comm2D, my_rank, ndims, coord);
+  MPI_Cart_rank(comm2D, coord, &my_cart_rank);
+  MPI_Cart_shift(comm2D, 0, 1, &left_nbor, &right_nbor);
+  MPI_Cart_shift(comm2D, 1, 1, &down_nbor, &up_nbor);
+
   const int num_nbors = 4;
   const int my_width  = TotalDataWidth / comm_dim;
   const int my_height = TotalDataHeight / comm_dim;
-  const int left_nbor  = nbor(mod(my_x-1, comm_dim), my_y);
-  const int right_nbor = nbor(mod(my_x+1, comm_dim), my_y);
-  const int down_nbor  = nbor(my_x, mod(my_y-1, comm_dim));
-  const int up_nbor    = nbor(my_x, mod(my_y+1, comm_dim));
+  const int my_x = coord[0];
+  const int my_y = coord[1];
+
+  // Allocate buffers
   double *pressure_old = new double[my_width*my_height]; // time t-1
   double *pressure     = new double[my_width*my_height]; // time t
   double *pressure_new = new double[my_width*my_height]; // time t+1
@@ -92,6 +94,11 @@ int main(int argc, char **argv) {
     }
   }
 
+#if AMPI_WITH_LIVE_VIZ
+  unsigned char *intensity = new unsigned char[3*my_width*my_height]; // liveViz pixel buffer
+  AMPI_Init_live_viz(my_x*my_width, my_y*my_height, my_width, my_height, intensity);
+#endif
+
   for (int iter = 0; iter < NumIters; iter++) {
     // Exchange edge buffers with neighboring ranks
     MPI_Irecv(buffers[right], my_height, MPI_DOUBLE, right_nbor, right, MPI_COMM_WORLD, &request[0]);
@@ -99,7 +106,7 @@ int main(int argc, char **argv) {
     MPI_Irecv(buffers[up],    my_width,  MPI_DOUBLE, up_nbor,    up,    MPI_COMM_WORLD, &request[2]);
     MPI_Irecv(buffers[down],  my_width,  MPI_DOUBLE, down_nbor,  down,  MPI_COMM_WORLD, &request[3]);
 
-    for (int i = 0; i < my_height; i++) { left_edge[i]  = pressure[i*my_width]; }
+    for (int i = 0; i < my_height; i++) { left_edge[i] = pressure[i*my_width]; }
     MPI_Isend(left_edge, my_height, MPI_DOUBLE, left_nbor, right, MPI_COMM_WORLD, &request[4]);
     for (int i = 0; i < my_height; i++) { right_edge[i] = pressure[i*my_width + my_width-1]; }
     MPI_Isend(right_edge, my_height, MPI_DOUBLE, right_nbor, left, MPI_COMM_WORLD, &request[5]);
@@ -113,9 +120,9 @@ int main(int argc, char **argv) {
     for (int i = 0; i < my_height; i++) {
       for (int j = 0; j < my_width; j++) {
         // Current time's pressures for neighboring array locations
-        double L = (j==0          ? buffers[left][i]  : pressure[i*my_width+j-1]);
+        double L = (j==0           ? buffers[left][i]  : pressure[i*my_width+j-1]);
         double R = (j==my_width-1  ? buffers[right][i] : pressure[i*my_width+j+1]);
-        double U = (i==0          ? buffers[up][j]    : pressure[(i-1)*my_width+j]);
+        double U = (i==0           ? buffers[up][j]    : pressure[(i-1)*my_width+j]);
         double D = (i==my_height-1 ? buffers[down][j]  : pressure[(i+1)*my_width+j]);
         // Current time's pressure for this array location
         double curr = pressure[i*my_width+j];
@@ -132,12 +139,18 @@ int main(int argc, char **argv) {
     pressure = pressure_new;
     pressure_new = tmp;
 
+    // Continuously update the liveViz buffer, which liveViz/AMPI
+    // will use in the background periodically
+    if (iter % LIVE_VIZ_BUFFER_UPDATE_FREQ == 0) {
+      update_live_viz_buffer(my_width, my_height, pressure, intensity);
+    }
+
     //MPI_Barrier(MPI_COMM_WORLD);
     if (my_rank == 0 && iter % 20 == 0) {
       printf("Completed %d iterations\n", iter);
     }
 #if CMK_MEM_CHECKPOINT
-    if (iter != 0 && iter % 200 == 0) {
+    if (iter != 0 && iter % CHECKPT_FREQ == 0) {
       AMPI_Migrate(AMPI_INFO_CHKPT_IN_MEMORY);
     }
 #endif
@@ -155,28 +168,25 @@ int main(int argc, char **argv) {
   delete [] buffers[down];
   delete [] right_edge;
   delete [] left_edge;
+#if AMPI_WITH_LIVE_VIZ
+  delete [] intensity;
+#endif
+  MPI_Comm_free(&comm2D);
   MPI_Finalize();
   return 0;
 }
 
-#if USE_LIVEVIZ
-// Provide my portion of the image to the graphical liveViz client
-void requestNextFrame(liveVizRequestMsg *m) {
-  // Draw my part of the image, plus a nice 1px border along my right/bottom boundary
-  int sx = my_x*my_width; // Where my portion of the image is located
-  int sy = my_y*my_height;
-  int w = my_width; // Size of my rectangular portion of the image
-  int h = my_height;
-
+// Update the portion of the image that the graphical liveViz client will illustrate
+void update_live_viz_buffer(int w, int h, double* pressure, unsigned char* intensity)
+{
+#if AMPI_WITH_LIVE_VIZ
   // Set the output pixel values for my rectangle
   // Each RGB component is a char which can have 256 possible values.
-  unsigned char *intensity = new unsigned char[3*w*h];
-  for (int i = 0; i < my_height; i++) {
-    for (int j = 0; j < my_width; j++) {
-      double p = pressure[i*my_width+j];
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      double p = pressure[i*w+j];
       if (p > 255.0) p = 255.0;   // Keep values in valid range
       if (p < -255.0) p = -255.0; // Keep values in valid range
-
       if (p > 0) { // Positive values are red
         intensity[3*(i*w+j)+0] = 255;   // RED component
         intensity[3*(i*w+j)+1] = 255-p; // GREEN component
@@ -188,7 +198,6 @@ void requestNextFrame(liveVizRequestMsg *m) {
       }
     }
   }
-
   // Draw a green border on right and bottom of this rank's pixel buffer.
   // This will overwrite some pressure values at these pixels.
   for (int i = 0; i < h; i++) {
@@ -201,8 +210,6 @@ void requestNextFrame(liveVizRequestMsg *m) {
     intensity[3*((h-1)*w+i)+1] = 255; // GREEN component
     intensity[3*((h-1)*w+i)+2] = 0;   // BLUE component
   }
-
-  liveVizDeposit(m, sx, sy, w, h, intensity, this);
-  delete [] intensity;
-}
 #endif
+}
+
