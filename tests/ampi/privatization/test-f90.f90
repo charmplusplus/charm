c8ff223ccbb820cd2a5a200cbe138b5f62016894
Sam White <white67@illinois.edu>
2018-12-06 14:13:59 -0600

    AMPI: add more Fortran global/static variables to the privatization test
    Change-Id: Ibd7c89096320cfacccb7d9ec5e27fd9d0015cd93

--- tests/ampi/privatization/test-f90.f90
+++ tests/ampi/privatization/test-f90.f90
@@ -1,7 +1,22 @@
+      !!! Fortran module variables, implicit and explicit save variables,
+      !!! and common blocks are all unsafe if mutable, and must be privatized.
+      !!! Parameter variables are immutable and so need not be privatized.
+
+      module test_mod
+
+        implicit none
+
+        integer, parameter :: parameter_variable = 0
+        integer :: module_variable
+
+      end module test_mod
+
+
       subroutine mpi_main
 
         implicit none
         include 'mpif.h'
+
         integer :: ierr
 
         call mpi_init(ierr)
@@ -15,11 +30,18 @@
 
       subroutine perform_test_batch(failed, rank, my_wth)
 
+        use test_mod
         implicit none
-        integer failed, rank, my_wth
-        integer global_variable
-        common /globals/ global_variable
 
-        call test_privatization(failed, rank, my_wth, global_variable)
+        integer :: failed, rank, my_wth
+        integer :: save_variable1 = 0
+        integer, save :: save_variable2
+        integer :: common_variable
+        common /commons/ common_variable
+
+        call test_privatization(failed, rank, my_wth, module_variable)
+        call test_privatization(failed, rank, my_wth, save_variable1)
+        call test_privatization(failed, rank, my_wth, save_variable2)
+        call test_privatization(failed, rank, my_wth, common_variable)
 
       end subroutine perform_test_batch
