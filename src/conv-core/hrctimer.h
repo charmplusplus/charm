54e347f26960612569c4cfa90931c6d1adf8b831
Evan Ramos <evan@hpccharm.com>
2019-02-14 14:23:58 -0600

    Resolve build failure due to inttypes.h inclusion in hrctimer.h
    Change-Id: I4ae37d943be93c0ec0c112f5c00c8ccdce20dedc

--- src/conv-core/hrctimer.h
+++ src/conv-core/hrctimer.h
@@ -1,13 +1,24 @@
 #ifndef HRCTIMER_H
 #define HRCTIMER_H
+
+#ifndef __STDC_FORMAT_MACROS
+# define __STDC_FORMAT_MACROS
+#endif
+#ifndef __STDC_LIMIT_MACROS
+# define __STDC_LIMIT_MACROS
+#endif
 #include <inttypes.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-double inithrc();
-double gethrctime();
-uint64_t gethrctime_micro();
+
+double inithrc(void);
+double gethrctime(void);
+uint64_t gethrctime_micro(void);
+
 #ifdef __cplusplus
 }
 #endif
+
 #endif
