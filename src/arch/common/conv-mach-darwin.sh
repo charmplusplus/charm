1d71c7c3da8bcec50f9b0ac984062d1dc22f13f0
Evan Ramos <evan@hpccharm.com>
2019-02-27 19:56:44 -0600

    Clean up macOS deployment target handling
    Setting MACOSX_DEPLOYMENT_TARGET=10.7 implies -stdlib=libstdc++, and that was
    affecting ChaNGa's configure tests under newer Xcode versions that lack
    libstdc++.
    
    We still pass "-mmacosx-version-min=10.7 -stdlib=libc++" to the toolchain,
    which is safe.
    
    Change-Id: I4ffa0e049a88665ef81243a5e971455a3ddca746

--- src/arch/common/conv-mach-darwin.sh
+++ src/arch/common/conv-mach-darwin.sh
@@ -11,13 +11,8 @@ CMK_CXX_FLAGS="$CMK_CXX_FLAGS -fPIC -dynamic -fno-common -stdlib=libc++ "
 CMK_LDXX_FLAGS="$CMK_LDXX_FLAGS -multiply_defined suppress -Wl,-no_pie -stdlib=libc++ "
 
 # setting for shared lib
-# need -lc++ for c++ reference, and it needs to be put at very last
-# of command line.
-# Mac environment variable
-test -z "$MACOSX_DEPLOYMENT_TARGET" && export MACOSX_DEPLOYMENT_TARGET=10.7
 CMK_SHARED_SUF="dylib"
 CMK_LD_SHARED=" -dynamic -dynamiclib -undefined dynamic_lookup "
-CMK_LD_SHARED_LIBS='-lc++'
 CMK_LD_SHARED_ABSOLUTE_PATH=true
 
 CMK_DEFS='-mmacosx-version-min=10.7 -D_DARWIN_C_SOURCE'
