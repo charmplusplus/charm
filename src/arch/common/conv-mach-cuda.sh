8082cb6a4ffb1d4fdd6649677877984c1775d33e
Jaemin Choi <minitu@users.noreply.github.com>
2019-05-11 12:01:55 -0500

    Bug #2062: Don't force -lcudart to non-CUDA programs
    Change-Id: I857c852bd88ef890de1c29ed151a6f269c546f19

--- src/arch/common/conv-mach-cuda.sh
+++ src/arch/common/conv-mach-cuda.sh
@@ -1,5 +1,5 @@
 BUILD_CUDA=1
-CMK_INCDIR="-I$CUDA_DIR/include $CMK_INCDIR "
-CMK_LIBDIR="-L$CUDA_DIR/lib64 $CMK_LIBDIR "
-CMK_LIBS="-lcudart -lcudahybridapi $CMK_LIBS "
+CMK_INCDIR="$CMK_INCDIR "
+CMK_LIBDIR="$CMK_LIBDIR "
+CMK_LIBS="-lcudahybridapi $CMK_LIBS "
 
