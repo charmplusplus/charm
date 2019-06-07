4b0ea093785f1654ce9421cb86919b9ab953a540
Eric Mikida <epmikida@hpccharm.com>
2019-06-07 16:47:39 -0400

    Make the within-node broadcast test more rigorous

--- tests/charm++/within_node_bcast/within_node_bcast.C
+++ tests/charm++/within_node_bcast/within_node_bcast.C
@@ -1,74 +1,43 @@
 #include "within_node_bcast.decl.h"
 
+#define ITERATIONS 100
+#define DATA_SIZE 1024
+
 CProxy_TestGroup gProxy;
 CProxy_TestNodeGroup ngProxy;
 
 class TestMessage : public CMessage_TestMessage {
 public:
-  int number;
+  int test_num;
+  int* data;
   std::atomic<int> counter;
   CkCallback cb;
 
-  TestMessage(int i, CkCallback c) : number(i), cb(c), counter(0) {}
+  TestMessage(int t, CkCallback c) : test_num(t), cb(c), counter(0) {}
 };
 
 class Main : public CBase_Main {
 private:
+  Main_SDAG_CODE
   int test_num;
-  int result_count;
-  int expected_result;
 public:
-  Main(CkArgMsg* msg) : test_num(0), result_count(0), expected_result(0) {
+  Main(CkArgMsg* msg) {
     delete msg;
 
     ngProxy = CProxy_TestNodeGroup::ckNew();
     gProxy = CProxy_TestGroup::ckNew();
 
+    // Defined as an SDAG method in the .ci file
     thisProxy.runTests();
-  }
 
-  void runTests() {
-    CkPrintf("Starting test %i\n", test_num);
-
-    result_count = 0;
-    expected_result = 0;
-
-    CkCallback cb(CkReductionTarget(Main, testDone), thisProxy);
-    TestMessage* msg = new TestMessage(test_num, cb);
-
-    switch (test_num) {
-      case 0:
-        ngProxy.recv(msg);
-        break;
-      case 1:
-        ngProxy.recvCopy(msg);
-        break;
-      default:
-        CkPrintf("Tests complete!\n");
-        CkExit();
-    }
-  }
-
-  void testDone(int result) {
-    if (result_count == 0) {
-      result_count++;
-      expected_result = result;
-    } else if (result == expected_result) {
-      CkPrintf("Test %i PASSED!\n", test_num);
-      test_num++;
-      thisProxy.runTests();
-    } else {
-      CkPrintf("The results do not match: %i != %i\n", result, expected_result);
-      CkAbort("Failed expectations!\n");
-    }
+    // Make sure QD counters are correctly updated for WNB
+    CkStartQD(CkCallback(CkCallback::ckExit));
   }
 };
 
 class TestNodeGroup : public CBase_TestNodeGroup {
 public:
-  TestNodeGroup() {
-    CkPrintf("TestNodeGroup created on node %i\n", CkMyNode());
-  }
+  TestNodeGroup() {}
 
   void recv(TestMessage* msg) {
     // Here the expectation is that the counter is incremented once by each PE
@@ -81,9 +50,9 @@ public:
 
   void recvCopy(TestMessage* msg) {
     // Here, there will be one copy of the message per PE, so each PE will
-    // contribute msg->number, and my sum is therefor my size * msg->number.
-    msg->counter.store(msg->number);
-    int myExpectation = msg->number * CkMyNodeSize();
+    // contribute msg->test_num, and my sum is therefor my size * msg->test_num.
+    msg->counter.store(msg->test_num);
+    int myExpectation = msg->test_num * CkMyNodeSize();
     contribute(sizeof(int), &myExpectation, CkReduction::sum_int, msg->cb);
 
     CkBroadcastWithinNode(CkIndex_TestGroup::recvCopy(NULL), msg, gProxy);
@@ -92,29 +61,38 @@ public:
 
 class TestGroup : public CBase_TestGroup {
 public:
-  TestGroup() {
-    CkPrintf("TestGroup created on PE %i\n", CkMyPe());
-  }
+  TestGroup() {}
 
   // Marked [nokeep], so I share this message with every PE on my node, and
   // should not delete it.
   void recv(TestMessage* msg) {
+    for (int i = 0; i < DATA_SIZE; i++) {
+      if (msg->data[i] != msg->test_num) {
+        CkPrintf("msg->data incorrect for [nokeep] test %i[%i]! (%i)\n",
+            msg->test_num, i, msg->data[i]);
+        CkAbort("Test failed in data transmission!\n");
+      }
+    }
+
     int val = msg->counter.fetch_add(1);
-    CkPrintf("PE %i received message #%i, with receiver counter %i\n",
-        CkMyPe(), msg->number, val);
     contribute(sizeof(int), &val, CkReduction::sum_int, msg->cb);
   }
 
   // Not marked [nokeep], so I get my own copy of the message and I'm in charge
   // of deallocation.
   void recvCopy(TestMessage* msg) {
-    int val = msg->counter.fetch_add(1);
-    CkPrintf("PE %i received a copy of message #%i with receiver counter %i\n",
-        CkMyPe(), msg->number, val);
+    for (int i = 0; i < DATA_SIZE; i++) {
+      if (msg->data[i] != msg->test_num) {
+        CkPrintf("msg->data incorrect for copy test %i[%i]! (%i)\n",
+            msg->test_num, i, msg->data[i]);
+        CkAbort("Test failed in data transmission!\n");
+      }
+    }
 
-    // The value of what I received should always be the message number since
+    int val = msg->counter.fetch_add(1);
+    // The value of what I received should always be the message test_num since
     // I get my own copy of the message.
-    CkAssert(val == msg->number);
+    CkAssert(val == msg->test_num);
 
     contribute(sizeof(int), &val, CkReduction::sum_int, msg->cb);
     delete msg;
