61a2e231cf236ba21fd7f6341c2b53cf50787f2b
Aayush Raj <aayushr2@illinois.edu>
2019-06-11 12:55:41 -0400

    Added MPI version of NPB 3.3

---
+++ examples/ampi/privatisation_tutorial/NPB3.3/IS/npbparams.h
@@ -0,0 +1,15 @@
+#define CLASS 'W'
+#define NUM_PROCS 32
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   */
+   
+#define COMPILETIME "11 Jun 2019"
+#define NPBVERSION "3.3.1"
+#define MPICC "mpicc"
+#define CFLAGS "-O"
+#define CLINK "$(MPICC)"
+#define CLINKFLAGS "-O"
+#define CMPI_LIB "-L/usr/local/lib -lmpi"
+#define CMPI_INC "-I/usr/local/include"
--- examples/ampi/privatisation_tutorial/common.mk
+++ examples/ampi/privatisation_tutorial/common.mk
@@ -1,6 +1,6 @@
 # Assume that charm is installed in ../charm/
 THIS_DIR:=$(dir $(CURDIR)/$(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST)))
-CHARMBASE:=$(abspath $(THIS_DIR)/../../../netlrts-darwin-x86_64)
+CHARMBASE:=$(abspath $(THIS_DIR)/../../../multicore-linux-gcc-x86_64)
 
 AMPICC:=$(abspath $(CHARMBASE)/bin/ampicc) $(OPTS)
 AMPICXX:=$(abspath $(CHARMBASE)/bin/ampicxx) $(OPTS)
