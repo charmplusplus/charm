f2b819c3057f169bda0811b176adf26d33511095
Evan Ramos <evan@hpccharm.com>
2019-07-23 15:45:53 -0500

    tlsglobals: Initialize segment using original values from startup
    Previously, it would duplicate the current segment.

--- src/util/cmitls.C
+++ src/util/cmitls.C
@@ -349,6 +349,7 @@ void CmiTLSInit()
 
     CmiTLSStatsInit();
     CmiTLSDefaultInfo.size = CMIALIGN(CmiTLSDefaultInfo.size, CmiTLSDefaultInfo.align);
+    CmiTLSDefaultInfo.memseg = (Addr)getTLS();
   }
 #endif
 }
@@ -360,7 +361,7 @@ void CmiTLSAllocNewSeg(tlsseg_t* t, CthThread th)
   if (CmiTLSDefaultInfo.size > 0)
   {
     t->memseg = (Addr)CmiIsomallocMallocAlignForThread(th, t->align, t->size);
-    memcpy((void*)t->memseg, (char *)getTLS() - t->size, t->size);
+    memcpy((void*)t->memseg, (char *)CmiTLSDefaultInfo.memseg - t->size, t->size);
     t->memseg = (Addr)( ((char *)(t->memseg)) + t->size );
     /* printf("[%d] 2 ALIGN %d MEM %p SIZE %d\n", CmiMyPe(), t->align, t->memseg, t->size); */
   }
