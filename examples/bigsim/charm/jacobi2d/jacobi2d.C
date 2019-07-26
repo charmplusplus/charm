a478129fee5002fa34c27db805ca3cbccf016701
Matthias Diener <mdiener@illinois.edu>
2019-07-26 17:28:22 -0500

    Allow printf-style formatting in CkAbort/CmiAbort (#2420)
    * fix '%' in CkAbort format strings

--- examples/bigsim/charm/jacobi2d/jacobi2d.C
+++ examples/bigsim/charm/jacobi2d/jacobi2d.C
@@ -95,9 +95,9 @@ class Main : public CBase_Main
       }
 
       if (arrayDimX < blockDimX || arrayDimX % blockDimX != 0)
-        CkAbort("array_size_X % block_size_X != 0!");
+        CkAbort("array_size_X %% block_size_X != 0!");
       if (arrayDimY < blockDimY || arrayDimY % blockDimY != 0)
-        CkAbort("array_size_Y % block_size_Y != 0!");
+        CkAbort("array_size_Y %% block_size_Y != 0!");
 
       // store the main proxy
       mainProxy = thisProxy;
