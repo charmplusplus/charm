a5d83cd453314479be0bcdb89447aa973d201319
Evan Ramos <evan@hpccharm.com>
2019-07-31 16:32:07 -0500

    Bug #2445: Use hwloc logical PU indices during cpuAffinityRecvHandler (#2448)
    * Fix cpu affinity process/thread ID printing

--- src/conv-core/cpuaffinity.C
+++ src/conv-core/cpuaffinity.C
@@ -137,10 +137,8 @@ static int set_process_affinity(hwloc_topology_t topology, hwloc_cpuset_t cpuset
 {
 #ifdef _WIN32
   HANDLE process = GetCurrentProcess();
-#define PRINTF_PROCESS "p"
 #else
   pid_t process = getpid();
-#define PRINTF_PROCESS "d"
 #endif
 
   if (cmi_hwloc_set_proc_cpubind(topology, process, cpuset, HWLOC_CPUBIND_PROCESS|HWLOC_CPUBIND_STRICT))
@@ -158,13 +156,12 @@ static int set_process_affinity(hwloc_topology_t topology, hwloc_cpuset_t cpuset
   {
     char *str;
     cmi_hwloc_bitmap_asprintf(&str, cpuset);
-    CmiPrintf("HWLOC> [%d] Process %" PRINTF_PROCESS " bound to cpuset: %s\n", CmiMyPe(), process, str);
+    CmiPrintf("HWLOC> [%d] Process %p bound to cpuset: %s\n", CmiMyPe(), (const void *)process, str);
     free(str);
   }
 #endif
 
   return 0;
-#undef PRINTF_PROCESS
 }
 
 #if CMK_SMP
@@ -172,10 +169,8 @@ static int set_thread_affinity(hwloc_topology_t topology, hwloc_cpuset_t cpuset)
 {
 #ifdef _WIN32
   HANDLE thread = GetCurrentThread();
-#define PRINTF_THREAD "p"
 #else
   pthread_t thread = pthread_self();
-#define PRINTF_THREAD "lu"
 #endif
 
   if (cmi_hwloc_set_thread_cpubind(topology, thread, cpuset, HWLOC_CPUBIND_THREAD|HWLOC_CPUBIND_STRICT))
@@ -193,13 +188,12 @@ static int set_thread_affinity(hwloc_topology_t topology, hwloc_cpuset_t cpuset)
   {
     char *str;
     cmi_hwloc_bitmap_asprintf(&str, cpuset);
-    CmiPrintf("HWLOC> [%d] Thread %" PRINTF_THREAD " bound to cpuset: %s\n", CmiMyPe(), thread, str);
+    CmiPrintf("HWLOC> [%d] Thread %p bound to cpuset: %s\n", CmiMyPe(), (const void *)thread, str);
     free(str);
   }
 #endif
 
   return 0;
-#undef PRINTF_THREAD
 }
 #endif
 
@@ -237,6 +231,47 @@ int CmiSetCPUAffinity(int mycore)
 
   if (result == -1)
     CmiError("Error: CmiSetCPUAffinity failed to bind PE #%d to PU #%d.\n", CmiMyPe(), mycore);
+
+  return result;
+}
+
+int CmiSetCPUAffinityLogical(int mycore)
+{
+  int core = mycore;
+  if (core < 0) {
+    core = CmiHwlocTopologyLocal.num_pus + core;
+  }
+  if (core < 0) {
+    CmiError("Error: Invalid parameter to CmiSetCPUAffinityLogical: %d\n", mycore);
+    CmiAbort("CmiSetCPUAffinityLogical failed!");
+  }
+  hwloc_topology_t topology;
+
+  cmi_hwloc_topology_init(&topology);
+  cmi_hwloc_topology_load(topology);
+
+  int thread_unitcount = cmi_hwloc_get_nbobjs_by_type(topology, HWLOC_OBJ_PU);
+  int thread_assignment = core % thread_unitcount;
+
+  hwloc_obj_t thread_obj = cmi_hwloc_get_obj_by_type(topology, HWLOC_OBJ_PU, thread_assignment);
+
+  int result = -1;
+
+  if (thread_obj != nullptr)
+  {
+#if CMK_SMP
+    result = set_thread_affinity(topology, thread_obj->cpuset);
+#else
+    result = set_process_affinity(topology, thread_obj->cpuset);
+#endif
+
+    CpvAccess(myCPUAffToCore) = thread_obj->os_index;
+  }
+
+  cmi_hwloc_topology_destroy(topology);
+
+  if (result == -1)
+    CmiError("Error: CmiSetCPUAffinityLogical failed to bind PE #%d to PU #%d.\n", CmiMyPe(), mycore);
 
   return result;
 }
@@ -485,7 +520,7 @@ static void cpuAffinityRecvHandler(void *msg)
 
   DEBUGP(("[%d %d] set to core #: %d\n", CmiMyNode(), CmiMyPe(), myrank));
 
-  if (-1 != CmiSetCPUAffinity(myrank)) {
+  if (-1 != CmiSetCPUAffinityLogical(myrank)) {
     DEBUGP(("Processor %d is bound to core #%d on node #%d\n", CmiMyPe(), myrank, mynode));
   }
   else{
