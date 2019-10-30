7cebb20231cced24a3518d6509dc8584285e8070
Matthias Diener <mdiener@illinois.edu>
2019-10-30 13:43:30 -0500

    add generated ctest files

--- cdash/generate_ctest.py
+++ cdash/generate_ctest.py
@@ -10,7 +10,7 @@ import pyctest.helpers as helpers
 import urllib
 import urllib.parse
 
-parser = helpers.ArgumentParser("ProjectName", source_dir=os.getcwd()+'/../', binary_dir=os.getcwd()+'/../',
+parser = helpers.ArgumentParser("ProjectName", source_dir=os.getcwd(), binary_dir=os.getcwd(),
                                 vcs_type="git", submit=True)
 args = parser.parse_args()
 
@@ -21,25 +21,29 @@ pyctest.PROJECT_NAME = urllib.parse.quote("Charm++")
 pyctest.set("CTEST_DROP_SITE", "my.cdash.org")
 pyctest.DROP_LOCATION = "/cdash/submit.php?project=" + pyctest.PROJECT_NAME
 pyctest.BUILD_NAME = "netlrts-darwin-x86_64"
-pyctest.BUILD_COMMAND = "./build LIBS netlrts-darwin-x86_64 -j5 -g"
+pyctest.BUILD_COMMAND = "../build LIBS netlrts-darwin-x86_64 -j5 -g"
 
 # Define test for tests/ directory
 tests = pyctest.test()
 tests.SetName("tests/")
-tests.SetCommand(["make", "-C", "tests/", "test"])
+tests.SetCommand(["make", "-C", "../tests/", "test"])
 tests.SetProperty("TIMEOUT", "1200")
 
 # Define test for examples/ directory
 examples = pyctest.test()
 examples.SetName("examples/")
-examples.SetCommand(["make", "-C", "examples/", "test"])
+examples.SetCommand(["make", "-C", "../examples/", "test"])
 examples.SetProperty("TIMEOUT", "1200")
 
 # Define test for benchmarks/ directory
 benchmarks = pyctest.test()
 benchmarks.SetName("benchmarks/")
-benchmarks.SetCommand(["make", "-C", "benchmarks/", "test"])
+benchmarks.SetCommand(["make", "-C", "../benchmarks/", "test"])
 benchmarks.SetProperty("TIMEOUT", "1200")
 
 # Run all tests
-pyctest.run()
+# pyctest.run()
+
+pyctest.generate_config()
+pyctest.generate_test_file()
+
--- cdash/runautobuild.sh
+++ cdash/runautobuild.sh
@@ -5,4 +5,4 @@ set -o errexit -o nounset
 rm -rf charm
 git clone --branch cdash https://github.com/UIUC-PPL/charm
 cd charm
-ctest -VV -S Stages.cmake -DSTAGES="Start;Update;Build;Test;Submit"
+ctest -VV -S cdash/Stages.cmake -DSTAGES="Start;Update;Build;Test;Submit"
