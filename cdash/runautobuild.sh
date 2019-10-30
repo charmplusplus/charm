0314582ae0185b724c5e526de0edefb57484c028
Matthias Diener <mdiener@illinois.edu>
2019-10-30 16:50:55 -0500

    AUTOBUILD_BUILD_COMMAND

--- cdash/runautobuild.sh
+++ cdash/runautobuild.sh
@@ -7,6 +7,7 @@ set -o errexit -o nounset
 AUTOBUILD_MACHINE_NAME="Quartz"
 AUTOBUILD_BRANCH="cdash"
 AUTOBUILD_BUILD_NAME="ofi-linux-x86_64"
+AUTOBUILD_BUILD_COMMAND="./build LIBS $AUTOBUILD_BUILD_NAME -j8 -g --with-production"
 
 # Configuration ends here
 
@@ -19,5 +20,6 @@ echo "set(CTEST_SOURCE_DIRECTORY \"$(pwd)/cdash\")"    >> cdash/CTestCustom.cmak
 echo "set(CTEST_BINARY_DIRECTORY \"$(pwd)/cdash\")"    >> cdash/CTestCustom.cmake
 echo "set(CTEST_SITE \"$AUTOBUILD_MACHINE_NAME\")"     >> cdash/CTestCustom.cmake
 echo "set(CTEST_BUILD_NAME \"$AUTOBUILD_BUILD_NAME\")" >> cdash/CTestCustom.cmake
+echo "set(CTEST_BUILD_COMMAND \"sh -c \'cd .. && $AUTOBUILD_BUILD_COMMAND\'\")" >> cdash/CTestCustom.cmake
 
 ctest -VV -S cdash/Stages.cmake -DSTAGES="Start;Update;Build;Test;Submit"
