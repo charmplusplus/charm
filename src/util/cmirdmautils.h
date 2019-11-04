48a2f1e8d5c15949e4f8e7633c683a1e8c94bf59
Nitin Bhat <nbhat4@illinois.edu>
2019-11-04 16:24:11 -0600

    ZC Bcast API: Fix de-registration bug on parent nodes after reverse op (#2596)
    Previously, on intermediate nodes, when rgets on the child nodes were being
    executed as reverse operations (PUT from source instead of a GET from destination),
    the source buffer was de-registered twice on the intermediate nodes, causing a failure.
    This de-registration (for UNREG modes) is only applicable to root nodes.
    With this commit, the root node for a ZC Bcast is stored in NcpyOperationInfo
    and de-registration is performed only when the reverse operation involves
    the root node as the source. (and not when an intermediate node is the source)

--- src/util/cmirdmautils.h
+++ src/util/cmirdmautils.h
@@ -53,6 +53,8 @@ typedef struct ncpystruct{
 
   int ncpyOpInfoSize;
 
+  int rootNode; // used only for Broadcast, -1 for p2p operations
+
 }NcpyOperationInfo;
 
 #ifdef __cplusplus
@@ -88,6 +90,7 @@ void setNcpyOpInfo(
   unsigned short int isdestRegistered,
   int destPe,
   const void *destRef,
+  int rootNode,
   NcpyOperationInfo *ncpyOpInfo);
 
 void resetNcpyOpInfoPointers(NcpyOperationInfo *ncpyOpInfo);
