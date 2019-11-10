15e69b2294fce5127e6290bd76de3524a5144aa4
Matthias Diener <mdiener@illinois.edu>
2019-11-10 14:23:51 -0600

    fix spelling

--- cdash/CTestTestfile.cmake
+++ cdash/CTestTestfile.cmake
@@ -2,5 +2,5 @@ add_test(tests/ "make" "-C" "../tests" "test" "TESTOPTS=$ENV{AUTOBUILD_TEST_OPTS
 set_tests_properties(tests/ PROPERTIES  TIMEOUT "1200")
 add_test(examples/ "make" "-C" "../examples" "test" "TESTOPTS=$ENV{AUTOBUILD_TEST_OPTS}")
 set_tests_properties(examples/ PROPERTIES  TIMEOUT "1200")
-add_test(benchmarks/ "make" "-C" "../benchmarks" "test" "TESTOPTS=+$ENV{AUTOBUILD_TEST_OPTS}")
+add_test(benchmarks/ "make" "-C" "../benchmarks" "test" "TESTOPTS=$ENV{AUTOBUILD_TEST_OPTS}")
 set_tests_properties(benchmarks/ PROPERTIES  TIMEOUT "1200")
