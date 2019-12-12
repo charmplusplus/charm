cd90d4795e5d98a712ff68c0219c3de71e68b3ce
Sam White <white67@illinois.edu>
2019-12-12 10:38:43 -0600

    Fix PUP routine for vector<bool>

--- src/util/pup_stl.h
+++ src/util/pup_stl.h
@@ -237,15 +237,23 @@ namespace PUP {
   template<>
   inline void PUP_stl_container_items<std::vector<bool>, bool>(er &p, std::vector<bool> &c, size_t nElem)
   {
-    if (p.isUnpacking())
+    if (p.isUnpacking()) {
       c.resize(nElem);
-
-    std::deque<bool> q(c.begin(), c.end());
-    
-    for (std::deque<bool>::iterator it = q.begin(); it != q.end(); it++)
-    {
-      p.syncComment(sync_item);
-      p|*it;
+      for (size_t i = 0; i < nElem; ++i)
+      {
+        p.syncComment(sync_item);
+        detail::TemporaryObjectHolder<bool> n;
+        p|n;
+        c[i] = n.t;
+      }
+    }
+    else {
+      std::deque<bool> q(c.begin(), c.end());
+      for (std::deque<bool>::iterator it = q.begin(); it != q.end(); it++)
+      {
+        p.syncComment(sync_item);
+        p|*it;
+      }
     }
   }
 
