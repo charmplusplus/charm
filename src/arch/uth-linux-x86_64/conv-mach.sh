b6b5fea33384636b2bc30504541677cbecc34dd4
Matthias Diener <mdiener@illinois.edu>
2020-01-22 12:19:53 -0600

    uth: use gfortran

--- src/arch/uth-linux-x86_64/conv-mach.sh
+++ src/arch/uth-linux-x86_64/conv-mach.sh
@@ -1,8 +1,7 @@
 . $CHARMINC/cc-gcc.sh
+. $CHARMINC/conv-mach-gfortran.sh
 
 CMK_CXX_FLAGS="$CMK_CXX_FLAGS -Wno-deprecated"
-CMK_CF77='f77'
-CMK_CF90='f90'
 CMK_QT='generic64-light'
 CMK_XIOPTS=''
 CMK_NO_PARTITIONS="1"
