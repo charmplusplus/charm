2521800b48d830521a4f458f051a8a273a6eaf67
Jaemin Choi <jchoi157@illinois.edu>
2020-02-12 17:41:14 -0500

    Move GPUManager method definitions to inside class to prevent duplicate definitions

--- src/arch/cuda/hybridAPI/gpumanager.h
+++ src/arch/cuda/hybridAPI/gpumanager.h
@@ -85,9 +85,9 @@ struct GPUManager {
   std::vector<DeviceManager> device_managers;
   std::unordered_map<int, DeviceManager*> device_map;
 
-  // Eager communication buffer
-  bool use_eager_comm_buffer;
-  size_t eager_comm_buffer_size;
+  // Device communication buffer
+  bool use_comm_buffer;
+  size_t comm_buffer_size;
 
   // POSIX shared memory for sharing CUDA IPC handles between processes on the same host
   void* shm_ptr;
@@ -103,322 +103,308 @@ struct GPUManager {
   // CUDA IPC handles opened for processes on the same node
   std::vector<cuda_ipc_local_info> cuda_ipc_local_infos;
 
-  void init();
-  int createStreams();
-  int createNStreams(int);
-  void destroyStreams();
-  cudaStream_t getNextStream();
-  cudaStream_t getStream(int);
-  int getNStreams();
-  void allocateBuffers(hapiWorkRequest*);
-  void hostToDeviceTransfer(hapiWorkRequest*);
-  void deviceToHostTransfer(hapiWorkRequest*);
-  void freeBuffers(hapiWorkRequest*);
-  void runKernel(hapiWorkRequest*);
-
-};
-
-void GPUManager::init() {
-  next_buffer_ = NUM_BUFFERS;
-  streams_ = NULL;
-  n_streams_ = 0;
-  last_stream_id_ = -1;
-  running_kernel_idx_ = 0;
-  data_setup_idx_ = 0;
-  data_cleanup_idx_ = 0;
+  void init() {
+    next_buffer_ = NUM_BUFFERS;
+    streams_ = NULL;
+    n_streams_ = 0;
+    last_stream_id_ = -1;
+    running_kernel_idx_ = 0;
+    data_setup_idx_ = 0;
+    data_cleanup_idx_ = 0;
 
 #if CMK_SMP
-  // create mutex locks
-  queue_lock_ = CmiCreateLock();
-  progress_lock_ = CmiCreateLock();
-  stream_lock_ = CmiCreateLock();
-  mempool_lock_ = CmiCreateLock();
-  inst_lock_ = CmiCreateLock();
-  device_mapping_lock = CmiCreateLock();
+    // create mutex locks
+    queue_lock_ = CmiCreateLock();
+    progress_lock_ = CmiCreateLock();
+    stream_lock_ = CmiCreateLock();
+    mempool_lock_ = CmiCreateLock();
+    inst_lock_ = CmiCreateLock();
+    device_mapping_lock = CmiCreateLock();
 #endif
 
 #ifdef HAPI_TRACE
-  time_idx_ = 0;
+    time_idx_ = 0;
 #endif
 
-  // Number of PEs mapped to each device
-  pes_per_device = -1;
+    // Number of PEs mapped to each device
+    pes_per_device = -1;
 
-  // Eager communication buffer
-  use_eager_comm_buffer = true;
-  eager_comm_buffer_size = 1 << 26; // 64MB by default
+    // Device communication buffer
+    use_comm_buffer = true;
+    comm_buffer_size = 1 << 26; // 64MB by default
 
-  // Shared memory region for CUDA IPC
-  shm_ptr = NULL;
-  shm_name = NULL;
-  shm_file = -1;
-  shm_chunk_size = 0;
-  shm_size = 0;
-  shm_my_ptr = NULL;
+    // Shared memory region for CUDA IPC
+    shm_ptr = NULL;
+    shm_name = NULL;
+    shm_file = -1;
+    shm_chunk_size = 0;
+    shm_size = 0;
+    shm_my_ptr = NULL;
 
-  // Number of CUDA IPC events per PE
-  ipc_event_pool_size = -1;
+    // Number of CUDA IPC events per PE
+    ipc_event_pool_size = -1;
 
-  // allocate host/device buffers array (both user and system-addressed)
-  host_buffers_ = new void*[NUM_BUFFERS*2];
-  device_buffers_ = new void*[NUM_BUFFERS*2];
+    // allocate host/device buffers array (both user and system-addressed)
+    host_buffers_ = new void*[NUM_BUFFERS*2];
+    device_buffers_ = new void*[NUM_BUFFERS*2];
 
-  // initialize device array to NULL
-  for (int i = 0; i < NUM_BUFFERS*2; i++) {
-    device_buffers_[i] = NULL;
-  }
+    // initialize device array to NULL
+    for (int i = 0; i < NUM_BUFFERS*2; i++) {
+      device_buffers_[i] = NULL;
+    }
 
 #ifdef HAPI_TRACE
-  traceRegisterUserEvent("GPU Memory Setup", GpuMemSetup);
-  traceRegisterUserEvent("GPU Kernel Execution", GpuKernelExec);
-  traceRegisterUserEvent("GPU Memory Cleanup", GpuMemCleanup);
+    traceRegisterUserEvent("GPU Memory Setup", GpuMemSetup);
+    traceRegisterUserEvent("GPU Kernel Execution", GpuKernelExec);
+    traceRegisterUserEvent("GPU Memory Cleanup", GpuMemCleanup);
 #endif
 
-  // set up mempool metadata
-  mempool_initialized_ = false;
-  mempool_boundaries_.resize(HAPI_MEMPOOL_NUM_SLOTS);
+    // set up mempool metadata
+    mempool_initialized_ = false;
+    mempool_boundaries_.resize(HAPI_MEMPOOL_NUM_SLOTS);
 
-  size_t buf_size = HAPI_MEMPOOL_MIN_BUFFER_SIZE;
-  for(int i = 0; i < HAPI_MEMPOOL_NUM_SLOTS; i++){
-    mempool_boundaries_[i] = buf_size;
-    buf_size = buf_size << 1;
-  }
+    size_t buf_size = HAPI_MEMPOOL_MIN_BUFFER_SIZE;
+    for(int i = 0; i < HAPI_MEMPOOL_NUM_SLOTS; i++){
+      mempool_boundaries_[i] = buf_size;
+      buf_size = buf_size << 1;
+    }
 
 #ifdef HAPI_INSTRUMENT_WRS
-  init_instr_ = false;
+    init_instr_ = false;
 #endif
-}
-
-// Creates streams equal to the maximum number of concurrent kernels,
-// which depends on the compute capability of the device.
-// Returns the number of created streams.
-int GPUManager::createStreams() {
-  int device;
-  cudaDeviceProp device_prop;
-  hapiCheck(cudaGetDevice(&device));
-  hapiCheck(cudaGetDeviceProperties(&device_prop, device));
-
-  int new_n_streams = 0;
-
-  if (device_prop.major == 3) {
-    if (device_prop.minor == 0)
-      new_n_streams = 16;
-    else if (device_prop.minor == 2)
-      new_n_streams = 4;
-    else // 3.5, 3.7 or unknown 3.x
-      new_n_streams = 32;
   }
-  else if (device_prop.major == 5) {
-    if (device_prop.minor == 3)
-      new_n_streams = 16;
-    else // 5.0, 5.2 or unknown 5.x
-      new_n_streams = 32;
-  }
-  else if (device_prop.major == 6) {
-    if (device_prop.minor == 1)
-      new_n_streams = 32;
-    else if (device_prop.minor == 2)
-      new_n_streams = 16;
-    else // 6.0 or unknown 6.x
+
+  // Creates streams equal to the maximum number of concurrent kernels,
+  // which depends on the compute capability of the device.
+  // Returns the number of created streams.
+  int createStreams() {
+    int device;
+    cudaDeviceProp device_prop;
+    hapiCheck(cudaGetDevice(&device));
+    hapiCheck(cudaGetDeviceProperties(&device_prop, device));
+
+    int new_n_streams = 0;
+
+    if (device_prop.major == 3) {
+      if (device_prop.minor == 0)
+        new_n_streams = 16;
+      else if (device_prop.minor == 2)
+        new_n_streams = 4;
+      else // 3.5, 3.7 or unknown 3.x
+        new_n_streams = 32;
+    }
+    else if (device_prop.major == 5) {
+      if (device_prop.minor == 3)
+        new_n_streams = 16;
+      else // 5.0, 5.2 or unknown 5.x
+        new_n_streams = 32;
+    }
+    else if (device_prop.major == 6) {
+      if (device_prop.minor == 1)
+        new_n_streams = 32;
+      else if (device_prop.minor == 2)
+        new_n_streams = 16;
+      else // 6.0 or unknown 6.x
+        new_n_streams = 128;
+    }
+    else // unknown (future) compute capability
       new_n_streams = 128;
-  }
-  else // unknown (future) compute capability
-    new_n_streams = 128;
 #if !CMK_SMP
-  // Allocate total physical streams between GPU managers sharing a device...
-  // i.e. PEs / num devices
-  int device_count;
-  hapiCheck(cudaGetDeviceCount(&device_count));
-  int pes_per_device = CmiNumPesOnPhysicalNode(0) / device_count;
-  pes_per_device = pes_per_device > 0 ? pes_per_device : 1;
-  new_n_streams =  (new_n_streams + pes_per_device - 1) / pes_per_device;
+    // Allocate total physical streams between GPU managers sharing a device...
+    // i.e. PEs / num devices
+    int device_count;
+    hapiCheck(cudaGetDeviceCount(&device_count));
+    int pes_per_device = CmiNumPesOnPhysicalNode(0) / device_count;
+    pes_per_device = pes_per_device > 0 ? pes_per_device : 1;
+    new_n_streams =  (new_n_streams + pes_per_device - 1) / pes_per_device;
 #endif
 
-  int total_n_streams = createNStreams(new_n_streams);
+    int total_n_streams = createNStreams(new_n_streams);
 
-  return total_n_streams;
-}
+    return total_n_streams;
+  }
 
-int GPUManager::createNStreams(int new_n_streams) {
-  if (new_n_streams <= n_streams_) {
+  int createNStreams(int new_n_streams) {
+    if (new_n_streams <= n_streams_) {
+      return n_streams_;
+    }
+
+    cudaStream_t* old_streams = streams_;
+
+    streams_ = new cudaStream_t[new_n_streams];
+
+    int i = 0;
+    // Copy old streams
+    for (; i < n_streams_; i++) {
+      // TODO alt. use memcpy?
+      streams_[i] = old_streams[i];
+    }
+
+    // Create new streams
+    for (; i < new_n_streams; i++) {
+      hapiCheck(cudaStreamCreate(&streams_[i]));
+    }
+
+    // Update
+    n_streams_ = new_n_streams;
+    delete [] old_streams;
+
     return n_streams_;
   }
 
-  cudaStream_t* old_streams = streams_;
-
-  streams_ = new cudaStream_t[new_n_streams];
-
-  int i = 0;
-  // Copy old streams
-  for (; i < n_streams_; i++) {
-    // TODO alt. use memcpy?
-    streams_[i] = old_streams[i];
-  }
-
-  // Create new streams
-  for (; i < new_n_streams; i++) {
-    hapiCheck(cudaStreamCreate(&streams_[i]));
-  }
-
-  // Update
-  n_streams_ = new_n_streams;
-  delete [] old_streams;
-
-  return n_streams_;
-}
-
-void GPUManager::destroyStreams() {
-  if (streams_) {
-    for (int i = 0; i < n_streams_; i++) {
-      hapiCheck(cudaStreamDestroy(streams_[i]));
+  void destroyStreams() {
+    if (streams_) {
+      for (int i = 0; i < n_streams_; i++) {
+        hapiCheck(cudaStreamDestroy(streams_[i]));
+      }
     }
   }
-}
 
-cudaStream_t GPUManager::getNextStream() {
-  if (streams_ == NULL)
-    return NULL;
+  cudaStream_t getNextStream() {
+    if (streams_ == NULL)
+      return NULL;
 
-  last_stream_id_ = (++last_stream_id_) % n_streams_;
-  return streams_[last_stream_id_];
-}
+    last_stream_id_ = (++last_stream_id_) % n_streams_;
+    return streams_[last_stream_id_];
+  }
 
-cudaStream_t GPUManager::getStream(int i) {
-  if (streams_ == NULL)
-    return NULL;
+  cudaStream_t getStream(int i) {
+    if (streams_ == NULL)
+      return NULL;
 
-  if (i < 0 || i >= n_streams_)
-    CmiAbort("[HAPI] invalid stream ID");
-  return streams_[i];
-}
+    if (i < 0 || i >= n_streams_)
+      CmiAbort("[HAPI] invalid stream ID");
+    return streams_[i];
+  }
 
-int GPUManager::getNStreams() {
-  if (!streams_) // NULL - default stream
-    return 1;
+  int getNStreams() {
+    if (!streams_) // NULL - default stream
+      return 1;
 
-  return n_streams_;
-}
+    return n_streams_;
+  }
 
-// Allocates device buffers.
-void GPUManager::allocateBuffers(hapiWorkRequest* wr) {
-  for (int i = 0; i < wr->getBufferCount(); i++) {
-    hapiBufferInfo& bi = wr->buffers[i];
-    int index = bi.id;
-    size_t size = bi.size;
+  // Allocates device buffers.
+  void allocateBuffers(hapiWorkRequest* wr) {
+    for (int i = 0; i < wr->getBufferCount(); i++) {
+      hapiBufferInfo& bi = wr->buffers[i];
+      int index = bi.id;
+      size_t size = bi.size;
 
-    // if index value is invalid, use an available ID
-    if (index < 0 || index >= NUM_BUFFERS) {
-      bool is_found = false;
-      for (int j = next_buffer_; j < NUM_BUFFERS*2; j++) {
-        if (device_buffers_[j] == NULL) {
-          index = j;
-          is_found = true;
-          break;
-        }
-      }
-
-      // if no index was found, try to search for a value at the
-      // beginning of the system addressed space
-      if (!is_found) {
-        for (int j = NUM_BUFFERS; j < next_buffer_; j++) {
+      // if index value is invalid, use an available ID
+      if (index < 0 || index >= NUM_BUFFERS) {
+        bool is_found = false;
+        for (int j = next_buffer_; j < NUM_BUFFERS*2; j++) {
           if (device_buffers_[j] == NULL) {
             index = j;
             is_found = true;
             break;
           }
         }
+
+        // if no index was found, try to search for a value at the
+        // beginning of the system addressed space
+        if (!is_found) {
+          for (int j = NUM_BUFFERS; j < next_buffer_; j++) {
+            if (device_buffers_[j] == NULL) {
+              index = j;
+              is_found = true;
+              break;
+            }
+          }
+        }
+
+        if (!is_found) {
+          CmiAbort("[HAPI] ran out of device buffer indices");
+        }
+
+        next_buffer_ = index + 1;
+        if (next_buffer_ == NUM_BUFFERS*2) {
+          next_buffer_ = NUM_BUFFERS;
+        }
+
+        bi.id = index;
       }
 
-      if (!is_found) {
-        CmiAbort("[HAPI] ran out of device buffer indices");
+      if (device_buffers_[index] == NULL) {
+        // allocate device memory
+        hapiCheck(cudaMalloc((void **)&device_buffers_[index], size));
+
+#ifdef HAPI_DEBUG
+        CmiPrintf("[HAPI] allocated buffer %d at %p, time: %.2f, size: %zu\n",
+               index, device_buffers_[index], cutGetTimerValue(timerHandle),
+               size);
+#endif
       }
+    }
+  }
 
-      next_buffer_ = index + 1;
-      if (next_buffer_ == NUM_BUFFERS*2) {
-        next_buffer_ = NUM_BUFFERS;
+  // Initiates host-to-device data transfer.
+  void hostToDeviceTransfer(hapiWorkRequest* wr) {
+    for (int i = 0; i < wr->getBufferCount(); i++) {
+      hapiBufferInfo& bi = wr->buffers[i];
+      int index = bi.id;
+      size_t size = bi.size;
+      host_buffers_[index] = bi.host_buffer;
+
+      if (bi.transfer_to_device) {
+        hapiCheck(cudaMemcpyAsync(device_buffers_[index], host_buffers_[index], size,
+                                  cudaMemcpyHostToDevice, wr->stream));
+
+#ifdef HAPI_DEBUG
+        CmiPrintf("[HAPI] transferring buffer %d from host to device, time: %.2f, "
+               "size: %zu\n", index, cutGetTimerValue(timerHandle), size);
+#endif
       }
-
-      bi.id = index;
-    }
-
-    if (device_buffers_[index] == NULL) {
-      // allocate device memory
-      hapiCheck(cudaMalloc((void **)&device_buffers_[index], size));
-
-#ifdef HAPI_DEBUG
-      CmiPrintf("[HAPI] allocated buffer %d at %p, time: %.2f, size: %zu\n",
-             index, device_buffers_[index], cutGetTimerValue(timerHandle),
-             size);
-#endif
     }
   }
-}
 
-// Initiates host-to-device data transfer.
-void GPUManager::hostToDeviceTransfer(hapiWorkRequest* wr) {
-  for (int i = 0; i < wr->getBufferCount(); i++) {
-    hapiBufferInfo& bi = wr->buffers[i];
-    int index = bi.id;
-    size_t size = bi.size;
-    host_buffers_[index] = bi.host_buffer;
+  // Initiates device-to-host data transfer.
+  void deviceToHostTransfer(hapiWorkRequest* wr) {
+    for (int i = 0; i < wr->getBufferCount(); i++) {
+      hapiBufferInfo& bi = wr->buffers[i];
+      int index = bi.id;
+      size_t size = bi.size;
 
-    if (bi.transfer_to_device) {
-      hapiCheck(cudaMemcpyAsync(device_buffers_[index], host_buffers_[index], size,
-                                cudaMemcpyHostToDevice, wr->stream));
+      if (bi.transfer_to_host) {
+        hapiCheck(cudaMemcpyAsync(host_buffers_[index], device_buffers_[index], size,
+                                  cudaMemcpyDeviceToHost, wr->stream));
 
 #ifdef HAPI_DEBUG
-      CmiPrintf("[HAPI] transferring buffer %d from host to device, time: %.2f, "
-             "size: %zu\n", index, cutGetTimerValue(timerHandle), size);
+        CmiPrintf("[HAPI] transferring buffer %d from device to host, time %.2f, "
+               "size: %zu\n", index, cutGetTimerValue(timerHandle), size);
 #endif
+      }
     }
   }
-}
 
-// Initiates device-to-host data transfer.
-void GPUManager::deviceToHostTransfer(hapiWorkRequest* wr) {
-  for (int i = 0; i < wr->getBufferCount(); i++) {
-    hapiBufferInfo& bi = wr->buffers[i];
-    int index = bi.id;
-    size_t size = bi.size;
+  // Frees device buffers.
+  void freeBuffers(hapiWorkRequest* wr) {
+    for (int i = 0; i < wr->getBufferCount(); i++) {
+      hapiBufferInfo& bi = wr->buffers[i];
+      int index = bi.id;
 
-    if (bi.transfer_to_host) {
-      hapiCheck(cudaMemcpyAsync(host_buffers_[index], device_buffers_[index], size,
-                                cudaMemcpyDeviceToHost, wr->stream));
+      if (bi.need_free) {
+        hapiCheck(cudaFree(device_buffers_[index]));
+        device_buffers_[index] = NULL;
 
 #ifdef HAPI_DEBUG
-      CmiPrintf("[HAPI] transferring buffer %d from device to host, time %.2f, "
-             "size: %zu\n", index, cutGetTimerValue(timerHandle), size);
+        CmiPrintf("[HAPI] freed buffer %d, time %.2f\n",
+               index, cutGetTimerValue(timerHandle));
 #endif
+      }
     }
   }
-}
 
-// Frees device buffers.
-void GPUManager::freeBuffers(hapiWorkRequest* wr) {
-  for (int i = 0; i < wr->getBufferCount(); i++) {
-    hapiBufferInfo& bi = wr->buffers[i];
-    int index = bi.id;
-
-    if (bi.need_free) {
-      hapiCheck(cudaFree(device_buffers_[index]));
-      device_buffers_[index] = NULL;
-
-#ifdef HAPI_DEBUG
-      CmiPrintf("[HAPI] freed buffer %d, time %.2f\n",
-             index, cutGetTimerValue(timerHandle));
-#endif
+  // Run the user's kernel for the given work request.
+  // This used to be a switch statement defined by the user to allow the runtime
+  // to execute the correct kernel.
+  void runKernel(hapiWorkRequest* wr) {
+    if (wr->runKernel) {
+      wr->runKernel(wr, wr->stream, device_buffers_);
     }
+    // else, might be only for data transfer (or might be a bug?)
   }
-}
-
-// Run the user's kernel for the given work request.
-// This used to be a switch statement defined by the user to allow the runtime
-// to execute the correct kernel.
-void GPUManager::runKernel(hapiWorkRequest* wr) {
-	if (wr->runKernel) {
-		wr->runKernel(wr, wr->stream, device_buffers_);
-	}
-	// else, might be only for data transfer (or might be a bug?)
-}
+};
 
 #endif // __GPUMANAGER_H_
