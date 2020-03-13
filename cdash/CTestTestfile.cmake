0eed2883c5ffa954921a42eded03d65a62beb248
Matthias Diener <mdiener@illinois.edu>
2020-03-13 13:18:25 -0500

    restore the parallel build of tests before running them

--- cdash/CTestTestfile.cmake
+++ cdash/CTestTestfile.cmake
@@ -1,6 +1,9 @@
-# Disabled since that won't work in the cmake build:
-# add_test(build-tests "make" "-C" "../tmp" "-j4" "all-test-tests" "all-test-examples" "all-test-benchmarks" "OPTS=\"-g\"")
+# Build the tests using parallel make
+add_test(build-tests      "make" "-C" "../tests"      "-j4"  "OPTS=\"-g\"")
+add_test(build-examples   "make" "-C" "../examples"   "-j4"  "OPTS=\"-g\"")
+add_test(build-benchmarks "make" "-C" "../benchmarks" "-j4"  "OPTS=\"-g\"")
 
+# Run the tests
 add_test(tests/ "make" "-C" "../tests" "test" "TESTOPTS=$ENV{AUTOBUILD_TEST_OPTS}")
 set_tests_properties(tests/ PROPERTIES  TIMEOUT "2400")
 add_test(examples/ "make" "-C" "../examples" "test" "TESTOPTS=$ENV{AUTOBUILD_TEST_OPTS}")
