8059aba7805a73dea50469fee2fd4532306e9397
Jaemin Choi <jchoi157@illinois.edu>
2020-03-20 15:22:00 -0700

    SDAG example: Remove host-side zerocopy

--- examples/charm++/cuda/zerocopy/sdag/sdag.C
+++ examples/charm++/cuda/zerocopy/sdag/sdag.C
@@ -66,8 +66,6 @@ class Block : public CBase_Block {
   double* h_remote_data;
   double* d_local_data;
   double* d_remote_data;
-  double* rdma_local_data;
-  double* rdma_remote_data;
   int* reg_local_data;
   int* reg_remote_data;
 
@@ -81,8 +79,6 @@ class Block : public CBase_Block {
     hapiCheck(cudaFreeHost(h_remote_data));
     hapiCheck(cudaFree(d_local_data));
     hapiCheck(cudaFree(d_remote_data));
-    free(rdma_local_data);
-    free(rdma_remote_data);
     free(reg_local_data);
     free(reg_remote_data);
     cudaStreamDestroy(stream);
@@ -100,8 +96,6 @@ class Block : public CBase_Block {
     hapiCheck(cudaMallocHost(&h_remote_data, sizeof(double) * block_size));
     hapiCheck(cudaMalloc(&d_local_data, sizeof(double) * block_size));
     hapiCheck(cudaMalloc(&d_remote_data, sizeof(double) * block_size));
-    rdma_local_data = (double*)malloc(sizeof(double) * block_size);
-    rdma_remote_data = (double*)malloc(sizeof(double) * block_size);
     reg_local_data = (int*)malloc(sizeof(int) * block_size);
     reg_remote_data = (int*)malloc(sizeof(int) * block_size);
     cudaStreamCreate(&stream);
@@ -109,7 +103,6 @@ class Block : public CBase_Block {
     // Initialize data
     invokeInitKernel(d_local_data, block_size, (double)thisIndex, stream);
     invokeInitKernel(d_remote_data, block_size, (double)thisIndex, stream);
-    for (int i = 0; i < block_size; i++) rdma_local_data[i] = thisIndex;
     for (int i = 0; i < block_size; i++) reg_local_data[i] = thisIndex;
 
     // Start iterating once data is initialized
@@ -117,18 +110,17 @@ class Block : public CBase_Block {
     hapiAddCallback(stream, cb);
   }
 
-  void receive(int ref, int &size1, double *&arr1, int &size2, double *&arr2,
-      int size3, int *arr3, CkNcpyBufferPost *ncpyPost) {
+  void receive(int ref, int &size1, double *&arr1, int size2, int *arr2,
+      CkNcpyBufferPost *ncpyPost) {
     // Inform the runtime where the incoming data should be stored
     // and which CUDA stream should be used for the transfer
     arr1 = d_remote_data;
-    arr2 = rdma_remote_data;
     ncpyPost[0].cuda_stream = stream;
 
     // Last array should be available here as it is not RDMA
     // Copy it over for validation
-    CkAssert(size3 == block_size);
-    memcpy(reg_remote_data, arr3, sizeof(int) * block_size);
+    CkAssert(size2 == block_size);
+    memcpy(reg_remote_data, arr2, sizeof(int) * block_size);
   }
 
   void validateData() {
@@ -143,11 +135,6 @@ class Block : public CBase_Block {
       if (h_remote_data[i] != (double)peer) {
         CkPrintf("h_remote_data[%d] = %lf invalid! Expected %lf\n", i,
             h_remote_data[i], (double)peer);
-        validated = false;
-      }
-      if (rdma_remote_data[i] != (double)peer) {
-        CkPrintf("rdma_remote_data[%d] = %lf invalid! Expected %lf\n", i,
-            rdma_remote_data[i], (double)peer);
         validated = false;
       }
       if (reg_remote_data[i] != peer) {
