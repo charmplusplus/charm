f6e77f7bef975e6df897fc5a2c797e8262030e68
Nitin Bhat <nbhat4@illinois.edu>
2020-04-30 13:46:56 -0500

    Add message tracking header fields to other layers

--- src/arch/uth/conv-common.h
+++ src/arch/uth/conv-common.h
@@ -1,7 +1,7 @@
 
 #define CMK_UTH_VERSION                                    1
 
-#define CMK_MSG_HEADER_FIELD  CmiUInt2 hdl,xhdl,info,stratid,root,redID,pad2,pad3; CmiUInt1 cmaMsgType:2, nokeep:1;
+#define CMK_MSG_HEADER_FIELD  CmiUInt2 hdl,xhdl,info,stratid,root,redID,pad2,pad3; CmiUInt1 cmaMsgType:2, nokeep:1, msgLayerType:1; CmiInt4 uniqMsgId; CmiUInt4 msgSrcPe;
 #define CMK_MSG_HEADER_BASIC  CMK_MSG_HEADER_EXT
 #define CMK_MSG_HEADER_EXT    { CMK_MSG_HEADER_FIELD }
 
