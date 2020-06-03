11c384bd9cc1d60fdd2c17a33d6294d2ab1f7103
Jaemin Choi <jchoi157@illinois.edu>
2020-06-03 00:59:53 -0500

    Add CcdSCHEDLOOP and move HAPI cleanup (#2895)
    * Add CcdSCHEDLOOP for functions to be executed at every scheduler loop, move HAPI cleanup to Charm++ exit

--- src/ck-core/init.C
+++ src/ck-core/init.C
@@ -666,6 +666,16 @@ static void _exitHandler(envelope *env)
 #if CMK_SHRINK_EXPAND
       ConverseCleanup();
 #endif
+
+#if CMK_CUDA
+      // Ensure all PEs have finished GPU work
+      CmiNodeBarrier();
+
+      if (CmiMyRank() == 0) {
+        hapiExitCsv();
+      }
+#endif
+
       //everyone exits here - there may be issues with leftover messages in the queue
 #if !CMK_WITH_STATS && !CMK_WITH_WARNINGS
       DEBUGF(("[%d] Calling converse exit from ReqStatMsg \n",CkMyPe()));
@@ -1669,6 +1679,9 @@ void _initCharm(int unused_argc, char **argv)
       CmiNodeBarrier();
 
       hapiMapping(argv); // Perform PE-device mapping
+
+      // Register polling function to be invoked at every scheduler loop
+      CcdCallOnConditionKeep(CcdSCHEDLOOP, hapiPollEvents, NULL);
 
       CmiNodeBarrier();
 
