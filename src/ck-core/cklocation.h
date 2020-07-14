5035318029b6836a433c599927279ca9889f4d13
Ronak Buch <rabuch2@illinois.edu>
2020-07-14 13:53:55 -0500

    Fix libs build errors

--- src/ck-core/cklocation.h
+++ src/ck-core/cklocation.h
@@ -47,9 +47,8 @@ public:
 
 /* Utility */
 //#if CMK_LBDB_ON
-class LBManager;
-class MetaBalancer;
-#include "lbdb.h"
+#include "LBManager.h"
+#include "MetaBalancer.h"
 //#endif
 
 // Forward declarations
