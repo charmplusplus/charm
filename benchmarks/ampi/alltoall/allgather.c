1049fa3a504c727eddcbe3f71d234697622a25bd
Nitin Bhat <nbhat4@illinois.edu>
2020-07-27 15:47:01 -0500

    Use MPI_WTime for AMPI alltoall benchmark programs

--- benchmarks/ampi/alltoall/allgather.c
+++ benchmarks/ampi/alltoall/allgather.c
@@ -1,62 +1,6 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-
-#define INIT_SEC 1000000
-
-struct itimerval* tim;
-unsigned int Timers = 0;
-
-void Create_Timers(int n);
-void Start_Timer(int i, int which);
-float Read_Timer(int i, int which);
-
-void Create_Timers(int n)
-{
-  if (Timers > 0)
-  {
-    fprintf(stderr, "Create_Timers: timers already created!\n");
-    exit(-1);
-  }
-
-  tim = (struct itimerval*)malloc(n * sizeof(struct itimerval));
-  Timers = n;
-}
-
-void Start_Timer(int i, int which)
-{
-  if (i >= Timers)
-  {
-    fprintf(stderr, "Start_Timers: out-of-range timer index %d\n", i);
-    exit(-1);
-  }
-
-  tim[i].it_value.tv_sec = INIT_SEC;
-  tim[i].it_value.tv_usec = 0;
-  tim[i].it_interval.tv_sec = INIT_SEC;
-  tim[i].it_interval.tv_usec = 0;
-
-  setitimer(which, &(tim[i]), NULL);
-}
-
-float Read_Timer(int i, int which)
-{
-  float elapsed_time;
-
-  if (i >= Timers)
-  {
-    fprintf(stderr, "Read_Timer: out-of-range timer index %d\n", i);
-    exit(-1);
-  }
-
-  getitimer(which, &(tim[i]));
-
-  elapsed_time = ((float)INIT_SEC - tim[i].it_value.tv_sec) -
-                 ((float)tim[i].it_value.tv_usec / 1000000);
-
-  return elapsed_time;
-}
 
 int main(int argc, char** argv)
 {
@@ -65,8 +9,9 @@ int main(int argc, char** argv)
   char* message; /* storage for the message */
   int i, k, max_msgs, msg_size;
   MPI_Status status; /* return status for receive */
-  float elapsed_time_msec;
-  float bandwidth;
+  double startTime = 0;
+  double elapsed_time_sec;
+  double bandwidth;
   char *sndbuf, *recvbuf;
 
   MPI_Init(&argc, &argv);
@@ -98,8 +43,7 @@ int main(int argc, char** argv)
   {
     int flag = 0;
     printf("Starting benchmark on %d processors with %d iterations\n", p, max_msgs);
-    Create_Timers(1);
-    Start_Timer(0, ITIMER_REAL);
+    startTime = MPI_Wtime();
   }
   for (i = 0; i < max_msgs; i++)
   {
@@ -110,11 +54,11 @@ int main(int argc, char** argv)
 
   if (my_id == 0)
   {
-    elapsed_time_msec = Read_Timer(0, ITIMER_REAL) * 1000.0 / max_msgs;
-    bandwidth = 2 * 8 * msg_size * (p - 1) / (1000.0 * elapsed_time_msec);
+    elapsed_time_sec = MPI_Wtime() - startTime / max_msgs;
+    bandwidth = msg_size * (p - 1) / elapsed_time_sec;
 
     fprintf(stdout, "%5d %7d\t ", max_msgs, msg_size);
-    fprintf(stdout, "%8.3f msec,\t %8.3f Mbits/sec\n", elapsed_time_msec, bandwidth);
+    fprintf(stdout, "%8.3f us,\t %8.3f MB/sec\n", elapsed_time_sec * 1e6, bandwidth / 1e6);
   }
 
   free(sndbuf);
