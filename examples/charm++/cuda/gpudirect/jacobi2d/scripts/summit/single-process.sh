c0713e82a43447a09270338c3e81486d0eb75904
Jaemin Choi <jchoi157@illinois.edu>
2020-08-06 00:43:00 -0400

    Add single-process script for Summit

---
+++ examples/charm++/cuda/gpudirect/jacobi2d/scripts/summit/single-process.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#BSUB -W 10
+#BSUB -P csc357
+#BSUB -nnodes 1
+#BSUB -J jacobi2d
+
+cd $HOME/work/charm/examples/charm++/cuda/gpudirect/jacobi2d
+
+ppn=4
+n_iters=100
+warmup_iters=10
+grid_size=32768
+zerocopy=""
+sync=""
+pemap="L0,84,88,92"
+
+echo "# Single-process Jacobi2D"
+
+for div in 2 4 8 16 32
+do
+  block_size=$((grid_size / div))
+  echo "# Block size $block_size"
+  for iter in 1 2 3
+  do
+    echo "# Iteration $iter"
+    jsrun -n1 -a1 -c$ppn -g4 ./jacobi2d -s $grid_size -b $block_size -i $n_iters -w $warmup_iters $zerocopy $sync +ppn $ppn +pemap $pemap +gpumap block +gpunoshm
+  done
+done
