af850bf01a965a8d20a52562dce3a16642e96ea1
Jaemin Choi <jchoi157@illinois.edu>
2020-09-01 02:11:47 -0400

    Jacobi3D: Update Summit scripts

--- examples/charm++/cuda/gpudirect/jacobi3d/scripts/summit/benchmark.sh
+++ examples/charm++/cuda/gpudirect/jacobi3d/scripts/summit/benchmark.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #BSUB -W 10
 #BSUB -P csc357
-#BSUB -nnodes 16
-#BSUB -J jacobi3d-e-n16
+#BSUB -nnodes 1
+#BSUB -J jacobi3d-e-n1
 
 # These need to be changed between submissions
 file=jacobi3d-e
-n_nodes=16
+n_nodes=1
 n_procs=$((n_nodes * 6))
-grid_width=6144
-grid_height=4096
-grid_depth=2048
+grid_width=1536
+grid_height=1536
+grid_depth=1536
 
 # Function to display commands
 exe() { echo "\$ $@" ; "$@" ; }
@@ -25,38 +25,15 @@ sync=""
 
 echo "# Jacobi3D Performance Benchmarking"
 
-for overdecomp in 1 2 3 4 5
+for overdecomp in 1 2 4 8 16
 do
-  if [ $overdecomp -eq 1 ]
-  then
-    block_width=1024
-    block_height=1024
-    block_depth=512
-  elif [ $overdecomp -eq 2 ]
-  then
-    block_width=1024
-    block_height=512
-    block_depth=512
-  elif [ $overdecomp -eq 3 ]
-  then
-    block_width=512
-    block_height=512
-    block_depth=512
-  elif [ $overdecomp -eq 4 ]
-  then
-    block_width=512
-    block_height=512
-    block_depth=256
-  else
-    block_width=512
-    block_height=256
-    block_depth=256
-  fi
+  num_chares=$((n_procs * overdecomp))
 
-  echo "# Block size $block_width x $block_height x $block_depth"
+  echo -e "# ODF-$overdecomp\n"
   for iter in 1 2 3
   do
-    echo "# Iteration $iter"
-    exe jsrun -n$n_procs -a1 -c$ppn -g1 -K3 -r6 ./$file -X $grid_width -Y $grid_height -Z $grid_depth -x $block_width -y $block_height -z $block_depth -w $warmup_iters -i $n_iters +ppn $ppn +pemap $pemap
+    date
+    echo "# Run $iter"
+    exe jsrun -n$n_procs -a1 -c$ppn -g1 -K3 -r6 ./$file -c $num_chares -x $grid_width -y $grid_height -z $grid_depth -w $warmup_iters -i $n_iters +ppn $ppn +pemap $pemap
   done
 done
--- examples/charm++/cuda/gpudirect/jacobi3d/scripts/summit/nsys.sh
+++ examples/charm++/cuda/gpudirect/jacobi3d/scripts/summit/nsys.sh
@@ -2,15 +2,15 @@
 #BSUB -W 10
 #BSUB -P csc357
 #BSUB -nnodes 1
-#BSUB -J jacobi3d-nsys
+#BSUB -J jacobi3d-nsys-n1
 
 # These need to be changed between submissions
 file=jacobi3d-e
 n_nodes=1
 n_procs=$((n_nodes * 6))
-grid_width=3072
-grid_height=2048
-grid_depth=512
+grid_width=1536
+grid_height=1536
+grid_depth=1536
 
 # Function to display commands
 exe() { echo "\$ $@" ; "$@" ; }
@@ -20,39 +20,15 @@ cd $HOME/work/charm/examples/charm++/cuda/gpudirect/jacobi3d
 ppn=1
 pemap="L0,4,8,84,88,92"
 n_iters=100
-warmup_iters=10
+warmup_iters=0
 sync=""
 
 echo "# Jacobi3D Performance Benchmarking"
 
-for overdecomp in 1 2 3 4 5
+for overdecomp in 1 2 4 8 16
 do
-  if [ $overdecomp -eq 1 ]
-  then
-    block_width=1024
-    block_height=1024
-    block_depth=512
-  elif [ $overdecomp -eq 2 ]
-  then
-    block_width=1024
-    block_height=512
-    block_depth=512
-  elif [ $overdecomp -eq 3 ]
-  then
-    block_width=512
-    block_height=512
-    block_depth=512
-  elif [ $overdecomp -eq 4 ]
-  then
-    block_width=512
-    block_height=512
-    block_depth=256
-  else
-    block_width=512
-    block_height=256
-    block_depth=256
-  fi
+  num_chares=$((n_procs * overdecomp))
 
-  echo "# Block size $block_width x $block_height x $block_depth"
-  exe jsrun -n$n_procs -a1 -c$ppn -g1 -K3 -r6 nsys profile -f true -o jacobi3d-n$n_nodes-x$block_width-y$block_height-z$block_depth-p%q{OMPI_COMM_WORLD_RANK} ./$file -X $grid_width -Y $grid_height -Z $grid_depth -x $block_width -y $block_height -z $block_depth -w $warmup_iters -i $n_iters +ppn $ppn +pemap $pemap
+  echo -e "# ODF-$overdecomp\n"
+  exe jsrun -n$n_procs -a1 -c$ppn -g1 -K3 -r6 nsys profile -t cuda -s none -b none -f true -o jacobi3d-n$n_nodes-odf$overdecomp-p%q{OMPI_COMM_WORLD_RANK} ./$file -c $num_chares -x $grid_width -y $grid_height -z $grid_depth -w $warmup_iters -i $n_iters +ppn $ppn +pemap $pemap
 done
