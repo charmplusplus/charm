b9687e0afd319c9d8fa9b5123d0601476cfa753c
Matthias Diener <mdiener@illinois.edu>
2020-10-19 14:42:38 -0500

    fix package-tarball.sh

--- package-tarball.sh
+++ package-tarball.sh
@@ -65,7 +65,7 @@ git add -u
 object_id=$(git write-tree)
 
 # Construct the target file/folder name
-version="charm-$(grep CHARM_VERSION src/scripts/charm-version.h | awk '{print $3}')"
+version="charm-$(grep CHARM_VERSION src/scripts/charm-version.h | awk '{print $3}' | tr -d \")"
 
 # Generate the distribution tarball
 git archive --format=tar.gz --prefix="$version/" -o "$version.tar.gz" "$object_id"
