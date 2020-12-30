11227b86a195e8a888f92f2cb719646219aa447e
Jaemin Choi <jchoi157@illinois.edu>
2020-12-30 00:11:28 -0500

    Add AMPI OSU bandwidth benchmark

--- benchmarks/ampi/cuda/gpudirect/osu_bw/osu_bw.c
+++ benchmarks/ampi/cuda/gpudirect/osu_bw/osu_bw.c
@@ -11,12 +11,11 @@
 #define FLOAT_PRECISION 2
 
 int main (int argc, char *argv[]) {
-  int myid, numprocs, i;
+  int myid, numprocs, i, j;
   int size;
-  MPI_Status reqstat;
   char *s_buf, *r_buf;
   char *s_buf_host, *r_buf_host;
-  double t_start = 0.0, t_end = 0.0;
+  double t_start = 0.0, t_end = 0.0, t = 0.0;
   int po_ret = 0;
 
   size_t min_size = 1;
@@ -24,11 +23,12 @@ int main (int argc, char *argv[]) {
   int n_iters_reg = 1000;
   int n_iters_large = 100;
   int warmup_iters = 10;
-  int cuda_aware = 0;
+  int cuda_aware = 1; // TODO: Add non-CUDA-aware
+  int window_size = 64;
 
   // Process command line arguments
   int c;
-  while ((c = getopt(argc, argv, "s:x:i:l:w:g")) != -1) {
+  while ((c = getopt(argc, argv, "s:x:i:l:w:g:d")) != -1) {
     switch (c) {
       case 's':
         min_size = atoi(optarg);
@@ -47,6 +47,9 @@ int main (int argc, char *argv[]) {
         break;
       case 'g':
         cuda_aware = 1;
+        break;
+      case 'd':
+        window_size = atoi(optarg);
         break;
       default:
         fprintf(stderr, "Unknown command line argument detected\n");
@@ -68,13 +71,14 @@ int main (int argc, char *argv[]) {
   }
 
   if (myid == 0) {
-    fprintf(stdout, "# AMPI OSU Latency Benchmark\n"
+    fprintf(stdout, "# AMPI OSU Bandwidth Benchmark\n"
         "# Message sizes: %lu - %lu bytes\n"
         "# Iterations: %d regular, %d large\n"
         "# Warmup: %d\n"
-        "# CUDA-aware: %d\n",
-        min_size, max_size, n_iters_reg, n_iters_large, warmup_iters, cuda_aware);
-    fprintf(stdout, "%-*s%*s\n", FIELD_WIDTH, "Message size", FIELD_WIDTH, "Latency (us)");
+        "# CUDA-aware: %d\n"
+        "# Window size: %d\n",
+        min_size, max_size, n_iters_reg, n_iters_large, warmup_iters, cuda_aware, window_size);
+    fprintf(stdout, "%-*s%*s\n", FIELD_WIDTH, "Message size", FIELD_WIDTH, "Bandwidth (MB/s)");
   }
 
   cudaMalloc((void**)&s_buf, max_size);
@@ -83,6 +87,9 @@ int main (int argc, char *argv[]) {
     cudaMallocHost((void**)&s_buf_host, max_size);
     cudaMallocHost((void**)&r_buf_host, max_size);
   }
+
+  MPI_Request request[window_size];
+  MPI_Status reqstat[window_size];
 
   for (size = min_size; size <= max_size; size = (size ? size * 2 : 1)) {
     cudaMemset(s_buf, 'a', size);
@@ -99,35 +106,38 @@ int main (int argc, char *argv[]) {
         }
 
         if (cuda_aware) {
-          MPI_Send(s_buf, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
-          MPI_Recv(r_buf, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD, &reqstat);
+          for (j = 0; j < window_size; j++) {
+            MPI_Isend(s_buf, size, MPI_CHAR, 1, 100, MPI_COMM_WORLD, request + j);
+          }
+
+          MPI_Waitall(window_size, request, reqstat);
+          MPI_Recv(r_buf, 4, MPI_CHAR, 1, 101, MPI_COMM_WORLD, &reqstat[0]);
         } else {
-          cudaMemcpy(s_buf_host, s_buf, size, cudaMemcpyDeviceToHost);
-          MPI_Send(s_buf_host, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
-          MPI_Recv(r_buf_host, size, MPI_CHAR, 1, 1, MPI_COMM_WORLD, &reqstat);
-          cudaMemcpy(r_buf, r_buf_host, size, cudaMemcpyHostToDevice);
+          // TODO
         }
       }
 
       t_end = MPI_Wtime();
+      t = t_end - t_start;
     } else if (myid == 1) {
       for (i = 0; i < n_iters + warmup_iters; i++) {
         if (cuda_aware) {
-          MPI_Recv(r_buf, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &reqstat);
-          MPI_Send(s_buf, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+          for (j = 0; j < window_size; j++) {
+            MPI_Irecv(r_buf, size, MPI_CHAR, 0, 100, MPI_COMM_WORLD, request + j);
+          }
+
+          MPI_Waitall(window_size, request, reqstat);
+          MPI_Send(s_buf, 4, MPI_CHAR, 0, 101, MPI_COMM_WORLD);
         } else {
-          MPI_Recv(r_buf_host, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &reqstat);
-          cudaMemcpy(r_buf, r_buf_host, size, cudaMemcpyHostToDevice);
-          cudaMemcpy(s_buf_host, s_buf, size, cudaMemcpyDeviceToHost);
-          MPI_Send(s_buf_host, size, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+          // TODO
         }
       }
     }
 
     if (myid == 0) {
-      double latency = (t_end - t_start) * 1e6 / (2.0 * n_iters);
+      double tmp = size / 1e6 * n_iters * window_size;
 
-      fprintf(stdout, "%-*d%*.*f\n", FIELD_WIDTH, size, FIELD_WIDTH, FLOAT_PRECISION, latency);
+      fprintf(stdout, "%-*d%*.*f\n", FIELD_WIDTH, size, FIELD_WIDTH, FLOAT_PRECISION, tmp / t);
       fflush(stdout);
     }
   }
