1c3db2f4ad0afdd2894249c1df2ebb64506c75cb
Jaemin Choi <jchoi157@illinois.edu>
2021-01-24 23:12:10 -0500

    Complete AMPI Jacobi3D example

--- examples/ampi/cuda/gpudirect/jacobi3d/jacobi3d.C
+++ examples/ampi/cuda/gpudirect/jacobi3d/jacobi3d.C
@@ -79,8 +79,8 @@ struct Block {
   DataType* d_temperature;
   DataType* d_new_temperature;
 
-  DataType* h_ghosts[DIR_COUNT];
-  DataType* d_ghosts[DIR_COUNT];
+  DataType* h_send_ghosts[DIR_COUNT];
+  DataType* h_recv_ghosts[DIR_COUNT];
   DataType* d_send_ghosts[DIR_COUNT];
   DataType* d_recv_ghosts[DIR_COUNT];
 
@@ -90,6 +90,7 @@ struct Block {
   cudaEvent_t unpack_compute_event;
 
   bool bounds[DIR_COUNT];
+  std::vector<size_t> ghost_sizes;
 
   Block(Param param_, int x_, int y_, int z_)
     : param(param_), x(x_), y(y_), z(z_), my_iter(0), neighbors(0) {
@@ -110,7 +111,7 @@ struct Block {
 
     // Determine neighbor ranks
     for (int i = 0; i < DIR_COUNT; i++) {
-      neighbor_ranks[i] = -1;
+      neighbor_ranks[i] = MPI_PROC_NULL;
     }
     if (!bounds[LEFT]) {
       int coords[NDIMS] = {x-1, y, z};
@@ -154,18 +155,14 @@ struct Block {
     cudaCheck(cudaMalloc((void**)&d_new_temperature,
           sizeof(DataType) * (param.block_width+2) * (param.block_height+2)
           * (param.block_depth+2)));
-    std::vector<size_t> ghost_sizes
-      = {param.x_surf_size, param.x_surf_size, param.y_surf_size,
-         param.y_surf_size, param.z_surf_size, param.z_surf_size};
-    if (param.use_zerocopy) {
-      for (int i = 0; i < DIR_COUNT; i++) {
-        cudaCheck(cudaMalloc((void**)&d_send_ghosts[i], ghost_sizes[i]));
-        cudaCheck(cudaMalloc((void**)&d_recv_ghosts[i], ghost_sizes[i]));
-      }
-    } else {
-      for (int i = 0; i < DIR_COUNT; i++) {
-        cudaCheck(cudaMallocHost((void**)&h_ghosts[i], ghost_sizes[i]));
-        cudaCheck(cudaMalloc((void**)&d_ghosts[i], ghost_sizes[i]));
+    ghost_sizes = {param.x_surf_size, param.x_surf_size, param.y_surf_size,
+                   param.y_surf_size, param.z_surf_size, param.z_surf_size};
+    for (int i = 0; i < DIR_COUNT; i++) {
+      cudaCheck(cudaMalloc((void**)&d_send_ghosts[i], ghost_sizes[i]));
+      cudaCheck(cudaMalloc((void**)&d_recv_ghosts[i], ghost_sizes[i]));
+      if (!param.use_zerocopy) {
+        cudaCheck(cudaMallocHost((void**)&h_send_ghosts[i], ghost_sizes[i]));
+        cudaCheck(cudaMallocHost((void**)&h_recv_ghosts[i], ghost_sizes[i]));
       }
     }
 
@@ -189,26 +186,20 @@ struct Block {
     std::vector<int> ghost_counts
       = {param.x_surf_count, param.x_surf_count, param.y_surf_count,
          param.y_surf_count, param.z_surf_count, param.z_surf_count};
-    if (param.use_zerocopy) {
-      std::vector<DataType*> send_ghosts;
-      std::vector<DataType*> recv_ghosts;
-      for (int i = 0; i < DIR_COUNT; i++) {
-        send_ghosts.push_back(d_send_ghosts[i]);
-        recv_ghosts.push_back(d_recv_ghosts[i]);
-      }
-      invokeGhostInitKernels(send_ghosts, ghost_counts, compute_stream);
-      invokeGhostInitKernels(recv_ghosts, ghost_counts, compute_stream);
-    } else {
-      std::vector<DataType*> ghosts;
-      for (int i = 0; i < DIR_COUNT; i++) {
-        ghosts.push_back(d_ghosts[i]);
-      }
-      invokeGhostInitKernels(ghosts, ghost_counts, compute_stream);
-
+    std::vector<DataType*> send_ghosts;
+    std::vector<DataType*> recv_ghosts;
+    for (int i = 0; i < DIR_COUNT; i++) {
+      send_ghosts.push_back(d_send_ghosts[i]);
+      recv_ghosts.push_back(d_recv_ghosts[i]);
+    }
+    invokeGhostInitKernels(send_ghosts, ghost_counts, compute_stream);
+    invokeGhostInitKernels(recv_ghosts, ghost_counts, compute_stream);
+    if (!param.use_zerocopy) {
       for (int i = 0; i < DIR_COUNT; i++) {
         int ghost_count = ghost_counts[i];
         for (int j = 0; j < ghost_count; j++) {
-          h_ghosts[i][j] = 0;
+          h_send_ghosts[i][j] = 0;
+          h_recv_ghosts[i][j] = 0;
         }
       }
     }
@@ -236,15 +227,12 @@ struct Block {
     cudaCheck(cudaFreeHost(h_temperature));
     cudaCheck(cudaFree(d_temperature));
     cudaCheck(cudaFree(d_new_temperature));
-    if (param.use_zerocopy) {
-      for (int i = 0; i < DIR_COUNT; i++) {
-        cudaCheck(cudaFree(d_send_ghosts[i]));
-        cudaCheck(cudaFree(d_recv_ghosts[i]));
-      }
-    } else {
-      for (int i = 0; i < DIR_COUNT; i++) {
-        cudaCheck(cudaFreeHost(h_ghosts[i]));
-        cudaCheck(cudaFree(d_ghosts[i]));
+    for (int i = 0; i < DIR_COUNT; i++) {
+      cudaCheck(cudaFree(d_send_ghosts[i]));
+      cudaCheck(cudaFree(d_recv_ghosts[i]));
+      if (!param.use_zerocopy) {
+        cudaCheck(cudaFreeHost(h_send_ghosts[i]));
+        cudaCheck(cudaFreeHost(h_recv_ghosts[i]));
       }
     }
 
@@ -258,35 +246,30 @@ struct Block {
 
   void updateAndPack() {
     // Enforce unpack -> compute dependency
-    hapiCheck(cudaEventRecord(unpack_compute_event, unpack_stream));
-    hapiCheck(cudaStreamWaitEvent(compute_stream, unpack_compute_event, 0));
+    cudaCheck(cudaEventRecord(unpack_compute_event, unpack_stream));
+    cudaCheck(cudaStreamWaitEvent(compute_stream, unpack_compute_event, 0));
 
     // Invoke GPU kernel for Jacobi computation
     invokeJacobiKernel(d_temperature, d_new_temperature, param.block_width,
         param.block_height, param.block_depth, compute_stream);
-    hapiCheck(cudaEventRecord(compute_pack_event, compute_stream));
-
-    std::vector<size_t> ghost_sizes
-      = {param.x_surf_size, param.x_surf_size, param.y_surf_size,
-         param.y_surf_size, param.z_surf_size, param.z_surf_size};
+    cudaCheck(cudaEventRecord(compute_pack_event, compute_stream));
 
     for (int i = 0; i < DIR_COUNT; i++) {
       if (!bounds[i]) {
         // Enforce compute -> pack dependency
-        hapiCheck(cudaStreamWaitEvent(pack_stream, compute_pack_event, 0));
+        cudaCheck(cudaStreamWaitEvent(pack_stream, compute_pack_event, 0));
 
         // Pack
-        DataType* pack_ghost = param.use_zerocopy ? d_send_ghosts[i] : d_ghosts[i];
-        invokePackingKernel(d_temperature, pack_ghost, i, param.block_width,
+        invokePackingKernel(d_temperature, d_send_ghosts[i], i, param.block_width,
             param.block_height, param.block_depth, pack_stream);
 
         if (!param.use_zerocopy) {
           // Enforce pack -> d2h dependency
-          hapiCheck(cudaEventRecord(pack_d2h_events[i], pack_stream));
-          hapiCheck(cudaStreamWaitEvent(d2h_stream, pack_d2h_events[i], 0));
+          cudaCheck(cudaEventRecord(pack_d2h_events[i], pack_stream));
+          cudaCheck(cudaStreamWaitEvent(d2h_stream, pack_d2h_events[i], 0));
 
           // Transfer ghosts from device to host when packing kernel completes
-          hapiCheck(cudaMemcpyAsync(h_ghosts[i], d_ghosts[i], ghost_sizes[i],
+          cudaCheck(cudaMemcpyAsync(h_send_ghosts[i], d_send_ghosts[i], ghost_sizes[i],
                 cudaMemcpyDeviceToHost, d2h_stream));
         }
       }
@@ -305,71 +288,63 @@ struct Block {
     std::swap(d_temperature, d_new_temperature);
     my_iter++;
 
+    // Data sizes
+    size_t data_sizes[DIR_COUNT]
+      = {param.x_surf_count * sizeof(DataType), param.x_surf_count * sizeof(DataType),
+         param.y_surf_count * sizeof(DataType), param.y_surf_count * sizeof(DataType),
+         param.z_surf_count * sizeof(DataType), param.z_surf_count * sizeof(DataType)};
+
     // Send ghosts to neighbors
-    MPI_Request requests[DIR_COUNT*2];
-    if (param.use_zerocopy) {
-      if (!bounds[LEFT])
-        MPI_Isend(d_send_ghosts[LEFT], param.x_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[LEFT], my_iter * DIR_COUNT + RIGHT,
-            param.cart_comm, &requests[LEFT]);
-      if (!bounds[RIGHT])
-        MPI_Isend(d_send_ghosts[RIGHT], param.x_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[RIGHT], my_iter * DIR_COUNT + LEFT,
-            param.cart_comm, &requests[RIGHT]);
-      if (!bounds[TOP])
-        MPI_Isend(d_send_ghosts[TOP], param.y_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[TOP], my_iter * DIR_COUNT + BOTTOM,
-            param.cart_comm, &requests[TOP]);
-      if (!bounds[BOTTOM])
-        MPI_Isend(d_send_ghosts[BOTTOM], param.y_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[BOTTOM], my_iter * DIR_COUNT + TOP,
-            param.cart_comm, &requests[BOTTOM]);
-      if (!bounds[FRONT])
-        MPI_Isend(d_send_ghosts[FRONT], param.z_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[FRONT], my_iter * DIR_COUNT + BACK,
-            param.cart_comm, &requests[FRONT]);
-      if (!bounds[BACK])
-        MPI_Isend(d_send_ghosts[BACK], param.z_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[BACK], my_iter * DIR_COUNT + FRONT,
-            param.cart_comm, &requests[BACK]);
-    } else {
-      // TODO
+    MPI_Request send_requests[DIR_COUNT];
+    int send_count = 0;
+    for (int dir = 0; dir < DIR_COUNT; dir++) {
+      DataType* send_ghost = param.use_zerocopy ? d_send_ghosts[dir] : h_send_ghosts[dir];
+      int rev_dir = (dir % 2 == 0) ? (dir + 1) : (dir - 1);
+      if (!bounds[dir])
+        MPI_Isend(send_ghost, data_sizes[dir], MPI_CHAR, neighbor_ranks[dir],
+            my_iter * DIR_COUNT + rev_dir, param.cart_comm, &send_requests[send_count++]);
     }
 
     // Receive ghosts from neighbors
-    MPI_Request* recv_requests = requests + DIR_COUNT;
-    if (param.use_zerocopy) {
-      if (!bounds[LEFT])
-        MPI_Irecv(d_recv_ghosts[LEFT], param.x_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[LEFT], my_iter * DIR_COUNT + LEFT,
-            param.cart_comm, &recv_requests[LEFT]);
-      if (!bounds[RIGHT])
-        MPI_Irecv(d_recv_ghosts[RIGHT], param.x_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[RIGHT], my_iter * DIR_COUNT + RIGHT,
-            param.cart_comm, &recv_requests[RIGHT]);
-      if (!bounds[TOP])
-        MPI_Irecv(d_recv_ghosts[TOP], param.y_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[TOP], my_iter * DIR_COUNT + TOP,
-            param.cart_comm, &recv_requests[TOP]);
-      if (!bounds[BOTTOM])
-        MPI_Irecv(d_recv_ghosts[BOTTOM], param.y_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[BOTTOM], my_iter * DIR_COUNT + BOTTOM,
-            param.cart_comm, &recv_requests[BOTTOM]);
-      if (!bounds[FRONT])
-        MPI_Irecv(d_recv_ghosts[FRONT], param.z_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[FRONT], my_iter * DIR_COUNT + FRONT,
-            param.cart_comm, &recv_requests[FRONT]);
-      if (!bounds[BACK])
-        MPI_Irecv(d_recv_ghosts[BACK], param.z_surf_count * sizeof(DataType),
-            MPI_CHAR, neighbor_ranks[BACK], my_iter * DIR_COUNT + BACK,
-            param.cart_comm, &recv_requests[BACK]);
-    } else {
-      // TODO
+    MPI_Request recv_requests[DIR_COUNT];
+    int recv_count = 0;
+    int recv_dirs[DIR_COUNT];
+    for (int dir = 0; dir < DIR_COUNT; dir++) {
+      DataType* recv_ghost = param.use_zerocopy ? d_recv_ghosts[dir] : h_recv_ghosts[dir];
+      if (!bounds[dir]) {
+        MPI_Irecv(recv_ghost, data_sizes[dir], MPI_CHAR, neighbor_ranks[dir],
+            my_iter * DIR_COUNT + dir, param.cart_comm, &recv_requests[recv_count]);
+        recv_dirs[recv_count++] = dir;
+      }
     }
 
-    // Wait for communication to complete
-    MPI_Status statuses[DIR_COUNT*2];
-    MPI_Waitall(DIR_COUNT*2, requests, statuses);
+    // Invoke unpacking kernel for each received ghost
+    MPI_Status recv_statuses[DIR_COUNT];
+    for (int i = 0; i < recv_count; i++) {
+      int index;
+      // Wait for a ghost to arrive
+      MPI_Waitany(recv_count, recv_requests, &index, recv_statuses);
+      int dir = recv_dirs[index];
+
+      if (!param.use_zerocopy) {
+        // Copy ghost from host to device buffer
+        cudaCheck(cudaMemcpyAsync(d_recv_ghosts[dir], h_recv_ghosts[dir],
+              data_sizes[dir], cudaMemcpyHostToDevice, h2d_stream));
+
+        // Enforce h2d -> unpack dependency
+        cudaCheck(cudaEventRecord(h2d_unpack_events[dir], h2d_stream));
+        cudaCheck(cudaStreamWaitEvent(unpack_stream, h2d_unpack_events[dir], 0));
+      }
+
+      // Unpack
+      invokeUnpackingKernel(d_temperature, d_recv_ghosts[dir], dir,
+          param.block_width, param.block_height, param.block_depth,
+          unpack_stream);
+    }
+
+    // Wait for sends to complete
+    MPI_Status send_statuses[DIR_COUNT];
+    MPI_Waitall(send_count, send_requests, send_statuses);
   }
 };
 
@@ -503,16 +478,37 @@ int main (int argc, char *argv[]) {
   MPI_Cart_create(MPI_COMM_WORLD, NDIMS, proc_dims, periods, 0, &param.cart_comm);
   MPI_Cart_coords(param.cart_comm, rank, NDIMS, coords);
   Block block(param, coords[0], coords[1], coords[2]);
-  printf("Rank %d: pos (%d, %d, %d)\n", rank, block.x, block.y, block.z);
 
   // Initialize block
+  if (rank == 0) printf("Initializing blocks...\n");
+
   block.init();
 
-  // TODO
-  for (int i = 0; i < param.n_iters; i++) {
+  if (rank == 0) printf("Blocks initialized\n");
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  // Main iteration loop
+  if (rank == 0) printf("Running iterations...\n");
+  double start_time;
+  for (int i = 0; i < param.n_iters + param.warmup_iters; i++) {
+    if (i == param.warmup_iters) start_time = MPI_Wtime();
+
     block.updateAndPack();
+
     block.exchangeGhosts();
   }
+  double total_time = MPI_Wtime() - start_time;
+
+  // Finalize
+  if (rank == 0) {
+    printf("Completed %d iterations (%d warmup), "
+        "total time: %.3lf s (avg %.3lf ms)\n",
+        param.n_iters, param.warmup_iters,
+        total_time, total_time / param.n_iters * 1e3);
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
 
   MPI_Finalize();
 
