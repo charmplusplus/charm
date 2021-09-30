dd4e60d8a7429bab100986f4916f2244f61ca9ed
Ronak Buch <rabuch2@illinois.edu>
2021-09-30 13:59:45 -0400

    LB: Add ORBLB
    New position API-based orthogonal recursive bisection load balancer
    based on ChaNGa's Orb3dLB_notopo.

--- src/ck-ldb/TreeStrategyFactory.h
+++ src/ck-ldb/TreeStrategyFactory.h
@@ -7,6 +7,7 @@
 
 #include "TreeStrategyBase.h"
 #include "greedy.h"
+#include "orb.h"
 #include "refine.h"
 
 namespace TreeStrategy
@@ -23,7 +24,8 @@ namespace TreeStrategy
   STRATEGY(RefineB, false, false)     \
   STRATEGY(Random, false, false)      \
   STRATEGY(Dummy, false, false)       \
-  STRATEGY(Rotate, false, false)
+  STRATEGY(Rotate, false, false)      \
+  STRATEGY(ORB, false, true)
 
 #define STRINGIFYLB(_name, _, __) #_name,
 const auto LBNames = {FOREACH_STRATEGY(STRINGIFYLB)};
