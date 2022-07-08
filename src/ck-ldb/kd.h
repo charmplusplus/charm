4015cf89e0fd0ed8f87fe552d9e54611a1fd44cb
Ronak Buch <rabuch2@illinois.edu>
2022-07-08 17:17:03 -0400

    Add option to enforce constraints during migration
    When constraining things like memory footprint, the amount of memory
    used may exceed what is assigned to an element during migration, since
    it is possible for all old objects + all incoming new objects to be
    resident at one time. This commits adds a "constrain_during_migration"
    option to the configuration to allow users to enable this for specific
    constraints.

--- src/ck-ldb/kd.h
+++ src/ck-ldb/kd.h
@@ -44,6 +44,29 @@ class BaseKdConstraint : public Strategy<O, P, S>
 {
 private:
   std::vector<KDFloatType> constraints;
+  std::vector<bool> duringMigration;
+
+  void assign(O& obj, P& proc, S& solution, const bool considerMigration)
+  {
+    solution.assign(obj, proc);
+
+    // If this object is staying on its old PE, subtract out the constrained
+    // dimensions that we are considering during migration. These values were
+    // already added to the PE load once in the prep phase of solve, and then
+    // again when the object was assigned above, so subtract them out.
+    if (proc.id == obj.oldPe && considerMigration)
+    {
+      const auto numConstraints = constraints.size();
+      for (int i = O::dimension - numConstraints; i < O::dimension; i++)
+      {
+        if (duringMigration[i - (O::dimension - numConstraints)])
+          proc.unassignDim(obj, i);
+      }
+    }
+
+    // Else, if the object is moving, do nothing, as those dimensions should
+    // be counted on the old PE and on the new PE
+  }
 
 public:
   BaseKdConstraint(json& config)
@@ -55,6 +78,17 @@ public:
     else
     {
       CkPrintf("[KdConstraint] Warning: no constraints provided!\n");
+    }
+
+    if (config.contains("constrain_during_migration"))
+    {
+      CkAssert(duringMigration.size() == constraints.size());
+      duringMigration = config["constrain_during_migration"].get<std::vector<bool>>();
+    }
+    else
+    {
+      // if not provided, default to all false
+      duringMigration.resize(constraints.size(), false);
     }
   }
 
@@ -70,18 +104,44 @@ public:
         CkPrintf(" %f", value);
       }
       CkPrintf("\n");
+      CkPrintf("[KdConstraint] Constrain during migration:");
+      for (const auto& value : duringMigration)
+      {
+        CkPrintf(" %d", value);
+      }
+      CkPrintf("\n");
     }
-
 
     // TODO: Sort only by non-constraint dimensions
     // Sorts by maxload in vector
     if (!objsSorted) std::sort(objs.begin(), objs.end(), CmpLoadGreater<O>());
 
+    // If some value in duringMigration is true, then we must ensure that the constraint
+    // is not violated even during migrations, meaning we must constrain the sum of
+    // existing object + incoming objects in that dimension.
+    const bool considerMigration = std::any_of(
+        duringMigration.begin(), duringMigration.end(), [](const bool b) { return b; });
+    if (considerMigration)
+    {
+      std::vector<int> procMap(CkNumPes(), -1);
+      for (int i = 0; i < procs.size(); i++) procMap[procs[i].id] = i;
+      for (const auto& o : objs)
+      {
+        auto& oldPe = procs[procMap[o.oldPe]];
+
+        for (int i = O::dimension - numConstraints; i < O::dimension; i++)
+        {
+          if (duringMigration[i - (O::dimension - numConstraints)])
+            oldPe.assignDim(o, i);
+        }
+      }
+    }
+
     auto objsIter = objs.begin();
     T* tree = nullptr;
     for (int i = 0; i < procs.size() && objsIter != objs.end(); i++, objsIter++)
     {
-      solution.assign(*objsIter, procs[i]);
+      assign(*objsIter, procs[i], solution, considerMigration);
       tree = T::insert(tree, procs[i], numConstraints);
     }
 
@@ -90,7 +150,7 @@ public:
       auto proc = *(T::findMinNormConstraints(tree, *objsIter, constraints));
 
       tree = T::remove(tree, proc);
-      solution.assign(*objsIter, proc);
+      assign(*objsIter, proc, solution, considerMigration);
       tree = T::insert(tree, proc, numConstraints);
     }
   }
