0e6b874d2020d89e186ba240e9f644064681cb08
EC2 Default User <ec2-user@ip-172-31-13-224.ec2.internal>
2022-10-05 18:17:58 +0000

    Wrote example for writing with CkIO. Convert the line by line comments to be block comments

---
+++ examples/charm++/ckio/iotest.C
@@ -0,0 +1,55 @@
+#include "iotest.decl.h"
+#include <vector>
+
+class Main : public CBase_Main{
+	Main_SDAG_CODE;
+	std::vector<Ck::IO::File> _files; // holds all of the CkIO File objects
+	int _num_writers;
+	int _num_iterations;
+	CProxy_Writer writers;
+public:
+	Main(CkArgMsg* msg){
+		// make sure the example program is being used correctly
+		if(msg -> argc != 3){
+			ckout << "Usage: ./<program_name> <number_writers> <number_of_files>" << endl;
+			CkExit();
+		}
+		_num_writers = atoi(msg -> argv[1]); // assign the number of writers
+		_num_iterations = atoi(msg -> argv[2]); // assign the number of files to write
+		int num_files = _num_iterations; // save the number of files
+		_files.resize(_num_iterations);
+		for(int i = 0; i < num_files; ++i){
+			thisProxy.startWritingCycle(i); // start writing to the file numbered i
+		}
+		delete msg;
+
+	}
+
+	void decrementRemaining(){
+		_num_iterations--;
+		if(!_num_iterations){
+			ckout << "Successfully completed parallel output!" << endl;
+			CkExit();
+		}
+	}	
+
+};
+
+
+class Writer : public CBase_Writer {
+	
+public:
+	// constructor for a writer, takes in a pointer to the incoming Session
+	Writer(Ck::IO::Session incoming_session){
+		char out[11]; // 10 bytes for the message, 1 for the nullbyte
+		sprintf(out, "Writer[%d]\n", thisIndex);
+		Ck::IO::write(incoming_session, out, 10, 10*thisIndex); // writing 10 bytes starting at 10*thisIndex from the beginning of the file
+	}
+	
+	// doesn't do anything, just by convention?
+	Writer(CkMigrateMessage* m){
+	
+	}
+};
+
+#include "iotest.def.h"
