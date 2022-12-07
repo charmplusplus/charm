84ca3e3ff52eeecca4e5bb158632cd90cc0a05dd
Mathew Jacob <mjacob1002@gmail.com>
2022-12-07 17:03:20 -0500

    Removed the ckouts from debugging

--- src/libs/ck-libs/io/ckio.C
+++ src/libs/ck-libs/io/ckio.C
@@ -537,23 +537,14 @@ namespace Ck { namespace IO {
 				_data_buffer[start_idx + counter] = ch;
 			}
 			_bytes_left -= num_bytes; // decrement the number of remaining bytes to read
-			ckout << _bytes_left << " more bytes to collect at offset " << _read_offset << endl;	
 			if(_bytes_left) return; // if there are bytes still to read, just return
 			char* buffer = _data_buffer.data(); 
-			ckout << "The buffer: ";
-			for(size_t i = 0; i < _data_buffer.size(); ++i){
-				ckout << buffer[i];
-			}
-			ckout << endl;
 			ReadCompleteMsg* msg = new (_data_buffer.size()) ReadCompleteMsg();
 			memcpy(msg -> data, buffer, _data_buffer.size());
 			msg -> offset= _read_offset;
 			msg -> bytes = _data_buffer.size();
-			ckout << _read_offset << " is about to send the message \n";
 			_after_read.send(msg);
 			// have some method of cleaning up this chare after invoking the callback
-			ckout << _read_offset << " has sent the message \n";
-			// delete data;
 			delete this;
 		}
     	};
