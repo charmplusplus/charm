6f5b0b8d756409fbeb883058f33e87412ed3ac8a
Sam White <white67@illinois.edu>
2023-04-15 18:00:56 -0500

    Darwin: bump Mac OS X version minimum to 10.14

--- src/arch/multicore-darwin-x86_64/conv-mach.sh
+++ src/arch/multicore-darwin-x86_64/conv-mach.sh
@@ -1,9 +1,9 @@
 . $CHARMINC/cc-clang.sh
 . $CHARMINC/conv-mach-darwin.sh
 
-CMK_DEFS="$CMK_DEFS -mmacosx-version-min=10.7"
+CMK_DEFS="$CMK_DEFS -mmacosx-version-min=10.14"
 # Assumes gfortran compiler:
-CMK_FDEFS="$CMK_FDEFS -mmacosx-version-min=10.7"
+CMK_FDEFS="$CMK_FDEFS -mmacosx-version-min=10.14"
 
 CMK_DEFS="$CMK_DEFS -D_REENTRANT"
 
--- src/arch/netlrts-darwin-x86_64/conv-mach.sh
+++ src/arch/netlrts-darwin-x86_64/conv-mach.sh
@@ -1,6 +1,6 @@
 . $CHARMINC/cc-clang.sh
 . $CHARMINC/conv-mach-darwin.sh
 
-CMK_DEFS="$CMK_DEFS -mmacosx-version-min=10.7"
+CMK_DEFS="$CMK_DEFS -mmacosx-version-min=10.14"
 # Assumes gfortran compiler:
-CMK_FDEFS="$CMK_FDEFS -mmacosx-version-min=10.7"
+CMK_FDEFS="$CMK_FDEFS -mmacosx-version-min=10.14"
