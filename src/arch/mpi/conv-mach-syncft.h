621adc4486b22c09141e6ea47353550420791b8c
Aditya Bhosale <adityapb1546@gmail.com>
2023-05-09 21:07:31 -0500

    Avoid padding in converse message header due to alignment issues

--- src/arch/mpi/conv-mach-syncft.h
+++ src/arch/mpi/conv-mach-syncft.h
@@ -2,7 +2,7 @@
 #undef CMK_MSG_HEADER_EXT_
 //#undef CMK_MSG_HEADER_EXT
 /* expand the header to store the restart phase counter(pn) */
-#define CMK_MSG_HEADER_EXT_   CmiUInt2 rank, root, hdl,xhdl,info, type, pn,d7; unsigned char cksum, magic, mpiMsgType; CmiUInt2 redID; CmiUInt1 zcMsgType:4, cmaMsgType:2, nokeep:1;
+#define CMK_MSG_HEADER_EXT_   CmiUInt2 rank, root, hdl,xhdl,info, type, pn,d7,redID; unsigned char cksum, magic, mpiMsgType; CmiUInt1 zcMsgType:4, cmaMsgType:2, nokeep:1;
 //#define CMK_MSG_HEADER_EXT    { CMK_MSG_HEADER_EXT_ }
 
 #define CmiGetRestartPhase(m)       ((((CmiMsgHeaderExt*)m)->pn))
