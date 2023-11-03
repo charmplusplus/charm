5ee524b5c3d44d12575d46fa435e282804c2254a
Eric Bohm <eric@hpccharm.com>
2023-11-03 15:05:12 -0500

    switch back to sbatch approach

--- .github/workflows/delta-sbatch-slurm.sh
+++ .github/workflows/delta-sbatch-slurm.sh
@@ -1,17 +1,14 @@
 #!/bin/bash -l
-#SBATCH -N 1
-#SBATCH -t 02:00:00
+#SBATCH -N 2
+#SBATCH -n 32
+#SBATCH -t 0:30:00
 #SBATCH -J autobuild
-#SBATCH -o $output
-#SBATCH -e $output
-#SBATCH -p regular
-#SBATCH --partition=cpu-interactive
-#SBATCH --pty bash
+#SBATCH -p cpu
 #SBATCH -A mzu-delta-cpu
 #cd $indir
 set -x
 module load libfabric; module load cmake
-./build all-test $target --with-production --enable-error-checking -j16 -g
+./build all-test $target --with-production --enable-error-checking -j -g
 make -C $target/tmp test TESTOPTS+="+setcpuaffinity"
 #cd $testdir
 #$make clean
