b982df5c9f3c9f54c37cc131d2587426c27d80e5
Eric Bohm <eric@hpccharm.com>
2023-11-10 13:27:09 -0600

    batch version revised to use SLURM variables for filenames

--- .github/workflows/delta-sbatch-slurm.sh
+++ .github/workflows/delta-sbatch-slurm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -l
 #SBATCH -N 2
-#SBATCH -n 32
+#SBATCH -n 64
+#SBATCH -o %j.output
+#SBATCH -e %j.output
 #SBATCH -t 0:30:00
 #SBATCH -J autobuild
 #SBATCH -p cpu
@@ -8,13 +10,12 @@
 #cd $indir
 set -x
 module load libfabric; module load cmake
-./build all-test $target --with-production --enable-error-checking -j -g
-make -C $target/tmp test TESTOPTS+="+setcpuaffinity"
-#cd $testdir
-#$make clean
-#$make -C ../tests $target OPTS="$flags" TESTOPTS="$testopts" $maketestopts
-#$make -C ../examples $target OPTS="$flags" TESTOPTS="$testopts" $maketestopts
-#$make -C ../benchmarks $target OPTS="$flags" TESTOPTS="$testopts" $maketestopts
+./build all-test $target --with-production --enable-error-checking -j64 -g
+#
+cd $target
+make -C tests test OPTS="$flags" TESTOPTS="$testopts" $maketestopts
+make -C examples test OPTS="$flags" TESTOPTS="$testopts" $maketestopts
+make -C benchmarks test OPTS="$flags" TESTOPTS="$testopts" $maketestopts
 # Save make exit status
-#status=\$?
-#echo \$? > $result
+status=$?
+echo $status > ../$SLURM_JOBID.result
