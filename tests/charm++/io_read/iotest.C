92e866de64ddfdef74114b4e129092e7a2dfc2d3
Maya Taylor <mtaylor5@br011.ib.bridges2.psc.edu>
2024-03-14 10:52:51 -0400

    cleanup test code and add smp

--- tests/charm++/io_read/iotest.C
+++ tests/charm++/io_read/iotest.C
@@ -1,8 +1,8 @@
 #include "iotest.decl.h"
-#include <vector>
-#include <time.h>
 #include <assert.h>
 #include <iostream>
+#include <time.h>
+#include <vector>
 CProxy_Main mainproxy;
 std::string global_fname;
 class Main : public CBase_Main
@@ -16,56 +16,37 @@ class Main : public CBase_Main
   Ck::IO::File f;
 
   size_t fileSize;
-  int i;
-  int iters;
-  double allTimes[10];
-  double avgTime;
-  double avg1;
-  double avg2;
-  double avg3;
 
   double start_time;
-  // clock_t start_time_clock;
-  // clock_t stage3_start;
-  // clock_t stage2_start;
-
-  // double stage1_time;
-  // double stage2_time;
-  // double stage3_time;
-
   int numBufChares;
-  int numBufRemaining;
   std::string filename;
 
 public:
-  Main(CkArgMsg *m)
+  Main(CkArgMsg* m)
   {
-    numBufChares = atoi(m->argv[1]); // arg 1 = number of buffer chares
+    numBufChares = atoi(m->argv[1]);  // arg 1 = number of buffer chares
 
-    fileSize = atoi(m->argv[2]);// file size = arg 2
+    fileSize = atoi(m->argv[2]);  // file size = arg 2
 
-    n = atoi(m->argv[3]); // arg 3 = number of readers
+    n = atoi(m->argv[3]);  // arg 3 = number of readers
 
-    iters = atoi(m->argv[4]); // arg 4 = number of test iterations
-
-    std::string fn(m->argv[5]);
+    std::string fn(m->argv[4]);  // arg 4 = filename
     filename = fn;
     global_fname = fn;
-    CkPrintf("Reached this point ig\n");
+
+    CkPrintf("Parsed args.\n");
+
     mainproxy = thisProxy;
-    thisProxy.run(); // open files
+    thisProxy.run();  // open files
     delete m;
   }
 
-  void iterDone()
-  {
-    CkExit();
-  }
+  void iterDone() { CkExit(); }
 };
 
 class Test : public CBase_Test
 {
-  char *dataBuffer;
+  char* dataBuffer;
   int size;
   std::string _fname;
 
@@ -73,42 +54,53 @@ public:
   Test(Ck::IO::Session token, size_t bytesToRead, std::string filename)
   {
     CkPrintf("Inside the constructor of tester %d\n", thisIndex);
-    _fname = filename; 
+    _fname = filename;
     CkCallback sessionEnd(CkIndex_Test::readDone(0), thisProxy[thisIndex]);
     try
     {
       dataBuffer = new char[bytesToRead];
     }
-    catch (const std::bad_alloc &e)
+    catch (const std::bad_alloc& e)
     {
-      CkPrintf("ERROR: Data buffer malloc of %zu bytes in Test chare %d failed.\n", bytesToRead, thisIndex);
+      CkPrintf("ERROR: Data buffer malloc of %zu bytes in Test chare %d failed.\n",
+               bytesToRead, thisIndex);
       CkExit();
     }
     size = bytesToRead;
     Ck::IO::read(token, bytesToRead, bytesToRead * thisIndex, dataBuffer, sessionEnd);
   }
 
-  Test(CkMigrateMessage *m) {}
+  Test(CkMigrateMessage* m) {}
 
-  void readDone(Ck::IO::ReadCompleteMsg *m)
+  void readDone(Ck::IO::ReadCompleteMsg* m)
   {
     CkCallback done(CkIndex_Main::test_read(0), mainproxy);
     FILE* fp = fopen(_fname.c_str(), "r");
-    if(!fp){
-	CkPrintf("FILE* is null on %d for file %s\n", thisIndex, _fname.c_str());
+    if (!fp)
+    {
+      CkPrintf("FILE* is null on %d for file %s\n", thisIndex, _fname.c_str());
     }
-    CkPrintf("On reader[%d], Tryin to seek to %zu\n", thisIndex, (m -> offset));
-    int ret = fseek(fp, m -> offset, SEEK_SET);
-    if(ret){
-	CkPrintf("Something didn't return correctly in the fseek\n");
+    // CkPrintf("On reader[%d], Tryin to seek to %zu\n", thisIndex, (m->offset));
+
+    int ret = fseek(fp, m->offset, SEEK_SET);
+
+    if (ret)
+    {
+      CkPrintf("Something didn't return correctly in the fseek\n");
     }
-    char* verify_buffer = new char[m -> bytes];
-    fread(verify_buffer, 1, m -> bytes, fp);
-    for(int i = 0; i < size; ++i){
-	if(verify_buffer[i] != dataBuffer[i]) {
-		CkPrintf("From reader %d, offset=%d, bytes=%d, verify_buuffer[%d]=%c, dataBuffer[%d]=%c\n", thisIndex, (m -> offset), (m -> bytes), i, verify_buffer[i], i, dataBuffer[i]);
-	} 
-	assert(verify_buffer[i] == dataBuffer[i]);
+
+    char* verify_buffer = new char[m->bytes];
+    fread(verify_buffer, 1, m->bytes, fp);
+    for (int i = 0; i < size; ++i)
+    {
+      if (verify_buffer[i] != dataBuffer[i])
+      {
+        CkPrintf(
+            "From reader %d, offset=%d, bytes=%d, verify_buuffer[%d]=%c, "
+            "dataBuffer[%d]=%c\n",
+            thisIndex, (m->offset), (m->bytes), i, verify_buffer[i], i, dataBuffer[i]);
+      }
+      assert(verify_buffer[i] == dataBuffer[i]);
     }
     delete[] verify_buffer;
     delete[] dataBuffer;
