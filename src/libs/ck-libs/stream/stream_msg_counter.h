c0a244ade62d72e8c9f4ca160982fc0e3ebb5055
Mathew Jacob <mjacob1002@gmail.com>
2024-05-20 22:47:05 -0500

    temp

---
+++ src/libs/ck-libs/stream/stream_msg_counter.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <unordered_map>
+
+#include "streamtoken.h"
+namespace Ck { namespace Stream { namespace impl {
+class StreamMessageCounter{
+	std::unordered_map<size_t, size_t> _counter;
+	size_t _num_sent_messages = 0;
+	size_t _write_acks = 0;
+	bool _stream_write_closed = false;
+	StreamToken _stream = 0;
+public:
+	StreamMessageCounter();
+	StreamMessageCounter(StreamToken);
+	bool isStreamClosed();
+	void setStreamWriteClosed();
+	void processIncomingMessage(size_t);
+	void addSentMessage();
+	void addWriteAck(size_t);
+	bool allAcked();
+
+
+};
+
+}}}
