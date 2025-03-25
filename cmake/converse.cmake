7fab7e168982654759649a32ffc5c40e626910ba
Ritvik Rao <rsrao2@illinois.edu>
2025-03-25 08:44:03 -0500

    reduce cldb to just none

--- cmake/converse.cmake
+++ cmake/converse.cmake
@@ -201,11 +201,11 @@ set(conv-ldb-h-sources
 )
 
 add_library(ldb-none src/conv-ldb/cldb.none.C ${conv-ldb-h-sources})
-add_library(ldb-test src/conv-ldb/cldb.test.C ${conv-ldb-h-sources})
-add_library(ldb-rand src/conv-ldb/cldb.rand.C ${conv-ldb-h-sources})
-add_library(ldb-neighbor src/conv-ldb/cldb.neighbor.C src/conv-ldb/cldb.neighbor.h ${conv-ldb-h-sources})
-add_library(ldb-workstealing src/conv-ldb/cldb.workstealing.C src/conv-ldb/cldb.workstealing.h ${conv-ldb-h-sources})
-add_library(ldb-spray src/conv-ldb/cldb.spray.C ${conv-ldb-h-sources})
+#add_library(ldb-test src/conv-ldb/cldb.test.C ${conv-ldb-h-sources})
+#add_library(ldb-rand src/conv-ldb/cldb.rand.C ${conv-ldb-h-sources})
+#add_library(ldb-neighbor src/conv-ldb/cldb.neighbor.C src/conv-ldb/cldb.neighbor.h ${conv-ldb-h-sources})
+#add_library(ldb-workstealing src/conv-ldb/cldb.workstealing.C src/conv-ldb/cldb.workstealing.h ${conv-ldb-h-sources})
+#add_library(ldb-spray src/conv-ldb/cldb.spray.C ${conv-ldb-h-sources})
 # add_library(ldb-prioritycentralized src/conv-ldb/cldb.prioritycentralized.C src/conv-ldb/cldb.prioritycentralized.h ${conv-ldb-h-sources})
 
 # TopoManager
