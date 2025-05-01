187d899e3c16eb07572f64bf348eb925302490a1
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-05-01 20:52:34 +0530

    fix: remove early free of data

--- examples/charm++/allGather/user.cc
+++ examples/charm++/allGather/user.cc
@@ -100,7 +100,6 @@ void simBox::done(allGatherMsg* msg)
     ckout << endl;
   }
   CkRdmaFree(result);
-  CkRdmaFree(data);
   CkCallback cbfini(CkReductionTarget(start, fini), startProxy);
   contribute(cbfini);
 }
