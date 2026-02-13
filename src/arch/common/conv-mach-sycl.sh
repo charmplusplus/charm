6a539e18585b7dde1d4c5ff3406e36b153649167
Aditya Bhosale <adityapb1546@gmail.com>
2026-02-12 22:32:11 -0600

    Add missing libraries for HAPI

--- src/arch/common/conv-mach-sycl.sh
+++ src/arch/common/conv-mach-sycl.sh
@@ -1,4 +1,4 @@
 BUILD_SYCL=1
 CMK_INCDIR="-I/opt/intel/oneapi/level-zero/latest/include $CMK_INCDIR "
 CMK_LIBDIR="-L/opt/intel/oneapi/level-zero/latest/lib $CMK_LIBDIR "
-CMK_LIBS="-lhybridapi -lze_loader -fsycl $CMK_LIBS "
+CMK_LIBS="-lhybridapi -lze_loader -lrt -fsycl $CMK_LIBS "
