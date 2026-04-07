882a326bf5e9f47a9fe69cf627b0fd5080008c76
Sh0g0-1758 <shouryagoel10000@gmail.com>
2026-04-07 15:10:10 +0530

    manually add definition for findTransferModeDevice

--- src/ck-core/ckrdmadevice.C
+++ src/ck-core/ckrdmadevice.C
@@ -52,6 +52,22 @@
 
 #if CMK_CUDA || CMK_HIP
 
+CmiNcpyModeDevice findTransferModeDevice(int srcPe, int dstPe) {
+  CmiEnforce((srcPe >= 0) && (srcPe <= CmiNumPes()));
+  CmiEnforce((dstPe >= 0) && (dstPe <= CmiNumPes()));
+
+  if (CmiNodeOf(srcPe) == CmiNodeOf(dstPe)) {
+    // Same logical node
+    return CmiNcpyModeDevice::MEMCPY;
+  } else if (CmiPeOnSamePhysicalNode(srcPe, dstPe)) {
+    // Different logical nodes, same physical node
+    return CmiNcpyModeDevice::IPC;
+  } else {
+    // Different physical nodes, requires GPUDirect RDMA
+    return CmiNcpyModeDevice::RDMA;
+  }
+}
+
 #include "hapi.h"
 #include "gpumanager.h"
 
