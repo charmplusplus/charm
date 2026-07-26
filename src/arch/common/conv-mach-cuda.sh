bc00875d48d582aaec6b11b1a497c79a2ec9235e
Aditya Bhosale <adityapb1546@gmail.com>
2026-07-26 13:09:06 -0500

    Fix build

--- src/arch/common/conv-mach-cuda.sh
+++ src/arch/common/conv-mach-cuda.sh
@@ -1,4 +1,7 @@
 BUILD_CUDA=1
 CMK_INCDIR="-I$CUDA_DIR/include -I$CUDA_DIR/extras/CUPTI/include $CMK_INCDIR "
-CMK_LIBDIR="-L$CUDA_DIR/lib64 -L$CUDA_DIR/extras/CUPTI/lib64 $CMK_LIBDIR "
-CMK_LIBS="-lhybridapi -lcudart -lcupti -lrt $CMK_LIBS "
+# lib64/stubs provides libcuda.so for linking on nodes without a driver
+# (the real driver library is loaded at runtime via its soname).
+CMK_LIBDIR="-L$CUDA_DIR/lib64 -L$CUDA_DIR/extras/CUPTI/lib64 -L$CUDA_DIR/lib64/stubs $CMK_LIBDIR "
+# -lcuda: hybridapi's +gpuflagpoll uses the driver API (cuStreamWriteValue32)
+CMK_LIBS="-lhybridapi -lcudart -lcuda -lcupti -lrt $CMK_LIBS "
